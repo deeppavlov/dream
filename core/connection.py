08845c2c0f3a50217f08809cf4b0e1b223cc764f
Olga Gureenkova <ol.gure@gmail.com>
2019-06-19 18:00:18 +0300

    fix: env variable name

--- core/connection.py
+++ core/connection.py
@@ -3,7 +3,7 @@ from mongoengine import connect
 from os import getenv
 from core.config import HOST, PORT, DB_NAME
 
-if getenv('LAUNCHING_ENVIRONMENT') != 'docker':
+if getenv('DPA_LAUNCHING_ENV') != 'docker':
     HOST = '0.0.0.0'
 
 state_storage = connect(host=HOST, port=PORT, db=DB_NAME)
