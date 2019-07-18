00229d4967350e9e837c2d5495f3ae7e566faa31
Fedor Ignatov <ignatov.fedor@gmail.com>
2019-07-18 17:01:44 +0300

    Test rewrited in class

--- tests/poller_tests.py
+++ tests/poller_tests.py
@@ -8,57 +8,60 @@ from aiohttp import web
 import poller
 
 
-server_port = 7000
+class PollerTester:
+    def __init__(self, tests: list = None):
+        server_port = 7000
 
-class Crutch:
-    def __init__(self):
-        Args = namedtuple('Args', ['model_url', 'host', 'port', 'token'])
-        self.args = Args('http://0.0.0.0:5000/answer', '0.0.0.0', f'{server_port}','some_token')
-    def parse_args(self):
-        return self.args
+        self.result = []
+        self.test_start_time = 0
+        self.messages_to_process = 0
+        self.all_messages_processed = asyncio.Event()
+        self.all_messages_processed.set()
+        self._test_samples = tests
 
-loop = asyncio.get_event_loop()
-result = []
-test_start_time = 0
-messages_to_process = 0
-all_messages_processed = asyncio.Event()
-all_messages_processed.set()
+        # Start poller
+        class Crutch:
+            def __init__(self):
+                Args = namedtuple('Args', ['model_url', 'host', 'port', 'token'])
+                self.args = Args('http://0.0.0.0:5000/answer', '0.0.0.0', f'{server_port}', 'some_token')
 
-poller.parser = Crutch()
-poller_process = Process(target=poller.main)
-poller_process.start()
+            def parse_args(self):
+                return self.args
 
-async def handle_updates(request: web.Request):
-    global test_start_time, messages_to_process, result, messages_to_process
-    res = result
-    result = []
-    if res:
-        messages_to_process = len(res)
-        test_start_time = time.perf_counter()
-    return web.json_response({'result': res})
+        poller.parser = Crutch()
+        poller_process = Process(target=poller.main)
+        poller_process.start()
 
-async def handle_message(request: web.Request):
-    global messages_to_process, all_messages_processed
-    messages_to_process -= 1
-    if messages_to_process == 0:
-        test_finish_time = time.perf_counter()
-        print(f'took {test_finish_time - test_start_time}')
-        all_messages_processed.set()
-    return web.Response(status=200)
+        loop = asyncio.get_event_loop()
+        loop.create_task(self._start_tests())
+        app = web.Application(loop=loop)
+        app.add_routes([web.get('/bot{token}/getUpdates', self._handle_updates),
+                        web.post('/bot{token}/sendMessage', self._handle_message)])
+        web.run_app(app, port=server_port)
 
-test_samples = [[{'message':{'text': 'asdf asdf', 'chat': {'id': i}}} for i in range(200)],
-                [{'message':{'text': 'asdf asdf', 'chat': {'id': 42}}} for _ in range(200)]]
+    async def _handle_updates(self, request: web.Request):
+        res = self.result
+        self.result = []
+        if res:
+            self.messages_to_process = len(res)
+            self.test_start_time = time.perf_counter()
+        return web.json_response({'result': res})
 
-async def tests():
-    global result, all_messages_processed, test_samples
-    for data in test_samples:
-        await all_messages_processed.wait()
-        all_messages_processed.clear()
-        result = data
+    async def _handle_message(self, request: web.Request):
+        self.messages_to_process -= 1
+        if self.messages_to_process == 0:
+            test_finish_time = time.perf_counter()
+            print(f'took {test_finish_time - self.test_start_time}')
+            self.all_messages_processed.set()
+        return web.Response(status=200)
 
-if __name__=='__main__':
-    loop.create_task(tests())
-    app = web.Application(loop=loop)
-    app.add_routes([web.get('/bot{token}/getUpdates', handle_updates),
-                    web.post('/bot{token}/sendMessage', handle_message)])
-    web.run_app(app, port=server_port)
+    async def _start_tests(self):
+        for data in self._test_samples:
+            await self.all_messages_processed.wait()
+            self.all_messages_processed.clear()
+            self.result = data
+
+
+if __name__ == '__main__':
+    tester = PollerTester([[{'message': {'text': 'asdf asdf', 'chat': {'id': i}}} for i in range(2)],
+                          [{'message': {'text': 'asdf asdf', 'chat': {'id': 42}}} for _ in range(2)]])
