dcd84ed692749ac7946c5fae12a055a06919f22e
ignatov <ignatov.fedor@gmail.com>
2019-08-13 19:06:40 +0300

    refactor: changed names and fixed typing in Wrapper class

--- poller.py
+++ poller.py
@@ -24,31 +24,33 @@ parser.add_argument('--convai', action='store_true')
 
 
 class Wrapper:
-    def __init__(self, config: dict):
-        self.config = config
-        self.in_queue = Queue()
-        self.loop = asyncio.get_event_loop()
+    _chat_events: Optional[Dict[int, List[asyncio.Event]]]
 
-        self.poller = Poller(config, self.in_queue)
-        self.poller.start()
-        self.chat_events = None
+    def __init__(self, config: Dict) -> None:
+        self._config = config
+        self._in_queue = Queue()
+        self._loop = asyncio.get_event_loop()
+
+        self._poller = Poller(config, self._in_queue)
+        self._poller.start()
+        self._chat_events = None
         self._states = {}
 
-        logging_config.dictConfig(self.config["logging"])
-        self.log = logging.root.manager.loggerDict['wrapper_logger']
+        logging_config.dictConfig(self._config["logging"])
+        self._log = logging.root.manager.loggerDict['wrapper_logger']
 
-        self.log.info('Wrapper initiated')
+        self._log.info('Wrapper initiated')
 
         while True:
-            input_q = self.in_queue.get()
-            self.log.info('Payload received')
-            self.loop.run_until_complete(self._process_input(input_q))
+            input_q = self._in_queue.get()
+            self._log.info('Payload received')
+            self._loop.run_until_complete(self._process_input(input_q))
 
-    async def _process_input(self, input_q: dict) -> None:
+    async def _process_input(self, input_q: Dict) -> None:
         buffer = defaultdict(list)
 
         for message in input_q['result']:
-            if self.config['convai_mode'] is True:
+            if self._config['convai_mode'] is True:
                 chat_item = message['message']
             else:
                 chat_item = self._process_payload(message['message']['payload'])
@@ -65,42 +67,43 @@ class Wrapper:
             chat_ids.append(chat_id)
             log_msg = f'{log_msg}, {str(chat_id)}' if log_msg else f'Processing messages for chats: {str(chat_id)}'
 
-        self.chat_events = {chat_id: [asyncio.Event() for _ in range(len(chat))] for chat_id, chat in buffer.items()}
-        for events in self.chat_events.values():
-            events[-1].set()
+        if self._config['send_state'] is False:
+            self._chat_events = {chat_id: [asyncio.Event() for _ in range(len(chat))] for chat_id, chat in buffer.items()}
+            for events in self._chat_events.values():
+                events[-1].set()
 
         if log_msg:
-            self.log.info(log_msg)
+            self._log.info(log_msg)
 
         # "slices" of replicas from all conversations, each slice contains replicas from different conversation
         batched_chats = zip_longest(*chats, fillvalue=None)
-        if self.config['send_state'] is True:
+        if self._config['send_state'] is True:
             for layer_id, chats_batch in enumerate(batched_chats):
                 await self._process_chats_batch(chats_batch, layer_id, chat_ids)
         else:
-            tasks = (self.loop.create_task(self._process_chats_batch(chats_batch,
-                                                                 layer_id,
-                                                                 chat_ids)) for layer_id, chats_batch in enumerate(batched_chats))
+            tasks = (self._loop.create_task(self._process_chats_batch(chats_batch,
+                                                                      layer_id,
+                                                                      chat_ids)) for layer_id, chats_batch in enumerate(batched_chats))
             await asyncio.gather(*tasks)
 
     async def _process_chats_batch(self, chats_batch: List[str], layer_id: int, chat_ids: List[int]) -> None:
         utts_batch = [(chat_ids[utt_id], utt) for utt_id, utt in enumerate(chats_batch) if utt]
-        j = self.config['infer_batch_length']
+        j = self._config['infer_batch_length']
         chunked_utts_batch = [utts_batch[i * j:(i + 1) * j] for i in range((len(utts_batch) + j - 1) // j)]
-        await asyncio.gather(*(self.loop.create_task(self._process_chunk(chunk, layer_id)) for chunk in chunked_utts_batch))
+        await asyncio.gather(*(self._loop.create_task(self._process_chunk(chunk, layer_id)) for chunk in chunked_utts_batch))
 
     async def _process_chunk(self, chunk: List[Tuple[int, str]], layer_id: int) -> None:
         ids_utts_batch = list(zip(*chunk))
         ids_batch = list(ids_utts_batch[0])
         utts_batch = list(ids_utts_batch[1])
-        data = {self.config["model_args_names"][0]: utts_batch}
-        if self.config['send_state']:
-            data[self.config["model_args_names"][1]] = [self._states.get(chat_id) for chat_id in ids_batch]
+        data = {self._config["model_args_names"][0]: utts_batch}
+        if self._config['send_state']:
+            data[self._config["model_args_names"][1]] = [self._states.get(chat_id) for chat_id in ids_batch]
         try:
-            response = await self.loop.run_in_executor(None, functools.partial(requests.post,
-                                                                               self.config['model_url'],
-                                                                               json=data,
-                                                                               timeout=self.config['request_timeout']))
+            response = await self._loop.run_in_executor(None, functools.partial(requests.post,
+                                                                                self._config['model_url'],
+                                                                                json=data,
+                                                                                timeout=self._config['request_timeout']))
         except requests.exceptions.ReadTimeout:
             response = requests.Response()
             response.status_code = 503
@@ -108,9 +111,9 @@ class Wrapper:
         if response.status_code == 200:
             zip_batch = zip(ids_batch, response.json())
         else:
-            self.log.error(f'Got {response.status_code} code from {self.config["model_url"]}')
+            self._log.error(f'Got {response.status_code} code from {self._config["model_url"]}')
             zip_batch = zip_longest(ids_batch, [], fillvalue='Server error')
-        tasks = (self.loop.create_task(self._send_results(chat_id, chat_resp, layer_id)) for (chat_id, chat_resp) in zip_batch)
+        tasks = (self._loop.create_task(self._send_results(chat_id, chat_resp, layer_id)) for (chat_id, chat_resp) in zip_batch)
 
         await asyncio.gather(*tasks)
 
@@ -123,21 +126,28 @@ class Wrapper:
         return message
 
     async def _send_results(self, chat_id: int, response: list, layer_id: int) -> None:
-        if self.config['send_state'] is True:
+        if self._config['send_state'] is True:
             self._states[chat_id] = response[1]
             resp_text = json.dumps({'text': str(response[0])})
         else:
             buf = {'text': ' '.join(str(element) for element in response)}
             resp_text = json.dumps(buf)
+
         payload = {
             'chat_id': chat_id,
             'text': resp_text
         }
-        await self.chat_events[chat_id][layer_id-1].wait()
+
+        if self._config['send_state'] is False:
+            await self._chat_events[chat_id][layer_id - 1].wait()
+
         async with aiohttp.ClientSession() as session:
-            await session.post(self.config['send_message_url'], json=payload)
-        self.chat_events[chat_id][layer_id].set()
-        self.log.info(f'Sent response to chat: {str(chat_id)}')
+            await session.post(self._config['send_message_url'], json=payload)
+
+        if self._config['send_state'] is False:
+            self._chat_events[chat_id][layer_id].set()
+
+        self._log.info(f'Sent response to chat: {str(chat_id)}')
 
 
 class Poller(Process):
