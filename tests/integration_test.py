3d55f919a63ce8208b0a7f995c6b947f80b397cd
ignatov <ignatov.fedor@gmail.com>
2019-08-14 18:27:09 +0300

    refactor: corrected docstrings, typing

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -8,27 +8,31 @@ import asyncio
 import copy
 import json
 from subprocess import Popen
-from typing import Dict, List, Optional
+from typing import Dict, Optional
 
 import aiohttp
 import pexpect.popen_spawn
 from aiohttp import client_exceptions
 
+with open('../config.json') as poller_config_file, open('integration_test_config.json') as test_config_file:
+    config = json.load(test_config_file)
+    poller_config = json.load(poller_config_file)
+    model_args_names = poller_config["model_args_names"]
+    host = config['emulator_host']
+    port = config['emulator_port']
+    watchdog_delay = config['watchdog_delay']
+    token = 'x'
+
 
 class IntegrationTester:
-    def __init__(self, poller_config_path, test_config_path) -> None:
-        with open(poller_config_path) as poller_config_file, open(test_config_path) as test_config_file:
-            self._config = json.load(test_config_file)
-            poller_config = json.load(poller_config_file)
-            self.model_args_names = poller_config["model_args_names"]
-        self._host = self._config['emulator_host']
-        self._port = self._config['emulator_port']
+    def __init__(self, tests) -> None:
+        self._tests = tests
 
     def _msg(self, chat_id: int, text: Optional[str]) -> Dict:
         if text is None:
-            template = self._config['messages']['command']
+            template = config['messages']['command']
         else:
-            template = self._config['messages']['text']
+            template = config['messages']['text']
         msg = copy.deepcopy(template)
         msg['message']['chat']['id'] = chat_id
         msg['message']['payload']['text'] = text
@@ -39,8 +43,8 @@ class IntegrationTester:
         return {'chat_id': chat_id, 'text': f'{{"text": "{text}"}}'}
 
     async def _start_poller(self, convai, state):
-        poller_call = ['python ../poller.py', f'--port {self._port}', f'--host {self._host}',
-                       f'--model http://{self._host}:{self._port}/answer', '--token x']
+        poller_call = ['python ../poller.py', f'--port {port}', f'--host {host}',
+                       f'--model http://{host}:{port}/answer', f'--token {token}']
         if convai is True:
             poller_call.append('--convai')
         if state is True:
@@ -55,22 +59,22 @@ class IntegrationTester:
         async with aiohttp.ClientSession() as session:
             while True:
                 try:
-                    resp = await session.post(f'http://{self._host}:{self._port}/newTest', json=payload)
+                    resp = await session.post(f'http://{host}:{port}/bot{token}/getUpdates', json=payload)
                     print(await resp.json())
                     break
                 except client_exceptions.ClientConnectionError:
                     pass
 
     async def run_tests(self):
-        for test in self._config['tests']:
+        for test in self._tests:
             await self._start_poller(test['convai'], test['state'])
 
             for test_case in test['test_cases']:
                 poller_input = [self._msg(chat_id, text) for chat_id, text in test_case['poller_input']]
                 model_input = {arg_name: arg_value for arg_name, arg_value in
-                         zip(self.model_args_names, test_case['model_input'])}
+                         zip(model_args_names, test_case['model_input'])}
                 if test['convai'] is True:
-                    model_input[self.model_args_names[0]] = [t['message'] for t in poller_input]
+                    model_input[model_args_names[0]] = [t['message'] for t in poller_input]
                 expected_output = [self.output_message(*s) for s in test_case['expected_output']]
                 payload = {'convai': test['convai'], 'state': test['state'], 'poller_input': poller_input, 'model_input': model_input,
                            'expected_output': expected_output}
@@ -78,9 +82,10 @@ class IntegrationTester:
 
             await self._stop_poller()
 
+
 if __name__ == '__main__':
-    server = Popen('python router_bot_emulator.py'.split())
-    tester = IntegrationTester('../config.json', 'integration_test_config.json')
+    server = Popen(f'python router_bot_emulator.py -p {port} -w {watchdog_delay}'.split())
+    tester = IntegrationTester(config['tests'])
     loop = asyncio.get_event_loop()
     loop.run_until_complete(tester.run_tests())
     server.kill()
--- tests/router_bot_emulator.py
+++ tests/router_bot_emulator.py
@@ -2,12 +2,14 @@ import argparse
 import asyncio
 import logging
 import sys
+from typing import Dict, List, Union
 
 from aiohttp import web
 from aiohttp.web_response import Response
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--port', default=5000, type=int)
+parser.add_argument('-p', '--port', default=None, type=int)
+parser.add_argument('-w', '--watchdog-delay', default=None, type=int)
 
 
 def ordered(obj):
@@ -20,30 +22,53 @@ def ordered(obj):
 
 
 class Server:
-    """Server to emulate router bot and DeepPavlov model launched as REST Api.
+    """Server emulates both router bot and launched as REST api DeepPavlov model.
 
-    DeepPavlov model on each infer returns tuple with upper text and unchanged infer.
+    Pseudo-DeepPavlov model in "non-convai" and "non-state" mode on each payload returns tuple with upper text and
+    unchanged text. In "state" "non-convai" mode model returns tuple with upper text and new state. State must be None
+    or List. New state list is obtained by adding request text to the end of the old state list. List with value of
+    'first_model_arg' key is returned if old state is None.
 
-    Example:
-        'Infer' -> ('INFER', 'Infer')
+    Pseudo-DeepPavlov model in "convai" and "non-state" mode returns tuple with upper text and unchanged text if
+    message payload contains text and does not contain command. Model returns tuple with command text if message payload
+    contains command. Command text is added to state list in "convai" and "state" mode.
+
+    Non-state non-convai mode example:
+        Request payload: {'first_model_arg': ['Text1', 'Text2']}
+        Return: [('TEXT1', 'Text1'), ('TEXT2', 'Text2')]
+
+    State non-convai mode example:
+        Request payload: {'first_model_arg': ['Text2', 'Text3'], 'second_model_arg': [['Text0', 'Text1'], None]}
+        Return: [('TEXT2', ['Text0', 'Text1', 'Text2']), ('TEXT3', ['Text3'])]
 
     Server endpoints:
-        /bot{token}/getUpdates (get): Returns messages from user.
-        /bot{token}/sendMessage (post): Receives payload that passed to DeepPavlov model.
-        /answer (post): Endpoint emulates DeepPavlov REST API endpoint.
-        /newTest (post): Add messages from user
+        /bot{token}/getUpdates (GET, POST): Returns messages from router bot on GET-request, receives messages from
+            router bot on POST-request.
+        /bot{token}/sendMessage (POST): Receives messages for router bot.
+        /answer (POST): Endpoint emulates DeepPavlov REST API endpoint.
 
     """
+    _config: Dict[str, Union[List, Dict, bool]]
+    _event: asyncio.Event
+    _log: logging.Logger
     _loop: asyncio.events.AbstractEventLoop
+    _test_result: Dict[str, bool]
+    _watchdog_task: asyncio.Task
 
-    def __init__(self, port: int) -> None:
+    def __init__(self, parsed_args: argparse.Namespace) -> None:
+        """Launches aiohttp server.
+
+        Args:
+            parsed_args: Arguments from terminal.
+        """
         self._config = {
-            'poller_input': [],
-            'model_input': {},
-            'expected_output': [],
             'convai': False,
-            'state': False
+            'state': False,
+            'poller_input': [],
+            'model_input': dict(),
+            'expected_output': []
         }
+        self._watchdog_delay = parsed_args.watchdog_delay
 
         log_handler = logging.StreamHandler(sys.stderr)
         self._log = logging.getLogger('server')
@@ -53,12 +78,21 @@ class Server:
         self._loop = asyncio.get_event_loop()
         self._app = web.Application(loop=self._loop)
         self._app.add_routes([web.get('/bot{token}/getUpdates', self._handle_updates),
+                              web.post('/bot{token}/getUpdates', self._set_new_test),
                               web.post('/bot{token}/sendMessage', self._handle_message),
-                              web.post('/answer', self._dp_model),
-                              web.post('/newTest', self._set_new_test)])
-        web.run_app(self._app, port=port)
+                              web.post('/answer', self._dp_model)])
+        web.run_app(self._app, port=parsed_args.port)
 
     async def _dp_model(self, request: web.Request) -> Response:
+        """Handler for POST-requests to /answer endpoint. Simulates DeepPavlov model REST api interface.
+
+        Args:
+            request: Request with payload for DeepPavlov model.
+
+        Returns:
+            See class docstring.
+
+        """
         data = await request.json()
         self._test_result['model_input_correct'] = (ordered(data) == ordered(self._config['model_input']))
         ret = []
@@ -91,9 +125,22 @@ class Server:
         return web.json_response(ret)
 
     async def _handle_updates(self, request: web.Request) -> Response:
+        """Handler for GET-requests to /bot{token}/getUpdates endpoint."""
         return web.json_response({'result': self._config.pop('poller_input', [])})
 
     async def _handle_message(self, request: web.Request) -> Response:
+        """Handler for POST-requests to /bot{token}/sendMessage endpoint.
+
+        Sets _test_result['poller_output_correct'] True and sets self._event after receiving all correct messages.
+
+        Args:
+            request: Request with dictionary to send model response to user. Every correct payload removes item from
+                config['expected_output'].
+
+        Returns:
+            Response(200)
+
+        """
         data = await request.json()
         try:
             self._config['expected_output'].remove(data)
@@ -101,33 +148,45 @@ class Server:
             self._event.set()
         else:
             if not self._config['expected_output']:
+                self._watchdog_task.cancel()
                 self._test_result['poller_output_correct'] = True
                 self._event.set()
         return web.Response(status=200)
 
     async def _set_new_test(self, request: web.Request) -> Response:
-        """Sets test data.
+        """Handler for POST-requests to /bot{token}/getUpdates endpoint.
 
-        Request must contain dictionary with following keys:
-            'updates' (list): Messages from router bot.
-            'model_input' (str): Serialized JSON object that router bot poller should send to the DeepPavlov model.
-            'send_messages' (List[str]): Each item is serialized JSON object that poller sends to router bot's
-                /sendMessage endpoint.
+        Method returns response after poller sent all messages to /bot{token}/sendMessage endpoint or after
+        self._watchdog_delay s passed (to prevent test from hanging).
 
+        Args:
+            request: Request payload must contain dictionary with following keys:
+                'convai' (bool): True if dealing with poller in convai mode.
+                'state' (bool): True if dealing with poller in state mode
+                'poller_input' (List[Dict]): Messages from router bot.
+                'model_input' (Dict[str, List]): DeepPavlov model arguments.
+                'expected_output' (List[Dict]): Messages that poller sends to /bot{token}/sendMessage endpoint.
+
+        Returns:
+            Response contains dictionary with two keys: 'model_input_correct' and 'poller_output_correct'.
+            'model_input_correct' value is True if poller send correct payload to DeepPavlov model REST api endpoint
+            simulator. 'poller_output_correct' value is True if poller send to router bot's /sendMessage endpoint
+            correct messages.
         """
         self._event = asyncio.Event()
         self._test_result = {'model_input_correct': False, 'poller_output_correct': False}
-        self._loop.create_task(self._timer())
+        self._watchdog_task = self._loop.create_task(self._watchdog_timer())
         data = await request.json()
         self._config.update(data)
         await self._event.wait()
         return web.json_response(self._test_result)
 
-    async def _timer(self):
-        await asyncio.sleep(10)
+    async def _watchdog_timer(self):
+        await asyncio.sleep(self._watchdog_delay)
         self._log.info('timer called event.set()')
         self._event.set()
 
+
 if __name__ == '__main__':
     args = parser.parse_args()
-    Server(args.port)
+    Server(args)
