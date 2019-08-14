1d682418fc88cca68180dd02114bc28010562916
ignatov <ignatov.fedor@gmail.com>
2019-08-14 12:58:21 +0300

    refactor: test launching with config from json file

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -23,12 +23,12 @@ with open('../config.json', 'r') as config_file, open('integration_test_config.j
 
 
 class Message:
-    def __init__(self, convai, state) -> None:
-        self._convai = convai
-        self._state = state
+    def __init__(self, config) -> None:
+        self._config = config
+        self._convai = config['convai']
+        self._state = config['state']
         self._cmd_template = integration_config['messages']['command']
         self._txt_template = integration_config['messages']['text']
-        self._n_letter = 0
 
     def _msg(self, chat_id: int, text: Optional[str]) -> Dict:
         template = self._cmd_template if text is None else self._txt_template
@@ -37,22 +37,16 @@ class Message:
         msg['message']['payload']['text'] = text
         return msg
 
-    def _get_letter(self):
-        ret = ascii_lowercase[self._n_letter]
-        self._n_letter = (self._n_letter + 1) % len(ascii_lowercase)
-        return ret
-
     @staticmethod
     def send_msg(chat_id: int, text: str) -> Dict:
         return {'chat_id': chat_id, 'text': f'{{"text": "{text}"}}'}
 
-    async def gen_test(self, poller_input: List[Tuple[int, Optional[str]]], expected_output: List[Tuple[int, str]],
-                       model_input: List) -> None:
-        updates = [self._msg(chat_id, text) for chat_id, text in poller_input]
-        infer = {arg_name: arg_value for arg_name, arg_value in zip(poller_config["model_args_names"], model_input)}
+    async def gen_test(self, test_case: Dict) -> None:
+        updates = [self._msg(chat_id, text) for chat_id, text in test_case['poller_input']]
+        infer = {arg_name: arg_value for arg_name, arg_value in zip(poller_config["model_args_names"], test_case['model_input'])}
         if self._convai is True:
             infer[poller_config["model_args_names"][0]] = [t['message'] for t in updates]
-        send_msgs = [self.send_msg(*s) for s in expected_output]
+        send_msgs = [self.send_msg(*s) for s in test_case['expected_output']]
         payload = {'convai': self._convai, 'state': self._state, 'updates': updates, 'infer': infer, 'send_messages': send_msgs}
         async with aiohttp.ClientSession() as session:
             while True:
@@ -63,33 +57,29 @@ class Message:
                 except client_exceptions.ClientConnectionError:
                     pass
 
+    async def test(self):
+        for test_case in self._config['test_cases']:
+            await self.gen_test(test_case)
+
 
 async def test0():
-    message = Message(convai=False, state=False)
-    await message.gen_test(poller_input=[(0, None), (1, 'a'), (2, 'b')], expected_output=[(1, 'A a'), (2, 'B b')], model_input=[['a', 'b']])
+    message = Message(integration_config['tests'][0])
+    await message.test()
 
 
 async def test1():
-    message = Message(convai=True, state=False)
-    await message.gen_test(poller_input=[(0, None), (1, 'a'), (2, 'b')], expected_output=[(0, 'start'), (1, 'A a'), (2, 'B b')], model_input=[None])
+    message = Message(integration_config['tests'][1])
+    await message.test()
 
 
 async def test2():
-    message = Message(convai=False, state=True)
-    await message.gen_test(poller_input=[(0, None), (0, 'a')], expected_output=[(0, 'A')], model_input=[['a'], [None]])
-
-    await message.gen_test(poller_input=[(0, 'b')], expected_output=[(0, 'B')], model_input=[['b'], [['a']]])
-
-    await message.gen_test(poller_input=[(0, 'c')], expected_output=[(0, 'C')], model_input=[['c'], [['a', 'b']]])
+    message = Message(integration_config['tests'][2])
+    await message.test()
 
 
 async def test3():
-    message = Message(convai=True, state=True)
-    await message.gen_test(poller_input=[(0, None), (1, None)], expected_output=[(0, 'start'), (1, 'start')], model_input=[None, [None, None]])
-
-    await message.gen_test(poller_input=[(0, 'a'), (1, 'b')], expected_output=[(0, 'A'), (1, 'B')], model_input=[None, [['start'], ['start']]])
-
-    await message.gen_test(poller_input=[(0, 'c'), (1, 'd')], expected_output=[(0, 'C'), (1, 'D')], model_input=[None, [['start', 'a'], ['start', 'b']]])
+    message = Message(integration_config['tests'][3])
+    await message.test()
 
 if __name__ == '__main__':
     server = Popen('python router_bot_emulator.py'.split())
