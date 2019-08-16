ce1e2d6e88c77d7c76ef883d76c25044813e3d32
Idris <i.yusupov@phystech.edu>
2019-08-16 16:14:26 +0300

    - gunicorn for alice skill - send only last n utterances to alice

--- skills/ALICEChatAPI/server.py
+++ skills/ALICEChatAPI/server.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 
 from flask import Flask, request, jsonify
+
 import ai
 import uuid
+import logging
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 app = Flask(__name__)
+bot = ai.Chatbot()
+bot.initialize("aiml-dir")
 
 
 @app.route("/respond", methods=['POST'])
@@ -14,14 +23,12 @@ def respond():
     user_sentences = request.json['sentences']
     response = "..."
     session_id = uuid.uuid4().hex
-    print(user_sentences, session_id)
+    logger.info("user_sentences: {}, session_id: {}".format(user_sentences, session_id))
     for s in user_sentences:
         response = bot.respond(s, session_id).replace("\n", "")
-    print("response: {}".format(response))
+    logger.info("response: {}".format(response))
     return jsonify([response])
 
 
 if __name__ == '__main__':
-    bot = ai.Chatbot()
-    bot.initialize("aiml-dir")
     app.run(debug=False, host='0.0.0.0', port=3000)
