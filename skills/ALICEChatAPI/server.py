f01c353666eb60d3a3717af32e3520a2c6ed6863
Idris <i.yusupov@phystech.edu>
2019-08-16 15:19:11 +0300

    Added skills/ALICEChatAPI

---
+++ skills/ALICEChatAPI/server.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from flask import Flask, request, jsonify
+import ai
+import uuid
+
+
+app = Flask(__name__)
+
+
+@app.route("/respond", methods=['POST'])
+def respond():
+    # bot.reset() not work
+    user_sentences = request.json['sentences']
+    response = "..."
+    session_id = uuid.uuid4().hex
+    print(user_sentences, session_id)
+    for s in user_sentences:
+        response = bot.respond(s, session_id).replace("\n", "")
+    print("response: {}".format(response))
+    return jsonify([response])
+
+
+if __name__ == '__main__':
+    bot = ai.Chatbot()
+    bot.initialize("aiml-dir")
+    app.run(debug=False, host='0.0.0.0', port=3000)
