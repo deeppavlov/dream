31ca600c5564384054543a5f073f3c9a55568c3d
dilyararimovna <dilyara.rimovna@gmail.com>
2019-08-22 19:03:14 +0300

    feat: skill sleector

--- skill_selectors/alexa_skill_selectors/rule_based_selector.py
+++ skill_selectors/alexa_skill_selectors/rule_based_selector.py
@@ -11,29 +11,23 @@ class RuleBasedSelector(Component):
     Rule: if AIML skill matches, turn it on, otherwise TransferTransfo
     """
 
-    def __init__(self,
-                 aiml_null_response: str = "I don't know",
-                 aiml_alice_null_response: str = "",
-                 **kwargs):
-        self.aiml_null_response = aiml_null_response
-        self.aiml_alice_null_response = aiml_alice_null_response
+    def __init__(self, **kwargs):
+        pass
 
     def __call__(self,
-                 user_replies_batch: List[str],
+                 user_utterances: List[str],
                  aiml_responses_batch: List[str] = None,  # aiml_skill
                  aiml_alice_responses_batch: List[str] = None,  # alice
                  aiml_chitchat_responses_batch: List[str] = None,  # chitchat
                  ) -> List[str]:
-        best_answers = []
+        skill_names = []
 
-        for i in range(len(user_replies_batch)):
-            if aiml_alice_responses_batch and aiml_alice_responses_batch[i][0] != self.aiml_alice_null_response:
-                best_answers.append(aiml_alice_responses_batch[i][0])
-            elif aiml_responses_batch and aiml_responses_batch[i][0] != self.aiml_null_response:
-                best_answers.append(aiml_responses_batch[i][0])
-            elif aiml_chitchat_responses_batch:
-                best_answers.append(aiml_chitchat_responses_batch[i][0])
-            else:
-                best_answers.append("")
+        for i in range(len(user_utterances)):
+            skills_for_uttr = []
+            if "aiml" in user_utterances[i]:
+                skills_for_uttr.append("aiml")
+            if "alice" in user_utterances[i]:
+                skills_for_uttr.append("alice")
+            skill_names.append(skills_for_uttr)
 
-        return best_answers
+        return skill_names
