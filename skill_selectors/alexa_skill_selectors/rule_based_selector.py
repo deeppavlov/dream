ac46c9de25d01ac93867de3ab396e0a41f45281a
dilyararimovna <dilyara.rimovna@gmail.com>
2019-08-22 14:01:05 +0300

    chore: non-working

--- skill_selectors/alexa_skill_selectors/rule_based_selector.py
+++ skill_selectors/alexa_skill_selectors/rule_based_selector.py
@@ -7,32 +7,33 @@ from deeppavlov.core.models.component import Component
 @register('rule_based_selector')
 class RuleBasedSelector(Component):
     """
-    Rule-based skill selector which choosing among TrnasferTransfo, Base AIML and Alice AIML.
-    Rule: if AIML skill matches, turn it on, otherwise TrnasferTransfo
+    Rule-based skill selector which choosing among TransferTransfo, Base AIML and Alice AIML.
+    Rule: if AIML skill matches, turn it on, otherwise TransferTransfo
     """
 
-    def __init__(self, **kwargs):
-        pass
+    def __init__(self,
+                 aiml_null_response: str = "I don't know",
+                 aiml_alice_null_response: str = "",
+                 **kwargs):
+        self.aiml_null_response = aiml_null_response
+        self.aiml_alice_null_response = aiml_alice_null_response
 
     def __call__(self,
                  user_replies_batch: List[str],
-                 aiml_alice_responses_batch: List[Tuple[str, float]] = None,  # alice
-                 aiml_responses_batch: List[Tuple[str, float]] = None,  # aiml_skill
-                 aiml_chitchat_responses_batch: List[Tuple[str, float]] = None,  # chitchat
-                 aiml_alice_null_response: str = "",
-                 aiml_null_response: str = "I don't know",
+                 aiml_responses_batch: List[str] = None,  # aiml_skill
+                 aiml_alice_responses_batch: List[str] = None,  # alice
+                 aiml_chitchat_responses_batch: List[str] = None,  # chitchat
                  ) -> List[str]:
-
-        skill_names = []
+        best_answers = []
 
         for i in range(len(user_replies_batch)):
-            if aiml_alice_responses_batch and aiml_alice_responses_batch[i][0] != aiml_alice_null_response:
-                skill_names.append("alice")
-            elif aiml_responses_batch and aiml_responses_batch[i][0] != aiml_null_response:
-                skill_names.append("aiml_skill")
+            if aiml_alice_responses_batch and aiml_alice_responses_batch[i][0] != self.aiml_alice_null_response:
+                best_answers.append(aiml_alice_responses_batch[i][0])
+            elif aiml_responses_batch and aiml_responses_batch[i][0] != self.aiml_null_response:
+                best_answers.append(aiml_responses_batch[i][0])
             elif aiml_chitchat_responses_batch:
-                skill_names.append("chitchat")
+                best_answers.append(aiml_chitchat_responses_batch[i][0])
             else:
-                skill_names.append("")
+                best_answers.append("")
 
-        return skill_names
+        return best_answers
