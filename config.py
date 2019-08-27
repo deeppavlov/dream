152bd94dfa7f48cf15483c76993f60f8c618d3a5
dilyararimovna <dilyara.rimovna@gmail.com>
2019-08-27 12:56:22 +0300

    feat: cobot classificators connected

--- config.py
+++ config.py
@@ -46,6 +46,36 @@ SKILLS = [
 ]
 
 ANNOTATORS = [
+    {
+        "name": "cobot_topics",
+        "protocol": "http",
+        "host": "cobot_topics",
+        "port": 8003,
+        "endpoint": "topics",
+        "external": True,
+        "path": "",
+        "formatter": cobot_qa_formatter  #base_annotator_formatter
+    },
+    {
+        "name": "cobot_sentiment",
+        "protocol": "http",
+        "host": "cobot_sentiment",
+        "port": 8004,
+        "endpoint": "sentiment",
+        "external": True,
+        "path": "",
+        "formatter": cobot_qa_formatter  #base_annotator_formatter
+    },
+    {
+        "name": "cobot_offensiveness",
+        "protocol": "http",
+        "host": "cobot_offensiveness",
+        "port": 8005,
+        "endpoint": "offensiveness",
+        "external": True,
+        "path": "",
+        "formatter": cobot_qa_formatter  #base_annotator_formatter
+    }
 ]
 
 SKILL_SELECTORS = [
