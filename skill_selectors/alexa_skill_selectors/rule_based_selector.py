d6225f7f7eebd1825a6e0df588dd0597ef79dd79
Kuznetsov Denis Pavlovich <kuznetsov.den.p@gmail.com>
2019-09-03 19:53:42 +0300

    wip (crutched_personality_catcher): add personality_catcher

--- skill_selectors/alexa_skill_selectors/rule_based_selector.py
+++ skill_selectors/alexa_skill_selectors/rule_based_selector.py
@@ -30,7 +30,9 @@ class RuleBasedSelector(Component):
 
             tokens = dialog['utterances'][-1]['text'].lower().split()
 
-            if len(set(tokens).intersection(self.wh_words)) > 0:
+            if "/new_persona" in dialog['utterances'][-1]['text']:
+                skills_for_uttr.append("personality_catcher")  # TODO: rm crutch of personality_catcher
+            elif len(set(tokens).intersection(self.wh_words)) > 0:
                 skills_for_uttr.append("aiml")
                 skills_for_uttr.append("alice")
                 skills_for_uttr.append("cobotqa")
