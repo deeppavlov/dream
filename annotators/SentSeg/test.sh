b1830a24b08ce7986ceeac0ff59c55dd7fb40649
Idris <i.yusupov@phystech.edu>
2019-09-05 19:33:40 +0300

    1. Deleted large 50 Mb files from sentseg, uploaded them  to google drive and downloading them during docker run in new setup.sh script 2. Renamed cobot_sentseg to sentseg because it is not cobot skill 3. Fixed test.sh - using correct port

--- annotators/SentSeg/test.sh
+++ annotators/SentSeg/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 curl -H "Content-Type: application/json" -XPOST \
-  -d '{"text": "hey alexa how are you"}' \
-  http://0.0.0.0:3000/sentseg
+  -d '{"sentences": ["hey alexa how are you"]}' \
+  http://0.0.0.0:8011/sentseg
