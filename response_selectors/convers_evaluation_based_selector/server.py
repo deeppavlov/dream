33a9c3581a09f1404263aea5feaa83d962304a6b
Idris <i.yusupov@phystech.edu>
2019-09-09 12:46:45 +0300

    - Updated select_response score calculation - using weighted sum - Deleted bad rule from aiml 2

--- response_selectors/convers_evaluation_based_selector/server.py
+++ response_selectors/convers_evaluation_based_selector/server.py
@@ -83,14 +83,22 @@ def respond():
 
 
 def select_response(curr_candidates, curr_scores, curr_confidences, dialog):
+    confidence_strength = 2
+    conv_eval_strength = 0.4
     # calculate curr_scores which is an array of values-scores for each candidate
-    curr_single_cores = [(cand_scores["isResponseOnTopic"] + cand_scores["isResponseInteresting"] +
-                          cand_scores["responseEngagesUser"] + cand_scores["isResponseComprehensible"] -
-                          cand_scores["isResponseErroneous"] + confidence)
-                         for cand_scores, confidence in zip(curr_scores, curr_confidences)]
-
-    for i, score in enumerate(curr_single_cores):
-        logger.info(f'Skill {list(curr_candidates.keys())[i]} has score: {score}')
+    curr_single_cores = []
+    for i in range(len(curr_scores)):
+        cand_scores = curr_scores[i]
+        confidence = curr_confidences[i]
+        skill_name = list(curr_candidates.keys())[i]
+        score_conv_eval = cand_scores["isResponseOnTopic"] + \
+            cand_scores["isResponseInteresting"] + \
+            cand_scores["responseEngagesUser"] + \
+            cand_scores["isResponseComprehensible"] - \
+            cand_scores["isResponseErroneous"]
+        score = conv_eval_strength*score_conv_eval + confidence_strength*confidence
+        logger.info(f'Skill {skill_name} has score: {score}. Cand scores: {cand_scores}')
+        curr_single_cores.append(score)
     best_id = np.argmax(curr_single_cores)
     best_skill_name = list(curr_candidates.keys())[best_id]
 
