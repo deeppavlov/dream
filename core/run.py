c8b507b07a2de8d7543208fd7ecaca5cef61e657
Olga Gureenkova <ol.gure@gmail.com>
2019-09-14 22:52:20 +0300

    style: fix minor issues

--- core/run.py
+++ core/run.py
@@ -13,7 +13,6 @@ from core.config_parser import parse_old_config
 from core.state_manager import StateManager
 from core.transform_config import DEBUG
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument("-ch", "--channel", help="run agent in telegram, cmd_client or http_client", type=str,
                     choices=['cmd_client', 'http_client'], default='cmd_client')
@@ -25,7 +24,7 @@ CHANNEL = args.channel
 def prepare_agent(services, endpoint: Service):
     pipeline = Pipeline(services)
     pipeline.add_responder_service(endpoint)
-    agent = Agent(pipeline, StateManager)
+    agent = Agent(pipeline, StateManager())
 
     return agent.register_msg, agent.process
 
@@ -38,14 +37,18 @@ async def run(register_msg):
             await register_msg(msg, user_id, 'cmd', datetime.now(), 'lab', CHANNEL)
 
 
-async def init_app(register_msg, intermediate_storage, on_startup, on_shutdown):
+async def on_shutdown(app):
+    await app['client_session'].close()
+
+
+async def init_app(register_msg, intermediate_storage, on_startup, on_shutdown_func=on_shutdown):
     app = web.Application(debug=True)
     handle_func = await api_message_processor(register_msg, intermediate_storage)
     app.router.add_post('/', handle_func)
     app.router.add_get('/dialogs', users_dialogs)
     app.router.add_get('/dialogs/{dialog_id}', dialog)
     app.on_startup.append(on_startup)
-    app.on_shutdown.append(on_shutdown)
+    app.on_shutdown.append(on_shutdown_func)
     return app
 
 
@@ -61,13 +64,10 @@ def prepare_startup(consumers, process_callable, session):
     return startup_background_tasks
 
 
-async def on_shutdown(app):
-    await app['client_session'].close()
-
-
 async def api_message_processor(register_msg, intermediate_storage):
     async def api_handle(request):
-        result = {}
+        user_id = None
+        bot_response = None
         if request.method == 'POST':
             if request.headers.get('content-type') != 'application/json':
                 raise web.HTTPBadRequest(reason='Content-Type should be application/json')
@@ -86,12 +86,15 @@ async def api_message_processor(register_msg, intermediate_storage):
             await event.wait()
             bot_response = intermediate_storage.pop(message_uuid)
 
+            if bot_response is None:
+                raise RuntimeError('Got None instead of a bot response.')
+
         return web.json_response({'user_id': user_id, 'response': bot_response})
 
     return api_handle
 
 
-async def users_dialogs(request):
+async def users_dialogs():
     from core.state_schema import Dialog
     exist_dialogs = Dialog.objects()
     result = list()
@@ -108,20 +111,21 @@ async def dialog(request):
         dialogs = Dialog.objects()
         return web.json_response([i.to_dict() for i in dialogs])
     elif len(dialog_id) == 24 and all(c in hexdigits for c in dialog_id):
-        dialog = Dialog.objects(id__exact=dialog_id)
-        if not dialog:
+        d = Dialog.objects(id__exact=dialog_id)
+        if not d:
             raise web.HTTPNotFound(reason=f'dialog with id {dialog_id} is not exist')
         else:
-            return web.json_response(dialog[0].to_dict())
+            return web.json_response(d[0].to_dict())
     else:
         raise web.HTTPBadRequest(reason='dialog id should be 24-character hex string')
 
 
-if __name__ == '__main__':
+def main():
     services, workers, session = parse_old_config()
 
     if CHANNEL == 'cmd_client':
-        endpoint = Service('cmd_responder', CmdOutputConnector(), None, 1, ['responder'], set(), simple_workflow_formatter)
+        endpoint = Service('cmd_responder', CmdOutputConnector(), None, 1, ['responder'], set(),
+                           simple_workflow_formatter)
         loop = asyncio.get_event_loop()
         loop.set_debug(DEBUG)
         register_msg, process = prepare_agent(services, endpoint)
@@ -139,6 +143,11 @@ if __name__ == '__main__':
         intermediate_storage = {}
         endpoint = Service('http_responder', HttpOutputConnector(intermediate_storage), None, 1, ['responder'])
         register_msg, process_callable = prepare_agent(services, endpoint)
-        app = init_app(register_msg, intermediate_storage, prepare_startup(workers, process_callable, session), on_shutdown)
+        app = init_app(register_msg, intermediate_storage, prepare_startup(workers, process_callable, session),
+                       on_shutdown)
 
         web.run_app(app, port=args.port)
+
+
+if __name__ == '__main__':
+    main()
