5df9b89267f65ef758dce992288f0f63c5cf679c
Olga Gureenkova <ol.gure@gmail.com>
2019-09-14 22:52:56 +0300

    refactor: rewrite agent batch test to new agent architecture

--- utils/agent_batch_test.py
+++ utils/agent_batch_test.py
@@ -1,68 +1,53 @@
 from datetime import datetime
 import uuid
 import argparse
-import os
 from random import choice
+
+import asyncio
 
 from core.agent import Agent
 from core.state_manager import StateManager
-from core.skill_manager import SkillManager
-from core.rest_caller import RestCaller
-from models.postprocessor import DefaultPostprocessor
-from models.response_selector import ConfidenceResponseSelector
-from core.transform_config import MAX_WORKERS, ANNOTATORS, SKILL_SELECTORS, SKILLS, RESPONSE_SELECTORS
+from core.pipeline import Pipeline, Service
+from core.config_parser import parse_old_config
+from core.connectors import HttpOutputConnector
 
 import logging
 
 logging.getLogger('requests.packages.urllib3.connectionpool').setLevel(logging.WARNING)
-
-os.environ["DPA_LAUNCHING_ENV"] = 'local'
 
 parser = argparse.ArgumentParser()
 parser.add_argument('phrasefile', help='name of the file with phrases for dialog', type=str,
                     default="../utils/ru_test_phrases.txt")
 
 
-def init_agent():
-    state_manager = StateManager()
-
-    preprocessors = []
-    for ants in ANNOTATORS:
-        if ants:
-            anno_names, anno_urls, anno_formatters = zip(
-                *[(a['name'], a['url'], a['formatter']) for a in ants])
-        else:
-            anno_names, anno_urls, anno_formatters = [], [], []
-        preprocessors.append(RestCaller(max_workers=MAX_WORKERS, names=anno_names, urls=anno_urls,
-                                        formatters=anno_formatters))
-    postprocessor = DefaultPostprocessor()
-    skill_caller = RestCaller(max_workers=MAX_WORKERS)
-
-    if RESPONSE_SELECTORS:
-        rs_names, rs_urls, rs_formatters = zip(
-            *[(rs['name'], rs['url'], rs['formatter']) for rs in RESPONSE_SELECTORS])
-        response_selector = RestCaller(max_workers=MAX_WORKERS, names=rs_names, urls=rs_urls,
-                                       formatters=rs_formatters)
-    else:
-        response_selector = ConfidenceResponseSelector()
-
-    skill_selector = None
-    if SKILL_SELECTORS:
-        ss_names, ss_urls, ss_formatters = zip(
-            *[(ss['name'], ss['url'], ss['formatter']) for ss in SKILL_SELECTORS])
-        skill_selector = RestCaller(max_workers=MAX_WORKERS, names=ss_names, urls=ss_urls,
-                                    formatters=ss_formatters)
-
-    skill_manager = SkillManager(skill_selector=skill_selector, response_selector=response_selector,
-                                 skill_caller=skill_caller,
-                                 profile_handlers=[skill['name'] for skill in SKILLS
-                                                   if skill.get('profile_handler')])
-
-    agent = Agent(state_manager, preprocessors, postprocessor, skill_manager)
-    return agent
+def init_agent(intermediate_storage):
+    services, workers, session = parse_old_config()
+    pipeline = Pipeline(services)
+    endpoint = Service('http_responder', HttpOutputConnector(intermediate_storage), None, 1, ['responder'])
+    pipeline.add_responder_service(endpoint)
+    agent = Agent(pipeline, StateManager())
+    return agent, session
 
 
-def main():
+class DummyOutputConnector:
+    def __init__(self, intermediate_storage):
+        self.intermediate_storage = intermediate_storage
+
+    async def send(self, payload):
+        self.intermediate_storage[payload['message_uuid']] = payload
+        payload['event'].set()
+
+
+async def process_message_return_event(agent, phrase, u_tg_id, u_d_type, date_time, location, ch_type, intermediate_storage):
+    event = asyncio.Event()
+    message_uuid = uuid.uuid4().hex
+    await agent.register_msg(utterance=phrase, user_telegram_id=u_tg_id, user_device_type=u_d_type, date_time=date_time,
+                             location=location, channel_type=ch_type, event=event, message_uuid=message_uuid)
+    await event.wait()
+    return intermediate_storage.pop(message_uuid)
+
+
+async def main():
     args = parser.parse_args()
     with open(args.phrasefile, 'r') as file:
         phrases = [line.rstrip('\n') for line in file]
@@ -73,13 +58,19 @@ def main():
     date_times = [datetime.utcnow()] * length
     locations = [choice(['moscow', 'novosibirsk', 'novokuznetsk']) for _ in range(length)]
     ch_types = ['cmd_client'] * length
+    intermediate_storage = {}
+    agent, session = init_agent(intermediate_storage)
+    result = []
+    for u, u_tg_id, u_d_type, dt, loc, ch_t in zip(phrases, u_tg_ids, u_d_types, date_times, locations, ch_types):
+        response = await process_message_return_event(agent, u, u_tg_id, u_d_type, dt, loc, ch_t, intermediate_storage)
+        result.append(response)
 
-    agent = init_agent()
+    await session.close()
 
-    responses = agent(utterances=phrases, user_telegram_ids=u_tg_ids, user_device_types=u_d_types,
-                      date_times=date_times, locations=locations, channel_types=ch_types)
-    return responses
+    return result
 
 
 if __name__ == "__main__":
-    print(main())
+    loop = asyncio.get_event_loop()
+    result = loop.run_until_complete(main())
+    print(result)
