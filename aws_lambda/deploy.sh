c62eb4f9a5187335d7f68bdd737de8a2826b6120
yurakuratov <yurakuratov@gmail.com>
2019-09-17 18:12:25 +0300

    feat: add lambda function name as arg and pip support

--- aws_lambda/deploy.sh
+++ aws_lambda/deploy.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
 # https://docs.aws.amazon.com/lambda/latest/dg/lambda-python-how-to-create-deployment-package.html
-pip3 install -r aws_lambda/requirements.txt --target aws_lambda/package
+
+LAMBDA_NAME=$1
+if [ "$LAMBDA_NAME" = "" ]; then
+	echo "Usage: aws_lambda/deploy.sh LAMBDA_FUNCTION_NAME"
+	exit 1
+fi
+
+if ! pip3 install -r aws_lambda/requirements.txt --target aws_lambda/package &> /dev/null; then
+    echo "pip3 was not found, trying pip.."
+
+    if ! pip install -r aws_lambda/requirements.txt --target aws_lambda/package; then
+        echo "pip was not found, canceling..."
+        exit 1
+    fi
+fi
+
 cp aws_lambda/main.py aws_lambda/package/
 cd aws_lambda/package && zip -r9 dp_agent_lambda.zip . && cd ../..
-aws lambda update-function-code --function-name dp_agent_lambda_proxy --zip-file fileb://aws_lambda/package/dp_agent_lambda.zip
+aws lambda update-function-code --function-name $LAMBDA_NAME --zip-file fileb://aws_lambda/package/dp_agent_lambda.zip
