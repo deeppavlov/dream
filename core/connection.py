31ea5eb975688ca50495f0fd71a5eb79eda52dce
Idris <i.yusupov@phystech.edu>
2019-09-17 22:17:29 +0300

    - DocumentDb credentials - ECR staging migration - DocumentDb staging migration (todo dev persistance)

--- core/connection.py
+++ core/connection.py
@@ -2,4 +2,4 @@ from mongoengine import connect
 
 from core.transform_config import DB_HOST, DB_PORT, DB_NAME
 
-state_storage = connect(host=DB_HOST, port=DB_PORT, db=DB_NAME)
+state_storage = connect(host=DB_HOST, db=DB_NAME)
