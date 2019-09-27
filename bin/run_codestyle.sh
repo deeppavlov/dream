7eb9aad3f41187adae526df7932481a34843f055
Idris <i.yusupov@phystech.edu>
2019-09-27 15:12:37 +0300

    Updated codestyle script - ignore dev branch

--- bin/run_codestyle.sh
+++ bin/run_codestyle.sh
@@ -2,4 +2,10 @@
 
 pip install flake8==3.7.8
 
-flake8 --statistics --count $(git diff --cached --name-only --diff-filter=ACMR dev | grep \.py\$)
+res=$(git diff --cached --name-only --diff-filter=ACMR dev | grep \.py\$ | tr -d "[:blank:]")
+if [ -z "$res" ]
+then
+  exit 0
+else
+  flake8 --statistics --count $(git diff --cached --name-only --diff-filter=ACMR dev | grep \.py\$)
+fi
