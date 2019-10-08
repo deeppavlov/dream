efb3ca754240c0fc23de53ec54ca31ac92ef5898
Daniil Cherniavskii <danchern97@mail.ru>
2019-10-09 00:59:50 +0300

    Feat/exit_intent skill (#16)
    * Added exit skill baseline with 'SimpleExiter'
    
    * Added bot and user farewells lists
    
    * Added staging
    
    * Added USE, still needs testing
    
    * Fixed model crashing bug
    
    * fix: download USE model in docker image, refactor
    
    * fix: we can see logs now
    
    * Fixed responder (tf.placeholders), added utils.py, create_emb.py
    
    * Fixed: Annotations analysis, new threshold
    
    * Initial commit for intent catcher
    
    * Added  for automatic threshold and embeddings calculation
    
    * Name fixed.
    
    * Moved intent detection to annotations, now only responders here
    
    * Modified README.md
    
    * Moved skills/intent_catcher -> annotators/IntentCatcher
    
    * Moved intent_catcher skill to annotations. Fixed bug: copy model into tfhub dir, faster building. Fixed
    
    * Moved intent_catcher skill to annotations. Fixed bug: copy model into tfhub dir, faster building. Fixed
    
    * IntentCatcher: refactor code
    
    * Exit skill: refactor code
    
    * Added exit skill handling
    
    * Modifications for intent annotator and exit skill handling
    
    * * Fixed IntentCatcher bug
    * Fixed codestyle
    * Fixed transfertransfo port
    
    * Fixed copying for tests passing.
    
    Uncomment this line for fast local deployment
    
    * * Fix IntentCatcher
    * Added some new phrases
    * New feature - IntentResponder instead of exit feature
    * Fixed formatter for responder and catcher
    
    * Fix rule_based_selector
    
    * Fix responder intent -> intent_catcher
    
    * Codestyle fix
    
    * * Fix bugs with dialog
    * Added phrases to repeat skill
    * Uncommented neccessary cobot servicies for exit skill
    
    * * Fix codestyle
    
    * * Bug fix: (see dp/dp_formatters.py)
    * Added more time to skills deployment (prevents critical timeout and 
    redeployment, saves time)
    
    * * Mofidied tests to fit responder skill
    
    * * Added confidence scaling
    
    * Added all
    
    * * Fix codestyle
    
    * fix: reoder commands in Dockerfile to make model downloading not so often
    
    * Modified to use segments from `sentseg` insted of tokenizer.
    
    * fix: exec order + codestyle
    
    * fix: sentseg annotation name
    
    * * fix: formatters (executing on bot and human phases)
    * add: new phrases, random phrases modification for creating thresholds
    
    * fix: loading data + delete unnecessary
    
    * modd: only 1 data file
    
    * fix: codestyle
    
    * * add: new knn-based detector (WIP)
    * add: metrics in intent_data.json
    
    * fix: json lib missed
    
    * fix: detector logger
    
    * fix: detector tf constant
    add: new phrases generation, new create_data logging, new threshold 
    managing
    
    * fix: Dockerfile
    
    * fix: skills namings in staging.yml

--- skill_selectors/rule_based_selector/server.py
+++ skill_selectors/rule_based_selector/server.py
@@ -35,6 +35,8 @@ class RuleBasedSelector():
             skills_for_uttr = []
 
             tokens = dialog['utterances'][-1]['text'].lower().split()
+            intents = dialog['utterances'][-1]['annotations']['intent_catcher'].values()
+            intent_detected = any([i['detected'] == 1 for i in intents])
 
             if "/new_persona" in dialog['utterances'][-1]['text']:
                 skills_for_uttr.append("personality_catcher")  # TODO: rm crutch of personality_catcher
@@ -42,6 +44,8 @@ class RuleBasedSelector():
                 skills_for_uttr.append("cobotqa")
                 skills_for_uttr.append("program_y")
                 skills_for_uttr.append("transfertransfo")
+            elif intent_detected:
+                skills_for_uttr.append("intent_responder")
             else:
                 skills_for_uttr.append("program_y")
                 skills_for_uttr.append("transfertransfo")
