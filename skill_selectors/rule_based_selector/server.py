a75ef3de132c9df3938e4edeca95852fe22580fd
Idris Y <sld@users.noreply.github.com>
2019-10-10 21:30:25 +0300

    - Detect exit intent in lambda handler (#72)
    * - Detect exit intent in lambda handler
    - Disable transfertransfo
    
    * Fix codestyle
    
    * Fix tests
    
    * Fix tests and lambda
    
    * disable transfertrasnfo for questions
    
    * feat: add first question words

--- skill_selectors/rule_based_selector/server.py
+++ skill_selectors/rule_based_selector/server.py
@@ -23,9 +23,16 @@ class RuleBasedSelector():
     Rule-based skill selector which choosing among TransferTransfo, Base AIML and Alice AIML
     """
     wh_words = {"what", "when", "where", "which", "who", "whom", "whose", "why", "how"}
+    first_question_words = {
+        "do", "have", "did", "had", "are", "is", "am", "will",
+        "would", "should", "shall", "may", "might", "can", "could"
+    }
 
     def __init__(self, **kwargs):
         logger.info("Skill selector Initialized")
+
+    def _is_question(self, tokens):
+        return tokens[0] in self.first_question_words or len(set(tokens).intersection(self.wh_words)) > 0
 
     def __call__(self, states_batch, **kwargs) -> List[List[str]]:
 
@@ -40,19 +47,19 @@ class RuleBasedSelector():
 
             if "/new_persona" in dialog['utterances'][-1]['text']:
                 skills_for_uttr.append("personality_catcher")  # TODO: rm crutch of personality_catcher
-            elif len(set(tokens).intersection(self.wh_words)) > 0:
+            elif self._is_question(tokens):
                 skills_for_uttr.append("dummy_skill")
                 skills_for_uttr.append("cobotqa")
                 skills_for_uttr.append("program_y")
-                skills_for_uttr.append("transfertransfo")
-                skills_for_uttr.append("retrieval_chitchat")
+                # skills_for_uttr.append("transfertransfo")
+                # skills_for_uttr.append("retrieval_chitchat")
             elif intent_detected:
                 skills_for_uttr.append("dummy_skill")
                 skills_for_uttr.append("intent_responder")
             else:
                 skills_for_uttr.append("dummy_skill")
                 skills_for_uttr.append("program_y")
-                skills_for_uttr.append("transfertransfo")
+                # skills_for_uttr.append("transfertransfo")
                 skills_for_uttr.append("retrieval_chitchat")
 
             skill_names.append(skills_for_uttr)
