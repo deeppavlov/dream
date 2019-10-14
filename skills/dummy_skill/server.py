e3e602096c507d18845f41427a0e1998f70e8325
yurakuratov <yurakuratov@gmail.com>
2019-10-14 16:13:49 +0300

    fix: typo in dummy skill response (#90)

--- skills/dummy_skill/server.py
+++ skills/dummy_skill/server.py
@@ -19,7 +19,7 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
 donotknow_answers = ["I really do not know what to answer.",
-                     "Sorry, probably, I din't get what you mean.",
+                     "Sorry, probably, I didn't get what you mean.",
                      "I didn't get it. Sorry.",
                      "Let's talk about something else."]
 
