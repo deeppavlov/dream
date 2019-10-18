9de3bd91173f702b0d1246de41ff13db0636a4ca
Kuznetsov Denis <kuznetsov.den.p@gmail.com>
2019-10-18 19:23:04 +0300

    Feat/tt smart infer (#74)
    * wip (TT): add beam search
    
    * wip (TT): not working cuda
    
    * wip (TT): fix time of calculation availible beam size
    
    * wip (TT): add elastic search & portprocessing
    
    * wip (TT): rm top sampler
    
    * wip (TT): postpocessing is ready
    
    * udp (TT): add emoji
    
    * fix (TT): fix dev.yml
    
    * udp (TT): rm elastic & refactor preprocessing
    
    * fix (TT): fix a codestyle
    
    * udp (TT): add confidence approximation
    
    * udp (TT): fix code stype
    
    * wip (TT_smart_infer): add tests
    
    * style (TT_smart_infer): fix style
    
    * tests (TT_smart_infer): add tests & fix style of tests
    
    * Update tests/runtests.sh
    
    Co-Authored-By: yurakuratov <yurakuratov@gmail.com>
    
    * wip (TT_smart_infer): switch off ngram replacing
    
    * wip (TT_smart_infer): fix style
    
    * feat (TT_smart_infer): rm alexa name
    
    * feat (TT_smart_infer): rm alexa name
    
    * feat (TT_smart_infer): rm elasticsearch
    
    * feat (TT_smart_infer): rm elasticsearch from reqs
    
    * fix (TT_smart_infer): add model warmuping
    
    * fix (TT_smart_infer): add artifacts
    
    * wip (TT_smart_infer): change TT to retrieval in a skills selector :sob:
    
    * feat (TT_smart_infer): fix tests & add heuristics
    
    * feat (TT_smart_infer): fix style
    
    * feat (TT_smart_infer): udp test output
    
    * feat (TT_smart_infer): rm test_results.json
    
    * feat (TT_smart_infer): switch off warmup

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,43 +1,37 @@
 #!/usr/bin/env bash
 
-
-
-for ARGUMENT in "$@"
-do
+for ARGUMENT in "$@"; do
 
     KEY=$(echo $ARGUMENT | cut -f1 -d=)
     VALUE=$(echo $ARGUMENT | cut -f2 -d=)
 
     case "$KEY" in
-            DEVICE)        DEVICE=${VALUE} ;;
-            MODE)          MODE=${VALUE} ;;
-            *)
+    DEVICE) DEVICE=${VALUE} ;;
+    MODE) MODE=${VALUE} ;;
+    *) ;;
     esac
 done
 
-function wait_service()
-{
+function wait_service() {
     local timeout=480
     local url=$1
     local reply_keyword=$2
     while [[ $timeout -gt 0 ]]; do
         local res=$(curl -s -XGET "$url" | grep "$reply_keyword")
-        if [ ! -z "$res" ]
-        then
+        if [ ! -z "$res" ]; then
             echo FOUND service $url
             echo REPLY: $res
             return 0
         fi
         sleep 1
-        (( timeout-- ))
+        ((timeout--))
         echo wait_service $url timeout in $timeout sec..
     done
     echo ERROR: $url is not responding
     return 1
 }
 
-function cleanup()
-{
+function cleanup() {
     local exit_status=${1:-$?}
     echo SHUTDOWN TESTING ENVIRONMENT..
 
@@ -68,22 +62,24 @@ function dockercompose_cmd() {
     fi
 }
 
-if [[ "$DEVICE" = "" ]]; then
-	DEVICE="gpu"
+function container_is_started() { [ "$(docker ps | grep $1)" ] && return 0 || return 1; }
+
+if [[ "$DEVICE" == "" ]]; then
+    DEVICE="gpu"
 fi
 
-if [[ "$MODE" = "" ]]; then
-	MODE="all"
+if [[ "$MODE" == "" ]]; then
+    MODE="all"
 fi
 
-if [[ "$MODE" = "clean" ]]; then
-	cleanup
-	exit 0
+if [[ "$MODE" == "clean" ]]; then
+    cleanup
+    exit 0
 fi
 
 set -e
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 trap cleanup EXIT
 
 echo running tests on $DEVICE in mode: $MODE
@@ -98,28 +94,42 @@ dockercompose_cmd logs -f --tail="all" --timestamps &
 wait_service "http://0.0.0.0:$AGENT_PORT/ping" pong
 
 if [[ "$MODE" == "test_dialog" || "$MODE" == "all" ]]; then
-  echo "Pass dialogs from dp-agent"
-  dockercompose_cmd exec -T -u $(id -u) agent python3 \
-    utils/http_api_test.py -u http://0.0.0.0:4242 -df tests/dream/test_dialogs.json -of tests/dream/output/test_dialogs_output.csv
+    echo "Pass dialogs from dp-agent"
+    dockercompose_cmd exec -T -u $(id -u) agent python3 \
+        utils/http_api_test.py -u http://0.0.0.0:4242 -df tests/dream/test_dialogs.json -of tests/dream/output/test_dialogs_output.csv
 
-  echo "Assert passed dialogs"
-  if [[ "$DEVICE" == "cpu" ]]; then
-      dockercompose_cmd exec -T -u $(id -u) agent python3 tests/dream/assert_test_dialogs.py -pred_f tests/dream/output/test_dialogs_output.csv -true_f tests/dream/test_dialogs_gold_phrases.csv -time_limit 20
-  else
-      dockercompose_cmd exec -T -u $(id -u) agent python3 tests/dream/assert_test_dialogs.py -pred_f tests/dream/output/test_dialogs_output.csv -true_f tests/dream/test_dialogs_gold_phrases.csv
-  fi
+    echo "Assert passed dialogs"
+    if [[ "$DEVICE" == "cpu" ]]; then
+        dockercompose_cmd exec -T -u $(id -u) agent python3 tests/dream/assert_test_dialogs.py -pred_f tests/dream/output/test_dialogs_output.csv -true_f tests/dream/test_dialogs_gold_phrases.csv -time_limit 20
+    else
+        dockercompose_cmd exec -T -u $(id -u) agent python3 tests/dream/assert_test_dialogs.py -pred_f tests/dream/output/test_dialogs_output.csv -true_f tests/dream/test_dialogs_gold_phrases.csv
+    fi
+fi
+
+if [[ "$MODE" == "test_skills" || "$MODE" == "all" ]]; then
+    echo "Passing test data to each skill selected for testing"
+
+    if container_is_started transfertransfo; then
+        echo "Passing test data to transfertransfo"
+        dockercompose_cmd exec -T -u $(id -u) transfertransfo python tests/run_test.py \
+            --true_file test_tasks.json \
+            --true_file tests/test_tasks.json \
+            --pred_file tests/test_results.json \
+            --from_url http://0.0.0.0:8007/transfertransfo
+    fi
+
 fi
 
 if [[ "$MODE" == "infer_questions" || "$MODE" == "all" ]]; then
-  echo "Passing questions to Alexa"
-  dockercompose_cmd exec -T -u $(id -u) agent python3 \
-    utils/xlsx_responder.py --url http://0.0.0.0:4242 \
-    --input 'tests/dream/test_questions.xlsx' \
-    --output 'tests/dream/output/test_questions_output.xlsx'
+    echo "Passing questions to Alexa"
+    dockercompose_cmd exec -T -u $(id -u) agent python3 \
+        utils/xlsx_responder.py --url http://0.0.0.0:4242 \
+        --input 'tests/dream/test_questions.xlsx' \
+        --output 'tests/dream/output/test_questions_output.xlsx'
 
-  echo "Computing Q&A metrics"
-  dockercompose_cmd exec -T -u $(id -u) agent python3 \
-    tests/dream/compute_qa_metrics.py \
-    --pred_file 'tests/dream/output/test_questions_output.xlsx' \
-    --output 'tests/dream/output/qa_metrics.txt'
+    echo "Computing Q&A metrics"
+    dockercompose_cmd exec -T -u $(id -u) agent python3 \
+        tests/dream/compute_qa_metrics.py \
+        --pred_file 'tests/dream/output/test_questions_output.xlsx' \
+        --output 'tests/dream/output/qa_metrics.txt'
 fi
