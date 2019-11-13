fe02ea3aad920c98c9c8aff47882033347fa97c2
yurakuratov <yurakuratov@gmail.com>
2019-11-13 21:31:22 +0300

    fix: intent catcher failed to run without dev.yml (#146)

--- annotators/IntentCatcher/src/server.py
+++ annotators/IntentCatcher/src/server.py
@@ -8,7 +8,7 @@ import sentry_sdk
 import uuid
 from flask import Flask, request, jsonify
 
-from src.detector import ClassifierDetector
+from detector import ClassifierDetector
 
 sentry_sdk.init(getenv('SENTRY_DSN'))
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
