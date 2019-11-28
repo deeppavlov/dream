43d0b3a3482b3bea51ceb705ef2bc775f8d55064
dimakarp1996 <dimakarp1996@yandex.ru>
2019-11-28 21:03:41 +0300

    minor bug fix

--- utils/collect_amazon_dialogs.py
+++ utils/collect_amazon_dialogs.py
@@ -144,7 +144,8 @@ async def main(args):
     new_conversations = pd.DataFrame(new_conversations)
     new_conversations['start_time'] = pd.to_datetime(new_conversations['start_time'])
     new_conversations = new_conversations.sort_values('start_time', ascending=False)
-    new_conversations = new_conversations.head(args.first_n)
+    if len(new_conversations)>args.first_n:
+        new_conversations = new_conversations.head(args.first_n)
     if args.with_requesting:
         new_conversations = await make_requests(new_conversations, args)
     print_to_file(new_conversations, args)
