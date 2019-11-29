e1770474c899f87c7ba6ba8486cbb2f9ec01b2c8
yurakuratov <yurakuratov@gmail.com>
2019-11-29 13:47:55 +0300

    Fix/fun facts 637 (#192)
    * fix: fun fact about fun
    
    * fix: facts about ok and africa
    
    * fix: fact about fact
    
    * fix: look up for what is fact in utterance

--- skills/CoBotQA/server.py
+++ skills/CoBotQA/server.py
@@ -3,10 +3,11 @@
 import json
 import logging
 import os
+import string
 from time import time
 
 import numpy as np
-from nltk.tokenize import sent_tokenize
+from nltk.tokenize import sent_tokenize, word_tokenize
 import requests
 from concurrent.futures import ThreadPoolExecutor
 from flask import Flask, request, jsonify
@@ -33,7 +34,16 @@ if COBOT_QA_SERVICE_URL is None:
 headers = {'Content-Type': 'application/json;charset=utf-8', 'x-api-key': f'{COBOT_API_KEY}'}
 
 with open("./google-10000-english-no-swears.txt", "r") as f:
-    UNIGRAMS = f.read().splitlines()[:1002]
+    UNIGRAMS = f.read().splitlines()[:1001]
+
+
+def remove_punct_and_articles(s, lowecase=True):
+    articles = ['a', 'the']
+    if lowecase:
+        s = s.lower()
+    no_punct = ''.join([c for c in s if c not in string.punctuation])
+    no_articles = ' '.join([w for w in word_tokenize(no_punct) if w.lower() not in articles])
+    return no_articles
 
 
 def send_cobotqa(question):
@@ -75,7 +85,12 @@ def respond():
     dialog_ids = []
     for i, dialog in enumerate(dialogs):
         curr_uttr = dialog["utterances"][-1]
-        questions.append(curr_uttr["annotations"]["sentrewrite"]["modified_sents"][-1])
+        curr_uttr_rewritten = curr_uttr["annotations"]["sentrewrite"]["modified_sents"][-1]
+        # fix to question what is fact, cobotqa gives random fact on such question
+        what_is_fact = 'what is fact'
+        if remove_punct_and_articles(curr_uttr_rewritten)[-len(what_is_fact):] == what_is_fact:
+            curr_uttr_rewritten = 'defenition of fact'
+        questions.append(curr_uttr_rewritten)
         dialog_ids += [i]
 
         entities = []
@@ -107,6 +122,13 @@ def respond():
     for i, response in enumerate(executor.map(send_cobotqa, questions)):
         logger.info("Question: {}".format(questions[i]))
         logger.info("Response: {}".format(response))
+
+        # fix for cases when fact is about fun, but fun is not in entities
+        fun_fact_q = 'Fun fact about'
+        if fun_fact_q in questions[i] and ' fun' not in questions[i][len(fun_fact_q):].lower() \
+                and 'Fun is defined by the Oxford English Dictionary as' in response:
+            response = ''
+
         responses.append(response)
 
         if len(response) > 0 and 'skill://amzn1' not in response:
