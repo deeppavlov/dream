78096c867b7ff82d1eb11cef05cce4bcf631333f
Dilyara Baymurzina <dilyara.rimovna@gmail.com>
2019-11-29 22:22:23 +0300

    Feat/black friday (#196)
    * feat: black fridday talk
    
    * test dialogs
    
    * fix: other answer template
    
    * fix: english grammar
    
    * fix: tests without gold phrases
    
    * Fix friday
    
    * Use tfidf only if dialog turens > 7
    
    * fix: upd tests
    
    * fix: revert tests
    
    * feat: counter question done
    
    * fix: tests
    
    * Apply suggestions from code review
    
    * Update tests/dream/test_dialogs_gold_phrases.csv
    
    * fix: tests

--- skill_selectors/rule_based_selector/server.py
+++ skill_selectors/rule_based_selector/server.py
@@ -75,7 +75,8 @@ class RuleBasedSelector():
                 skills_for_uttr.append("program_y")
                 skills_for_uttr.append("cobotqa")
                 skills_for_uttr.append("alice")
-                skills_for_uttr.append("tfidf_retrieval")
+                if len(dialog['utterances']) > 7:
+                    skills_for_uttr.append("tfidf_retrieval")
                 # skills_for_uttr.append("transfertransfo")
                 # skills_for_uttr.append("retrieval_chitchat")
             movie_cobot_dialogacts = {
