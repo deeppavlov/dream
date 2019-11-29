dfedd2bd54e34d859bce8a89dd97f67a2628ac96
dimakarp1996 <dimakarp1996@yandex.ru>
2019-11-29 13:28:11 +0300

    changed config.py

--- config.py
+++ config.py
@@ -113,7 +113,7 @@ SKILLS = [
         "endpoint": "api/rest/v1.0/ask",
         "external": True,
         "path": "",
-        "formatter": program_y_formatter
+        "formatter": alice_formatter
     },
     {
         "name": "movie_skill",
