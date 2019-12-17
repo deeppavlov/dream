43a2ccb227de6a436917a9a55913c582959ecb0d
yurakuratov <yurakuratov@gmail.com>
2019-12-17 12:52:31 +0300

    feat: just download everything from s3 bucket (#242)

--- utils/download_ratings.sh
+++ utils/download_ratings.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Downloading ratings"
-aws s3 cp s3://alexaprize/807746935730/conversation_feedback.csv ./ratings/
-aws s3 cp s3://alexaprize/807746935730/Ratings/ratings.csv ./ratings/
-
-echo "Downloading conversation assessment"
-python utils/download_conversation_assessment.py
-
-echo "Downloading frequent utterances"
-python utils/download_frequent_utterances.py
-
+echo "Downloading ratings, feedback, ..."
+aws s3 cp --recursive s3://alexaprize/807746935730 ./ratings
 echo "Done"
