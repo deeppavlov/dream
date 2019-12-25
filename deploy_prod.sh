0c85b7ac35143830492f3963dd9aa9a4cd263aed
yurakuratov <yurakuratov@gmail.com>
2019-12-25 22:59:07 +0300

    Feat/zero downtime prod deploy (#265)
    * feat: add deploy prod script
    
    * upd Jenkinsfile
    
    * fix: remove port from url
    
    * remove: debug output
    
    * upd log messages
    
    * fix: set up curl timeout
    
    * fix: curl timeout

---
+++ deploy_prod.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -e
+
+STAGING_DP_AGENT_URL=`cat .env.staging | grep -oP "DP_AGENT_URL=\K.*"`
+PROD_DP_AGENT_URL=`cat .env.prod | grep -oP "DP_AGENT_URL=\K.*"`
+
+echo "Deploying PROD version to staging cluster..."
+./deploy.sh MODE=all TARGET=dev
+# sleep to make sure that old agent is stopped
+sleep 30;
+while [[ "$(curl -m 5 -s -o /dev/null -w ''%{http_code}'' $STAGING_DP_AGENT_URL/ping)" != "200" ]]; do
+  echo "waiting till agent is up on staging...";
+  sleep 5;
+done
+# to finish services loading
+sleep 60;
+echo "Deploying PROD version to staging cluster... done"
+
+echo "Changing PROD lambda url to staging..."
+sed -i 's/DP_AGENT_URL=.*/'"$(printf '%s\n' "$(cat .env.staging | grep DP_AGENT_URL)" | sed 's:[][\/.^$*]:\\&:g')"'/g' .env.prod
+./deploy.sh MODE=lambda TARGET=prod
+echo "Changing PROD lambda url to staging... done"
+
+echo "Deploying agent to PROD cluster..."
+./deploy.sh MODE=agent TARGET=prod
+sleep 30;
+while [[ "$(curl -m 5 -s -o /dev/null -w ''%{http_code}'' $PROD_DP_AGENT_URL/ping)" != "200" ]]; do
+  echo "waiting till agent is up on prod...";
+  sleep 5;
+done
+sleep 60;
+echo "Deploying agent to PROD cluster... done"
+
+echo "Changing PROD lambda url to prod..."
+git checkout .env.prod
+./deploy.sh MODE=lambda TARGET=prod
+echo "Changing PROD lambda url to prod... done"
+
+# echo "Deploying dev to staging"
+# git checkout dev # how to make run it on Jenkins?
+# ./deploy.sh MODE=all TARGET=dev
+
+echo "PROD is successfully deployed"
+exit 0
