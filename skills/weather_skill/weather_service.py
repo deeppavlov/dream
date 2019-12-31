8f670d59d8991535b07d7f60b8ddc8edc64411e2
Alexander Dmitrievskiy <acriptis@gmail.com>
2019-12-31 17:02:57 +0300

    Weather skill (#277)
    * push initial version of weather skill
    
    * fix intent_phrases and train IntentCatcher
    
    * add openweathermap and cobot as wether providers. use openweathermap as default. update tests
    
    * update dockerfile
    
    * Update skills/weather_skill/Dockerfile
    
    Co-Authored-By: Idris Y <sld@users.noreply.github.com>
    
    * Update skills/weather_skill/requirements.txt
    
    Co-Authored-By: Idris Y <sld@users.noreply.github.com>
    
    * Update skills/weather_skill/server.py
    
    Co-Authored-By: Idris Y <sld@users.noreply.github.com>
    
    * Update skills/weather_skill/server.py
    
    Co-Authored-By: Dilyara Baymurzina <dilyara.rimovna@gmail.com>
    
    * Update skills/weather_skill/server.py
    
    Co-Authored-By: Dilyara Baymurzina <dilyara.rimovna@gmail.com>
    
    * update IntentCatcher Dockerfile to use new intent_data.json
    
    * fix travis blaming
    
    * update weather service to use requests lib and grab exceptions
    
    * feat: ymls
    
    * feat: turn on in selector
    
    * fix: grammar
    
    * fix: dockerfile without cobotqa copy
    
    * update dp_agent_alexa tests
    
    * feat: intent catcher of weather
    
    * feat: turning on of weather skill
    
    * fix: weather skill
    
    * fix weather skill integration
    
    * fix travis blaming
    
    * make convers logs more readable
    
    * handle weather service unavaliable problem
    
    * fix travis
    
    * tune up confidence
    
    * fix tests in weather skill
    
    * convert kelvins to fahrenheits
    
    * hotfix with wrong code
    
    * fix travis blaming
    
    * fix travis
    
    * update intent catcher with new cases for weather skill and missed comma fix
    
    * improve weather skill template and service problems template. add random_crazy_forecast for more fun answers
    
    * fix travis
    
    * fixed crazy forecast ideas
    
    Co-authored-by: Idris Y <sld@users.noreply.github.com>
    Co-authored-by: Dilyara Baymurzina <dilyara.rimovna@gmail.com>

--- skills/weather_skill/weather_service.py
+++ skills/weather_skill/weather_service.py
@@ -2,6 +2,7 @@ import logging
 import sentry_sdk
 from os import getenv
 import requests
+import random
 
 sentry_sdk.init(getenv('SENTRY_DSN'))
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -61,6 +62,38 @@ FAHRENHEIT_OFFSET = 32.0
 FAHRENHEIT_DEGREE_SCALE = 1.8
 
 
+def random_crazy_forecast(city_str):
+    """
+    This function produces joke-like response with funny weather forecast.
+
+    May be launched in case of problems with Basic service or in case of bad LOCation recognition
+    :param city_str:
+    :return: weather forecast as string for Human
+    """
+
+    # just a list of crazy forecasts:
+    crazy_description = random.choice(["Fish rain may occur.",
+                                       "Skittles chewies falling is possible.",
+                                       "Toy Godzilla may come to the city.",
+                                       "Potato storm may wonder you.",
+                                       "Pancake tornado may be seen in the sky",
+                                       "magic powder from the sky",
+                                       "rainbow leaks may occur",
+                                       "Teletubbies invasion is still probable",
+                                       "Chuk Norris' thunderstorm may be unexpected occasion",
+                                       "Hyperbolic UFOs may be flying over the sky tonight",
+                                       "Santa Claus dance may cause tickling feeling under the nose",
+                                       "Drunk deer may knock the roof"
+                                       ])
+
+    temperature_prediction = random.randrange(0, 100)
+
+    template = "Currently I have problems with getting weather in %s. But I would guess... %s . " \
+               "And the temperature may be around %d fahrenheit. Would you enjoy it?" % (
+                   city_str, crazy_description, temperature_prediction)
+    return template
+
+
 def kelvin_to_fahrenheit(kelvintemp):
     """
     # taken from
@@ -83,6 +116,43 @@ def owm_requests_weather_forecast_now(city_str):
 
     :param city_str: string with city name
     :return: response for user as string
+
+    Result Example:
+        {
+            "coord":
+                {"lon":-0.13,"lat":51.51},
+            "weather": [
+                {
+                    "id":300,
+                    "main":"Drizzle",
+                    "description":"light intensity drizzle",
+                    "icon":"09d"}
+                ],
+            "base":"stations",
+            "main":
+                {
+                "temp":280.32,
+                "pressure":1012,
+                "humidity":81,
+                "temp_min":279.15,
+                "temp_max":281.15},
+            "visibility":10000,
+            "wind":
+                {
+                "speed":4.1,
+                "deg":80},
+            "clouds":{
+                "all":90},
+            "dt":1485789600,
+            "sys":{
+                "type":1,"id":5091,"message":0.0103,
+                "country":"GB",
+                "sunrise":1485762037,
+                "sunset":1485794875},
+            "id":2643743,
+            "name":"London",
+            "cod":200
+        }
     """
     api_key = "644515bef5492fff0a5913f73ac212ae"
     url = "http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s" % (city_str, api_key)
@@ -91,10 +161,23 @@ def owm_requests_weather_forecast_now(city_str):
         json_data = resp.json()
         try:
             description = json_data['weather'][0]['description']
+
             temperature = kelvin_to_fahrenheit(json_data['main']['temp'])
+            # min_temperature = kelvin_to_fahrenheit(json_data['main']['temp_min'])
+            # max_temperature = kelvin_to_fahrenheit(json_data['main']['temp_max'])
+
             city_name = json_data['name']
-            response_template = "It is %s, temperature: %0.1f in %s" % (
-                description, temperature, city_name)
+
+            # meter/sec
+            wind_speed = json_data['wind']['speed']
+
+            # hPa
+            # pressure = json_data['main']['pressure']
+            # humidity = json_data['main']['humidity']
+
+            response_template = "It is %s, temperature is around %0.1f in %s. " \
+                                "Wind speed is about %f meters per second" % (
+                                    description, temperature, city_name, wind_speed)
         except Exception as e:
             # we have problems with weather service:
             # soltions:
@@ -102,7 +185,8 @@ def owm_requests_weather_forecast_now(city_str):
             # 2. use another weather provider?
             logger.exception("WeatherService Problems")
             sentry_sdk.capture_exception(e)
-            response_template = SORRY_TEMPLATE
+            response_template = random_crazy_forecast(city_str)
+            # response_template = SORRY_TEMPLATE
 
     except (requests.ConnectTimeout, requests.ReadTimeout) as e:
         sentry_sdk.capture_exception(e)
