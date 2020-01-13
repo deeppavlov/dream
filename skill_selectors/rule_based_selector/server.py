5d56fba61e923d8e8564305f17842031ef46bb42
yurakuratov <yurakuratov@gmail.com>
2020-01-13 20:57:56 +0300

    refactor: skill selector (#288)
    * refactor: skill selector
    
    * refactor: remove duplicate variables

--- skill_selectors/rule_based_selector/server.py
+++ skill_selectors/rule_based_selector/server.py
@@ -63,23 +63,6 @@ class RuleBasedSelector():
 
             blist_topics_detected = dialog['utterances'][-1]['annotations']['blacklisted_words']['restricted_topics']
 
-            if "/new_persona" in dialog['utterances'][-1]['text']:
-                skills_for_uttr.append("personality_catcher")  # TODO: rm crutch of personality_catcher
-            elif intent_detected:
-                skills_for_uttr.append("intent_responder")
-            elif blist_topics_detected or (sensitive_topics_detected and sensitive_dialogacts_detected):
-                skills_for_uttr.append("program_y_dangerous")
-                skills_for_uttr.append("cobotqa")
-            else:
-                skills_for_uttr.append("program_y")
-                skills_for_uttr.append("cobotqa")
-                skills_for_uttr.append("alice")
-                skills_for_uttr.append("christmas_new_year_skill")
-                if len(dialog['utterances']) > 7:
-                    skills_for_uttr.append("tfidf_retrieval")
-                # skills_for_uttr.append("transfertransfo")
-                # skills_for_uttr.append("retrieval_chitchat")
-                    skills_for_uttr.append("convert_reddit")
             movie_cobot_dialogacts = {
                 "Entertainment_Movies", "Sports", "Entertainment_Music", "Entertainment_General",
                 "Phatic"
@@ -88,55 +71,74 @@ class RuleBasedSelector():
                 "Movies_TV", "Celebrities", "Art_Event", "Entertainment",
                 "Fashion", "Games", "Music", "Sports"
             }
-            about_movies = (movie_cobot_dialogacts & cobot_dialogact_topics) | (movie_cobot_topics & cobot_topics)
-            if about_movies:
-                skills_for_uttr.append("movie_skill")
-                if "christmas_new_year_skill" not in skills_for_uttr:
-                    skills_for_uttr.append("christmas_new_year_skill")
-
-            about_music = ("Entertainment_Music" in cobot_dialogact_topics) | ("Music" in cobot_topics)
-            if about_music and len(dialog["utterances"]) > 2:
-                skills_for_uttr.append("music_tfidf_retrieval")
-                if "christmas_new_year_skill" not in skills_for_uttr:
-                    skills_for_uttr.append("christmas_new_year_skill")
-
             books_cobot_dialogacts = {
                 "Entertainment_General", "Entertainment_Books"
             }
             books_cobot_topics = {
                 "Entertainment", "Literature"
             }
+            about_movies = (movie_cobot_dialogacts & cobot_dialogact_topics) | (movie_cobot_topics & cobot_topics)
+            about_music = ("Entertainment_Music" in cobot_dialogact_topics) | ("Music" in cobot_topics)
             about_books = (books_cobot_dialogacts & cobot_dialogact_topics) | (books_cobot_topics & cobot_topics)
-            if about_books:
-                skills_for_uttr.append("book_skill")
-                if "christmas_new_year_skill" not in skills_for_uttr:
-                    skills_for_uttr.append("christmas_new_year_skill")
-            # always add dummy_skill
-            skills_for_uttr.append("dummy_skill")
-            skills_for_uttr.append("personal_info_skill")
 
-            if len(dialog['utterances']) > 1:
-                # skills_for_uttr += ["misheard_asr"]
-                # Use only misheard asr skill if asr is not confident and skip it for greeting
-                if dialog['utterances'][-1]['annotations']['asr']['asr_confidence'] == 'very_low':
-                    skills_for_uttr = ["misheard_asr"]
-                # previous user's reply hypotheses
-                hypotheses = dialog["utterances"][-3]["hypotheses"] if len(dialog["utterances"]) >= 3 else []
-                for hyp in hypotheses:
+            prev_user_uttr_hyp = dialog["utterances"][-3]["hypotheses"] if len(dialog["utterances"]) >= 3 else []
+            prev_bot_uttr = dialog["utterances"][-2] if len(dialog["utterances"]) >= 2 else {}
+
+            weather_city_slot_requested = any([
+                hyp.get("weather_forecast_interaction_city_slot_requested", False)
+                for hyp in prev_user_uttr_hyp if hyp["skill_name"] == "weather_skill"])
+
+            about_weather = dialog['utterances'][-1]['annotations']['intent_catcher'].get(
+                "weather_forecast_intent", {}).get("detected", False) or (prev_bot_uttr.get(
+                    "active_skill", "") == "weather_skill" and weather_city_slot_requested)
+
+            if "/new_persona" in dialog['utterances'][-1]['text']:
+                # process /new_persona command
+                skills_for_uttr.append("personality_catcher")  # TODO: rm crutch of personality_catcher
+            elif intent_detected:
+                # process intent with corresponding IntentResponder
+                skills_for_uttr.append("intent_responder")
+            elif blist_topics_detected or (sensitive_topics_detected and sensitive_dialogacts_detected):
+                # process user utterance with sensitive content
+                skills_for_uttr.append("program_y_dangerous")
+                skills_for_uttr.append("cobotqa")
+            else:
+                # process regular utterances
+                skills_for_uttr.append("program_y")
+                skills_for_uttr.append("cobotqa")
+                skills_for_uttr.append("alice")
+                skills_for_uttr.append("christmas_new_year_skill")
+                skills_for_uttr.append("personal_info_skill")
+
+                if len(dialog['utterances']) > 7:
+                    skills_for_uttr.append("tfidf_retrieval")
+                    skills_for_uttr.append("convert_reddit")
+
+                # thematic skills
+                if about_movies:
+                    skills_for_uttr.append("movie_skill")
+
+                if about_music and len(dialog["utterances"]) > 2:
+                    skills_for_uttr.append("music_tfidf_retrieval")
+
+                if about_books:
+                    skills_for_uttr.append("book_skill")
+
+                if about_weather:
+                    skills_for_uttr.append("weather_skill")
+
+                for hyp in prev_user_uttr_hyp:
                     # here we just forcibly add skills which return `can_continue` and it's not `no`
                     if hyp.get("can_continue", CAN_NOT_CONTINUE) in {CAN_CONTINUE, MUST_CONTINUE}:
                         skills_for_uttr.append(hyp["skill_name"])
 
-            # prev_user_uttr = dialog["utterances"][-3] if len(dialog["utterances"]) >= 3 else {}
-            prev_user_uttr_hyp = dialog["utterances"][-3]["hypotheses"] if len(dialog["utterances"]) >= 3 else []
-            weather_city_slot_requested = any([
-                hyp.get("weather_forecast_interaction_city_slot_requested", False)
-                for hyp in prev_user_uttr_hyp if hyp["skill_name"] == "weather_skill"])
-            prev_bot_uttr = dialog["utterances"][-2] if len(dialog["utterances"]) >= 2 else {}
-            if (dialog['utterances'][-1]['annotations']['intent_catcher'].get(
-                    "weather_forecast_intent", {}).get("detected", False) or (
-                    prev_bot_uttr.get("active_skill", "") == "weather_skill" and weather_city_slot_requested)):
-                skills_for_uttr.append("weather_skill")
+                if len(dialog['utterances']) > 1:
+                    # Use only misheard asr skill if asr is not confident and skip it for greeting
+                    if dialog['utterances'][-1]['annotations']['asr']['asr_confidence'] == 'very_low':
+                        skills_for_uttr = ["misheard_asr"]
+
+            # always add dummy_skill
+            skills_for_uttr.append("dummy_skill")
 
             skills_for_uttr = list(set(skills_for_uttr))
             skill_names.append(skills_for_uttr)
