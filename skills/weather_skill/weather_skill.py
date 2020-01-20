949a45ec60b3ba7f945c7c46703af7b10101414e
Alexander Dmitrievskiy <acriptis@gmail.com>
2020-01-20 14:11:07 +0300

    Weather skill (#311)
    * push initial version of weather skill
    
    * fix intent_phrases and train IntentCatcher
    
    * add openweathermap and cobot as wether providers. use openweathermap as default. update tests
    
    * update dockerfile
    
    * Update skills/weather_skill/Dockerfile
    
    Co-Authored-By: Idris Y <sld@users.noreply.github.com>
    
    * Update skills/weather_skill/requirements.txt
    
    Co-Authored-By: Idris Y <sld@users.noreply.github.com>
    
    * Update skills/weather_skill/server.py
    
    Co-Authored-By: Idris Y <sld@users.noreply.github.com>
    
    * Update skills/weather_skill/server.py
    
    Co-Authored-By: Dilyara Baymurzina <dilyara.rimovna@gmail.com>
    
    * Update skills/weather_skill/server.py
    
    Co-Authored-By: Dilyara Baymurzina <dilyara.rimovna@gmail.com>
    
    * update IntentCatcher Dockerfile to use new intent_data.json
    
    * fix travis blaming
    
    * update weather service to use requests lib and grab exceptions
    
    * feat: ymls
    
    * feat: turn on in selector
    
    * fix: grammar
    
    * fix: dockerfile without cobotqa copy
    
    * update dp_agent_alexa tests
    
    * feat: intent catcher of weather
    
    * feat: turning on of weather skill
    
    * fix: weather skill
    
    * fix weather skill integration
    
    * fix travis blaming
    
    * make convers logs more readable
    
    * handle weather service unavaliable problem
    
    * fix travis
    
    * tune up confidence
    
    * fix tests in weather skill
    
    * convert kelvins to fahrenheits
    
    * hotfix with wrong code
    
    * fix travis blaming
    
    * fix travis
    
    * update intent catcher with new cases for weather skill and missed comma fix
    
    * improve weather skill template and service problems template. add random_crazy_forecast for more fun answers
    
    * fix travis
    
    * fixed crazy forecast ideas
    
    * make wind speed as simple float without excessive zeros
    
    * update intent phrases
    
    * update intent phrases2
    
    * update IntentCatcher with new data
    
    * add sorry message for easy debugging hypotheses
    
    * fix test of the weather skill
    
    * update tests for weather
    
    * retrain intent catcher for weather
    
    * update gitignore
    
    * fix weather skill
    
    * release new GEO NER extractor for weather skill
    
    * clean code
    
    * fix travis
    
    Co-authored-by: Idris Y <sld@users.noreply.github.com>
    Co-authored-by: Dilyara Baymurzina <dilyara.rimovna@gmail.com>
    Co-authored-by: yurakuratov <yurakuratov@gmail.com>

--- skills/weather_skill/weather_skill.py
+++ skills/weather_skill/weather_skill.py
@@ -2,10 +2,14 @@ import logging
 from os import getenv
 import sentry_sdk
 import pprint
+from city_slot import OWMCitySlot
+
 sentry_sdk.init(getenv('SENTRY_DSN'))
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+city_slot_obj = OWMCitySlot()
 
 
 class DialogDataManager():
@@ -47,25 +51,29 @@ class DialogDataManager():
     def retrieve_location_entity_from_utterance(self, utterance_dict=None):
         """
         retrieves the first extracted entity froim utterance dict
-        :param utterance:
+        :param utterance_dict: raw dictionary with serialization of Utterance object.
         :return: city string
         """
         if not utterance_dict:
             # by default provide location extraction from last reply
             utterance_dict = self.get_last_utterance_dict()
 
-        annotations = utterance_dict["annotations"]
-        city = None
-        # logger.info("entities in lastest annotation:")
-        # logger.info(annotations["ner"])
-        for ent in annotations["ner"]:
-            if not ent:
-                continue
-            ent = ent[0]
-            if ent['type'] == "LOC":
-                city = ent['text']
-                # TODO normalize city...?
-                # TODO validate city?
+        # ### Alternative GEO extractor:
+        city = city_slot_obj(utterance_dict['text'])
+        if not city:
+            # if not extracted lets try to grab from NER?
+            annotations = utterance_dict["annotations"]
+            city = None
+            # logger.info("entities in lastest annotation:")
+            # logger.info(annotations["ner"])
+            for ent in annotations["ner"]:
+                if not ent:
+                    continue
+                ent = ent[0]
+                if ent['type'] == "LOC":
+                    city = ent['text']
+                    # TODO normalize city...?
+                    # TODO validate city?
 
         # TODO No NER detected location case?
         # TODO detect from keywords search
@@ -92,7 +100,6 @@ class WeatherSkill:
         current_reply = ""
         human_attr = {}
         bot_attr = {}
-        # TODO load attrs?
         ######################################################################
         #
         d_man = DialogDataManager(dialog)
