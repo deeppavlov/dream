c80e61e9857062a996f59946ddc25f71a33e0c36
yurakuratov <9271630+yurakuratov@users.noreply.github.com>
2020-01-24 16:12:33 +0300

    Feat/docker rm on deploy (#349)
    * feat: run docker rm on every deploy on every machine
    
    * fix: docker-rm service command

--- deploy.sh
+++ deploy.sh
@@ -52,6 +52,10 @@ if [[ "$MODE" == "agent" || "$MODE" == "all" ]]; then
     limit="$((limit-1))";
   done
 
+  printf "\t Removing stopped containers.."
+  DOCKER_HOST=$DOCKER_HOST docker service update --force docker-rm
+  sleep 10;
+
   printf "\t Docker stack deploy\n"
   VERSION="$(git rev-parse --short HEAD)" ENV_FILE=$ENV_FILE DOCKER_REGISTRY=807746935730.dkr.ecr.us-east-1.amazonaws.com DOCKER_HOST=$DOCKER_HOST docker stack deploy --prune --compose-file docker-compose.yml,staging.yml --with-registry-auth $REGISTRY_AUTH
 fi
