8580f6123368f3a933a0e1689c3e10b09d60d525
Dilyara Baymurzina <dilyara.rimovna@gmail.com>
2020-02-03 11:31:02 +0300

    Feat/sst sentiment (#366)
    * feat: sst sentiment model instead of yelp
    
    * fix: tests
    
    * fix: codestyle

--- annotators/DeepPavlovSentimentClassification/tests/run_test.py
+++ annotators/DeepPavlovSentimentClassification/tests/run_test.py
@@ -5,12 +5,20 @@ def get_input_json(text: str):
     return {"sentences": [text]}
 
 
-selected_classes = ["neutral", "positive", "negative"]
+selected_classes = ['positive', 'positive', 'negative',
+                    'positive', 'neutral', 'positive',
+                    'neutral',
+                    'neutral',
+                    'positive']
 
 
 def test_one_step_responses():
     url = "http://0.0.0.0:8024/sentiment_annotations"
-    input_data = {"sentences": ["ok", "i love you", "i hate you"]}
+    input_data = {"sentences": ["ok", "i love you", "i hate you",
+                                "yes", "no", "please",
+                                "tell me something",
+                                "let's talk about something else",
+                                "let's talk about it"]}
     response = requests.post(url, json=input_data).json()
     for tgt, pred in zip(selected_classes, response):
         assert tgt == pred[0][0]
