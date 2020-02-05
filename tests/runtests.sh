108bcb8d316c40161def7ca8d00073049d3b327a
yurakuratov <9271630+yurakuratov@users.noreply.github.com>
2020-02-05 11:39:41 +0300

    Feat/comet service (#377)
    * feat: add comet model Dockerfiles
    
    * feat: add comet service
    
    * test: add comet service test
    
    * fix: add comet to staging.yml
    
    * fix: typo

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -165,6 +165,11 @@ if [[ "$MODE" == "test_skills" || "$MODE" == "all" ]]; then
         echo "Run tests for news_skill"
         dockercompose_cmd exec -T -u $(id -u) news_skill python /src/src/test.py
     fi
+
+    if container_is_started comet; then
+        echo "Run tests for comet"
+        dockercompose_cmd exec -T -u $(id -u) comet python /comet/test.py
+    fi
 fi
 
 if [[ "$MODE" == "infer_questions" || "$MODE" == "all" ]]; then
