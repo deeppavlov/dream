83da824a0392a10b2fab29b3e0e903f026e581dd
Daniil Cherniavskii <danchern97@mail.ru>
2020-02-12 20:35:51 +0300

    Fix: tests

--- annotators/IntentCatcher/src/test.py
+++ annotators/IntentCatcher/src/test.py
@@ -8,10 +8,11 @@ def main_test():
     url = 'http://0.0.0.0:8014/detect'
     tests = json.load(open('tests.json'))
     for test in tests:
-        r = requests.post(url=url, json={'sentences': [test['sentence']]})
+        r = requests.post(url=url, json={'sentences': [[test['sentence']]]})
         assert r.ok
         if test['intent'] is not None:
-            assert r.json()[0].get(test['intent'], {'detected': 1}).get('detected', 0) == 1, print(f"Test failed: {test}")
+            assert r.json()[0].get(test['intent'], {'detected': 1}).get(
+                'detected', 0) == 1, print(f"TEST FAILED!\nTest: {test}\nResult:{r.json()}")
         else:
             assert all([intent['detected'] == 0 for intent in r.json().values()])
     print("Success")
