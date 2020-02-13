ffac8eb388d39730c2800a6a66be2a8279fce8c0
Kuznetsov Denis <kuznetsov.den.p@gmail.com>
2020-02-13 17:12:43 +0300

    Feat/valentines day skill (#411)
    * wip (feat/valentines_day_skill): add new valentines_day_skill
    
    * wip (feat/valentines_day_skill): fix style

--- skill_selectors/rule_based_selector/connector.py
+++ skill_selectors/rule_based_selector/connector.py
@@ -178,6 +178,7 @@ class RuleBasedSkillSelectorConnector:
             skills_for_uttr.append("christmas_new_year_skill")
             skills_for_uttr.append("superbowl_skill")
             skills_for_uttr.append("oscar_skill")
+            skills_for_uttr.append("valentines_day_skill")
             skills_for_uttr.append("personal_info_skill")
             skills_for_uttr.append("meta_script_skill")
 
