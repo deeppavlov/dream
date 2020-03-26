16ca2810e2cef5b3f5a38863b2fdcbd2c49ed4ac
yurakuratov <9271630+yurakuratov@users.noreply.github.com>
2020-03-26 13:10:31 +0300

    Fix/alice templates (#573)
    * tests: update alice test.sh
    
    * feat: add templates loading logging
    
    * fix: parse errors and fatal parse errors
    
    * fix: restore templates with POP
    
    Co-authored-by: Dilyara Baymurzina <dilyara.rimovna@gmail.com>

--- skills/ALICEChatAPI/ai.py
+++ skills/ALICEChatAPI/ai.py
@@ -2,38 +2,45 @@
 
 import aiml
 import os
-import sys
-import subprocess
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class Chatbot():
-  def __init__(self):
-    self._kernel = aiml.Kernel()
+    def __init__(self):
+        self._kernel = aiml.Kernel()
 
-  def initialize(self, aiml_dir):
-    self._kernel.learn(os.sep.join([aiml_dir, '*.aiml']))
-    properties_file = open(os.sep.join([aiml_dir, 'bot.properties']))
-    for line in properties_file:
-      parts = line.split('=')
-      key = parts[0]
-      value = parts[1]
-      self._kernel.setBotPredicate(key, value)
+    def initialize(self, aiml_dir):
+        for name in os.listdir(aiml_dir):
+            if name.endswith('.aiml'):
+                logger.info(f'loading templates from: {name}')
+                self._kernel.learn(os.sep.join([aiml_dir, name]))
+        properties_file = open(os.sep.join([aiml_dir, 'bot.properties']))
+        for line in properties_file:
+            parts = line.split('=')
+            key = parts[0]
+            value = parts[1]
+            self._kernel.setBotPredicate(key, value)
 
-  def respond(self, input, session_id):
-    response = self._kernel.respond(input, sessionID=session_id)
-    return response
+    def respond(self, input, session_id):
+        response = self._kernel.respond(input, sessionID=session_id)
+        return response
 
-  def reset(self):
-    self._kernel.resetBrain()
+    def reset(self):
+        self._kernel.resetBrain()
 
 
 def main():
-  chatbot = Chatbot()
-  chatbot.initialize("aiml-dir") # parameter is the aiml directory
+    chatbot = Chatbot()
+    chatbot.initialize("aiml-dir")  # parameter is the aiml directory
 
-  while True:
-    n = input("Input: ")
-    print(chatbot.respond(n))
+    while True:
+        n = input("Input: ")
+        print(chatbot.respond(n))
+
 
 if __name__ == '__main__':
-  main()
+    main()
--- skills/ALICEChatAPI/test.sh
+++ skills/ALICEChatAPI/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 curl -H "Content-Type: application/json" -XPOST \
-  -d '{"sentences": ["Are you bot?"]}' \
-  http://0.0.0.0:3000/respond
+  -d '{"sentences_batch": ["Are you bot or human?"]}' \
+  http://0.0.0.0:8000/respond
 
 
 curl -H "Content-Type: application/json" -XPOST \
-  -d '{"sentences": ["Hi!", "How are you?", "Do you know the text?"]}' \
-  http://0.0.0.0:3000/respond
+  -d '{"sentences_batch": [["Hi!", "How are you?", "Do you know the text?"]]}' \
+  http://0.0.0.0:8000/respond
