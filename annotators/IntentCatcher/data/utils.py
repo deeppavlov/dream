e4f52d645b0da0456f6e08c1335937b67efb1131
Daniil Cherniavskii <danchern97@mail.ru>
2020-03-26 12:48:00 +0300

    Fix/intent catcher 2 (#572)
    * Added tests
    
    * Update phrases + limit
    
    * Update README.md
    
    * Update respond phrases
    
    * fix: cant do phrases and misspelling
    
    Co-authored-by: dilyararimovna <dilyara.rimovna@gmail.com>

--- annotators/IntentCatcher/data/utils.py
+++ annotators/IntentCatcher/data/utils.py
@@ -79,7 +79,7 @@ def calculate_metrics(intents_min_pr, y_true, y_pred):
     return intent_data
 
 
-def generate_phrases(template_re, punctuation, limit=3000):
+def generate_phrases(template_re, punctuation, limit=20000):
     x = Xeger(limit=limit)
     phrases = []
     for regex in template_re:
