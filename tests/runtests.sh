7b8028e689dacee3be6d74020e406af40c6c5314
Dilyara Baymurzina <dilyara.rimovna@gmail.com>
2020-03-31 15:22:37 +0300

    fix: disable news_skill (#597)
    * fix: disable news_skill
    
    * fix: move to unused skills
    
    * tests: turn off eora news_skill tests
    
    Co-authored-by: yurakuratov <9271630+yurakuratov@users.noreply.github.com>

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -174,10 +174,10 @@ if [[ "$MODE" == "test_skills" || "$MODE" == "all" ]]; then
         dockercompose_cmd exec -T -u $(id -u) eliza python /src/test_server.py
     fi
 
-    if container_is_started news_skill; then
-        echo "Run tests for news_skill"
-        dockercompose_cmd exec -T -u $(id -u) news_skill python /src/src/test.py
-    fi
+    # if container_is_started news_skill; then
+    #     echo "Run tests for news_skill"
+    #     dockercompose_cmd exec -T -u $(id -u) news_skill python /src/src/test.py
+    # fi
 
     if container_is_started dummy_skill_dialog; then
         echo "Run tests for dummy_skill_dialog"
