15b00c2430ddba1085e0fa7a3c8b455f8beb01dd
yurakuratov <9271630+yurakuratov@users.noreply.github.com>
2020-04-01 21:25:18 +0300

    Fix/alice templates (#609)
    * fix: remove & fix some alice templates
    
    * alice uses last 2 human utt, programy - 5
    
    * fix: typo

--- skills/ALICEChatAPI/test.sh
+++ skills/ALICEChatAPI/test.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 curl -H "Content-Type: application/json" -XPOST \
+  -d '{"sentences_batch": [["I am GGGGG."]]}' \
+  http://0.0.0.0:8000/respond
+
+curl -H "Content-Type: application/json" -XPOST \
+  -d '{"sentences_batch": [["i am not", "i missed you that", "hang up walking", "yes"]]}' \
+  http://0.0.0.0:8000/respond
+
+curl -H "Content-Type: application/json" -XPOST \
   -d '{"sentences_batch": [["Are you bot or human?"]]}' \
   http://0.0.0.0:8000/respond
 
