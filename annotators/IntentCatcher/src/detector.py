8d12ab65cc2d1909dfe580f1721470f60a47f8bf
Daniil Cherniavskii <danchern97@mail.ru>
2020-04-21 15:40:42 +0300

    Fix/intent catcher update (#713)
    * Add: phrases, new intent
    
    * Response to new intent
    
    * Update phrases + tests
    
    * Update phrases + fix bugs
    
    * Modified answers
    
    * Fix: codestyle
    
    * Fix: codestyle

--- annotators/IntentCatcher/src/detector.py
+++ annotators/IntentCatcher/src/detector.py
@@ -204,8 +204,12 @@ class RegMD(MultilabelDetector):
 
     def __init__(self, logger):
         super().__init__(logger)
-        self.regexp = {intent: data['phrases'] + data.get('reg_phrases', [])
-                       for intent, data in json.load(open(INTENT_PHRASES_PATH))['intent_phrases'].items()}
+        self.regexp = {intent: list(
+            chain.from_iterable(
+                [[phrase + "\\" + punct for phrase in data['phrases']] for punct in data['punctuation']]
+            )
+        ) + data.get('reg_phrases', [])
+            for intent, data in json.load(open(INTENT_PHRASES_PATH))['intent_phrases'].items()}
         self.regexp = {intent: [re.compile(phrase) for phrase in phrases]
                        for intent, phrases in self.regexp.items()}
 
