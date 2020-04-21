b0f7bae80c41ac57a4011e538d0199b98c5432a7
Elena Ermakova <lena0707es@gmail.com>
2020-04-21 17:55:38 +0300

    Feat/news api skill fixes (#738)
    * feat: choice of what type q & changes in not found news case
    
    * feat: add what type questions
    
    * feat: add file for allowed duplicates for response selector
    
    * feat: add allowed duplicates
    
    * style: fix some blanks

--- common/news.py
+++ common/news.py
@@ -7,10 +7,16 @@ OFFERED_BREAKING_NEWS_STATUS = "offered_breaking_news"
 OFFERED_NEWS_DETAILS_STATUS = "offered_news_details"
 OPINION_REQUEST_STATUS = "opinion_request"
 NEWS_GIVEN = "offered_news_details"
+WHAT_TYPE_OF_NEWS = ["What other kinds of news would you want to discuss?",
+                     "What are the other kinds of news you'd love to hear about?",
+                     "What else would you want to hear news about?",
+                     "What type of news do you prefer?"]
 
 # from emo skill connections to news skill
 BREAKING_NEWS_CHECK_WORD = 'Breaking news'
 BREAKING_NEWS = f'Oh! {BREAKING_NEWS_CHECK_WORD}! Would you like to hear it?'
+
+NEWS_DUPLICATES = WHAT_TYPE_OF_NEWS
 
 
 def skill_trigger_phrases():
