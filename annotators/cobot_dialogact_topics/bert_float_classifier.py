dce46353688161ea59eb4d257ed818065aa9c941
Дмитрий Карпов <dimakarp19962008@ya.ru>
2020-06-26 13:40:13 +0300

    log annotators

---
+++ annotators/cobot_dialogact_topics/bert_float_classifier.py
@@ -0,0 +1,46 @@
+import logging
+from os import getenv
+from typing import List, Union
+
+import sentry_sdk
+from bert_dp.preprocessing import InputFeatures
+from overrides import overrides
+
+from deeppavlov.core.common.registry import register
+from deeppavlov.models.bert.bert_classifier import BertClassifierModel
+
+sentry_sdk.init(getenv("SENTRY_DSN"))
+
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class BertFloatClassifierModel(BertClassifierModel):
+    @overrides
+    def __call__(self, features: List[InputFeatures]) -> Union[List[int], List[List[float]]]:
+        """
+        Make prediction for given features (texts).
+
+        Args:
+            features: batch of InputFeatures
+
+        Returns:
+            predicted classes or probabilities of each class
+
+        """
+        logging.info(features)
+        input_ids = [f.input_ids for f in features]
+        input_masks = [f.input_mask for f in features]
+        input_type_ids = [f.input_type_ids for f in features]
+
+        feed_dict = self._build_feed_dict(input_ids, input_masks, input_type_ids)
+        if not self.return_probas:
+            pred = self.sess.run(self.y_predictions, feed_dict=feed_dict)
+        else:
+            pred = self.sess.run(self.y_probas, feed_dict=feed_dict)
+        batch_predictions = [{column: prob
+                              for column, prob in zip(self.used_columns, curr_pred)}
+                             for curr_pred in pred]
+        logging.info('Predictions are made')
+        return batch_predictions
+
