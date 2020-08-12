000c7e46000bcc7011343b591e3885b8c1464b77
Daniel Kornev <daniel@zetuniverse.com>
2020-08-12 13:59:43 +0300

    fixed code to have just a random fact

--- skills/factoid_qa/server.py
+++ skills/factoid_qa/server.py
@@ -46,10 +46,11 @@ def get_random_facts(ner_outputs_to_classify):
         for name in names:
             if len(fact_dict[name]) == max_fact_num:
                 # phrase_start = 'Here is a fact about {}'.format(name) + '. '
-                phrase_start = name
-                phrase_end = random.choice(fact_dict[name])
+                # phrase_start = name
+                random_fact = random.choice(fact_dict[name])
                 if response == '':
-                    response = phrase_start + phrase_end
+                    # response = phrase_start + phrase_end
+                    response = random_fact
         responses.append(response)
     return responses
 
