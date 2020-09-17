4d9756dc6a33757ea393d537c216e1a9a0cc8e3b
Kuznetsov Denis <kuznetsov.den.p@gmail.com>
2020-09-17 12:50:46 +0300

    Feat/convert db v3.sentseg (#203)
    * wip (feat/improve_conf_gcs): add converter code & upd db of convert_reddit & upd model
    
    * wip (feat/convert_db_v3.sentseg): add test

---
+++ skills/convert_reddit/test_server.py
@@ -0,0 +1,34 @@
+import requests
+
+
+dialog = [
+    "hi",
+    "hi",
+    "how are you?",
+    "I'm fine. how are you?",
+    "Good. What do you do?",
+    "I response test questions",
+]
+
+checked_cand_nums = [0, 2, 3, 3, 3]
+
+
+def history_gen(dialog):
+    for i in range(1, len(dialog) + 1):
+        history = dialog[:i]
+        yield history
+
+
+def test_skill():
+    url = "http://localhost:8029/convert_reddit"
+    for utterances, checked_cand_num in zip(history_gen(dialog), checked_cand_nums):
+        res = requests.post(
+            url, json={"utterances_histories": [utterances], "approximate_confidence_is_enabled": False},
+        )
+        assert str(res.status_code) == "200"
+        assert len([i for i in res.json()[0][0] if i]) == checked_cand_num
+    print("SUCCESS!")
+
+
+if __name__ == "__main__":
+    test_skill()
