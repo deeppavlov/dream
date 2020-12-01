32bb725b2c707f7a1b67286d72c09f830cdc70ca
dilyararimovna <dilyara.rimovna@gmail.com>
2020-12-01 12:17:32 +0300

    fix: path for loading

--- annotators/SentSeg/setup.sh
+++ annotators/SentSeg/setup.sh
@@ -4,9 +4,9 @@ MODEL_META_URL=files.deeppavlov.ai/alexaprize_data/sentseg/model.meta
 MODEL_DATA_URL=files.deeppavlov.ai/alexaprize_data/sentseg/model.data-00000-of-00001
 
 if (! test -f model.meta) then
-  curl $MODEL_META_URL > /data/model.meta
+  curl $MODEL_META_URL > ./model.meta
 fi
 
 if (! test -f model.data-00000-of-00001) then
-  curl $MODEL_DATA_URL > /data/model.data-00000-of-00001
+  curl $MODEL_DATA_URL > ./model.data-00000-of-00001
 fi
