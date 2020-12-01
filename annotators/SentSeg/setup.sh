7b629d2ad8229a92c2c9a0a1e4c4df40a34ba6ed
dilyararimovna <dilyara.rimovna@gmail.com>
2020-12-01 12:12:19 +0300

    fix: revert to curl in setup

---
+++ annotators/SentSeg/setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+MODEL_META_URL=files.deeppavlov.ai/alexaprize_data/sentseg/model.meta
+MODEL_DATA_URL=files.deeppavlov.ai/alexaprize_data/sentseg/model.data-00000-of-00001
+
+if (! test -f model.meta) then
+  curl $MODEL_META_URL > /data/model.meta
+fi
+
+if (! test -f model.data-00000-of-00001) then
+  curl $MODEL_DATA_URL > /data/model.data-00000-of-00001
+fi
