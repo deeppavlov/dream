419651313f44076539ca3f4e1171cef4d62e9961
dilyararimovna <dilyara.rimovna@gmail.com>
2020-12-01 12:49:33 +0300

    fix: data loading to /data/

--- annotators/SentSeg/setup.sh
+++ annotators/SentSeg/setup.sh
@@ -1,12 +1,2 @@
 #!/usr/bin/env bash
 
-MODEL_META_URL=files.deeppavlov.ai/alexaprize_data/sentseg/model.meta
-MODEL_DATA_URL=files.deeppavlov.ai/alexaprize_data/sentseg/model.data-00000-of-00001
-
-if (! test -f model.meta) then
-  curl $MODEL_META_URL > ./model.meta
-fi
-
-if (! test -f model.data-00000-of-00001) then
-  curl $MODEL_DATA_URL > ./model.data-00000-of-00001
-fi
