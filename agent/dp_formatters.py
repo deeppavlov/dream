4fc861af09cf7258dffdf2064f2dfdd71d2c526a
Fedor Ignatov <ignatov.fedor@gmail.com>
2020-12-07 18:18:57 +0300

    feat: removed redundant data

--- agent/dp_formatters.py
+++ agent/dp_formatters.py
@@ -10,14 +10,6 @@ def last_utt_dialog(dialog: Dict) -> Dict:
     return [{'sentences': [dialog['utterances'][-1]['text']]}]
 
 
-def base_formatter_service(payload: Dict) -> Dict:
-    """
-    Used by: dummy_skill_formatter, intent_responder_formatter, transfertransfo_formatter,
-             aiml_formatter, alice_formatter, tfidf_formatter
-    """
-    return {"text": payload[0], "confidence": payload[1], "skill_name": ""}
-
-
 def base_response_selector_formatter_service(payload: List) -> Dict:
     if len(payload) == 3:
         return {"skill_name": payload[0], "text": payload[1], "confidence": payload[2]}
@@ -33,11 +25,8 @@ def full_dialog(dialog: Dict):
 def base_skill_formatter(payload: Dict) -> Dict:
     return [{"text": payload[0], "confidence": payload[1]}]
 
+
 def simple_formatter_service(payload: List):
-    '''
-    Used by: punct_dialogs_formatter, intent_catcher_formatter, asr_formatter,
-    sent_rewrite_formatter, sent_segm_formatter, base_skill_selector_formatter
-    '''
     logging.info('answer ' + str(payload))
     return payload
 
@@ -58,28 +47,6 @@ def entity_linking_formatter(payload: List):
         response.append(item)
     return response
 
-
-def preproc_last_human_utt_dialog(dialog: Dict) -> Dict:
-    # Used by: sentseg over human uttrs
-    return [{'sentences': [dialog['human_utterances'][-1]['annotations']["spelling_preprocessing"]]}]
-
-def preproc_last_bot_utt_dialog(dialog: Dict) -> Dict:
-    # Used by: sentseg over human uttrs
-    return [{'sentences': [dialog['bot_utterances'][-1]['annotations']["spelling_preprocessing"]]}]
-
-def cobot_classifiers_formatter_service(payload: List):
-    # Used by: cobot_classifiers_formatter, sentiment_formatter
-    if len(payload) == 3:
-        return {"text": payload[0],
-                "confidence": payload[1],
-                "is_blacklisted": payload[2]}
-    elif len(payload) == 2:
-        return {"text": payload[0],
-                "confidence": payload[1]}
-    elif len(payload) == 1:
-        return {"text": payload[0]}
-    elif len(payload) == 0:
-        return {"text": []}
 
 def hypotheses_list(dialog: Dict) -> Dict:
     hypotheses = dialog["utterances"][-1]["hypotheses"]
