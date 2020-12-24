b1e6069772855a1ec0135031b4d47b7b58e51cb2
yurakuratov <9271630+yurakuratov@users.noreply.github.com>
2020-12-24 15:36:06 +0300

    Feat/question generation service (#35)
    * feat: add question generator service
    
    * fix: change _ to - in cpu.yml
    
    * fix: typo
    
    * fix: inputs_ids should be empty list
    
    * fix: chmod +x test.sh
    
    * feat: added question generator and fixed proxy
    
    * fix: remove dbg output
    
    * feat: added cuda_device for question generator on aws
    
    Co-authored-by: Fedor Ignatov <ignatov.fedor@gmail.com>

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -137,7 +137,7 @@ if [[ "$MODE" == "test_skills" || "$MODE" == "all" ]]; then
                      program-y-dangerous superbowl-skill oscar-skill valentines-day-skill eliza \
                      dummy-skill-dialog intent-catcher short-story-skill comet-atomic \
                      comet-conceptnet convers-evaluation-selector emotion-skill game-cooperative-skill \
-                     entity-linking odqa convert-reddit; do
+                     entity-linking odqa convert-reddit question-generator; do
 
         echo "Run tests for $container"
         dockercompose_cmd exec -T -u $(id -u) $container ./test.sh
