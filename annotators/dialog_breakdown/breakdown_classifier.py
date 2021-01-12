36b4d7c584d4b8205ec0306d58b6b785b49060cc
Dilyara Baymurzina <dilyara.rimovna@gmail.com>
2021-01-12 18:15:05 +0300

    Feat/breakdown model (#46)
    * fix: config and classifier
    
    * feat: config and tests
    
    * feat: docker compose breakdown
    
    * fix: no extra imports
    
    * fix: no extra imports
    
    * feat: add to pipeline
    
    * fix; correct tests
    
    * fix: wait for sentseg
    
    * fix: other format
    
    * fix; tests for new format
    
    * feat: configs
    
    * fix: as postannotator too
    
    * fix: connector correct for dialog breakdown
    
    * fix: wait for post annotators
    
    * feat: formatter for hyps annotations
    
    * fix: batch hyps
    
    * fix: for batch connector
    
    * feat: dialog breakdown gpu for kuber
    
    * fix: remove duplicates
    
    * fix: path to dataset

---
+++ annotators/dialog_breakdown/breakdown_classifier.py
@@ -0,0 +1,36 @@
+import logging
+from os import getenv
+
+import sentry_sdk
+
+from deeppavlov.core.common.registry import register
+from deeppavlov.models.torch_bert.torch_bert_classifier import TorchBertClassifierModel
+
+sentry_sdk.init(getenv("SENTRY_DSN"))
+
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+@register("torch_breakdown_classifier")
+class CustomClassifierModel(TorchBertClassifierModel):
+    columns = ["breakdown", "no_breakdown"]
+
+    def __call__(self, features):
+        """
+        Make prediction for given features (texts).
+
+        Args:
+            features: batch of InputFeatures
+
+        Returns:
+            predicted classes or probabilities of each class
+
+        """
+        pred = super().__call__(features=features)
+
+        batch_predictions = []
+        for i in range(len(pred)):
+            batch_predictions.append({self.columns[j]: pred[i, j] for j in range(len(self.columns))})
+
+        return batch_predictions
