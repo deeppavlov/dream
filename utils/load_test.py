e2393e582096a8e595cea5ffa4517ab1c870562e
Fedor Ignatov <ignatov.fedor@gmail.com>
2021-01-13 15:32:15 +0300

    Load tests (#73)
    * feat: load tests
    
    * fix: codestyle

---
+++ utils/load_test.py
@@ -0,0 +1,30 @@
+import csv
+import uuid
+from pathlib import Path
+
+from locust import HttpUser, task
+
+golden = Path('.').resolve().parent / 'tests/dream/test_dialogs_gold_phrases.csv'
+with open(golden, 'r') as f:
+    reader = csv.reader(f, delimiter=' ')
+    phrases = [row[1] for row in reader][1:]
+
+
+class QuickstartUser(HttpUser):
+    @task
+    def hello_world(self):
+        phrase = next(self.data, None)
+        if phrase is None:
+            self.on_start()
+            phrase = next(self.data)
+        # print(f"you: {phrase}")
+        ans = self.client.post("", json={"user_id": self.id, "payload": phrase})
+        if ans.status_code != 200:
+            print(ans.status_code, ans.text)
+#        else:
+#            print(f"bot: {ans.json()['response']}")
+
+    def on_start(self):
+        print('start')
+        self.id = uuid.uuid4().hex
+        self.data = (p for p in phrases)
