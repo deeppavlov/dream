31921b8496b775635a05bf042bed185e4e8299f8
dmitrijeuseew <dmitrij.euseew@yandex.ru>
2021-01-15 18:01:53 +0300

    timeouts of kbqa and odqa annotators (#71)
    * add timeouts of kbqa and odqa annotators
    
    * fix/codestyle
    
    * add new branch hash for kbqa

--- state_formatters/dp_formatters.py
+++ state_formatters/dp_formatters.py
@@ -411,25 +411,26 @@ def simple_formatter_service(payload: List):
     return payload
 
 
-def kbqa_response_formatter(payload: List):
-    if payload and len(payload[0]) == 2:
+def kbqa_response_formatter(payload: Dict):
+    if payload and "kbqa_res" in payload and payload["kbqa_res"] and len(payload["kbqa_res"][0]) == 2:
         return {"qa_system": "kbqa",
-                "answer": payload[0][0],
-                "confidence": payload[0][1]}
+                "answer": payload["kbqa_res"][0][0],
+                "confidence": payload["kbqa_res"][0][1]}
     else:
-        return {"qa_system": "kbqa",
-                "answer": "",
-                "confidence": 0.0}
+        {"qa_system": "kbqa",
+         "answer": "",
+         "confidence": 0.0}
 
 
-def odqa_response_formatter(payload: List):
-    if payload and len(payload) == 5:
+def odqa_response_formatter(payload: Dict):
+    if payload and "odqa_res" in payload and len(payload["odqa_res"]) >= 4:
+        odqa_res = payload["odqa_res"]
         return {"qa_system": "odqa",
-                "answer": payload[0],
-                "confidence": payload[1],
-                "answer_pos": payload[2],
-                "answer_sentence": payload[3],
-                "paragraph": payload[4]}
+                "answer": odqa_res[0],
+                "confidence": odqa_res[1],
+                "answer_pos": odqa_res[2],
+                "answer_sentence": odqa_res[3],
+                "paragraph": odqa_res[4]}
     else:
         return {"qa_system": "odqa",
                 "answer": "",
