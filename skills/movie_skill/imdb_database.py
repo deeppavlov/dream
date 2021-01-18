beddcb3125453996ac28e7fe2676ca7b1c89b76c
Dilyara Baymurzina <dilyara.rimovna@gmail.com>
2021-01-18 17:47:19 +0300

    Fix/remove movie okay (#103)
    * fix: get intents with function not from dict
    
    * fix: no super dirty hack
    
    * fix: code style
    
    * fix: no movie okay

--- skills/movie_skill/imdb_database.py
+++ skills/movie_skill/imdb_database.py
@@ -166,7 +166,7 @@ class IMDb:
                 "games", "see", "really", "my favorite movie", "i do", "what happened",
                 "me", "off", "nothing", "talk to her", "boy a", "play",
                 "i feel", "question", "thank you", "singing", "program", "other", "lets talk about",
-                "conversation", "good", "they", "hello", "make", "pretty good", "talk", "happy days"]:
+                "conversation", "good", "they", "hello", "make", "pretty good", "talk", "happy days", "ok", "okay"]:
             try:
                 self.with_ignored_movies_names.pop(proc_title)
             except KeyError:
