c63804f069d1ee7ffbfb5a00184254eabe2ca4c7
Dilyara Baymurzina <dilyara.rimovna@gmail.com>
2021-01-24 18:10:20 +0300

    fix: use deepcopy in formatters (#132)
    * fix: use deepcopy in formatters
    
    * fix: run tests without cache
    
    * try to build comets sequentially
    
    * add --no-cache to comet-atomic build
    
    * fix: --no-cache arg placement
    
    * revert build stage in runtests.sh
    
    * fix: turn off building images with BuildKit
    
    * fix: remove kg and kg-skill tests
    
    Co-authored-by: yurakuratov <9271630+yurakuratov@users.noreply.github.com>

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -140,7 +140,7 @@ if [[ "$MODE" == "test_skills" || "$MODE" == "all" ]]; then
                      comet-conceptnet convers-evaluation-selector emotion-skill game-cooperative-skill \
                      entity-linking kbqa odqa wiki-parser convert-reddit question-generator grounding-skill \
                      cobot-topics cobot-dialogact cobot-convers-evaluator-annotator \
-                     knowledge-grounding dialog-breakdown knowledge-grounding-skill book-skill; do
+                     dialog-breakdown book-skill; do
 
         echo "Run tests for $container"
         dockercompose_cmd exec -T -u $(id -u) $container ./test.sh
