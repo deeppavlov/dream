0f3d2f721b484fc52fe8d6d05d6c3b930466bfb5
dimakarp1996 <dimakarp1996@yandex.ru>
2021-01-25 19:44:41 +0300

    Book skill fixes (#124)
    * All suggested book fixes
    
    * Removed unused variable - previous commit was from the environment that erroneously made an empty output  on codestyle check
    
    * Now test_skills WORK. So dont abort
    
    * Suggested fix + sentry fix
    
    * minor fix
    
    * Fix of bug with returning None
    
    * Two suggested fixes
    
    * Suggested fixes
    
    * Change was not attached initially - fixing
    
    * fix: not insttead of nt
    
    Co-authored-by: dimakarp1996 <dimakarp1996@github.com>
    Co-authored-by: Дмитрий Карпов <dimakarp19962008@ya.ru>
    Co-authored-by: dilyararimovna <dilyara.rimovna@gmail.com>

--- common/tutor.py
+++ common/tutor.py
@@ -4,7 +4,7 @@ from random import choice
 tutor_phrases = [
     "Would you like to talk about something else? I can talk about movies, news,"
     " emotions, personality and other things. "
-    "For example, just say \"tell me a funny story\" and I will happily tell you it!"
+    "For example, just say \"talk about news\" and I will happily tell you the last news!"
 ]
 
 
