46a06bace94e647bb1367dbdb0606460192c7b7d
dmitrijeuseew <dmitrij.euseew@yandex.ru>
2021-01-26 10:08:45 +0300

    fix cases when no paragraphs are found (#141)

--- annotators/odqa/test_odqa.py
+++ annotators/odqa/test_odqa.py
@@ -5,9 +5,11 @@ def main():
     url = 'http://0.0.0.0:8078/model'
 
     request_data = [{"question_raw": ["Who played Sheldon Cooper in The Big Bang Theory?"]},
-                    {"question_raw": ["What is the highest mountain in the world?"]}]
+                    {"question_raw": ["What is the highest mountain in the world?"]},
+                    {"question_raw": ["/alexa_stop_handler."]},
+                    {"question_raw": [" "]}]
 
-    gold_answers = ["Jim Parsons", "Everest"]
+    gold_answers = ["Jim Parsons", "Everest", "", ""]
     count = 0
     for data, gold_answer in zip(request_data, gold_answers):
         result = requests.post(url, json=data).json()
