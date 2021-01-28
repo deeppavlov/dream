11c3274d27e6201eb00c596faf0d344adbecaa28
Kuznetsov Denis <kuznetsov.den.p@gmail.com>
2021-01-28 18:59:20 +0300

    upd/gpu_model_proxy: add proxy workers (#150)

--- annotators/odqa/proxy.sh
+++ annotators/odqa/proxy.sh
@@ -1,6 +1,6 @@
 gunicorn  --workers=1 --timeout 500 --graceful-timeout 500 server:app -b 0.0.0.0:8080 &
 MODEL_PID=$!
-gunicorn  --workers=1 --timeout 500 --graceful-timeout 500 server_proxy:app -b 0.0.0.0:8078 &
+gunicorn  --workers=20 --timeout 500 --graceful-timeout 500 server_proxy:app -b 0.0.0.0:8078 &
 PROXY_PID=$!
 while kill -0 $MODEL_PID && kill -0 $PROXY_PID ; do sleep 0.5; done
 echo $MODEL_PID is model
