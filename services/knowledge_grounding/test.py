b8a9d93d7c09790d949f1f5f039a487c49851abb
Alsu Sagirova <34598622+Saraharas@users.noreply.github.com>
2021-02-02 23:14:22 +0300

    fix: add kg skill to pipeline, decrease kg skill confidence, odqa paragparh before kbqa, update KG model (#90)
    * fix: added kg skill to pipeline, decreased kg skill confidence, placed odqa paragraph before kbqa
    
    * fix: dot at the end of kbqa
    
    * fix:codestyle
    
    * fix: defined empty responses for exception case
    
    * fix Dockerfile for new model
    
    * fix: new model for kg service
    
    * fix knowledge sentence in kg skill
    
    * fix codestyle
    
    * fix codestyle
    
    * fix Dockerfile for kg service
    
    * fix: freeze parlai version
    
    * fix: delete outdated debug print
    
    * fix: install parlai for forked repo
    
    * add removal of data and model archives to Dockerfile
    
    * add git install to Dockerfile
    
    * delete removing model archive
    
    * fix kg model loading params and test request text
    
    * revert rm of model archive in Dockerfile
    
    * update script for model loading and response generation
    
    * fixed skill server
    
    * fix codestyle
    
    * add/ confidences for different use cases
    
    * add lets_chat_about_intent related confidence and fix regexp
    
    * fix codestyle
    
    * add entities checking to set high confidence to kg skill
    
    * fix codestyle, add unigrams file
    
    * fix typo
    
    * fix: add tests of kg skill
    
    * fix: more not special chars
    
    * fix: if no ent
    
    * fix: tests kg ksill can answer
    
    * fix: tests
    
    * fix: kg skill writing in tests
    
    * fix: kg skill writing in tests
    
    Co-authored-by: Денис Кузнецов <kuznetsov.den.p@gmail.com>
    Co-authored-by: dilyararimovna <dilyara.rimovna@gmail.com>

--- services/knowledge_grounding/test.py
+++ services/knowledge_grounding/test.py
@@ -4,34 +4,29 @@ import requests
 def test_knowledge_grounding():
     url = 'http://0.0.0.0:8083/respond'
 
-    topic = "financial endowment"
-    knowledge = "<h1> financial endowment </h1> <h2> <anchor> criticisms </anchor> </h2> <p> officials in charge of " \
-                "the endowments of some universities have been criticized for ' hoarding ' and reinvesting too much " \
-                "of the endowment's income . \ngiven a historical endowment performance of 10 – 11 % , and a payout " \
-                "rate of 5 % , around half of the endowment's income is reinvested . \nroughly 3 % of the " \
-                "reinvestment is used to keep pace with inflation , leaving an inflation-adjusted 2 % annual " \
-                "growth of the endowment . \nof course , many endowments fail to earn 10 – 11 % . \n</p> <p> " \
-                "two arguments against inflation-adjusted endowment growth are : </p> <h3> hoarding money </h3> <p> " \
-                "large endowments have been criticized for ' hoarding ' money . \nmost philanthropies are required " \
-                "by federal law to distribute 5 % of their assets per year , but university endowments are not " \
-                "required to spend anything . \nmany universities with very large endowments would require less " \
-                "than 5 % to pay full tuition for all their students . \nfor example , it has been estimated that " \
-                "if in 2006 all the harvard students had paid the maximum in tuition and fees , it would have " \
-                "amounted to less than $ 300 million . \nin 2007 , if harvard <h3> size </h3> <p> financial " \
-                "endowments range in size depending on the size of the institution and the level of community " \
-                "support . \nat the large end of the spectrum , the total endowment can be over one billion " \
-                "dollars at many leading private universities . \nharvard university has the largest endowment " \
-                "in the world with $ 37.6 billion in assets as of june 30 , 2015 . \neach university typically " \
-                "has numerous endowments , each of which are frequently restricted to funding very specific areas " \
-                "of the university . \nthe most common examples are endowed professorships , and endowed " \
-                "scholarships or fellowships <h3> socially and environmentally responsible investing </h3> <p> " \
-                "many college and university endowments have come under fire in recent years for practices such " \
-                "as investing in fossil fuels , ' land grabs ' in poor countries and high-risk , high-return " \
-                "investment practices that led to the financial crisis . </p>"
-    text = "wow do you know about financial endowment?"
-    history = "hello how are you\n fine just got from work \n me too what do you do for living? \n i am a financist"
+    checked_sentence = 'When Mabel visited their home to play the piano, ' \
+                       'she occasionally glimpsed a flitting swirl of white in the next room, ' \
+                       'sometimes even received a note of thanks for calling, but she never actually ' \
+                       'spoke with the reclusive, almost spectral Emily.'
+    knowledge = 'The real-life soap opera behind the publication of Emily Dickinson’s poems\n' \
+                'When Mabel visited their home to play the piano, she occasionally glimpsed ' \
+                'a flitting swirl of white in the next room, sometimes even received a note of ' \
+                'thanks for calling, but she never actually spoke with the reclusive, almost spectral Emily.'
+    text = 'Yeah she was an icon she died in 1886 at the tender age of 55.'
+    history = 'Do you know who Emily Dickson is?\n' \
+              'Emily Dickinson? The poet? I do! "Tell all the truth, but tell it slant" ' \
+              'she once said. Do you like her poetry?'
 
-    request_data = {'batch': [{'topic': topic, 'knowledge': knowledge, 'text': text, 'history': history}]}
+    request_data = {
+        'batch': [
+            {
+                'checked_sentence': checked_sentence,
+                'knowledge': knowledge,
+                'text': text,
+                'history': history
+            }
+        ]
+    }
     result = requests.post(url, json=request_data).json()[0]
     assert result != '', f'Got empty string as a result'
     print('Got\n{}\nSuccess'.format(result))
