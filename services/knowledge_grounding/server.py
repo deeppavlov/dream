7c6819dcaf5fe6cea84644ab47f00825c561e940
Alsu Sagirova <34598622+Saraharas@users.noreply.github.com>
2021-02-02 23:14:22 +0300

    fix: add kg skill to pipeline, decrease kg skill confidence, odqa paragparh before kbqa, update KG model (#90)
    * fix: added kg skill to pipeline, decreased kg skill confidence, placed odqa paragraph before kbqa
    
    * fix: dot at the end of kbqa
    
    * fix:codestyle
    
    * fix: defined empty responses for exception case
    
    * fix Dockerfile for new model
    
    * fix: new model for kg service
    
    * fix knowledge sentence in kg skill
    
    * fix codestyle
    
    * fix codestyle
    
    * fix Dockerfile for kg service
    
    * fix: freeze parlai version
    
    * fix: delete outdated debug print
    
    * fix: install parlai for forked repo
    
    * add removal of data and model archives to Dockerfile
    
    * add git install to Dockerfile
    
    * delete removing model archive
    
    * fix kg model loading params and test request text
    
    * revert rm of model archive in Dockerfile
    
    * update script for model loading and response generation
    
    * fixed skill server
    
    * fix codestyle
    
    * add/ confidences for different use cases
    
    * add lets_chat_about_intent related confidence and fix regexp
    
    * fix codestyle
    
    * add entities checking to set high confidence to kg skill
    
    * fix codestyle, add unigrams file
    
    * fix typo
    
    * fix: add tests of kg skill
    
    * fix: more not special chars
    
    * fix: if no ent
    
    * fix: tests kg ksill can answer
    
    * fix: tests
    
    * fix: kg skill writing in tests
    
    * fix: kg skill writing in tests
    
    Co-authored-by: Денис Кузнецов <kuznetsov.den.p@gmail.com>
    Co-authored-by: dilyararimovna <dilyara.rimovna@gmail.com>

--- services/knowledge_grounding/server.py
+++ services/knowledge_grounding/server.py
@@ -6,11 +6,7 @@ import time
 import sentry_sdk
 import torch
 from flask import Flask, request, jsonify
-from parlai.core.params import ParlaiParser
-from parlai.core.agents import create_agent
-from parlai.core.worlds import create_task
-from parlai.core.script import ParlaiScript, register_script
-from parlai.agents.courier.courier import CourierAgent
+from parlai.core.script import ParlaiPreloadModelScript
 from sentry_sdk.integrations.flask import FlaskIntegration
 
 sentry_sdk.init(dsn=os.getenv('SENTRY_DSN'), integrations=[FlaskIntegration()])
@@ -30,85 +26,18 @@ else:
 
 logger.info(f'knowledge grounding is set to run on {device}')
 
+kg_script = ParlaiPreloadModelScript.main(
+    task='topical_chat:generator',
+    init_model='/opt/conda/lib/python3.7/site-packages'
+    '/data/models/topical_chat_blender90_1_sent_48_epochs/model.checkpoint',
+    model_file='/opt/conda/lib/python3.7/site-packages'
+    '/data/models/topical_chat_blender90_1_sent_48_epochs/model',
+    inference='nucleus',
+    topp=0.9,
+    skip_generation=False
+)
 
-logger.info('knowledge grounding script is preparing...')
-
-
-@register_script('get model response')
-class GetModelResponse(ParlaiScript):
-    @classmethod
-    def setup_args(cls):
-        parser = ParlaiParser(True, True, 'Get response from model in knowledge grounded conversation')
-        parser.add_argument(
-            '-it',
-            '--interactive-task',
-            type='bool',
-            default=True,
-            help='Create interactive version of task',
-        )
-        parser.add_argument(
-            '--user-input-topic',
-            type=str,
-            default='',
-            help='User input topic',
-        )
-        parser.add_argument(
-            '--user-input-knowledge',
-            type=str,
-            default='',
-            help='User input knowledge',
-        )
-        parser.add_argument(
-            '--user-input-text',
-            type=str,
-            default='',
-            help='User input text',
-        )
-        parser.add_argument(
-            '--user-input-history',
-            type=str,
-            default='',
-            help='User input history',
-        )
-        parser.set_defaults(interactive_mode=True, task='interactive')
-        return parser
-
-    def run(self):
-        opt = self.opt
-        if isinstance(self.opt, ParlaiParser):
-            logging.error('opt should be passed, not Parser')
-            opt = self.opt.parse_args()
-        # Create model and courier and assign them to the specified task
-        agent = create_agent(opt, requireModelExists=True)
-        courier_agent = CourierAgent(opt)
-        world = create_task(opt, [courier_agent, agent])
-        user_input = {
-            'topic': opt['user_input_topic'],
-            'knowledge': opt['user_input_knowledge'],
-            'text': opt['user_input_text'],
-            'history': opt['user_input_history'].split('\n') if opt['user_input_history'] else ['']
-        }
-        response = world.parley(user_input)
-        courier_agent.finished = True
-        return response['text']
-
-
-try:
-    GetModelResponse.main(
-        task='redditgk',
-        datatype='test',
-        user_input_topic='',
-        user_input_knowledge='.',
-        user_input_text='hi',
-        user_input_history='',
-        split_lines=False,
-        model_file='zoo:wizard_of_wikipedia/end2end_generator/model',
-    )
-except Exception as e:
-    sentry_sdk.capture_exception(e)
-    logger.exception(e)
-
-logger.info(f'knowledge grounding script is ready')
+logger.info('knowledge grounding script has loaded the model and is ready')
 
 app = Flask(__name__)
 
@@ -123,16 +52,13 @@ def respond():
         st_time = time.time()
         if sample['knowledge']:
             try:
-                response = GetModelResponse.main(
-                    task='redditgk',
-                    datatype='test',
-                    user_input_topic=sample['topic'],
-                    user_input_knowledge=sample['knowledge'],
-                    user_input_text=sample['text'],
-                    user_input_history=sample['history'],
-                    split_lines=False,
-                    model_file='zoo:wizard_of_wikipedia/end2end_generator/model',
-                )
+                user_input = {
+                    'checked_sentence': sample['checked_sentence'],
+                    'knowledge': sample['knowledge'],
+                    'text': sample['text'],
+                    'history': sample['history'].split('\n') if sample['history'] else ['']
+                }
+                response = kg_script.run(user_input)['text']
             except Exception as e:
                 sentry_sdk.capture_exception(e)
                 logger.exception(e)
--- skills/knowledge_grounding_skill/server.py
+++ skills/knowledge_grounding_skill/server.py
@@ -1,4 +1,5 @@
 import logging
+import re
 import time
 from os import getenv
 
@@ -8,6 +9,8 @@ from flask import Flask, request, jsonify
 from nltk import tokenize
 
 from common.constants import CAN_CONTINUE
+from common.universal_templates import if_lets_chat_about_topic
+from common.utils import join_sentences_in_or_pattern
 
 
 sentry_sdk.init(getenv('SENTRY_DSN'))
@@ -19,8 +22,28 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
 ANNTR_HISTORY_LEN = 3
-DEFAULT_CONFIDENCE = 0.85
+DEFAULT_CONFIDENCE = 0.9
+HAS_SPEC_CHAR_CONFIDENCE = 0.85
+HIGHEST_CONFIDENCE = 0.99
+LETS_CHAT_ABOUT_CONFIDENDENCE = 0.985
+NOUNPHRASE_ENTITY_CONFIDENCE = 0.95
 KNOWLEDGE_GROUNDING_SERVICE_URL = getenv('KNOWLEDGE_GROUNDING_SERVICE_URL')
+special_char_re = re.compile(r'[^0-9a-zA-Z \-\.\?,!]+')
+
+with open("./google-english-no-swears.txt", "r") as f:
+    UNIGRAMS = set(f.read().splitlines())
+
+
+def get_entities(utt):
+    entities = []
+
+    for ent in utt["annotations"].get("ner", []):
+        if not ent:
+            continue
+        ent = ent[0]["text"].lower()
+        if ent not in UNIGRAMS and not (ent == "alexa" and utt["text"].lower()[:5] == "alexa"):
+            entities.append(ent)
+    return entities
 
 
 def get_annotations_from_dialog(utterances, annotator_name, key_name):
@@ -37,11 +60,12 @@ def get_annotations_from_dialog(utterances, annotator_name, key_name):
         list of strings with values of specific key from specific annotator
     """
     result_values = []
-    for uttr in utterances:
+    for i, uttr in enumerate(utterances):
         annotation = uttr.get("annotations", {}).get(annotator_name, {})
         value = annotation.get(key_name, "")
+        # include only non-empty strs
         if value:
-            result_values.append(value)
+            result_values.append(((len(utterances) - i - 1) * 0.01, value))
     return result_values
 
 
@@ -51,20 +75,37 @@ def respond():
     st_time = time.time()
     dialogs_batch = request.json["dialogs"]
     input_batch = []
+    responses = []
+    confidences = []
     attributes = []
+    nounphrases = []
+    entities = []
     for dialog in dialogs_batch:
-        user_input_topic = ""
         user_input_text = dialog["human_utterances"][-1]["text"]
 
+        nounphrases.append(
+            re.compile(join_sentences_in_or_pattern(
+                dialog["human_utterances"][-1].get("annotations", {}).get("cobot_nounphrases", [])
+            ), re.IGNORECASE))
+        entities.append(
+            re.compile(join_sentences_in_or_pattern(
+                get_entities(dialog["human_utterances"][-1])), re.IGNORECASE))
+
+        lets_chat_about_intent = dialog["human_utterances"][-1].get("annotations",
+                                                                    {}).get("intent_catcher",
+                                                                            {}).get("lets_chat_about",
+                                                                                    {}).get("detected", False)
         user_input_history = [i["text"] for i in dialog["utterances"]]
         user_input_history = '\n'.join(user_input_history)
 
         user_input_knowledge = ""
+        anntrs_knowledge = ""
         # look for kbqa/odqa text in ANNTR_HISTORY_LEN previous human utterances
         annotators = {
-            "kbqa": "answer",
-            "odqa": "paragraph"
+            "odqa": "paragraph",
+            "kbqa": "answer"
         }
+        annotations_depth = {}
         for anntr_name, anntr_key in annotators.items():
             prev_anntr_outputs = get_annotations_from_dialog(
                 dialog["utterances"][-ANNTR_HISTORY_LEN * 2 - 1:],
@@ -72,11 +113,19 @@ def respond():
                 anntr_key
             )
             logger.debug(f"Prev {anntr_name} {anntr_key}s: {prev_anntr_outputs}")
-            if prev_anntr_outputs:
-                user_input_knowledge += '\n'.join(tokenize.sent_tokenize(prev_anntr_outputs[-1]))
+            # add final dot to kbqa answer to make it a sentence
+            if (prev_anntr_outputs) and (anntr_name == "kbqa"):
+                prev_anntr_outputs[-1][1] += "."
+            # concat annotations separated by space to make a paragraph
+            if (prev_anntr_outputs) and (prev_anntr_outputs[-1][1] != "Not Found"):
+                anntrs_knowledge += prev_anntr_outputs[-1][1] + " "
+                annotations_depth[anntr_name] = prev_anntr_outputs[-1][0]
+        if anntrs_knowledge:
+            user_input_knowledge += '\n'.join(tokenize.sent_tokenize(anntrs_knowledge))
+        user_input_checked_sentence = tokenize.sent_tokenize(user_input_knowledge)[0] if user_input_knowledge else ""
 
         user_input = {
-            'topic': user_input_topic,
+            'checked_sentence': user_input_checked_sentence,
             'knowledge': user_input_knowledge,
             'text': user_input_text,
             'history': user_input_history
@@ -84,20 +133,42 @@ def respond():
         input_batch.append(user_input)
         attributes.append({
             "knowledge_paragraph": user_input_knowledge,
-            "knowledge_sentence": tokenize.sent_tokenize(user_input_knowledge)[0] if user_input_knowledge else "",
+            "knowledge_checked_sentence": user_input_checked_sentence,
             "can_continue": CAN_CONTINUE
         })
-    try:
-        responses = requests.post(KNOWLEDGE_GROUNDING_SERVICE_URL, json={'batch': input_batch}, timeout=1.5)
-        if responses.status_code != 200:
-            logger.exception(f'service error status code: ' + str(responses.status_code))
+        if if_lets_chat_about_topic(user_input_text.lower()) or lets_chat_about_intent:
+            confidences.append(LETS_CHAT_ABOUT_CONFIDENDENCE
+                               - annotations_depth.get("odqa", 0.0)
+                               - 0.01 * int(dialog["bot_utterances"][-1]["active_skill"]
+                                            == "knowledge_grounding_skill"))
         else:
-            responses = responses.json()
+            confidences.append(DEFAULT_CONFIDENCE)
+    try:
+        resp = requests.post(KNOWLEDGE_GROUNDING_SERVICE_URL, json={'batch': input_batch}, timeout=1.5)
+        if resp.status_code != 200:
+            logger.exception(f'service error status code: ' + str(resp.status_code))
+        else:
+            responses = resp.json()
+            confidences = [
+                NOUNPHRASE_ENTITY_CONFIDENCE if (nounphrases[i].search(responses[i])
+                                                 or entities[i].search(responses[i]))
+                else confidences[i] for i in range(len(responses))
+            ]
+            confidences = [
+                HIGHEST_CONFIDENCE if (nounphrases[i].search(responses[i])
+                                       or entities[i].search(responses[i]))
+                and (lets_chat_about_intent
+                     or if_lets_chat_about_topic(input_batch[i]["text"].lower()))
+                else confidences[i] for i in range(len(responses))
+            ]
+            confidences = [
+                HAS_SPEC_CHAR_CONFIDENCE if special_char_re.search(responses[i])
+                else confidences[i] for i in range(len(responses))
+            ]
             logger.info(f"Respond exec time: {time.time() - st_time}")
     except Exception as ex:
         sentry_sdk.capture_exception(ex)
         logger.exception(ex)
-    confidences = [DEFAULT_CONFIDENCE] * len(responses)
     return jsonify(list(zip(responses, confidences, attributes)))
 
 
