4f6f4c04cada2f0be53d26a9a0e332f3f12307a7
dimakarp1996 <dimakarp1996@yandex.ru>
2021-02-16 19:31:21 +0300

    Reworked grounding skill (#230)
    * Kornev suggested fixes
    
    * added to tests
    
    * Tests and codestyle
    
    * minor codestyle fix
    
    * Update scenario.py
    
    added more individual phrases to topic_dict
    
    * codestyle fixes
    
    * turn skill on
    
    * fix: add to tests phrase
    
    Co-authored-by: Дмитрий Карпов <dimakarp19962008@ya.ru>
    Co-authored-by: dimakarp1996 <dimakarp1996@github.com>
    Co-authored-by: Daniel Kornev <daniel@zetuniverse.com>
    Co-authored-by: dilyararimovna <dilyara.rimovna@gmail.com>

--- skills/grounding_skill/scenario.py
+++ skills/grounding_skill/scenario.py
@@ -13,7 +13,46 @@ logger = logging.getLogger(__name__)
 
 DEFAULT_CONF = 0.99
 DONTKNOW_CONF = 0.5
-DONTKNOW_PHRASE = "I don't know what are we talking about"
+DONTKNOW_PHRASE = "Seems like I have no idea what we are talking about."
+
+
+def get_intent_dict(entity_name):
+    #  order DOES matter
+    intent_dict = {'Information_DeliveryIntent': f'You just told me about {entity_name}, right?',
+                   'Information_RequestIntent': f"You've asked me about {entity_name} haven't you?",
+                   'User_InstructionIntent': "You just gave me a command. Am I right?",
+                   "Opinion_ExpressionIntent": f"You just shared your opinion about {entity_name} with me, right?",
+                   "ClarificationIntent": f"You clarified me what you've just said about {entity_name}, right?",
+                   "Topic_SwitchIntent": "You wanted to change topic, right?",
+                   "Opinion_RequestIntent": f"You wanted to hear my thoughts about {entity_name}, am I correct?"}
+    return intent_dict
+
+
+def get_topic_dict():
+    #  order DOES matter
+    topic_dict = {'Phatic': "We are just trying to be polite to each other, aren't we?",
+                  "Other": "I can't figure out what we are talking about exactly. Can you spare a hand?",
+                  "Movies_TV": "We were discussing movies, am I right?",
+                  "Music": "Thought we were talking about music",
+                  "SciTech": "I was under impression we were chatting about technology stuff",
+                  "Literature": "We were discussing literature, am I right?",
+                  "Travel_Geo": "Thought we were talking about some travel stuff",
+                  "Celebrities": "We're discussing celebrities, right?",
+                  "Games": "We're talking about games, correct?",
+                  "Pets_Animals": "Thought we were talking about animals",
+                  "Sports": "So I thought we were talking about sports",
+                  "Psychology": "Correct me if I'm wrong but I thought we were talking about psychology",
+                  "Religion": "Aren't we talking about religion, my dear?",
+                  "Weather_Time": "Aren't we discussing the best topic of all times, weather?",
+                  "Food_Drink": "Thought we were discussing food stuff",
+                  "Politics": "Correct me if I'm wrong but I thought we were discussing politics",
+                  "Sex_Profanity": "This is a something I'd rather avoid talking about",
+                  "Art_Event": "My understanding is we are discussing arts, aren't we?",
+                  "Math": "My guess is we were talking about math stuff",
+                  "News": "Aren't we discussing news my dear friend?",
+                  "Entertainment": "Thought we were discussing something about entertainment",
+                  "Fashion": "We are talking about fashion am I right?"}
+    return topic_dict
 
 
 class GroundingSkillScenario:
@@ -43,45 +82,41 @@ class GroundingSkillScenario:
                 else:
                     prev_annotations = dialog['human_utterances'][-2].get('annotations', dict())
                     logger.debug('Running grounding skill')
-                    reply = 'To my understanding, we are talking'
 
                     entity_list = []
                     for tmp in prev_annotations.get('ner', []):
                         if len(tmp) > 0 and 'text' in tmp[0]:
                             entity_list.append(tmp[0]['text'])
-                    my_entities = ', '.join([str(k) for k in sorted(entity_list)])
-
-                    topic_list1 = get_topics(dialog['human_utterances'][-2], which='cobot_topics')
-                    topic_list2 = get_topics(dialog['human_utterances'][-2], which='cobot_dialogact_topics')
-
-                    topic_list = topic_list1 + topic_list2
-
-                    topic_list = list(set(topic_list))
-                    topic_list = [j.replace('_', ' ') for j in topic_list if j not in ['Other', 'Phatic']]
-                    my_topics = ', '.join([str(k) for k in sorted(topic_list)])
-
+                    if len(entity_list) == 1:
+                        entity_name = entity_list[0]
+                    elif len(entity_list) > 1:
+                        entity_name = ','.join(entity_list[::-1]) + ' and ' + entity_list[-1]
+                    else:
+                        entity_name = ''
+                    intent_dict = get_intent_dict(entity_name)
+                    intents_by_popularity = list(intent_dict.keys())[::-1]
                     intent_list = get_intents(dialog['human_utterances'][-2], which='cobot_dialogact_intents')
                     if 'text' in intent_list:
                         intent_list = intent_list['text']
+                    intent_list = list(set(intent_list))
 
-                    intent_list = [j.replace('_', ' ').replace('Intent', ' Intent') for j in intent_list if
-                                   j != 'OtherIntent']
-                    my_intents = ', '.join([str(k) for k in sorted(intent_list)])
+                    topic_list = get_topics(dialog['human_utterances'][-2], which='cobot_topics')
+                    if 'text' in topic_list:
+                        topic_list = topic_list['text']
+                    topic_list = list(set(topic_list))
+                    topic_dict = get_topic_dict()
+                    topics_by_popularity = list(topic_dict.keys())[::-1]
 
-                    logger.debug('Found entities topics intents')
-                    logger.debug(f' {entity_list} {topic_list} {intent_list}')
-
-                    if len(topic_list) > 0:
-                        reply = f'{reply} about the following topics: {my_topics}'
-                    if len(intent_list) > 0:
-                        reply = f'{reply} with the following intents: {my_intents}.'
-                    if len(entity_list) > 0:
-                        if len(topic_list) == len(intent_list) == 0:
-                            reply += 'I see '
-                        else:
-                            reply += 'I also see '
-                        reply = f'{reply} that you have mentioned the following entities: {my_entities}.'
-                    if reply == '':
+                    reply = None
+                    for intent in intents_by_popularity:  # start from least popular
+                        if intent in intent_dict and reply is None and len(entity_name) > 0:
+                            reply = intent_dict[intent]
+                    if len(entity_name) > 0 and reply is None:
+                        reply = f"We are discussing {entity_name}, aren't we?"
+                    for topic in topics_by_popularity:  # start from least popular
+                        if topic in topic_dict and reply is None:
+                            reply = topic_dict[topic]
+                    if reply is None:
                         reply, confidence = DONTKNOW_PHRASE, DONTKNOW_CONF
                     else:
                         confidence = DEFAULT_CONF
