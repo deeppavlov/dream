c3e3d9f7023ca470e148d884aa8e80bff9ad64c3
Dilyara Baymurzina <dilyara.rimovna@gmail.com>
2021-02-21 20:52:54 +0300

    Fix/configs remove old components (#284)
    * fix: remove old components from yml configs
    
    * fix: wait all hosts!
    
    * fix: remove from tests
    
    * fix: extra comma

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -142,7 +142,7 @@ if [[ "$MODE" == "test_skills" || "$MODE" == "all" ]]; then
                      dummy-skill-dialog intent-catcher short-story-skill comet-atomic \
                      comet-conceptnet convers-evaluation-selector emotion-skill game-cooperative-skill \
                      entity-linking kbqa odqa wiki-parser convert-reddit question-generator grounding-skill \
-                     cobot-topics cobot-dialogact cobot-convers-evaluator-annotator \
+                     cobot-convers-evaluator-annotator \
                      dialog-breakdown book-skill combined-classification knowledge-grounding-skill \
                      grounding-skill friendship-skill masked-lm entity-storer wikidata-dial-skill; do
 
