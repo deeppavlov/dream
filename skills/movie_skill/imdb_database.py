ad0f7194befaef7abbc82e213c109ed769515ed1
Dilyara Baymurzina <dilyara.rimovna@gmail.com>
2021-02-22 17:41:07 +0300

    fix: ignore movie travelling (#291)

--- skills/movie_skill/imdb_database.py
+++ skills/movie_skill/imdb_database.py
@@ -152,7 +152,7 @@ class IMDb:
         for proc_title in movie_titles_to_ignore + [
                 "angela", "earthquake", "abortion", "tricks", "isolation", "cage", "back", "stronger", "judy",
                 "live", "lunch", "weekend", "gays", "bean", "her", "eat", "she", "he", "thing", "pets",
-                "happy days", "foster"]:
+                "happy days", "foster", "travelling"]:
             try:
                 self.without_ignored_movies_names.pop(proc_title)
             except KeyError:
