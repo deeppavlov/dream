469b465468ab196f0830c3e57f60a585b9ea7a4b
shuu01 <shuu01@gmail.com>
2021-02-27 15:08:09 +0300

    feat/enable proxy script (#308)
    * copy md5 files to s3
    
    * fix
    
    * utils/proxy.sh

---
+++ utils/proxy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+usage() {
+  echo "Usage: $0 --create|--delete"
+  exit 1
+}
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -d|--delete) d=1;;
+        -c|--create) c=1;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+if [ -z "$d" ] && [ -z "$c" ]; then
+  usage
+  exit 1
+fi
+
+if [ -n "$d" ] && [ -n "$c" ]; then
+  usage
+  exit 1
+fi
+
+export AWS_ACCESS_KEY_ID='AKIAT2RXIFYZLDB66LZ3'
+export AWS_SECRET_ACCESS_KEY='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'
+export AWS_DEFAULT_REGION='us-east-1'
+export LB_NAME="a72bd67cd6eb54e188e4513c1f70ab0b"
+
+
+SG=$(aws elb describe-load-balancers --load-balancer-name $LB_NAME --query LoadBalancerDescriptions[*].SecurityGroups[] --output text)
+
+if [ -n "$d" ]; then
+  aws ec2 revoke-security-group-ingress --group-id $SG --protocol tcp --port 80 --cidr 0.0.0.0/0
+  aws ec2 revoke-security-group-ingress --group-id $SG --protocol tcp --port 443 --cidr 0.0.0.0/0
+fi
+
+if [ -n "$c" ]; then
+  aws ec2 authorize-security-group-ingress --group-id $SG --protocol tcp --port 80 --cidr 0.0.0.0/0
+  aws ec2 authorize-security-group-ingress --group-id $SG --protocol tcp --port 443 --cidr 0.0.0.0/0
+fi
