74b6c12caf0606ad6c9989a9251a4e3b39435e5f
Dilyara Baymurzina <dilyara.rimovna@gmail.com>
2021-02-28 17:53:00 +0300

    Fix/ban named entities (#315)
    * fix: ban named entities okay, oh
    
    * fix: ban named entities ocean, name
    
    * fix: correct work with ents
    
    * fix: codestyle
    
    * fix: more checks
    
    * fix: or to and

--- annotators/NER/server.py
+++ annotators/NER/server.py
@@ -1,5 +1,6 @@
 import logging
 import time
+from copy import deepcopy
 from os import getenv
 
 import ner_model
@@ -17,6 +18,8 @@ app = Flask(__name__)
 ner = ner_model.load_model()
 logger.info("ner model is loaded.")
 
+BANNED_ENTITIES = ["okay", "oh", "name", "ocean", "hey"]
+
 
 @app.route('/ner', methods=['POST'])
 def respond():
@@ -27,7 +30,28 @@ def respond():
     ret = []
     for utterance in last_utterances:
         sents = [word_tokenize(sent.lower()) for sent in utterance]
-        ret.append(ner.predict(sents))
+        preds = ner.predict(sents)
+        # each sample is a list of sentences of current utterance
+        # so, preds is a list of length = number of sents in utterances
+        # each element of preds is a list of entities.
+        # EXAMPLE:
+        # one sample = ["i have been in london and greece.", "my name is valentine and beatrice."]
+        # preds = [[{'confidence': 1, 'end_pos': 5, 'start_pos': 4, 'text': 'london', 'type': 'LOC'},
+        #           {'confidence': 1, 'end_pos': 7, 'start_pos': 6, 'text': 'greece', 'type': 'LOC'}],
+        #          [{'confidence': 1, 'end_pos': 4, 'start_pos': 3, 'text': 'valentine', 'type': 'PER'},
+        #           {'confidence': 1, 'end_pos': 6, 'start_pos': 5, 'text': 'beatrice', 'type': 'PER'}]]
+        good_preds = []
+        for entities_for_sent in preds:
+            good_entities_for_sent = []
+
+            for ent in entities_for_sent:
+                ent_text = ent["text"].lower()
+                if ent_text not in BANNED_ENTITIES and len(ent_text) > 2:
+                    good_entities_for_sent.append(deepcopy(ent))
+
+            good_preds.append(good_entities_for_sent)
+
+        ret.append(good_preds)
 
     logger.info(f"NER output: {ret}")
 
