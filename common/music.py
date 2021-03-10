35d3bd7bb6f984973384469208a27e58e463d5a2
Daniil Cherniavskii <danchern97@mail.ru>
2021-03-10 23:16:25 +0300

    Feat/music_skill (#341)
    * add skill tree
    
    * feat/sport_dff_skill: change dir
    
    * Tree expansion
    
    * Adding to system
    
    * Change in commit
    
    * Adding what_like
    
    * Mod what_like
    
    * Server for testing
    
    * Fix: system transition
    
    * Fix: transitions
    
    * Fix: transitions
    
    * Adding tests and fixing bugs
    
    * Commenting test lines
    
    * Fix: style
    
    * Fix: codestyle
    
    * Adding proxy.yml
    
    * feat/music_skill: revert food
    
    * feat/music_skill: revert movie_skill
    
    * feat/music_skill: revert odqa
    
    * feat/music_skill: rm volume with cobot qa
    
    * feat/music_skill: add error handle
    
    * Adding mods
    
    * Fix: codestyle
    
    * Fix
    
    * feat/music_skill: revert fss
    
    * feat/music_skill: post merge fixes
    
    * feat/music_skill: drop useless logging
    
    * Fix: confidence naming and re.compile
    
    * Fix: comment
    
    * Fix: adding end node
    
    * feat/music_skill: fix style & fix request funcs & upd tests
    
    Co-authored-by: Denis Kuznetosv <kuznetsov.den.p@gmail.com>

---
+++ common/music.py
@@ -0,0 +1,12 @@
+OPINION_REQUESTS_ABOUT_MUSIC = ["What music do you like?",
+                                "Do you like listening to music?",
+                                "What do you listen to?",
+                                "Who is your favourite artist?",
+                                "What is your favourite song?",
+                                "What is your favourite album?",
+                                "Do you have favourite artist?"
+                                ]
+
+
+def skill_trigger_phrases():
+    return OPINION_REQUESTS_ABOUT_MUSIC
