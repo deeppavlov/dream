40f176eeb161f3e6b5cccb4ec349de464706dca3
Alsu Sagirova <34598622+Saraharas@users.noreply.github.com>
2021-03-17 16:57:41 +0300

    fix/refactor kg skill server.py (#407)
    * refactor kg skill server.py
    
    * replace tabs with spaces
    
    * fix codestyle
    
    * upd get_news_api_fact args and get_lets_chat_topic

--- skills/knowledge_grounding_skill/server.py
+++ skills/knowledge_grounding_skill/server.py
@@ -35,12 +35,7 @@ KG_ACTIVE_DEPTH = 2
 LETS_CHAT_ABOUT_CONFIDENDENCE = 0.6
 NOUNPHRASE_ENTITY_CONFIDENCE = 0.95
 KNOWLEDGE_GROUNDING_SERVICE_URL = getenv('KNOWLEDGE_GROUNDING_SERVICE_URL')
-COBOT_DA_FILE_TOPICS_MATCH = {
-    'Entertainment_Movies': 'movies',
-    'Entertainment_Music': 'music',
-    'Science_and_Technology': 'science',
-    'Sports': 'sports'
-}
+
 special_char_re = re.compile(r'[^0-9a-zA-Z \-\.\'\?,!]+')
 greetings_farewells_re = re.compile(join_words_in_or_pattern(["have .* day", "have .* night", ".* bye",
                                                               "\bbye", "goodbye", "hello",
@@ -51,12 +46,6 @@ greetings_farewells_re = re.compile(join_words_in_or_pattern(["have .* day", "ha
                                                               "good luck", "great chat",
                                                               "get off .*", "thanks for the chat",
                                                               "thank.* for .* chat"]))
-special_intents = [
-    "cant_do", "repeat", "weather_forecast_intent", "what_are_you_talking_about",
-    "what_can_you_do", "what_is_your_job", "what_is_your_name", "what_time",
-    "where_are_you_from", "who_made_you"
-]
-
 tokenizer = tokenize.RegexpTokenizer(r'\w+')
 
 with open("./google-english-no-swears.txt", "r") as f:
@@ -109,6 +98,99 @@ def get_annotations_from_dialog(utterances, annotator_name, key_name):
     return result_values
 
 
+def get_cobot_nounphrases(utt):
+    cob_nounphs = utt.get("annotations", {}).get("cobot_nounphrases", [])
+    cobot_nounphrases = []
+    for ph in cob_nounphs:
+        if not pos_tag([ph])[0][1].startswith("VB"):
+            cobot_nounphrases.append(ph)
+    return cobot_nounphrases
+
+
+def get_intents_flags(utt):
+    special_intents = [
+        "cant_do", "repeat", "weather_forecast_intent", "what_are_you_talking_about",
+        "what_can_you_do", "what_is_your_job", "what_is_your_name", "what_time",
+        "where_are_you_from", "who_made_you"
+    ]
+    detected_intents = get_intents(utt, which="intent_catcher")
+    lets_chat_about_flag = if_lets_chat_about_topic(utt["text"].lower()) or (
+        "lets_chat_about" in detected_intents
+    )
+    special_intents_flag = any([si in detected_intents for si in special_intents])
+    return lets_chat_about_flag, special_intents_flag
+
+
+def get_lets_chat_topic(lets_chat_about_flag, utt, anntr_history_len):
+    lets_chat_topic = ""
+    COBOT_DA_FILE_TOPICS_MATCH = {
+        'Entertainment_Movies': 'movies',
+        'Entertainment_Music': 'music',
+        'Science_and_Technology': 'science',
+        'Sports': 'sports'
+    }
+    if lets_chat_about_flag:
+        anntr_history_len = 0
+        _get_topics = get_topics(utt, which="cobot_dialogact_topics")
+        for topic in _get_topics:
+            if topic in COBOT_DA_FILE_TOPICS_MATCH:
+                lets_chat_topic = COBOT_DA_FILE_TOPICS_MATCH[topic]
+    return lets_chat_topic, anntr_history_len
+
+
+def get_news_api_fact(bot_uttr, prev_human_uttr, not_switch_or_lets_chat_flag):
+    news_api_fact = ""
+    if (bot_uttr.get("active_skill", "") == "news_api_skill") and not_switch_or_lets_chat_flag:
+        prev_human_utt_hypotheses = prev_human_uttr.get("hypotheses", [])
+        news_api_hypothesis = [
+            h for h in prev_human_utt_hypotheses if (h.get("skill_name", "") == "news_api_skill")
+        ]
+        if news_api_hypothesis:
+            if news_api_hypothesis[0].get("news_status", "") == "opinion_request":
+                news_api_fact = news_api_hypothesis[0].get("curr_news", {}).get("description", "")
+    return news_api_fact
+
+
+def get_knowledge_from_annotators(annotators, uttrs, anntr_history_len):
+    user_input_knowledge = ""
+    anntrs_knowledge = ""
+    # look for kbqa/odqa text in anntr_history_len previous human utterances
+    annotations_depth = {}
+    for anntr_name, anntr_key in annotators.items():
+        prev_anntr_outputs = get_annotations_from_dialog(
+            uttrs[-anntr_history_len * 2 - 1:],
+            anntr_name,
+            anntr_key
+        )
+        logger.debug(f"Prev {anntr_name} {anntr_key}s: {prev_anntr_outputs}")
+        # add final dot to kbqa answer to make it a sentence
+        if prev_anntr_outputs and anntr_name == "kbqa":
+            prev_anntr_outputs[-1][1] += "."
+        # concat annotations separated by space to make a paragraph
+        if prev_anntr_outputs and prev_anntr_outputs[-1][1] != "Not Found":
+            anntrs_knowledge += prev_anntr_outputs[-1][1] + " "
+            annotations_depth[anntr_name] = prev_anntr_outputs[-1][0]
+    if anntrs_knowledge:
+        user_input_knowledge += '\n'.join(tokenize.sent_tokenize(anntrs_knowledge))
+    return user_input_knowledge, annotations_depth
+
+
+def space_join(x):
+    return " ".join(x) + " " if x else ""
+
+
+def get_penalties(bot_uttrs, curr_response):
+    already_was_active = 0
+    if bot_uttrs:
+        for bu in range(1, 1 + min(KG_ACTIVE_DEPTH, len(bot_uttrs))):
+            already_was_active += int(bot_uttrs[-bu].get(
+                "active_skill", "") == "knowledge_grounding_skill")
+    already_was_active *= AA_FACTOR
+    resp_tokens_len = len(tokenizer.tokenize(curr_response))
+    short_long_response = 0.5 * int(resp_tokens_len > 20 or resp_tokens_len < 4)
+    return already_was_active, short_long_response
+
+
 @app.route("/respond", methods=['POST'])
 def respond():
     print('response generation started')
@@ -128,103 +210,57 @@ def respond():
         try:
             user_input_text = dialog["human_utterances"][-1]["text"]
             bot_uttr = dialog["bot_utterances"][-1] if len(dialog["bot_utterances"]) > 0 else {}
+
             last_user_sent_text = dialog["human_utterances"][-1].get(
                 "annotations", {}).get("sentseg", {}).get("segments", [""])[-1].lower()
             switch_choose_topic = switch_topic_uttr(
                 dialog["human_utterances"][-1]) or if_choose_topic(
                 last_user_sent_text, prev_uttr=bot_uttr.get("text", "").lower())
-
-            # get cobot_nounphrases
-            cob_nounphs = dialog["human_utterances"][-1].get("annotations", {}).get("cobot_nounphrases", [])
-            cobot_nounphrases = []
-            for ph in cob_nounphs:
-                if not pos_tag([ph])[0][1].startswith("VB"):
-                    cobot_nounphrases.append(ph)
+            # cobot_nounphrases
+            cobot_nounphrases = get_cobot_nounphrases(dialog["human_utterances"][-1])
             nounphrases.append(re.compile(join_sentences_in_or_pattern(cobot_nounphrases),
                                           re.IGNORECASE) if cobot_nounphrases else "")
-
-            # get entities
+            # entities
             curr_ents = get_entities(dialog["human_utterances"][-1])
             entities.append(re.compile(join_sentences_in_or_pattern(curr_ents),
                                        re.IGNORECASE) if curr_ents else "")
-
             # intents
-            detected_intents = get_intents(dialog["human_utterances"][-1], which="intent_catcher")
-            lets_chat_about_flags.append(
-                if_lets_chat_about_topic(user_input_text.lower()) or (
-                    "lets_chat_about" in detected_intents
-                )
-            )
-            special_intents_flag = any([si in detected_intents for si in special_intents])
+            lets_chat_about_flag, special_intents_flag = get_intents_flags(dialog["human_utterances"][-1])
+            lets_chat_about_flags.append(lets_chat_about_flag)
             special_intents_flags.append(special_intents_flag)
 
-            # if detected lets_chat is about topic from the file
-            lets_chat_topic = ""
             anntr_history_len = DEFAULT_ANNTR_HISTORY_LEN
-            if lets_chat_about_flags[-1]:
-                anntr_history_len = 0
-                _get_topics = get_topics(dialog["human_utterances"][-1], which="cobot_dialogact_topics")
-                for topic in _get_topics:
-                    if topic in COBOT_DA_FILE_TOPICS_MATCH.keys():
-                        lets_chat_topic = COBOT_DA_FILE_TOPICS_MATCH[topic]
-
+            # if detected lets_chat is about topic from the file
+            lets_chat_topic, anntr_history_len = get_lets_chat_topic(
+                lets_chat_about_flag, dialog["human_utterances"][-1], anntr_history_len
+            )
             # if prev skill == news_api_skill get news description and create knowledge fact
-            news_api_fact = ""
-            if (bot_uttr.get("active_skill", "") == "news_api_skill") and (
-                not (switch_choose_topic or lets_chat_about_flags[-1])
-            ):
-                prev_human_utt_hypotheses = dialog["human_utterances"][-2].get("hypotheses", [])
-                news_api_hypothesis = [
-                    h for h in prev_human_utt_hypotheses if (h.get("skill_name", "") == "news_api_skill")
-                ]
-                if news_api_hypothesis:
-                    if news_api_hypothesis[0].get("news_status", "") == "opinion_request":
-                        news_api_fact = news_api_hypothesis[0].get("curr_news", {}).get("description", "")
-
+            news_api_fact = get_news_api_fact(
+                bot_uttr, dialog["human_utterances"][-2], not (switch_choose_topic or lets_chat_about_flag)
+            )
             # start creating data for kg service
-            user_input_history = [i["text"] for i in dialog["utterances"]]
-            user_input_history = '\n'.join(user_input_history)
+            user_input_history = '\n'.join([i["text"] for i in dialog["utterances"]])
 
-            user_input_knowledge = ""
-            anntrs_knowledge = ""
-            # look for kbqa/odqa text in anntr_history_len previous human utterances
             annotators = {
                 # "odqa": "answer_sentence",
                 # "kbqa": "answer"
             }
-            annotations_depth = {}
-            for anntr_name, anntr_key in annotators.items():
-                prev_anntr_outputs = get_annotations_from_dialog(
-                    dialog["utterances"][-anntr_history_len * 2 - 1:],
-                    anntr_name,
-                    anntr_key
-                )
-                logger.debug(f"Prev {anntr_name} {anntr_key}s: {prev_anntr_outputs}")
-                # add final dot to kbqa answer to make it a sentence
-                if prev_anntr_outputs and anntr_name == "kbqa":
-                    prev_anntr_outputs[-1][1] += "."
-                # concat annotations separated by space to make a paragraph
-                if prev_anntr_outputs and prev_anntr_outputs[-1][1] != "Not Found":
-                    anntrs_knowledge += prev_anntr_outputs[-1][1] + " "
-                    annotations_depth[anntr_name] = prev_anntr_outputs[-1][0]
-            if anntrs_knowledge:
-                user_input_knowledge += '\n'.join(tokenize.sent_tokenize(anntrs_knowledge))
-
+            user_input_knowledge, annotations_depth = get_knowledge_from_annotators(
+                annotators, dialog["utterances"], anntr_history_len
+            )
             # add nounphrases and entities to the knowledge
-            joined_nounphrases = " ".join(cobot_nounphrases) + " " if cobot_nounphrases else ""
-            joined_entities = " ".join(curr_ents) + " " if curr_ents else ""
-            user_input_checked_sentence = joined_nounphrases + joined_entities + tokenize.sent_tokenize(
+            user_input_checked_sentence = space_join(cobot_nounphrases)
+            + space_join(curr_ents) + tokenize.sent_tokenize(
                 user_input_knowledge)[0] if user_input_knowledge else ""
-
             user_input = {
                 'checked_sentence': user_input_checked_sentence,
                 'knowledge': user_input_knowledge,
                 'text': user_input_text,
                 'history': user_input_history
             }
+            input_batch.append(user_input)
             annotations_depths.append(annotations_depth)
             dial_ids.append(d_id)
-            input_batch.append(user_input)
 
             topical_chat_annots = get_annotations_from_dialog(
                 dialog["utterances"][-anntr_history_len * 2 - 1:],
@@ -238,9 +274,9 @@ def respond():
                     'text': user_input_text,
                     'history': user_input_history
                 }
+                input_batch.append(user_input)
                 annotations_depths.append({"odqa_topical_chat": topical_chat_annots[-1][0]})
                 dial_ids.append(d_id)
-                input_batch.append(user_input)
 
             odqa_1st_par_annots = get_annotations_from_dialog(
                 dialog["utterances"][-anntr_history_len * 2 - 1:],
@@ -254,35 +290,31 @@ def respond():
                     'text': user_input_text,
                     'history': user_input_history
                 }
+                input_batch.append(user_input)
                 annotations_depths.append({"odqa_1st_par": odqa_1st_par_annots[-1][0]})
                 dial_ids.append(d_id)
-                input_batch.append(user_input)
 
             if switch_choose_topic or lets_chat_topic:
                 if lets_chat_topic:
                     fact = random.sample(TOPICS_FACTS[lets_chat_topic], 1)[0]
-                    chosen_topics[d_id] = topic
-                    user_input = {
-                        'checked_sentence': fact,
-                        'knowledge': fact,
-                        'text': user_input_text,
-                        'history': user_input_history,
-                        'chosen_topic_fact': 'lets_chat_cobot_da'
-                    }
+                    chosen_topics[d_id] = lets_chat_topic
+                    _chosen_topic_fact = 'lets_chat_cobot_da'
                 else:
                     topic = random.sample(TOPICS_FACTS.keys(), 1)[0]
                     fact = random.sample(TOPICS_FACTS[topic], 1)[0]
                     chosen_topics[d_id] = topic
-                    user_input = {
-                        'checked_sentence': fact,
-                        'knowledge': fact,
-                        'text': user_input_text,
-                        'history': user_input_history,
-                        'chosen_topic_fact': 'switch_random'
-                    }
+                    _chosen_topic_fact = 'switch_random'
+                user_input = {
+                    'checked_sentence': fact,
+                    'knowledge': fact,
+                    'text': user_input_text,
+                    'history': user_input_history,
+                    'chosen_topic_fact': _chosen_topic_fact
+                }
+                input_batch.append(user_input)
                 annotations_depths.append({})
                 dial_ids.append(d_id)
-                input_batch.append(user_input)
+
             if news_api_fact:
                 user_input = {
                     'checked_sentence': news_api_fact,
@@ -291,9 +323,9 @@ def respond():
                     'history': user_input_history,
                     'news_api_fact': True
                 }
+                input_batch.append(user_input)
                 annotations_depths.append({})
                 dial_ids.append(d_id)
-                input_batch.append(user_input)
 
         except Exception as ex:
             sentry_sdk.capture_exception(ex)
@@ -318,21 +350,18 @@ def respond():
                     "can_continue": CAN_CONTINUE,
                     "confidence_case": ""
                 }
-                already_was_active = 0
-                if len(dialog["bot_utterances"]) > 0:
-                    for bu in range(1, 1 + min(KG_ACTIVE_DEPTH, len(dialog["bot_utterances"]))):
-                        already_was_active += int(dialog["bot_utterances"][-bu].get(
-                            "active_skill", "") == "knowledge_grounding_skill")
-                already_was_active *= AA_FACTOR
-                short_long_response = 0.5 * int(len(tokenizer.tokenize(raw_responses[curr_i])) > 20 or len(
-                    tokenizer.tokenize(raw_responses[curr_i])) < 4)
 
+                already_was_active, short_long_response = get_penalties(
+                    dialog["bot_utterances"],
+                    raw_responses[curr_i]
+                )
                 curr_nounphrase_search = nounphrases[i].search(raw_responses[curr_i]) if nounphrases[i] else False
                 curr_entities_search = entities[i].search(raw_responses[curr_i]) if entities[i] else False
-
                 no_penalties = False
+
                 topic = chosen_topics.get(i, "")
                 chosen_topic_fact_flag = input_batch[curr_i].get("chosen_topic_fact", "")
+
                 add_intro = ""
                 if topic and chosen_topic_fact_flag:
                     add_intro = f"Okay, Let's chat about {topic}. "
