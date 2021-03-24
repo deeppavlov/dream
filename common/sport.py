c6b8ed6f6d351015d3e3158adf8fbc166a77fc08
Sergey Kumeiko <56466368+purgen219@users.noreply.github.com>
2021-03-24 13:22:57 +0300

    Fix/dff_sport_skill (#435)
    * add new sport
    
    * new head branch
    
    * change
    
    * add new question
    
    * add q * new branch from START *
    
    * positive sentiment
    
    * add random choice for interesting
    
    * fix small bag in logic
    
    * fix bag : raname and import
    
    * fix bag
    
    * fix bag with .group
    
    * del space and nod * fix random
    
    * negative turn
    
    * dropped conf
    
    * fix codestyle
    
    * fix codestyle
    
    * fix codestyle
    
    * fix bag in test file - conf
    
    * del strange branch from start
    
    Co-authored-by: Сергей Кумейко <kumeiko.se@gpu10.ipavlov.mipt.ru>

--- common/sport.py
+++ common/sport.py
@@ -5,17 +5,21 @@ import re
 ##################################################################################################################
 
 
-BINARY_QUESTION_ABOUT_SPORT = ["Do you like sport?",
-                               "What do you think about an active lifestyle?",
-                               "What is your opinion about sports?",
+BINARY_QUESTION_ABOUT_SPORT = ["Do you like sport?"
                                "Do you think active lifestyle is cool?",
                                "Do you like fitness?",
                                "Let's chat about sport! Do you agree?"
                                ]
+BINARY_QUESTION_ABOUT_ATHLETE = ["Maybe let's talk about your favorite athletes?",
+                                 "Do you have a sports idol?"
+                                 ]
+
+BINARY_QUESTION_ABOUT_COMP = ["Well, Do you want to talk about sports competitions?"]
 
 
 def skill_trigger_phrases():
-    return BINARY_QUESTION_ABOUT_SPORT
+    return BINARY_QUESTION_ABOUT_SPORT + BINARY_QUESTION_ABOUT_ATHLETE + BINARY_QUESTION_ABOUT_COMP
+
 
 ##################################################################################################################
 # TEMPLATE
@@ -36,7 +40,7 @@ KIND_OF_SPORTS_TEMPLATE = re.compile(
     r"|long jump|martial arts|motorbike sports|mountaineering|orienteering"
     r"|parachuting|pole-vaulting|polo|riding|rowing|rugby|sailing|skis|snooker"
     r"|track-and-field|triathlon|tug of war|volleyball|water polo|waterski"
-    r"|weight lifting|working out|wrestling|sport|run|swim|fitness)",
+    r"|weight lifting|working out|wrestling|sport|run|swim|fitness|lacrosse|riding horses|ballet|marching|soccer)",
     re.IGNORECASE,
 )
 
@@ -51,13 +55,14 @@ KIND_OF_COMPETITION_TEMPLATE = re.compile(
 )
 
 ATHLETE_TEMPLETE = re.compile(
-    r"(athlete|sportsperson|games player|muscle person|player" r"|footballer|aquanaut|diver|jock|lifter)", re.IGNORECASE
+    r"(athlete|sportsperson|games player|muscle person|player"
+    r"|footballer|aquanaut|diver|jock|lifter)", re.IGNORECASE
 )
-
+SPORT_TEMPLATE = re.compile(r"(sport|active)", re.IGNORECASE)
 SUPPORT_TEMPLATE = re.compile(r"(support|a fan of)", re.IGNORECASE)
 
 QUESTION_TEMPLATE = re.compile(r"(what|did|do|which|who) (team )?(you )?(do|is|are|kind of|know|like)", re.IGNORECASE)
 
-LIKE_TEMPLATE = re.compile(r"(like|love|support|a fan of|favorite|enjoy)", re.IGNORECASE)
+LIKE_TEMPLATE = re.compile(r"(like|love|support|a fan of|favorite|enjoy|want to talk)?", re.IGNORECASE)
 
 COMPETITION_TEMPLATE = re.compile(r"(tournament|tourney|competition)", re.IGNORECASE)
