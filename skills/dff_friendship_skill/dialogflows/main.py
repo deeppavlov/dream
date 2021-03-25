6825531bdc700e9a748795f2a9a8b22846d7814d
Kuznetsov Denis <kuznetsov.den.p@gmail.com>
2021-03-25 11:43:16 +0300

    fix/fss/upd_weekend_request_func: reorder prioriry of weekend & use more weak conditions (#451)
    * fix/fss/upd_weekend_request_func: reorder prioriry of weekend & use more weak conditions
    
    * fix/fss/upd_weekend_request_func: fix style
    
    * feat/fact_retrieval/fact_ranker: revert all skills in skill selector

--- skills/dff_friendship_skill/dialogflows/main.py
+++ skills/dff_friendship_skill/dialogflows/main.py
@@ -1,12 +1,9 @@
 import logging
 
-import re
-
 from emora_stdm import CompositeDialogueFlow, DialogueFlow
 
 
 import common.dialogflow_framework.stdm.dialogflow_extention as dialogflow_extention
-import common.dialogflow_framework.utils.state as state_utils
 
 import dialogflows.flows.greeting as greeting_flow
 import dialogflows.flows.weekend as weekend_flow
@@ -44,31 +41,6 @@ def greeting_request(ngrams, vars):
 
 
 ##################################################################################################################
-# weekend
-##################################################################################################################
-
-
-patterns_bot = ["chat about", "talk about", "on your mind"]
-re_patterns_bot = re.compile("(" + "|".join(patterns_bot) + ")", re.IGNORECASE)
-
-patterns_human = ["no idea", "don't know", "nothing", "anything", "your favorite topic"]
-re_patterns_human = re.compile("(" + "|".join(patterns_human) + ")", re.IGNORECASE)
-
-
-def weekend_request(ngrams, vars):
-    flag = False
-
-    # ok we start with the idea that user has no idea what to talk about
-    last_bot_text = state_utils.get_last_bot_utterance(vars)["text"]
-    human_text = state_utils.get_last_human_utterance(vars)["text"]
-
-    flag = bool(re.search(re_patterns_bot, last_bot_text) and re.search(re_patterns_human, human_text))
-
-    logger.info(f"weekend_request={flag}")
-    return flag
-
-
-##################################################################################################################
 ##################################################################################################################
 # linking
 ##################################################################################################################
@@ -78,7 +50,7 @@ for node in [scopes.State.USR_ROOT, scopes.State.USR_ERR]:
     simplified_dialogflow.add_user_serial_transitions(
         node,
         {
-            (scopes.WEEKEND, weekend_flow.State.USR_START): weekend_request,
+            (scopes.WEEKEND, weekend_flow.State.USR_START): weekend_flow.std_weekend_request,
             (scopes.GREETING, greeting_flow.State.USR_START): greeting_request,
         },
     )
