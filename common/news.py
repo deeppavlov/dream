aba2db1bb160ce92103c42ed4a4fe2e391328452
Dilyara Baymurzina <dilyara.rimovna@gmail.com>
2021-03-31 00:33:14 +0300

    feat: news skill another linkto, now it's a question! (#503)
    * feat: news skill another linkto, now it's a question!
    
    * feat/news_new_linkto_question: upt test
    
    Co-authored-by: Denis Kuznetosv <kuznetsov.den.p@gmail.com>

--- common/news.py
+++ common/news.py
@@ -16,19 +16,15 @@ WHAT_TYPE_OF_NEWS = ["What other kinds of news would you want to discuss?",
                      "What else would you want to hear news about?",
                      "What type of news do you prefer?"]
 
-# from emo skill connections to news skill
-BREAKING_NEWS_CHECK_WORD = 'Breaking news'
-BREAKING_NEWS = f'Oh! {BREAKING_NEWS_CHECK_WORD}! Would you like to hear it?'
-
 NEWS_DUPLICATES = WHAT_TYPE_OF_NEWS
 
 
 def skill_trigger_phrases():
-    return [BREAKING_NEWS]
+    return [OFFER_BREAKING_NEWS]
 
 
 def is_breaking_news_requested(prev_bot_utt, user_utt):
-    if BREAKING_NEWS_CHECK_WORD.lower() in prev_bot_utt.get('text', '').lower():
+    if OFFER_BREAKING_NEWS.lower() in prev_bot_utt.get('text', '').lower():
         if is_yes(user_utt):
             return True
     return False
