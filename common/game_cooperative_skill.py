2886c8ea1e0ae980a6b39974df449c173737c494
Kuznetsov Denis <kuznetsov.den.p@gmail.com>
2021-04-13 09:41:38 +0300

    upd/gcs/link_to: upd link_to (#664)
    * upd/gcs/link_to: upd link_to
    
    * upd/gcs/link_to: up confidence for link_to
    
    * upd/gcs/link_to: fix style

--- common/game_cooperative_skill.py
+++ common/game_cooperative_skill.py
@@ -1,7 +1,12 @@
 import re
 
 GAMES_COMPILED_PATTERN = re.compile(r"(\bgame\b|\bgames|videogame|\bgaming)", re.IGNORECASE)
+TRIGGER_PHRASES = ["do you love video games?"]
 
 
 def skill_trigger_phrases():
-    return ["If you want to talk about games just say, \"let's talk about games\""]
+    return TRIGGER_PHRASES
+
+
+def game_skill_was_proposed(prev_bot_utt):
+    return any([phrase.lower() in prev_bot_utt.get("text", "").lower() for phrase in TRIGGER_PHRASES])
