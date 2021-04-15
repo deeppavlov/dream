5e0158f24cd283f4122bdce62e13b1d293448e92
Dilyara Baymurzina <dilyara.rimovna@gmail.com>
2021-04-15 20:06:44 +0300

    feat: response selector no required dialog act category (#701)
    * fix: response selector no required dialog act category
    
    * fix: codestyle
    
    * fix: codestyle

--- response_selectors/convers_evaluation_based_selector/tag_based_selection.py
+++ response_selectors/convers_evaluation_based_selector/tag_based_selection.py
@@ -251,12 +251,12 @@ def tag_based_response_selection(dialog, candidates, scores, confidences, bot_ut
     _is_force_intent = any([_intent in all_user_intents for _intent in FORCE_INTENTS_IC.keys()])
     _is_require_action_intent = any([_intent in all_user_intents for _intent in REQUIRE_ACTION_INTENTS.keys()])
     _force_intents_detected = [_intent for _intent in FORCE_INTENTS_IC.keys() if _intent in all_user_intents]
-    _require_action_intents_detected = [_intent for _intent in REQUIRE_ACTION_INTENTS.keys()
-                                        if _intent in all_user_intents]
+    # _require_action_intents_detected = [_intent for _intent in REQUIRE_ACTION_INTENTS.keys()
+    #                                     if _intent in all_user_intents]
     _force_intents_skills = sum([FORCE_INTENTS_IC.get(_intent, [])
                                  for _intent in _force_intents_detected], [])
-    _required_actions = sum([REQUIRE_ACTION_INTENTS.get(_intent, [])
-                             for _intent in _require_action_intents_detected], [])
+    # _required_actions = sum([REQUIRE_ACTION_INTENTS.get(_intent, [])
+    #                          for _intent in _require_action_intents_detected], [])
     _contains_entities = len(get_entities(annotated_uttr, only_named=False, with_labels=False)) > 0
     _is_active_skill_can_not_continue = False
 
@@ -356,24 +356,25 @@ def tag_based_response_selection(dialog, candidates, scores, confidences, bot_ut
                 _is_active_skill, _can_continue, _same_topic_entity, _is_dialog_abandon,
                 _is_required_da=False)
 
-        elif _is_require_action_intent:
-            # =====user intent requires particular action=====
-
-            CASE = "User intent requires action."
-            _is_grounding_reqda = skill_name == "grounding_skill" and cand_uttr.get("type", "") == "universal_response"
-
-            if set(all_cand_intents).intersection(set(_required_actions)) or _is_grounding_reqda:
-                # -----one of the can intent is in intents required by user-----
-                categorized_hyps, categorized_prompts = categorize_candidate(
-                    cand_id, skill_name, categorized_hyps, categorized_prompts, _is_just_prompt,
-                    _is_active_skill, _can_continue, _same_topic_entity, _is_dialog_abandon,
-                    _is_required_da=True)
-            else:
-                # -----NO required dialog acts-----
-                categorized_hyps, categorized_prompts = categorize_candidate(
-                    cand_id, skill_name, categorized_hyps, categorized_prompts, _is_just_prompt,
-                    _is_active_skill, _can_continue, _same_topic_entity, _is_dialog_abandon,
-                    _is_required_da=False)
+        # elif _is_require_action_intent:
+        #     # =====user intent requires particular action=====
+        #
+        #     CASE = "User intent requires action."
+        #     _is_grounding_reqda = skill_name == "grounding_skill" and cand_uttr.get(
+        #         "type", "") == "universal_response"
+        #
+        #     if set(all_cand_intents).intersection(set(_required_actions)) or _is_grounding_reqda:
+        #         # -----one of the can intent is in intents required by user-----
+        #         categorized_hyps, categorized_prompts = categorize_candidate(
+        #             cand_id, skill_name, categorized_hyps, categorized_prompts, _is_just_prompt,
+        #             _is_active_skill, _can_continue, _same_topic_entity, _is_dialog_abandon,
+        #             _is_required_da=True)
+        #     else:
+        #         # -----NO required dialog acts-----
+        #         categorized_hyps, categorized_prompts = categorize_candidate(
+        #             cand_id, skill_name, categorized_hyps, categorized_prompts, _is_just_prompt,
+        #             _is_active_skill, _can_continue, _same_topic_entity, _is_dialog_abandon,
+        #             _is_required_da=False)
 
         else:
             # =====user intent does NOT require particular action=====
