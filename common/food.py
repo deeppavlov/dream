4b15a29e7e0feeeaccd4e4b5f240143e7e89db67
Alsu Sagirova <34598622+Saraharas@users.noreply.github.com>
2021-04-21 00:03:44 +0300

    fix/food_skill: add acknowledgements & turn on regexps (#747)
    * add acknowledgements
    
    * upd regexps to turn on food skill
    
    * fix codestyle
    
    * fix typ
    
    * add line endings
    
    * fix typos
    
    * fix typos
    
    * i'm not fond of it
    
    * fix weather false positive turn on
    
    Co-authored-by: Denis Kuznetosv <kuznetsov.den.p@gmail.com>

--- common/food.py
+++ common/food.py
@@ -1,4 +1,5 @@
 import re
+from common.utils import join_sentences_in_or_pattern
 
 OPINION_REQUESTS_ABOUT_FOOD = ["Do you like cooking?",
                                "Do you think cooking is cool?",
@@ -9,14 +10,25 @@ OFFER_TALK_ABOUT_FOOD = ["Would you like to chat about food?",
                          "Would you like to talk about food?",
                          "I'd like to talk about food, would you?"]
 
+FOOD_WORDS = r"(food|cook|cooking|bake|baking|cuisine|daily bread|meals|foodstuffs" \
+    "|edibles|drink|pepperoni|pizza|strawberries|chocolate|coffee|eat|dinner" \
+    "|breakfast|pasta|burger|cheese|tasty|waffles)"
+
+FOOD_SKILL_TRANSFER_PHRASES = r"(do you know .* most (favorite|favourite) food?" \
+    "|.*what is your (favorite|favourite) food?" \
+    "|.*by the way, what food do you like?|do you like .* cuisine?" \
+    "|.*what kind of cuisine do you like?)"
+FOOD_SKILL_TRANSFER_PHRASES_RE = re.compile(FOOD_SKILL_TRANSFER_PHRASES, re.IGNORECASE)
+
+WHAT_COOK = r"(what should i|what do you suggest me to)" \
+    " (cook|make for dinner)( tonight| today| tomorrow){0,1}"
+
 TRIGGER_PHRASES = OPINION_REQUESTS_ABOUT_FOOD + OFFER_TALK_ABOUT_FOOD
-FOOD_COMPILED_PATTERN = re.compile(r"\bfood", re.IGNORECASE)
-FOOD_SKILL_TRANSFER_PHRASES_RE = re.compile(
-    r"(do you know .* most (favorite|favourite) food?|.*what is your (favorite|favourite) food?"
-    "|.*by the way, what food do you like?|do you like .* cuisine?"
-    "|.*what kind of cuisine do you like?)",
-    re.IGNORECASE
-)
+FOOD_COMPILED_PATTERN = re.compile(join_sentences_in_or_pattern(
+    [
+        FOOD_WORDS, FOOD_SKILL_TRANSFER_PHRASES, WHAT_COOK
+    ]
+), re.IGNORECASE)
 
 
 def skill_trigger_phrases():
