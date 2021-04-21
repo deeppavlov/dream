49b8a9fea85926a510464f0afb151a49a3644af5
Alsu Sagirova <34598622+Saraharas@users.noreply.github.com>
2021-04-21 15:37:06 +0300

    upd active and almost_active skills lists (#799)
    Co-authored-by: Denis Kuznetosv <kuznetsov.den.p@gmail.com>

--- common/response_selection.py
+++ common/response_selection.py
@@ -7,10 +7,10 @@ ACTIVE_SKILLS = [
     "valentines_dat_skill", "weather_skill", "wikidata_dial_skill",  #
     "comet_dialog_skill",
     "dff_animals_skill", "dff_food_skill", "dff_music_skill", "dff_sport_skill", "dff_travel_skill",
-    "dff_celebrity_skill",
+    "dff_celebrity_skill", "dff_bot_persona_skill", "dff_gaming_skill",
 ]
 ALMOST_ACTIVE_SKILLS = [
-    "small_talk_skill", "friendship_skill", "dff_friendship_skill",
+    "small_talk_skill", "friendship_skill", "dff_friendship_skill", "knowledge_grounding_skill",
 ]
 CAN_NOT_BE_DISLIKED_SKILLS = ["meta_script_skill", "personal_info_skill"]
 NOT_ADD_PROMPT_SKILLS = ["alexa_handler", "intent_responder", "misheard_asr", "program_y_dangerous"]
