fcfa4f9541279009da4c0fcd760fae61ed48268e
Dilyara Baymurzina <dilyara.rimovna@gmail.com>
2021-04-21 19:39:26 +0300

    feat: general acknowledgements (#806)

---
+++ common/acknowledgements.py
@@ -0,0 +1,9 @@
+
+GENERAL_ACKNOWLEDGEMENTS = {
+    "positive":
+        ["Sounds cool! ", "Great! ", "Wonderful! ", 'Cool!', "Nice!"],
+    "neutral":
+        ["Okay. ", "Oh. ", "Huh. ", "Well. ", "Gotcha. ", "Hmm. ", "Aha. "],
+    "megative":
+        ["Huh... ", "Sounds sad... ", "Sorry... ", "I see.", "That's okay.", "Okay."]
+}
