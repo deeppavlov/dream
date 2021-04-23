33ee27a7074c4ae7f5f4aa04a0bc1e98aaf7391e
Dilyara Baymurzina <dilyara.rimovna@gmail.com>
2021-04-24 01:26:18 +0300

    feat: remove alexa from the beginning of the utterance in spell preproc (#829)

--- annotators/spelling_preprocessing/server.py
+++ annotators/spelling_preprocessing/server.py
@@ -17,6 +17,7 @@ app = Flask(__name__)
 
 templates = []
 
+templates += [(re.compile(r"^alexa ", flags=re.IGNORECASE), "")]
 templates += [(re.compile(r"(?<= [a-z])\. (?=[a-z]\.)|(?<= [a-z]\. [a-z])\.(?! [a-z]\.)", flags=re.IGNORECASE), "")]
 templates += [(re.compile(r"\bwon'?t\b", flags=re.IGNORECASE), "will not")]
 templates += [(re.compile(r"\bhaven'?t\b", flags=re.IGNORECASE), "have not")]
