f320908b98be73167bd7ce3a7a65dc9de9bea86a
Dilyara Baymurzina <dilyara.rimovna@gmail.com>
2021-04-28 12:53:51 +0300

    fix: small talk can be active (#868)

--- common/response_selection.py
+++ common/response_selection.py
@@ -1,16 +1,17 @@
 # this is a list of skills which are not one-lines
 ACTIVE_SKILLS = [
     "book_skill", "christmas_new_year_skill", "coronavirus_skill", "dummy_skill_dialog",
-    "emotion_skill", "game_cooperative_skill",  # "knowledge_grounding_skill",
+    "emotion_skill", "game_cooperative_skill",
     "meta_script_skill", "movie_skill", "news_api_skill", "oscar_skill", "personal_info_skill",
-    "reddit_ner_skill", "short_story_skill", "superbowl_skill",  #
-    "valentines_dat_skill", "weather_skill", "wikidata_dial_skill",  #
+    "reddit_ner_skill", "short_story_skill", "superbowl_skill",
+    "valentines_dat_skill", "weather_skill", "wikidata_dial_skill",
     "comet_dialog_skill",
     "dff_animals_skill", "dff_food_skill", "dff_music_skill", "dff_sport_skill", "dff_travel_skill",
-    "dff_celebrity_skill", "dff_bot_persona_skill", "dff_gaming_skill", "dff_gossip_skill"
+    "dff_celebrity_skill", "dff_bot_persona_skill", "dff_gaming_skill", "dff_gossip_skill",
+    "small_talk_skill",
 ]
 ALMOST_ACTIVE_SKILLS = [
-    "small_talk_skill", "friendship_skill", "dff_friendship_skill", "knowledge_grounding_skill",
+    "friendship_skill", "dff_friendship_skill", "knowledge_grounding_skill",
 ]
 CAN_NOT_BE_DISLIKED_SKILLS = ["meta_script_skill", "personal_info_skill"]
 NOT_ADD_PROMPT_SKILLS = ["alexa_handler", "intent_responder", "misheard_asr", "program_y_dangerous"]
