915fe872adbda80787f6179e2b297b3fb1df2b9c
dmitrijeuseew <dmitrij.euseew@yandex.ru>
2021-05-03 19:50:42 +0300

    fix bug in tests (#941)

--- skills/dff_animals_skill/dialogflows/flows/animals.py
+++ skills/dff_animals_skill/dialogflows/flows/animals.py
@@ -47,8 +47,8 @@ def lets_talk_about_request(vars):
     isyes = is_yes(user_uttr)
     chat_about = if_chat_about_particular_topic(user_uttr, bot_uttr, compiled_pattern=ANIMALS_FIND_TEMPLATE)
     dont_like = re.findall(DONT_LIKE, user_uttr["text"])
-    if not dont_like and have_pets or chat_about or (found_prompt and isyes) \
-            and not is_last_state(vars, AS.SYS_WHAT_ANIMALS):
+    if not dont_like and (have_pets or (chat_about and not is_last_state(vars, AS.SYS_WHAT_ANIMALS))
+                          or (found_prompt and isyes)):
         flag = True
     logger.info(f"lets_talk_about_request={flag}")
     return flag
