584e2fb504c4bb9d8dcb062eb268eb03c8146ef6
dmitrijeuseew <dmitrij.euseew@yandex.ru>
2021-05-03 17:11:22 +0300

    extend dff_wiki_skill (#930)
    * add tests
    
    * fix tests
    
    * fix tests
    
    * fix tests
    
    * update
    
    * add function
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * fix config
    
    * fix tests
    
    * fix bug
    
    * fix bug
    
    * update
    
    * update
    
    * fix tests
    
    * update
    
    * update
    
    * update
    
    * codestyle
    
    * fix download
    
    * add timeout
    
    Co-authored-by: Denis Kuznetosv <kuznetsov.den.p@gmail.com>

--- skills/dff_wiki_skill/dialogflows/flows/wiki_states.py
+++ skills/dff_wiki_skill/dialogflows/flows/wiki_states.py
@@ -7,6 +7,12 @@ class State(Enum):
     SYS_TELL_FACT = auto()
     USR_TELL_FACT = auto()
     #
+    SYS_MORE_DETAILED = auto()
+    USR_MORE_DETAILED = auto()
+    #
+    SYS_FACTOID_Q = auto()
+    USR_FACTOID_Q = auto()
+    #
     SYS_START_TALK = auto()
     USR_START_TALK = auto()
     #
