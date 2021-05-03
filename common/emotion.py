17b38caef3aed5058455e417c68b4c3aaf4bbc13
dimakarp1996 <dimakarp1996@yandex.ru>
2021-05-03 18:34:44 +0300

    Update emotion.py (#938)

--- common/emotion.py
+++ common/emotion.py
@@ -21,11 +21,11 @@ NEGATIVE_EMOTION = 'negative_emotion'
 
 HOW_DO_YOU_FEEL = 'How do you feel?'
 
-LONELINESS_TEMPLATE = r"(i am alone|lonely|loneliness)"
 SAD_TEMPLATE = r"\b(sad|horrible|depressed|awful|dire|pretty bad|pain|bad)\b"
 POOR_ASR_TEMPLATE = r'^say$'
-SAD_TEMPLATE = rf"({SAD_TEMPLATE}|{POOR_ASR_TEMPLATE})"
 
+LONELINESS_TEMPLATE = re.compile(r"(i am alone|lonely|loneliness)", re.IGNORECASE)
+SAD_TEMPLATE = re.compile(rf"({SAD_TEMPLATE}|{POOR_ASR_TEMPLATE})", re.IGNORECASE)
 BORING_TEMPLATE = r"(boring|bored)"  # The template is used to EXCLUDE answers on this intent
 JOKE_REQUEST_COMPILED_PATTERN = re.compile(r"(tell me|tell|hear)( [a-z]+){0,3} jokes?", re.IGNORECASE)
 TALK_ABOUT_EMO_TEMPLATE = re.compile(r'talk about emotion', re.IGNORECASE)
