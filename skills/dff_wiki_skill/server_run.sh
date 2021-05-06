d4d78343136303ca9dd2dcc04ef1a497c52497c6
Kuznetsov Denis <kuznetsov.den.p@gmail.com>
2021-05-06 16:56:37 +0300

    feat/dff-wiki/speedup_test2: other try to speedup tests (#991)

--- skills/dff_wiki_skill/server_run.sh
+++ skills/dff_wiki_skill/server_run.sh
@@ -2,6 +2,8 @@
 
 mkdir -p /root/.deeppavlov
 
-wget -ncq ${DATA_URL} -P /root/.deeppavlov
+echo "wget ${DATA_URL}"
+wget -nc -q ${DATA_URL} -P /root/.deeppavlov
+echo "wget finished"
 
 gunicorn --workers=1 server:app -b 0.0.0.0:${SERVICE_PORT} --reload --timeout 800
