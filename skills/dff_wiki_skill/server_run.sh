106ce282841fc1862ac0c7f9f85679068b9e97f5
Kuznetsov Denis <kuznetsov.den.p@gmail.com>
2021-05-06 21:53:12 +0300

    feat/dff-wiki/add_downloading_by_deeppavlov: upd downloading (#992)
    * feat/dff-wiki/add_downloading_by_deeppavlov: upd downloading
    
    * feat/dff-wiki/add_downloading_by_deeppavlov: upd downloading

--- skills/dff_wiki_skill/server_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p /root/.deeppavlov
-
-echo "wget ${DATA_URL}"
-wget -nc -q ${DATA_URL} -P /root/.deeppavlov
-echo "wget finished"
-
-gunicorn --workers=1 server:app -b 0.0.0.0:${SERVICE_PORT} --reload --timeout 800
