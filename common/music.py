d0dd6e42993c279f680edbdb1eaee1326b1313ec
Dilyara Baymurzina <dilyara.rimovna@gmail.com>
2021-05-06 21:47:55 +0300

    fix: remove some linkto questions for music skill (#989)
    Co-authored-by: Denis Kuznetosv <kuznetsov.den.p@gmail.com>

--- common/music.py
+++ common/music.py
@@ -1,12 +1,12 @@
 import re
 
 OPINION_REQUESTS_ABOUT_MUSIC = ["What music do you like?",
-                                "Do you like listening to music?",
+                                # "Do you like listening to music?",
                                 "What do you listen to?",
                                 "Who is your favorite artist?",
                                 "What is your favorite song?",
                                 "What is your favorite album?",
-                                "Do you have a favorite artist?"
+                                # "Do you have a favorite artist?"
                                 ]
 
 MUSIC_COMPILED_PATTERN = re.compile(r"(music|song)", re.IGNORECASE)
