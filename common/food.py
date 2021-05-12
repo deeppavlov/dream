6a1056b1adeca3f9d67acd45b906dcc75c222e99
Alsu Sagirova <34598622+Saraharas@users.noreply.github.com>
2021-05-13 00:10:18 +0300

    fix food linkto phrases (#1039)

--- common/food.py
+++ common/food.py
@@ -6,10 +6,6 @@ OPINION_REQUESTS_ABOUT_FOOD = ["Do you like cooking?",
                                "Many people say they adore cooking. Do you agree?",
                                "Do you think cooking is a great thing?"]
 
-OFFER_TALK_ABOUT_FOOD = ["Would you like to chat about food?",
-                         "Would you like to talk about food?",
-                         "I'd like to talk about food, would you?"]
-
 FOOD_WORDS = r"(food|cook|cooking|\bbake\b|baking|cuisine|daily bread|meals|foodstuffs" \
     r"|edibles|drink|pepperoni|pizza|strawberries|chocolate|coffee|\beat\b|\bate\b" \
     r"|\beating\b|\beats\b|dinner|breakfast|\bpasta\b|burger|cheese|tasty|" \
@@ -27,7 +23,7 @@ FOOD_SKILL_TRANSFER_PHRASES_RE = re.compile(FOOD_SKILL_TRANSFER_PHRASES, re.IGNO
 WHAT_COOK = r"(what should i|what do you suggest me to)" \
     r" (cook|make for dinner)( tonight| today| tomorrow){0,1}"
 
-TRIGGER_PHRASES = OPINION_REQUESTS_ABOUT_FOOD + OFFER_TALK_ABOUT_FOOD
+TRIGGER_PHRASES = OPINION_REQUESTS_ABOUT_FOOD
 FOOD_COMPILED_PATTERN = re.compile(join_sentences_in_or_pattern(
     [
         FOOD_WORDS, FOOD_SKILL_TRANSFER_PHRASES, WHAT_COOK
