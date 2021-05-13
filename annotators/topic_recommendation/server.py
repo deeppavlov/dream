84334714b1f434150e76cceb2bf1406a1a1ff9b8
Elena Ermakova <lena0707es@gmail.com>
2021-05-13 11:59:11 +0300

    feat: update topic recommendation model (#1042)
    * feat: update topic recommendation model
    
    * fix: code style
    
    Co-authored-by: Denis Kuznetosv <kuznetsov.den.p@gmail.com>

--- annotators/topic_recommendation/server.py
+++ annotators/topic_recommendation/server.py
@@ -1,12 +1,11 @@
 import logging
 import time
-import pickle
-import json
-import numpy as np
 from os import getenv
 
 from flask import Flask, request, jsonify
 import sentry_sdk
+
+from convert import get_ranked_list
 
 sentry_sdk.init(getenv("SENTRY_DSN"))
 
@@ -16,99 +15,41 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
-topic2skills = json.load(open("topic2skills.json"))
-
 MODEL_PATH = getenv("MODEL_PATH")
 TOP_K = 3
 
-TOPICS = [
-    "ordinal",
-    "albumname",
-    "genre",
-    "sportteam",
-    "songname",
-    "vehicle",
-    "event",
-    "wear",
-    "channelname",
-    "party",
-    "person",
-    "sport",
-    "venue",
-    "gamename",
-    "position",
-    "softwareapplication",
-    "bookname",
-    "device",
-    "videoname",
-    "location",
-    "sportrole",
-    "organization",
-]
+topic2response = {"Let's chat about food": "dff_food_skill",
+                  "Let's chat about books": "book_skill",
+                  "Let's chat about sport": "dff_sport_skill",
+                  "Let's chat about movies": "movie_skill",
+                  "Let's chat about animals": "dff_animals_skill",
+                  "Let's chat about games": "game_cooperative_skill",
+                  "Let's chat about music": "dff_music_skill",
+                  "Let's chat about news": "news_api_skill",
+                  "Let's chat about travels": "dff_travel_skill"}
 
-embeddings = np.identity(len(TOPICS)).tolist()
-topics_embeddings = {topic: emb for topic, emb in zip(TOPICS, embeddings)}
-
-try:
-    with open(MODEL_PATH, "rb") as file:
-        model = pickle.load(file)
-    model.predict_proba([embeddings[0] * 3])
-    logger.info("test query processed")
-except Exception as exc:
-    sentry_sdk.capture_exception(exc)
-    logger.exception(exc)
-    raise exc
-
-
-def choose_candidate_topics(current_topics, context_topics):
-    if len(context_topics) > 0:
-        history_embedding = sum(np.array([topics_embeddings[topic] for topic in context_topics if topic in TOPICS]))
-        normalized_history_embedding = (history_embedding / sum(history_embedding)).tolist()
-    else:
-        normalized_history_embedding = [0] * len(TOPICS)
-    if len(current_topics) > 0:
-        current_topic_embeddings = [topics_embeddings[topic] for topic in current_topics]
-    else:
-        current_topic_embeddings = [[0] * len(TOPICS)]
-    samples = []
-    for topic_emb in current_topic_embeddings:
-        for topic, embedding in topics_embeddings.items():
-            samples.append({"topic": topic, "embedding": topic_emb + embedding + normalized_history_embedding})
-    samples_embeddings = [el["embedding"] for el in samples]
-    preds = model.predict_proba(samples_embeddings)[:, 1]
-    top_idx = np.argsort(preds)[::-1][:TOP_K]
-    candidate_topics = [el["topic"] for i, el in enumerate(samples) if i in top_idx]
-    candidate_topics = list(set(candidate_topics))
-    return candidate_topics
-
-
-def get_entities(utter_entities):
-    all_entities = sum(utter_entities, [])
-    all_entities = [ent.get("label", "") for ent in all_entities]
-    all_entities = [ent for ent in all_entities if ent in TOPICS]
-    return all_entities
+responses = list(topic2response.keys())
 
 
 def handler(requested_data):
     st_time = time.time()
 
-    utter_entities_batch = requested_data["utter_entities_batch"]
-    responses = []
-    for utter_entities in utter_entities_batch:
+    utter_sentences_batch = requested_data["utterances_histories"]
+    candidate_topics_batch = []
+    for utter_sentences in utter_sentences_batch:
         try:
-            context_topics = get_entities(utter_entities)
-            current_topics = get_entities(utter_entities[-1:])
-            candidate_topics = choose_candidate_topics(current_topics, context_topics)
-            candidate_topics = sum([topic2skills.get(topic, []) for topic in candidate_topics], [])
-            responses.append(candidate_topics)
+            topic_ranked_list = get_ranked_list(utter_sentences, responses)
+            candidate_topics = [topic2response[responses[i]] for i in topic_ranked_list[:TOP_K]]
+            candidate_topics_batch.append(candidate_topics)
         except Exception as exc:
             logger.exception(exc)
             sentry_sdk.capture_exception(exc)
-            responses.append([])
+            candidate_topics_batch.append([])
 
     total_time = time.time() - st_time
     logger.info(f"topic_recommendation exec time: {total_time:.3f}s")
-    return responses
+    logger.warning(candidate_topics_batch)
+    return candidate_topics_batch
 
 
 @app.route("/respond", methods=["POST"])
