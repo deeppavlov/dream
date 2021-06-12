d70ef31674e9a3641736b1b32191d471ac1688d0
dimakarp1996 <dimakarp1996@yandex.ru>
2021-06-12 12:21:58 +0300

    Book fixes (#1371)
    * Book fixes. If user liked or disliked propose fav book or genre
    
    * Book fix - techn
    
    * codestyle fixed - tests await
    
    * Several genre books + no upload of unnesessary file + publication year request
    
    * expanded published year template
    
    * expanded published year template
    
    * Update book_utils.py
    
    * Finding author by regexp
    
    * Start phrase fix
    
    * Start phrase fix
    
    * Book multiple fixes
    
    * Book skill fixes with passing tests
    
    * Fixed description bug along with other book bugs
    
    * Multiple book skill fixes
    
    * Book skill fix - with no debug
    
    Co-authored-by: Dmitry <dimakarp1996@github.com>
    Co-authored-by: Дмитрий Карпов <dimakarp19962008@ya.ru>

--- skills/book_skill/test.py
+++ skills/book_skill/test.py
@@ -21,26 +21,26 @@ def main_test():
     url = 'http://0.0.0.0:8032/book_skill'
     input_data = json.load(open("test_configs/test_dialog.json", "r"))
     sliced_data = [slice_(input_data, i) for i in range(1, 20)]
-    gold_phrases = ["OK, let's talk about books. Books are my diamonds. Do you love reading?",
-                    "Why do you enjoy reading?",
+    gold_phrases = ["Books are my diamonds. Do you love reading?",
+                    "I enjoy reading so much! Books help me understand humans much better. Why do you enjoy reading?",
                     "That's great. Outside of a dog, a book is a man's best friend. "
                     "What is the last book you have read?",
                     "You have a great taste in books! "
                     "I also adore books of J R R Tolkien, especially The Hobbit. "
                     "It's a real showpiece. Have you read it?",
-                    "You can read it. You won't regret it!  May I tell you something about this book?",
+                    "May I tell you something about this book?",
                     "The main subject of this book is Tolkien's legendarium. "
                     "The action of this book takes place in Middle-earth. "
                     "Do you know when it was first published?",
-                    " years ago! I didn't exist in that time. Do you know what is the genre of this book?",
+                    "Do you know what is the genre of this book?",
                     "The Hobbit is a fairy tale.  I have read a plenty of books from different genres. "
                     "What book genre do you like?",
-                    "Amazing! Have you read The Outsiders? And if you have read it, what do you think about it?",
+                    "Amazing! Have you read The Outsiders?",
                     "The Outsiders is about two weeks in the life of a 14-year-old boy. "
                     "The novel tells the story of Ponyboy Curtis and his struggles with right and wrong "
                     "in a society in which he believes that he is an outsider. "
                     "Do you want to know what my favourite book is?",
-                    "My favourite book is \"The catcher in the rye\" by J. D. Salinger.  "
+                    "My favourite book is \"The catcher in the rye\" by Jerome David Salinger.  "
                     "May I tell you something about this book?",
                     "The novel \"The catcher in the rye\" tells the story of a teenager "
                     "who has been kicked out of a boarding school."
@@ -49,7 +49,7 @@ def main_test():
                     "The action of this book takes place in New York City. "
                     "One of the main characters of this book is Holden Caulfield. "
                     "Do you know when it was first published?",
-                    " years ago! I didn't exist in that time. Do you know what is the genre of this book?",
+                    "Do you know what is the genre of this book?",
                     "The Catcher in the Rye is a novel",
                     "I know that Bible is one of the most widespread books on the Earth. "
                     "It forms the basic of the Christianity. Have you read the whole Bible?",
