dc09603ad42809c1b45688d2808e999692dca883
Daniel Kornev <daniel@zetuniverse.com>
2021-06-21 14:01:15 +0300

    removed internal checks for Co-Bot & MIDAS intents, replaced with their equivalents from common_utils

--- skills/dff_generic_responses_skill/dialogflows/flows/utils.py
+++ skills/dff_generic_responses_skill/dialogflows/flows/utils.py
@@ -67,16 +67,12 @@ agree_patterns_re = re.compile("(" + "|".join(patterns_agree) + ")", re.IGNORECA
 
 
 def is_speech_function_agree(vars):
-    # fallback to MIDAS
     human_utterance = state_utils.get_last_human_utterance(vars)
     sf_type, sf_confidence = get_speech_function_for_human_utterance(human_utterance)
     flag = sf_type and bool(re.search(agree_patterns_re, sf_type))
-    # fallback to MIDAS
-    flag = flag or is_midas_positive_answer(vars)
     # fallback to yes/no intents
     flag = flag or common_utils.is_yes(human_utterance)
 
-    flag = flag and not is_not_interested_speech_function(vars)
     return flag
 
 
@@ -95,12 +91,9 @@ def is_speech_function_disagree(vars):
     human_utterance = state_utils.get_last_human_utterance(vars)
     sf_type, sf_confidence = get_speech_function_for_human_utterance(human_utterance)
     flag = sf_type and bool(re.search(disagree_patterns_re, sf_type))
-    # fallback to MIDAS
-    flag = flag or is_midas_negative_answer(vars)
     # fallback to yes/no intents
     flag = flag or common_utils.is_no(human_utterance)
 
-    flag = flag and not is_not_interested_speech_function(vars)
     return flag
 
 
@@ -110,26 +103,13 @@ patterns_express_opinion = [
 express_opinion_patterns_re = re.compile("(" + "|".join(patterns_express_opinion) + ")", re.IGNORECASE)
 
 
-def is_cobot_opinion_expressed(vars):
-    intents = common_utils.get_intents(state_utils.get_last_human_utterance(vars), which="all")
-    opinion_expression_detected = "Opinion_ExpressionIntent" in intents
-    return bool(opinion_expression_detected)
-
-
-def is_cobot_opinion_demanded(vars):
-    intents = common_utils.get_intents(state_utils.get_last_human_utterance(vars), which="all")
-    opinion_request_detected = "Opinion_RequestIntent" in intents
-    return bool(opinion_request_detected)
-
-
 def is_speech_function_express_opinion(vars):
     human_utterance = state_utils.get_last_human_utterance(vars)
     sf_type, sf_confidence = get_speech_function_for_human_utterance(human_utterance)
     flag = sf_type and bool(re.search(express_opinion_patterns_re, sf_type))
-    # fallback to MIDAS
-    flag = flag or is_midas_opinion_expression(vars)
+    # fallback to MIDAS & CoBot
+    flag = flag or common_utils.is_opinion_expression(human_utterance)
     # # fallback to CoBot intents
-    flag = flag or is_cobot_opinion_expressed(vars)
     flag = flag or common_utils.is_no(human_utterance)
     # bug check (sometimes opinion by MIDAS can be incorrectly detected in a simple yes/no answer from user)
     flag = flag and not common_utils.is_no(human_utterance) and not common_utils.is_yes(human_utterance)
@@ -146,8 +126,8 @@ def is_speech_function_demand_opinion(vars):
     human_utterance = state_utils.get_last_human_utterance(vars)
     sf_type, sf_confidence = get_speech_function_for_human_utterance(human_utterance)
     flag = sf_type and bool(re.search(demand_opinion_patterns_re, sf_type))
-    # # fallback to CoBot intents
-    flag = flag or is_cobot_opinion_demanded(vars)
+    # # fallback to CoBot & MIDAS intents
+    flag = flag or common_utils.is_opinion_request(human_utterance)
     flag = flag or common_utils.is_no(human_utterance)
     # bug check (sometimes opinion by MIDAS can be incorrectly detected in a simple yes/no answer from user)
     flag = flag and not common_utils.is_no(human_utterance) and not common_utils.is_yes(human_utterance)
@@ -172,69 +152,3 @@ def get_not_used_and_save_generic_response(proposed_sf, vars):
     used_resp = last_responses + [resp]
     state_utils.save_to_shared_memory(vars, last_responses=used_resp[-2:])
     return resp
-
-
-##################################################################################################################
-# more specific intents
-##################################################################################################################
-
-
-patterns_not_interested = [
-    "not interested",
-    "don't care",
-    "move on",
-    "skip",
-    "cancel",
-    "avoid",
-    "not into",
-    "not really into",
-    "no interest for me",
-    "I don't bother",
-    "don't really want to talk about this",
-    "don't feel comfortable discussing this",
-    "We’d better not to enter this subject",
-    "I'd rather not go there right now",
-    "have no interest in discussing that",
-    "What an idiotic topic of conversation",
-    "That subject really bothers me and I don't want to talk about it",
-    "Can we talk about something else",
-    "Must we discuss this",
-    "Can we discuss this later",
-]
-patterns_not_interested_re = re.compile("(" + "|".join(patterns_not_interested) + ")", re.IGNORECASE)
-
-
-def is_not_interested_speech_function(vars):
-    human_text = state_utils.get_last_human_utterance(vars)["text"]
-
-    flag = bool(re.search(patterns_not_interested_re, human_text))
-
-    return flag
-
-
-##################################################################################################################
-# MIDAS
-##################################################################################################################
-
-
-def is_midas_positive_answer(vars):
-    midas_classes = common_utils.get_intents(state_utils.get_last_human_utterance(vars), which="midas")
-
-    intent_detected = any([intent in midas_classes for intent in ["pos_answer"]])
-
-    return intent_detected
-
-
-def is_midas_negative_answer(vars):
-    midas_classes = common_utils.get_intents(state_utils.get_last_human_utterance(vars), which="midas")
-
-    intent_detected = any([intent in midas_classes for intent in ["neg_answer"]])
-
-    return intent_detected
-
-
-def is_midas_opinion_expression(vars):
-    midas_classes = common_utils.get_intents(state_utils.get_last_human_utterance(vars), which="midas")
-    intent_detected = any([intent in midas_classes for intent in ["opinion"]])
-
-    return intent_detected
