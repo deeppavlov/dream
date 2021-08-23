c7e910307f73626c673868820b35e1f4bdceeb6d
dimakarp1996 <dimakarp1996@yandex.ru>
2021-08-23 15:39:00 +0300

    Convers eval replacement with all suggested changes

--- skills/grounding_skill/test.py
+++ skills/grounding_skill/test.py
@@ -10,14 +10,14 @@ def get_input_json(fname):
 
 
 def main_test():
-    url = 'http://0.0.0.0:8080/grounding_skill'
+    url = "http://0.0.0.0:8080/grounding_skill"
     input_data = get_input_json("test_configs/test_dialog.json")
     with open("universal_intent_responses.json", "r") as f:
         UNIVERSAL_INTENT_RESPONSES = json.load(f)
 
     response = requests.post(url, json=input_data)
     reply = "You just told me about star wars, right?"
-    assert reply in response.text.replace('  ', ' '), response.json()
+    assert reply in response.text.replace("  ", " "), response.json()
 
     # check acknowledgement
     new_input_data = deepcopy(input_data)
@@ -27,10 +27,11 @@ def main_test():
     }
     new_input_data["dialogs"][0]["human_utterances"][-1]["annotations"]["entity_detection"] = {
         "entities": ["horses"],
-        "labelled_entities": [{"text": "horses", "label": "misc"}]
+        "labelled_entities": [{"text": "horses", "label": "misc"}],
     }
-    new_input_data["dialogs"][0]["human_utterances"][-1]["annotations"]["midas_classification"] = [{
-        "open_question_opinion": 1.0}]
+    new_input_data["dialogs"][0]["human_utterances"][-1]["annotations"]["midas_classification"] = [
+        {"open_question_opinion": 1.0}
+    ]
     response = requests.post(url, json=new_input_data)
     assert "horses" in response.text, response.json()
 
@@ -41,10 +42,11 @@ def main_test():
     }
     new_input_data["dialogs"][0]["human_utterances"][-1]["annotations"]["entity_detection"] = {
         "entities": ["horses"],
-        "labelled_entities": [{"text": "horses", "label": "misc"}]
+        "labelled_entities": [{"text": "horses", "label": "misc"}],
     }
-    new_input_data["dialogs"][0]["human_utterances"][-1]["annotations"]["midas_classification"] = [{
-        "open_question_factual": 1.0}]
+    new_input_data["dialogs"][0]["human_utterances"][-1]["annotations"]["midas_classification"] = [
+        {"open_question_factual": 1.0}
+    ]
     response = requests.post(url, json=new_input_data)
     assert "what are the horses" in response.text, response.json()
 
@@ -55,10 +57,11 @@ def main_test():
     }
     new_input_data["dialogs"][0]["human_utterances"][-1]["annotations"]["entity_detection"] = {
         "entities": ["horses name"],
-        "labelled_entities": [{"text": "horses", "label": "misc"}]
+        "labelled_entities": [{"text": "horses", "label": "misc"}],
     }
-    new_input_data["dialogs"][0]["human_utterances"][-1]["annotations"]["midas_classification"] = [{
-        "open_question_personal": 1.0}]
+    new_input_data["dialogs"][0]["human_utterances"][-1]["annotations"]["midas_classification"] = [
+        {"open_question_personal": 1.0}
+    ]
     response = requests.post(url, json=new_input_data)
     assert "what is my horses name" in response.text, response.json()
 
@@ -69,10 +72,11 @@ def main_test():
     }
     new_input_data["dialogs"][0]["human_utterances"][-1]["annotations"]["entity_detection"] = {
         "entities": ["horses"],
-        "labelled_entities": [{"text": "horses", "label": "misc"}]
+        "labelled_entities": [{"text": "horses", "label": "misc"}],
     }
-    new_input_data["dialogs"][0]["human_utterances"][-1]["annotations"]["midas_classification"] = [{
-        "yes_no_question": 1.0}]
+    new_input_data["dialogs"][0]["human_utterances"][-1]["annotations"]["midas_classification"] = [
+        {"yes_no_question": 1.0}
+    ]
     response = requests.post(url, json=new_input_data)
     assert "whether I know about horses".lower() in response.text.lower(), response.json()
 
@@ -80,7 +84,8 @@ def main_test():
     new_input_data = deepcopy(input_data)
     new_input_data["dialogs"][0]["human_utterances"][-1]["annotations"]["midas_classification"] = [{"opinion": 1.0}]
     new_input_data["dialogs"][0]["human_utterances"][-1]["annotations"]["cobot_dialogact"] = {
-        "intents": ["General_ChatIntent"], "topics": ["Other"]
+        "intents": ["General_ChatIntent"],
+        "topics": ["Other"],
     }
     response = requests.post(url, json=new_input_data)
     assert any([resp in response.text for resp in UNIVERSAL_INTENT_RESPONSES["opinion"]]), response.json()
@@ -88,5 +93,5 @@ def main_test():
     print("Success!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main_test()
