5ed6bf66b7cd06e0c919cad68696d96108e50a09
Denis Kuznetosv <kuznetsov.den.p@gmail.com>
2021-09-03 14:07:52 +0300

    feat/dff/template: upd template

--- skills/dff_template_skill/scenario/condition.py
+++ skills/dff_template_skill/scenario/condition.py
@@ -1,12 +1,15 @@
 import logging
-# import re
 
-# from dff.core import Context, Actor
+from dff.core import Context, Actor
 
+from common.dff.integration import condition as int_cnd
 
 logger = logging.getLogger(__name__)
 # ....
 
 
-# def next(ctx: Context, actor: Actor, *args, **kwargs) -> bool:
-#     return bool(re.findall("go next", ctx.last_request, re.IGNORECASE))
+def example_lets_talk_about():
+    def example_lets_talk_about_handler(ctx: Context, actor: Actor, *args, **kwargs) -> str:
+        return int_cnd.is_lets_chat_about_topic_human_initiative(ctx, actor)
+
+    return example_lets_talk_about_handler
