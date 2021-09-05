8e87453bc2be3ab5c9eb92839870932ef78dd5d1
Arsenaut <senya.ds.researcher@gmail.com>
2021-09-05 12:51:49 +0300

    device id better validation

--- annotators/COMeT/comet_commonsense/config.py
+++ annotators/COMeT/comet_commonsense/config.py
@@ -6,21 +6,26 @@ PRETRAINED_MODEL_PATH_FOLDER = ["comet_commonsense", "pretrained_models"]
 
 
 class AppConfig(BaseSettings):
-    SERVICE_NAME: str = 'atomic'
-    SERVICE_PORT: int = 8053
-    SENTRY_DSN: Optional[str] = None
-    RANDOM_SEED: Optional[int] = 2718  # Magic number!
+    SERVICE_NAME: str
+    SERVICE_PORT: int
+    SENTRY_DSN: Optional[str]
 
-    GRAPH: str = 'atomic'
-    PRETRAINED_MODEL: str = 'atomic_pretrained_model.pickle'
-    DECODING_ALGO: str = 'beam-3'
+    GRAPH: str
+    PRETRAINED_MODEL: str
+    DECODING_ALGO: str
 
-    CUDA_VISIBLE_DEVICES: Union[int, str] = "cpu"
+    CUDA_VISIBLE_DEVICES: Union[int, str]
 
     @validator("PRETRAINED_MODEL")
     def create_full_model_path(cls, pretrained_model_pkl):
         model_file = os.path.split(pretrained_model_pkl)[1]
         return os.path.join(*PRETRAINED_MODEL_PATH_FOLDER, model_file)
+
+    @validator("CUDA_VISIBLE_DEVICES")
+    def device_validator(cls, device):
+        if isinstance(device, int):
+            return device
+        return "cpu"
 
 
 settings = AppConfig()
