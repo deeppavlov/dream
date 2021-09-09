e672ce843f2bbc88b88b4109932a1cae637e3e76
matiiv <matiiv.r.a@gmail.com>
2021-09-09 07:57:51 +0300

    change class_name in config for user page_preprocessor.py from /common dir

--- annotators/fact_retrieval/page_preprocessor.py
+++
@@ -1,6 +0,0 @@
-# imported class will use in other place via configs
-# page_extractor.json
-# whow_page_extractor.json
-# use for exclude duplication page_preprocessor.py
-
-from common.page_preprocessor import *
