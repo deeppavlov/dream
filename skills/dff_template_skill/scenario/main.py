4c1e1c6ee408cf72c9291420c21f2474c4c99390
Denis Kuznetosv <kuznetsov.den.p@gmail.com>
2021-09-10 15:44:33 +0300

    feat/dff/template_2: fix style

--- skills/dff_template_skill/scenario/main.py
+++ skills/dff_template_skill/scenario/main.py
@@ -18,21 +18,21 @@ flows = {
             "start_node": {
                 RESPONSE: "",
                 TRANSITIONS: {
-                    "choose_story_node": cnd.all([loc_cnd.is_tell_me_a_story, loc_rsp.get_story_type, loc_rsp.get_story_left]),
+                    "choose_story_node": cnd.all(
+                        [loc_cnd.is_tell_me_a_story, loc_rsp.get_story_type, loc_rsp.get_story_left]
+                    ),
                     "which_story_node": cnd.all([loc_cnd.is_tell_me_a_story, cnd.neg(loc_rsp.get_story_type)]),
                     "fallback_node": cnd.true,
                 },
             },
-
             "choose_story_node": {
                 RESPONSE: loc_rsp.choose_story_response,
-                TRANSITIONS: {     
+                TRANSITIONS: {
                     "tell_punchline_node": cnd.any([int_cnd.is_yes_vars, int_cnd.is_do_not_know_vars]),
                     "which_story_node": int_cnd.is_no_vars,
                     "fallback_node": cnd.true,
                 },
             },
-
             "which_story_node": {
                 RESPONSE: loc_rsp.which_story_response,
                 TRANSITIONS: {
@@ -40,19 +40,18 @@ flows = {
                     "fallback_node": cnd.true,
                 },
             },
-            
             "tell_punchline_node": {
                 RESPONSE: loc_rsp.tell_punchline_response,
                 TRANSITIONS: {
                     "fallback_node": cnd.true,
-                    },
+                },
             },
             "fallback_node": {
                 RESPONSE: loc_rsp.fallback_response,
-                TRANSITIONS:{
+                TRANSITIONS: {
                     "which_story_node": cnd.all([loc_cnd.is_asked_for_a_story, int_cnd.is_yes_vars]),
                     trn.repeat(): cnd.true,
-                }
+                },
             },
         }
     }
--- skills/dff_template_skill/scenario/response.py
+++ skills/dff_template_skill/scenario/response.py
@@ -1,4 +1,7 @@
-import logging, json, re, random
+import logging
+import json
+import re
+import random
 
 from dff.core import Context, Actor
 
@@ -7,20 +10,20 @@ import common.dff.integration.condition as int_cnd
 
 logger = logging.getLogger(__name__)
 
-with open( "data/stories.json",
-    ) as stories_json:
-        stories = json.load(stories_json)
+with open("data/stories.json") as stories_json:
+    stories = json.load(stories_json)
 
-with open( "data/phrases.json",
-    ) as phrases_json:
-        phrases = json.load(phrases_json)
- 
-def get_previous_node(ctx:Context) -> str:
+with open("data/phrases.json") as phrases_json:
+    phrases = json.load(phrases_json)
+
+
+def get_previous_node(ctx: Context) -> str:
     try:
         return [node_tuple[1] for node_tuple in ctx.node_labels.values()][-2]
-    except:
+    except Exception:
         return "start_node"
-    
+
+
 def get_story_type(ctx: Context, actor: Actor) -> str:
     human_sentence = ctx.last_request
     if re.search("fun((ny)|(niest)){0,1}", human_sentence):
@@ -28,61 +31,66 @@ def get_story_type(ctx: Context, actor: Actor) -> str:
     elif re.search("(horror)|(scary)|(frightening)|(spooky)", human_sentence):
         return "scary"
     elif re.search("(bedtime)|(good)|(kind)|(baby)|(children)|(good night)|(for kid(s){0,1})", human_sentence):
-        return "bedtime"    
+        return "bedtime"
     else:
         return ""
- 
+
+
 def get_story_left(ctx: Context, actor: Actor) -> str:
     story_type = get_story_type(ctx, actor)
-    stories_left = list(set(stories.get(story_type, []))-set(ctx.misc.get("stories_told",[])))
+    stories_left = list(set(stories.get(story_type, [])) - set(ctx.misc.get("stories_told", [])))
     try:
         return random.choice(stories_left)
-    except:
+    except Exception:
         return ""
+
 
 def choose_story_response(ctx: Context, actor: Actor, *args, **kwargs) -> str:
     prev_node = get_previous_node(ctx)
-    story = get_story_left(ctx,actor)
+    story = get_story_left(ctx, actor)
     story_type = get_story_type(ctx, actor)
     setup = stories.get(story_type, {}).get(story, {}).get("setup", "")
-    what_happend_next_phrase = random.choice(phrases.get("what_happend_next",[]))
+    what_happend_next_phrase = random.choice(phrases.get("what_happend_next", []))
 
-    # include sure if user defined a type of story at the beginnig, otherwise include nothing 
-    sure_phrase = random.choice(phrases.get("sure",[])) if prev_node == 'start_node' else ""
-    
-    ctx.misc["stories_told"] = ctx.misc.get("stories_told",[]) + [story]
+    # include sure if user defined a type of story at the beginnig, otherwise include nothing
+    sure_phrase = random.choice(phrases.get("sure", [])) if prev_node == "start_node" else ""
+
+    ctx.misc["stories_told"] = ctx.misc.get("stories_told", []) + [story]
     ctx.misc["story"] = story
     ctx.misc["story_type"] = story_type
 
-    return sure_phrase +setup+ "..."+ what_happend_next_phrase
+    return sure_phrase + setup + "..." + what_happend_next_phrase
+
 
 def which_story_response(ctx: Context, actor: Actor, *args, **kwargs) -> str:
 
     prev_node = get_previous_node(ctx)
-    if prev_node in ["start_node","fallback_node"]:
+    if prev_node in ["start_node", "fallback_node"]:
         int_ctx.set_can_continue(ctx, actor, "MUST_CONTINUE")
 
         # include sure if user asked to tell a story, include nothing if agent proposed to tell a story
-        sure_phrase = random.choice(phrases.get("sure",[])) if prev_node == "start_node" else ""
-        return sure_phrase + random.choice(phrases.get("which_story",[]))
+        sure_phrase = random.choice(phrases.get("sure", [])) if prev_node == "start_node" else ""
+        return sure_phrase + random.choice(phrases.get("which_story", []))
     elif prev_node == "choose_story_node":
         int_ctx.set_can_continue(ctx, actor, "CANNOT_CONTINUE")
-        return random.choice(phrases.get("no",[]))
+        return random.choice(phrases.get("no", []))
     else:
         return "Ooops."
 
+
 def tell_punchline_response(ctx: Context, actor: Actor, *args, **kwargs) -> str:
     int_ctx.set_can_continue(ctx, actor, "CAN_CONTINUE")
-    int_ctx.set_confidence(ctx, actor, 0.8) if int_cnd.is_do_not_know_vars(ctx,actor) else None
+    int_ctx.set_confidence(ctx, actor, 0.8) if int_cnd.is_do_not_know_vars(ctx, actor) else None
     stories = ctx.misc.get("stories", {})
     story = ctx.misc.get("story", "")
     story_type = ctx.misc.get("story_type", "")
-    
+
     try:
         return stories[story_type][story]["punchline"]
 
-    except:
+    except Exception:
         return "Oh, sorry, i lost the track of what I was talking about."
+
 
 def fallback_response(ctx: Context, actor: Actor, *args, **kwargs) -> str:
     prev_node = get_previous_node(ctx)
@@ -93,14 +101,14 @@ def fallback_response(ctx: Context, actor: Actor, *args, **kwargs) -> str:
     if prev_node == "which_story_node" and story_type and not story_left:
         int_ctx.set_can_continue(ctx, actor, "CANNOT_CONTINUE")
         return "Oh, sorry, but I've run out of stories."
-        
+
     # no stories
     elif prev_node == "which_story_node" and not story_type:
         int_ctx.set_can_continue(ctx, actor, "CAN_CONTINUE")
-        return random.choice(phrases.get("no_stories",[]))
+        return random.choice(phrases.get("no_stories", []))
 
     # if prev_node is tell_punchline_node or fallback_node
     else:
         int_ctx.set_can_continue(ctx, actor, "MUST_CONTINUE")
-        int_ctx.set_confidence(ctx, actor, 0.5) if int_cnd.is_do_not_know_vars(ctx,actor) else None
-        return random.choice(phrases.get("start_phrases",[]))
+        int_ctx.set_confidence(ctx, actor, 0.5) if int_cnd.is_do_not_know_vars(ctx, actor) else None
+        return random.choice(phrases.get("start_phrases", []))
