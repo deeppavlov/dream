a9eb925e24110ec3c002812fc97d13bfd7014873
Denis Kuznetosv <kuznetsov.den.p@gmail.com>
2021-09-10 15:41:34 +0300

    feat/dff/template_2: fix style

--- skills/dff_template_skill/scenario/condition.py
+++ skills/dff_template_skill/scenario/condition.py
@@ -4,14 +4,17 @@ from dff.core import Context, Actor
 
 logger = logging.getLogger(__name__)
 
-def get_previous_node(ctx:Context) -> str:
+
+def get_previous_node(ctx: Context) -> str:
     try:
         return [node_tuple[1] for node_tuple in ctx.node_labels.values()][-2]
-    except:
+    except Exception:
         return "start_node"
 
+
 def is_tell_me_a_story(ctx: Context, actor: Actor, *args, **kwargs) -> bool:
-    return ctx.misc.get("intents","") == "tell_me_a_story"
+    return ctx.misc.get("intents", "") == "tell_me_a_story"
+
 
 def is_asked_for_a_story(ctx: Context, actor: Actor, *args, **kwargs) -> bool:
     prev_node = get_previous_node(ctx)
