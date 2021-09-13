a50ae25a1b2b0adcfca713ad5faf6e0cc0a9d5a7
kpetyxova <kpetyxova@mail.ru>
2021-09-13 16:42:43 +0300

    added ASR

--- deepy/services/speech/thirdparty/nemo/main.py
+++ deepy/services/speech/thirdparty/nemo/main.py
@@ -1,32 +1,56 @@
 import re
+import shutil
+from logging import getLogger
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+
+import nemo.collections.asr as nemo_asr
 
 import requests
-from deeppavlov import build_model, configs
+
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import StreamingResponse, JSONResponse
+from fastapi.responses import JSONResponse
 from num2words import num2words
 
 app = FastAPI()
-asr = build_model("asr.json")
-tts = build_model("tts.json")
+logger = getLogger(__file__)
+quartznet = nemo_asr.models.EncDecCTCModel.from_pretrained(model_name="QuartzNet15x5Base-En")
+
+
+def save_upload_file(upload_file: UploadFile, destination: Path) -> None:
+    try:
+        with destination.open("wb") as buffer:
+            shutil.copyfileobj(upload_file.file, buffer)
+    finally:
+        upload_file.file.close()
+
+
+def save_upload_file_tmp(upload_file: UploadFile) -> Path:
+    try:
+        suffix = Path(upload_file.filename).suffix
+        with NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
+            shutil.copyfileobj(upload_file.file, tmp)
+            tmp_path = Path(tmp.name)
+    finally:
+        upload_file.file.close()
+    return tmp_path
+
 
 
 @app.post("/asr")
 async def infer_asr(user_id: str, file: UploadFile = File(...)):
-    transcript = asr([file.file])[0]
-    print(f'transcription is "{transcript}"')
+    tmp_path = save_upload_file_tmp(file)
+    try:
+        transcript = quartznet.transcribe(paths2audio_files=[str(tmp_path)])[0]
+    finally:
+        tmp_path.unlink()
+    logger.info(f'transcription is "{transcript}"')
     post_response = requests.post("http://agent:4242", json={"user_id": user_id, "payload": transcript})
     response_payload = post_response.json()
     response = response_payload["response"]
-    print(f'response is "{response}"')
+    logger.info(f'response is "{response}"')
     response = re.sub(r"([0-9]+)", lambda x: num2words(x.group(0)), response)
     response_payload["response"] = response
     return JSONResponse(content=response_payload, headers={"transcript": transcript})
 
 
-@app.post("/tts")
-async def infer_tts(text: str):
-    response = re.sub(r"([0-9]+)", lambda x: num2words(x.group(0)), text)
-    print(f'response is "{response}"')
-    audio_response = tts([response])[0]
-    return StreamingResponse(audio_response, media_type="audio/x-wav")
