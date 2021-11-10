d94673184bff8897ce0e7b3e30d9acd38aa7fd83
Dmitry <dimakarp1996@github.com>
2021-11-10 13:31:07 +0300

    PAL-BERT with passed tests

--- annotators/combined_classification/test.py
+++ annotators/combined_classification/test.py
@@ -34,9 +34,11 @@ def main_test():
         {"sentences": ["let's switch topic"], "task": "cobot_dialogact_intents", "answers": [["Topic_SwitchIntent"]]}
     ]
     for config in configs:
+        config['utterances_with_histories'] = [[k] for k in config['sentences']]
         responses = requests.post(url, json=config).json()
         batch_responses = requests.post(batch_url, json=config).json()
-        assert batch_responses[0]["batch"] == responses, "Batch responses not match to responses"
+        assert batch_responses[0]["batch"] == responses, f"Batch responses {batch_responses} " \
+                                                         f"not match to responses {responses}"
         responses = [j[config["task"]] for j in responses]
         for response, answer, sentence in zip(responses, config["answers"], config["sentences"]):
             predicted_classes = [class_ for class_ in response if response[class_] > 0.5]
