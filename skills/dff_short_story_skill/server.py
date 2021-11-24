cd12e9ef504650d829e9e4b8ad971f96740e231e
dilyararimovna <dilyara.rimovna@gmail.com>
2021-11-24 13:15:47 +0300

    fix: tests

--- skills/dff_short_story_skill/server.py
+++ skills/dff_short_story_skill/server.py
@@ -56,6 +56,19 @@ def handler(requested_data, random_seed=None):
     return responses
 
 
+# import pathlib
+# import json
+#
+# for in_file in pathlib.Path("tests").glob("./*_in.json"):
+#     logger.error(in_file)
+#     logger.info("Creating tests")
+#     test_in = json.load(in_file.open())
+#     responses = handler(test_in, RANDOM_SEED)
+#     out_file = str(in_file).replace("in.json", "out.json")
+#     import common.test_utils as t_utils
+#
+#     t_utils.save_to_test(responses, out_file, indent=4)  # TEST
+
 try:
     test_server.run_test(handler)
     logger.info("test query processed")
@@ -65,18 +78,6 @@ except Exception as exc:
     raise exc
 
 logger.info(f"{SERVICE_NAME} is loaded and ready")
-
-# import pathlib
-# import json
-
-# for in_file in pathlib.Path("tests").glob("./*_in.json"):
-#     logger.error(in_file)
-#     test_in = json.load(in_file.open())
-#     responses = handler(test_in, RANDOM_SEED)
-#     out_file = str(in_file).replace("in.json", "out.json")
-#     import common.test_utils as t_utils
-
-#     t_utils.save_to_test(responses, out_file, indent=4)  # TEST
 
 
 @app.route("/respond", methods=["POST"])
