dd4e108d9ea211d31a463fa658b739e59a76a146
dilyararimovna <dilyara.rimovna@gmail.com>
2021-11-29 13:21:09 +0300

    feat: turn on tests

--- skills/dff_grounding_skill/server.py
+++ skills/dff_grounding_skill/server.py
@@ -15,7 +15,7 @@ from common.dff.integration.actor import load_ctxs, get_response
 
 from scenario.main import actor
 
-# import test_server
+import test_server
 
 
 ignore_logger("root")
@@ -58,7 +58,7 @@ def handler(requested_data, random_seed=None):
 
 
 try:
-    # test_server.run_test(handler)
+    test_server.run_test(handler)
     logger.info("test query processed")
 except Exception as exc:
     sentry_sdk.capture_exception(exc)
@@ -67,17 +67,17 @@ except Exception as exc:
 
 logger.info(f"{SERVICE_NAME} is loaded and ready")
 
-import pathlib
-import json
-
-for in_file in pathlib.Path("tests").glob("./*_in.json"):
-    logger.error(in_file)
-    test_in = json.load(in_file.open())
-    responses = handler(test_in, RANDOM_SEED)
-    out_file = str(in_file).replace("in.json", "out.json")
-    import common.test_utils as t_utils
-
-    t_utils.save_to_test(responses, out_file, indent=4)  # TEST
+# import pathlib
+# import json
+#
+# for in_file in pathlib.Path("tests").glob("./*_in.json"):
+#     logger.error(in_file)
+#     test_in = json.load(in_file.open())
+#     responses = handler(test_in, RANDOM_SEED)
+#     out_file = str(in_file).replace("in.json", "out.json")
+#     import common.test_utils as t_utils
+#
+#     t_utils.save_to_test(responses, out_file, indent=4)  # TEST
 
 
 @app.route("/respond", methods=["POST"])
