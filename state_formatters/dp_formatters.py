119c92b394d4f3d9ca3ee5ef84117ee29719519c
dilyararimovna <dilyara.rimovna@gmail.com>
2021-11-30 11:42:32 +0300

    feat: program_y_dangerous in ymls

--- state_formatters/dp_formatters.py
+++ state_formatters/dp_formatters.py
@@ -770,6 +770,10 @@ def dff_wiki_skill_formatter(dialog: Dict) -> List[Dict]:
     )
 
 
+def dff_program_y_dangerous_formatter(dialog: Dict) -> List[Dict]:
+    return utils.dff_formatter(dialog, "dff_program_y_dangerous")
+
+
 def hypotheses_list_for_dialog_breakdown(dialog: Dict) -> List[Dict]:
     # Used by: dialog_breakdown
     dialog = utils.get_last_n_turns(dialog, bot_last_turns=2)
