9fdee7897e39ca1241b64f5c1cf65e43b6f67f58
Denis Kuznetosv <kuznetsov.den.p@gmail.com>
2021-12-04 14:18:06 +0300

    fix/style: fix style

--- annotators/spacy_nounphrases/test.py
+++ annotators/spacy_nounphrases/test.py
@@ -3,14 +3,9 @@ import requests
 
 def main():
     url = "http://0.0.0.0:8006/respond"
-    input_data = {
-        "sentences": [
-            "i like michal jordan",
-            "hey this is a white bear"
-        ]
-    }
+    input_data = {"sentences": ["i like michal jordan", "hey this is a white bear"]}
     result = requests.post(url, json=input_data)
-    assert result.json() == [['michal jordan'], ['a white bear']]
+    assert result.json() == [["michal jordan"], ["a white bear"]]
     print("Success!")
 
 
