a8a8445b8d3065cfea709b62dc4609852161c54f
dilyararimovna <dilyara.rimovna@gmail.com>
2021-12-21 17:58:16 +0300

    fix: revert post_annotator

---
+++ skill_selectors/post_annotator_selector/connector.py
@@ -0,0 +1,18 @@
+from typing import Dict, Callable
+
+
+class PostAnnotatorSelectorConnector:
+    def __init__(self, annotator_names):
+        self.annotator_names = set(annotator_names)
+
+    async def send(self, payload: Dict, callback: Callable):
+        try:
+            if payload["payload"]["bot_utterances"]:
+                existing_annotators = set(payload["payload"]["bot_utterances"][-1]["annotations"].keys())
+                response = list(self.annotator_names - existing_annotators)
+            else:
+                response = []
+            await callback(task_id=payload["task_id"], response=response)
+        except Exception as e:
+            response = e
+            await callback(task_id=payload["task_id"], response=response)
