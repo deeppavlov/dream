d19c44bb7f2490ec9eaf7989f5fd0fc0c45587c7
dilyararimovna <dilyara.rimovna@gmail.com>
2022-01-25 18:14:59 +0300

    fix: variable name

--- skills/dff_program_y/scenario/response.py
+++ skills/dff_program_y/scenario/response.py
@@ -8,11 +8,11 @@ from common.programy.model import get_programy_model
 logger = logging.getLogger(__name__)
 
 LANGUAGE = os.environ.get("LANGUAGE")
-DATA_FODLER = "data_ru" if LANGUAGE == "RUSSIAN" else "data"
+DATA_FOLDER = "data_ru" if LANGUAGE == "RUSSIAN" else "data"
 
 try:
     logger.info("Start to load model")
-    model = get_programy_model(DATA_FODLER)
+    model = get_programy_model(DATA_FOLDER)
     logger.info("Load model")
 except Exception as e:
     logger.exception(e)
