386b30debc0f685e4d11e3c34db4fef3111eac0f
dilyararimovna <dilyara.rimovna@gmail.com>
2022-01-25 18:18:36 +0300

    fix: russian tests

--- skills/dff_program_y/test_server.py
+++ skills/dff_program_y/test_server.py
@@ -7,6 +7,7 @@ import common.test_utils as test_utils
 SERVICE_PORT = int(os.getenv("SERVICE_PORT"))
 RANDOM_SEED = int(os.getenv("RANDOM_SEED", 2718))
 URL = f"http://0.0.0.0:{SERVICE_PORT}/respond"
+LANGUAGE = os.getenv("LANGUAGE")
 
 
 def handler(requested_data, random_seed):
@@ -17,6 +18,8 @@ def handler(requested_data, random_seed):
 def run_test(handler):
     in_data, out_data = test_utils.get_dataset()
     for test_name in in_data:
+        if LANGUAGE == "RUSSIAN" and "RU" not in test_name:
+            continue
         hypothesis = handler(in_data[test_name], RANDOM_SEED)
         print(f"test name: {test_name}")
         is_equal_flag, msg = test_utils.compare_structs(out_data[test_name], hypothesis, ignored_keys=["id"])
