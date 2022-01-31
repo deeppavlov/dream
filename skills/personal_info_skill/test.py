1ef332fd14dd2e5424fa4bb0284c8c2281e26251
dilyararimovna <dilyara.rimovna@gmail.com>
2022-01-31 16:15:44 +0300

    feat: add test files

---
+++ skills/personal_info_skill/test.py
@@ -0,0 +1,28 @@
+import requests
+
+
+NEWS_API_SKILL_URL = "http://0.0.0.0:8066/respond"
+
+topic = "sport"
+dialogs = {
+    "dialogs": [
+        {
+            "utterances": [],
+            "bot_utterances": [],
+            "human": {"attributes": {}},
+            "human_utterances": [
+                {
+                    "text": f"news about {topic}",
+                    "annotations": {"ner": [[{"text": topic}]], "cobot_topics": {"text": ["News"]}},
+                }
+            ],
+        }
+    ]
+}
+
+result = requests.post(NEWS_API_SKILL_URL, json=dialogs, timeout=1.5)
+result = result.json()
+
+assert result[0][1] == 1.0 and result[0][-1]["news_topic"] == "sport", print(result)
+
+print("SUCCESS")
