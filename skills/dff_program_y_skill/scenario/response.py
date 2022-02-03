cbe27235e5d89c4d04b38c7df27154005a0e0f82
Dilyara Baymurzina <dilyara.rimovna@gmail.com>
2022-02-03 16:05:48 +0300

    Feat/ru program y (#88)
    * feat: ru program-y version
    
    * fix: variable name
    
    * fix: russian tests
    
    * feat: test files
    
    * fix: dff program-y skill for russian
    
    * fix: dff program-y skill for russian docker compose
    
    * fix: dff program-y skip eng tests
    
    * fix: logs
    
    * fix: add variable env
    
    * fix: revert dangerous skil
    
    * fix: type
    
    * fix: imports types
    
    * fix: line buffering
    
    * fix: default value
    
    * fix: tests
    
    * fix: program-y patterns

--- skills/dff_program_y_skill/scenario/response.py
+++ skills/dff_program_y_skill/scenario/response.py
@@ -1,14 +1,19 @@
 import logging
+import os
+import pathlib
 
 from df_engine.core import Context, Actor
 
 from common.programy.model import get_programy_model
 
 logger = logging.getLogger(__name__)
+LANGUAGE = os.getenv("LANGUAGE", "ENGLISH")
+model_folder = "data_ru" if LANGUAGE == "RUSSIAN" else "data"
+logger.info(f"Selected dff-program-y-skill: {LANGUAGE} language.")
 
 try:
     logger.info("Start to load model")
-    model = get_programy_model("data")
+    model = get_programy_model(pathlib.Path(model_folder))
     logger.info("Load model")
 except Exception as e:
     logger.exception(e)
