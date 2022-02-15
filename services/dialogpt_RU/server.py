2f3159dd49468b93917ef86ecfa95762e77d67c4
dilyararimovna <dilyara.rimovna@gmail.com>
2022-02-15 16:25:34 +0300

    feat: basic integration of dialogpt_RU

--- services/dialogpt_RU/server.py
+++ services/dialogpt_RU/server.py
@@ -1,8 +1,12 @@
+"""
+Source code is https://github.com/Grossmend/DialoGPT/blob/master/src/service/service.py
+"""
 import logging
 import time
 import os
+from typing import Dict, List
 
-from transformers import BertTokenizer, BertForMaskedLM
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 from flask import Flask, request, jsonify
 from healthcheck import HealthCheck
@@ -15,27 +19,113 @@ sentry_sdk.init(dsn=os.getenv("SENTRY_DSN"), integrations=[FlaskIntegration()])
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-PRETRAINED_MODEL_NAME_OR_PATH = os.environ.get("PRETRAINED_MODEL_NAME_OR_PATH")
-logging.info(f"PRETRAINED_MODEL_NAME_OR_PATH = {PRETRAINED_MODEL_NAME_OR_PATH}")
-MASK_ID = 103
+PRETRAINED_MODEL_NAME_OR_PATH = os.environ.get("PRETRAINED_MODEL_NAME_OR_PATH",
+                                               "Grossmend/rudialogpt3_medium_based_on_gpt2")
+logger.info(f"PRETRAINED_MODEL_NAME_OR_PATH = {PRETRAINED_MODEL_NAME_OR_PATH}")
+
+cuda = torch.cuda.is_available()
+if cuda:
+    torch.cuda.set_device(0)
+    device = "cuda"
+else:
+    device = "cpu"
+
+logger.info(f"dialogpt-ru is set to run on {device}")
+
+params_default = {
+    'max_length': 256,
+    'no_repeat_ngram_size': 3,
+    'do_sample': True,
+    'top_k': 100,
+    'top_p': 0.9,
+    'temperature': 0.6,
+    'num_return_sequences': 3,
+    'device': device,
+    'is_always_use_length': True,
+    'length_generate': '1',
+}
+
+
+class RussianDialogGPT:
+
+    def __init__(self, path_model: str):
+        self.path_model = path_model
+        self.tokenizer = None
+        self.model = None
+        self._load_model()
+
+    def _load_model(self):
+        logger.info(f"dialogpt-ru Loading model: {self.path_model} ...")
+        self.tokenizer = AutoTokenizer.from_pretrained(self.path_model)
+        self.model = AutoModelForCausalLM.from_pretrained(self.path_model)
+
+    def get_responses(self, inputs: List[Dict], params: Dict) -> str:
+
+        params_ = {
+            'max_length': params.get('max_length', params_default['max_length']),
+            'no_repeat_ngram_size': params.get('no_repeat_ngram_size', params_default['no_repeat_ngram_size']),
+            'do_sample': params.get('do_sample', params_default['do_sample']),
+            'top_k': params.get('top_k', params_default['top_k']),
+            'top_p': params.get('top_p', params_default['top_p']),
+            'temperature': params.get('temperature', params_default['temperature']),
+            'num_return_sequences': params.get('num_return_sequences', params_default['num_return_sequences']),
+            'device': params.get('device', params_default['device']),
+            "is_always_use_length": params.get('is_always_use_length', params_default['is_always_use_length']),
+            "length_generate": params.get('length_generate', params_default['length_generate']),
+        }
+
+        inputs_text = ''
+        for input_ in inputs:
+            if params_['is_always_use_length']:
+                length_rep = len(self.tokenizer.encode(input_['text']))
+                if length_rep <= 15:
+                    length_param = '1'
+                elif length_rep <= 50:
+                    length_param = '2'
+                elif length_rep <= 256:
+                    length_param = '3'
+                else:
+                    length_param = '-'
+            else:
+                length_param = '-'
+            inputs_text += f"|{input_['speaker']}|{length_param}|{input_['text']}"
+        inputs_text += f"|1|{params_['length_generate']}|"
+
+        inputs_token_ids = self.tokenizer.encode(inputs_text, return_tensors='pt')
+
+        try:
+            outputs_token_ids = self.model.generate(
+                inputs_token_ids,
+                max_length=params_['max_length'],
+                no_repeat_ngram_size=params_['no_repeat_ngram_size'],
+                do_sample=params_['do_sample'],
+                top_k=params_['top_k'],
+                top_p=params_['top_p'],
+                temperature=params_['temperature'],
+                num_return_sequences=params_['num_return_sequences'],
+                device=params_['device'],
+                mask_token_id=self.tokenizer.mask_token_id,
+                eos_token_id=self.tokenizer.eos_token_id,
+                unk_token_id=self.tokenizer.unk_token_id,
+                pad_token_id=self.tokenizer.pad_token_id,
+            )
+        except Exception as e:
+            logger.info(f"dialogpt-ru Error generate: {str(e)}")
+            return ""
+
+        outputs = [self.tokenizer.decode(x, skip_special_tokens=True) for x in outputs_token_ids]
+        outputs = [x.split('|')[-1] for x in outputs]
+
+        return outputs
+
+
 try:
-    cuda = torch.cuda.is_available()
+    model = RussianDialogGPT(PRETRAINED_MODEL_NAME_OR_PATH)
+    model.model.eval()
     if cuda:
-        torch.cuda.set_device(0)  # singe gpu
-        device = torch.device("cuda")
-    else:
-        device = torch.device("cpu")
+        model.model.cuda()
 
-    logger.info(f"masked_lm is set to run on {device}")
-
-    # init model
-    tokenizer = BertTokenizer.from_pretrained(PRETRAINED_MODEL_NAME_OR_PATH)
-    model = BertForMaskedLM.from_pretrained(PRETRAINED_MODEL_NAME_OR_PATH)
-    model.eval()
-    if cuda:
-        model.cuda()
-
-    logger.info("masked_lm model is ready")
+    logger.info("dialogpt-ru model is ready")
 except Exception as e:
     sentry_sdk.capture_exception(e)
     logger.exception(e)
@@ -50,28 +140,23 @@ logging.getLogger("werkzeug").setLevel("WARNING")
 def respond():
     st_time = time.time()
 
-    text = request.json.get("text", [])
+    dialog_contexts = request.json.get("dialog_contexts", [])
     try:
-        inputs = tokenizer(text, return_tensors="pt", padding=True)
-        inputs = {k: v.cuda() for k, v in inputs.items()} if cuda else inputs
-        logits = model(**inputs).logits.cpu()
-        probs = torch.nn.functional.softmax(logits, dim=2)
+        batch_generated_responses = []
+        for context in dialog_contexts:
+            # context is a list of dicts, each dict contains text and speaker label
+            # context = [{"text": "utterance text", "speaker": "human"}, ...]
+            inputs = [{"text": uttr["text"], "speaker": 1 if uttr["speaker"] == "bot" else 0} for uttr in context][-3:]
+            result = model.get_responses(inputs, params={})
+            logger.info(f"dialogpt-ru result: {result}")
+            batch_generated_responses += result
 
-        batch_predicted_tokens = []
-        for batch_i in range(probs.shape[0]):
-            masked_tokens = probs[batch_i][inputs["input_ids"][batch_i] == MASK_ID]
-            predicted_tokens = []
-            for token_id in range(masked_tokens.shape[0]):
-                token_probs, token_ids = masked_tokens[token_id].topk(10)
-                token_probs = token_probs.tolist()
-                token_ids = [tokenizer.decode([id]) for id in token_ids.tolist()]
-                predicted_tokens.append({token: prob for token, prob in zip(token_ids, token_probs)})
-            batch_predicted_tokens.append(predicted_tokens)
     except Exception as exc:
         logger.exception(exc)
         sentry_sdk.capture_exception(exc)
-        batch_predicted_tokens = [[]] * len(text)
+        batch_generated_responses = [""] * len(dialog_contexts)
 
     total_time = time.time() - st_time
-    logger.info(f"masked_lm exec time: {total_time:.3f}s")
-    return jsonify({"predicted_tokens": batch_predicted_tokens})
+    logger.info(f"dialogpt-ru exec time: {total_time:.3f}s")
+
+    return jsonify({"generated_responses": batch_generated_responses})
