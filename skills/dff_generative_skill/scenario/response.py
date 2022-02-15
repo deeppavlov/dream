f0ccb5472c23daeb71321fddd53dbfe3b3a81863
dilyararimovna <dilyara.rimovna@gmail.com>
2022-02-15 19:37:40 +0300

    fix: input to dialogpt

--- skills/dff_generative_skill/scenario/response.py
+++ skills/dff_generative_skill/scenario/response.py
@@ -18,9 +18,19 @@ assert DIALOGPT_SERVICE_URL
 
 def compose_data_for_dialogpt(dialog):
     data = []
-    for uttr in dialog["utterances"][-3:]:
-        curr_uttr = {"speaker": uttr["user"]["user_type"], "text": uttr["text"]}
-        data.append(curr_uttr)
+    # for uttr in dialog["utterances"][-3:]:
+    #     curr_uttr = {"speaker": uttr["user"]["user_type"], "text": uttr["text"]}
+    #     data.append(curr_uttr)
+    if len(dialog["human_utterances"]) > 1:
+        data += [{"speaker": dialog["human_utterances"][-2]["user"]["user_type"],
+                  "text": dialog["human_utterances"][-2]["text"]}]
+
+    if len(dialog["bot_utterances"]) > 0:
+        data += [{"speaker": dialog["bot_utterances"][-1]["user"]["user_type"],
+                  "text": dialog["bot_utterances"][-1]["text"]}]
+
+    data += [{"speaker": dialog["human_utterances"][-1]["user"]["user_type"],
+              "text": dialog["human_utterances"][-1]["text"]}]
 
     return data
 
@@ -38,14 +48,13 @@ def generative_response(ctx: Context, actor: Actor, *args, **kwargs) -> Any:
             curr_attrs += [attr]
             logger.info(f"dff-generative-skill: {reply}")
 
-    if "agent" in ctx.misc:
-        request_data = compose_data_for_dialogpt(ctx.misc["agent"]["dialog"])
-        hypotheses = requests.post(
-            DIALOGPT_SERVICE_URL, json={"dialog_contexts": [request_data]}, timeout=1).json()["generated_responses"][0]
+    request_data = compose_data_for_dialogpt(ctx.misc["agent"]["dialog"])
+    hypotheses = requests.post(
+        DIALOGPT_SERVICE_URL, json={"dialog_contexts": [request_data]}, timeout=1).json()["generated_responses"][0]
 
-        for hyp in hypotheses:
-            reply, confidence, human_attr, bot_attr, attr = hyp, 0.95, {}, {}, {"can_continue": CAN_NOT_CONTINUE}
-            gathering_responses(reply, confidence, human_attr, bot_attr, attr)
+    for hyp in hypotheses:
+        reply, confidence, human_attr, bot_attr, attr = hyp, 0.95, {}, {}, {"can_continue": CAN_NOT_CONTINUE}
+        gathering_responses(reply, confidence, human_attr, bot_attr, attr)
 
     if len(curr_responses) == 0:
         gathering_responses("", 0.01, {}, {}, {})
