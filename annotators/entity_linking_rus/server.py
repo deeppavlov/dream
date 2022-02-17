eb35bda901c5be317d91bc9965408919b47093ec
dmitry <dmitrij.euseew@yandex.ru>
2022-02-17 13:01:26 +0300

    codestyle

--- annotators/entity_linking_rus/server.py
+++ annotators/entity_linking_rus/server.py
@@ -5,9 +5,7 @@ from flask import Flask, request, jsonify
 import sentry_sdk
 from deeppavlov import build_model
 
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 sentry_sdk.init(os.getenv("SENTRY_DSN"))
 
