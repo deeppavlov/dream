ac1bbf8b019da395cb511c418d4a66820d46db54
mtalimanchuk <mtalimanchuk@gmail.com>
2022-02-18 20:04:20 +0300

    fix cpu.yml, dockerfiles and test for sfc, sfp

--- annotators/speech_function_classifier/test_server.py
+++ annotators/speech_function_classifier/test_server.py
@@ -3,31 +3,52 @@ import os
 
 
 SERVICE_PORT = os.getenv("SERVICE_PORT")
-URL = f"http://0.0.0.0:{SERVICE_PORT}/model"
+URL = f"http://0.0.0.0:{SERVICE_PORT}"
+MODEL_URL = f"{URL}/model"
+ANNOTATION_URL = f"{URL}/annotation"
 
 
-def handler(requested_data):
-    hypothesis = requests.post(URL, json=requested_data).json()
-    return hypothesis
+def run_test():
+    model_test_data = {
+        "phrase": ["fine, thank you.", "and you?"],
+        "prev_phrase": "How are you doing today?",
+        "prev_speech_function": "Open.Demand.Fact",
+    }
+    model_hypothesis = requests.post(MODEL_URL, json=model_test_data).json()
 
-
-def run_test(handler):
-    hypothesis = handler(
-        {
-            "phrase": ["fine, thank you.", "and you?"],
-            "prev_phrase": "How are you doing today?",
-            "prev_speech_function": "Open.Demand.Fact",
-        }
-    )
-    print(f"test name: {hypothesis}")
-    assert hypothesis == [
+    print(f"test name: sfc model_hypothesis")
+    assert model_hypothesis == [
         [
             "React.Rejoinder.Support.Response.Resolve",
             "React.Rejoinder.Confront.Response.Re-challenge",
         ]
     ]
+
+    annotation_test_data = [
+        {
+            "phrase": "fine, thank you. and you?",
+            "prev_phrase": "How are you doing today?",
+            "prev_speech_function": "Open.Demand.Fact",
+        }
+    ]
+    annotation_hypothesis = requests.post(
+        ANNOTATION_URL, json=annotation_test_data
+    ).json()
+
+    print(f"test name: sfc annotation_hypothesis")
+    assert annotation_hypothesis == [
+        {
+            "batch": [
+                [
+                    "React.Rejoinder.Support.Response.Resolve",
+                    "React.Rejoinder.Confront.Response.Re-challenge",
+                ]
+            ]
+        }
+    ]
+
     print("Success")
 
 
 if __name__ == "__main__":
-    run_test(handler)
+    run_test()
