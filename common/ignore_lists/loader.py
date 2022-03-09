ba3652c9a0518bebc5785a4d7802c0b6b013ee1b
mtalimanchuk <mtalimanchuk@gmail.com>
2022-03-09 17:41:25 +0300

    add docstring

--- common/ignore_lists/loader.py
+++ common/ignore_lists/loader.py
@@ -1,9 +1,17 @@
-from typing import Union
+from typing import Union, List
 
 from pathlib import Path
 
 
-def _load_ignore_list(path: Union[Path, str]):
+def _load_ignore_list(path: Union[Path, str]) -> List[str]:
+    """Load list of ignored words/phrases from file
+
+    Args:
+        path: path to .txt file with one word/phrase per line
+
+    Returns: list of non-empty file lines without newline characters (other whitespaces are preserved)
+
+    """
     ignore_list = []
 
     with open(path, "r", encoding="utf-8") as ignore_f:
