a909ca60bfbec400de67b9838b9afca839c2e31b
Дмитрий Евсеев <dmitrij.euseew@yandex.ru>
2022-03-10 19:48:14 +0300

    fix tests

--- annotators/wiki_parser/test_wiki_parser.py
+++ annotators/wiki_parser/test_wiki_parser.py
@@ -1,50 +1,113 @@
+import os
 import requests
 
+
+if os.getenv("LANGUAGE", "ENGLISH") == "ENGLISH":
+    lang = "@en"
+else:
+    lang = "@ru"
 
 def main():
     url = "http://0.0.0.0:8077/model"
 
-    request_data = [
+    request_data_en = [
         {
             "parser_info": ["find_top_triplets"],
-            "query": [[{"entity_substr": "Jürgen Schmidhuber", "entity_ids": ["Q92735"]}]],
+            "query": [[{"entity_substr": "Jurgen Schmidhuber", "entity_ids": ["Q92735"]}]],
         }
     ]
-
-    gold_results = [
+    request_data_ru = [
+        {
+            "parser_info": ["find_top_triplets"],
+            "query": [[{"entity_substr": "Юрген Шмидхубер", "entity_ids": ["Q92735"]}]],
+        }
+    ]
+    gold_results_en = [
         [
             {
-                "entities_info": {
-                    "Jürgen Schmidhuber": {
-                        "age": 58,
-                        "country of sitizenship": [["Q183", "Germany"]],
-                        "date of birth": [['"+1963-01-17^^T"', "17 January 1963"]],
-                        "entity_label": "Jürgen Schmidhuber",
-                        "instance of": [["Q5", "human"]],
-                        "occupation": [
-                            ["Q15976092", "artificial intelligence researcher"],
-                            ["Q1622272", "university teacher"],
-                            ["Q82594", "computer scientist"],
+                'animals_skill_entities_info': {},
+                'entities_info': {
+                    'Jurgen Schmidhuber': {
+                        'age': 59, 'conf': 1.0,
+                        'country of sitizenship': [['Q183', 'Germany']],
+                        'date of birth': [['"+1963-01-17^^T"', '17 January 1963']],
+                        'entity_label': 'Jürgen Schmidhuber',
+                        'instance of': [['Q5', 'human']],
+                        'occupation': [
+                            ['Q15976092', 'artificial intelligence researcher'],
+                            ['Q1622272', 'university teacher'],
+                            ['Q82594', 'computer scientist']
                         ],
-                        "plain_entity": "Q92735",
+                        'plain_entity': 'Q92735','pos': 0, 'token_conf': 1.0,
+                        'types_2hop': [
+                            ['Q82594', 'computer scientist'], ['Q14565186', 'cognitive scientist'],
+                            ['Q66666607', 'academic profession'], ['Q1622272', 'university teacher'],
+                            ['Q37226', 'teacher'], ['Q3400985', 'academic'], ['Q41835716', 'faculty member'],
+                            ['Q15976092', 'artificial intelligence researcher'], ['Q5', 'human'],
+                            ['Q28640', 'profession'], ['Q901', 'scientist']
+                        ]
                     }
                 },
-                "topic_skill_entities_info": {},
-                "utt_num": 0,
+                'topic_skill_entities_info': {},
+                'utt_num': 0,
+                'wiki_skill_entities_info': {}
+            }
+        ]
+    ]
+    gold_results_ru = [
+        [
+            {
+                'animals_skill_entities_info': {},
+                'entities_info': {
+                    'Юрген Шмидхубер': {
+                        'age': 59, 'conf': 1.0,
+                        'country of sitizenship': [['Q183', 'Германия']],
+                        'date of birth': [['"+1963-01-17^^T"', '17 January 1963']],
+                        'entity_label': 'Шмидхубер, Юрген',
+                        'instance of': [['Q5', 'человек']],
+                        'occupation': [
+                            ['Q15976092', 'исследователь искусственного интеллекта'],
+                            ['Q1622272', 'преподаватель университета'],
+                            ['Q82594', 'специалист в области информатики']
+                        ],
+                        'plain_entity': 'Q92735', 'pos': 0, 'token_conf': 1.0,
+                        'types_2hop': [
+                            ['Q5', 'человек'], ['Q28640', 'профессия'], ['Q37226', 'учитель'],
+                            ['Q3400985', 'научный работник'], ['Q41835716', 'преподаватель'],
+                            ['Q901', 'учёный'], ['Q82594', 'специалист в области информатики'],
+                            ['Q15976092', 'исследователь искусственного интеллекта'],
+                            ['Q1622272', 'преподаватель университета'], ['Q66666607', 'академическая профессия']
+                        ]
+                    }
+                },
+                'topic_skill_entities_info': {},
+                'utt_num': 0,
+                'wiki_skill_entities_info': {}
             }
         ]
     ]
 
     count = 0
-    for data, gold_result in zip(request_data, gold_results):
-        result = requests.post(url, json=data).json()
-        if result == gold_result:
-            count += 1
-    else:
-        print(f"Got {result}, but expected: {gold_result}")
-
-    if count == len(request_data):
-        print("Success")
+    if lang == "@ru":
+        for data, gold_result in zip(request_data_ru, gold_results_ru):
+            result = requests.post(url, json=data).json()
+            if result == gold_result:
+                count += 1
+        else:
+            print(f"Got {result}, but expected: {gold_result}")
+        assert count == len(request_data_ru)
+        if count == len(request_data_ru):
+            print("Success")
+    elif lang == "@en":
+        for data, gold_result in zip(request_data_en, gold_results_en):
+            result = requests.post(url, json=data).json()
+            if result == gold_result:
+                count += 1
+        else:
+            print(f"Got {result}, but expected: {gold_result}")
+        assert count == len(request_data_en)
+        if count == len(request_data_en):
+            print("Success")
 
 
 if __name__ == "__main__":
