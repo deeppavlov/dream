4137d6fe3b029b2b9796c1ffe6a43c6d21bf379a
dilyararimovna <dilyara.rimovna@gmail.com>
2022-03-17 18:47:57 +0300

    fix: test for new version

--- skills/personal_info_skill/test.py
+++ skills/personal_info_skill/test.py
@@ -1,25 +1,43 @@
+import os
 import requests
 
 
 SKILL_URL = "http://0.0.0.0:8030/respond"
+LANGUAGE = os.getenv("LANGUAGE", "EN")
 
+if LANGUAGE == "RU":
+    dialogs = {
+        "dialogs": [
+            {
+                "utterances": [{"text": "Меня зовут Джо", "annotations": {"ner": [[{"text": "Джо", "type": "PER"}]]}}],
+                "bot_utterances": [],
+                "human": {"attributes": {}, "profile": {"name": None}},
+                "human_utterances": [
+                    {"text": "Меня зовут Джо.", "annotations": {"ner": [[{"text": "Джо", "type": "PER"}]]}}
+                ],
+            }
+        ]
+    }
+    gold = "Приятно познакомиться, Джо."
+else:
+    dialogs = {
+        "dialogs": [
+            {
+                "utterances": [
+                    {"text": "my name is john", "annotations": {"ner": [[{"text": "john", "type": "PER"}]]}}],
+                "bot_utterances": [],
+                "human": {"attributes": {}, "profile": {"name": None}},
+                "human_utterances": [
+                    {"text": "my name is john", "annotations": {"ner": [[{"text": "john", "type": "PER"}]]}}
+                ],
+            }
+        ]
+    }
+    gold = "Nice to meet you, john."
 
-dialogs = {
-    "dialogs": [
-        {
-            "utterances": [{"text": "my name is john", "annotations": {"ner": [[{"text": "john", "type": "PER"}]]}}],
-            "bot_utterances": [],
-            "human": {"attributes": {}, "profile": {"name": None}},
-            "human_utterances": [
-                {"text": "my name is john", "annotations": {"ner": [[{"text": "john", "type": "PER"}]]}}
-            ],
-        }
-    ]
-}
-gold = "Nice to meet you, john."
 result = requests.post(SKILL_URL, json=dialogs, timeout=2)
 result = result.json()
 
-assert result[0][0] == "Nice to meet you, John.", print(result)
+assert result[0][0] == gold, print(result)
 
 print("SUCCESS")
