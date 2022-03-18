5a64c468d6da56fe06d63a34d11cff08aed20a33
dilyararimovna <dilyara.rimovna@gmail.com>
2022-03-18 19:08:21 +0300

    fix: mtest format prints

--- skills/personal_info_skill/test.py
+++ skills/personal_info_skill/test.py
@@ -17,7 +17,7 @@ result = requests.post(SKILL_URL, json=dialogs, timeout=2)
 result = result.json()
 
 for i in range(len(dialogs['dialogs'])):
-    print(f"check for uttr `{dialogs['dialogs'][i]['human_utterances'][-1]['text']}`\n\tgold response: {gold[i]}")
+    print(f"check for uttr `{dialogs['dialogs'][i]['human_utterances'][-1]['text']}`\tgold response: `{gold[i]}`")
     assert result[i][0] == gold[i], print(result[i])
 
 print("SUCCESS")
