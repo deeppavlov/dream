2733fb79b504e0a4dac9a2f39f60317a38132990
dilyararimovna <dilyara.rimovna@gmail.com>
2022-03-21 16:17:56 +0300

    fix: test spacy annotator

--- annotators/spacy_annotator/test.py
+++ annotators/spacy_annotator/test.py
@@ -7,9 +7,9 @@ SERVICE_PORT = int(os.getenv("SERVICE_PORT"))
 
 def main():
     url = f"http://0.0.0.0:{SERVICE_PORT}/respond"
-    input_data = {"sentences": ["i like michal jordan", "hey this is a white bear"]}
-    result = requests.post(url, json=input_data)
-    assert result.json() == [["michal jordan"], ["a white bear"]]
+    input_data = {"sentences": ["джейсон стетхэм хочет есть."]}
+    result = requests.post(url, json=input_data).json()
+    assert result == [["michal jordan"], ["a white bear"]], print(result)
     print("Success!")
 
 
