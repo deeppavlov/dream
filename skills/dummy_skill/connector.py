74a2c51078de43b21bb4df4ae2276e680488c8f9
dilyararimovna <dilyara.rimovna@gmail.com>
2022-03-24 10:08:54 +0300

    fix: codestyle

--- skills/dummy_skill/connector.py
+++ skills/dummy_skill/connector.py
@@ -24,8 +24,12 @@ from common.link import (
 )
 from common.remove_lists import NP_REMOVE_LIST, NP_IGNORE_LIST
 from common.sensitive import is_sensitive_situation
-from common.universal_templates import opinion_request_question, is_switch_topic, if_choose_topic, \
-    DUMMY_DONTKNOW_RESPONSES
+from common.universal_templates import (
+    opinion_request_question,
+    is_switch_topic,
+    if_choose_topic,
+    DUMMY_DONTKNOW_RESPONSES,
+)
 from common.utils import get_topics, get_entities, is_no, get_intents, is_yes
 
 
