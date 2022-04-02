d9f6267bc6b02131be251d2d850b2501ce23d4a6
dilyararimovna <dilyara.rimovna@gmail.com>
2022-04-02 12:46:05 +0300

    feat: train intent catcher

---
+++ annotators/IntentCatcherTransformers/create_data_and_train_model.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+
+import os
+import json
+import argparse
+from collections import OrderedDict
+
+import numpy as np
+import torch
+from sklearn.metrics import f1_score, roc_auc_score, accuracy_score
+from datasets import Dataset, load_dataset
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer, EvalPrediction
+
+from utils import generate_phrases, create_label_map, dump_dataset, compute_metrics
+
+
+MODEL_NAME = "linear_classifier"
+MULTILABEL = True
+TRAIN_SIZE = 0.5
+DENSE_LAYERS = 2
+MODEL_NAME += "_h" + str(DENSE_LAYERS)
+INTENT_DATA_PATH = "./intent_data_h" + str(DENSE_LAYERS) + ".json"
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--intent_phrases_path", help="file with phrases for embedding generation", default="intent_phrases.json"
+)
+parser.add_argument("--model_path", help="path where to save the model", default="./models/" + MODEL_NAME + ".h5")
+parser.add_argument("--epochs", help="number of epochs to train model", default=7)
+# Whereas to calc metrics or not (default value = True)
+args = parser.parse_args()
+# Create metrics directory if not exists
+if not os.path.exists("../metrics/"):
+    os.makedirs("../metrics")
+
+TRANSFORMERS_MODEL_PATH = os.environ.get("TRANSFORMERS_MODEL_PATH", None)
+if TRANSFORMERS_MODEL_PATH is None:
+    TRANSFORMERS_MODEL_PATH = "sentence-transformers/distiluse-base-multilingual-cased-v2"
+BATCH_SIZE = int(os.environ.get("BATCH_SIZE", 8))
+MAX_LENGTH = int(os.environ.get("MAX_LENGTH", 64))
+
+tokenizer = AutoTokenizer.from_pretrained(TRANSFORMERS_MODEL_PATH)
+
+
+def encode_dataset(df):
+    global tokenizer
+
+    text_labels = np.array(df["intents"])
+    print(text_labels)
+    labels = list(set(df["intents"]))
+    labels = [label for label in labels if label != "none"]
+    print(labels)
+    # encode them
+    encoding = tokenizer(df["text"], padding="max_length", truncation=True, max_length=MAX_LENGTH)
+
+    # create numpy array of shape (batch_size, num_labels)
+    labels_matrix = np.zeros((len(df["text"]), len(labels)))
+    # fill numpy array
+    for idx, label in enumerate(labels):
+        labels_matrix[:, idx] = 1 * (text_labels == label)
+
+    encoding["labels"] = labels_matrix.tolist()
+    return encoding
+
+
+def load(train_path='intent_train.csv', valid_path='intent_valid.csv'):
+    dataset = load_dataset('csv', data_files={'train': [train_path],
+                                              'valid': valid_path})
+    encoded_dataset = dataset.map(encode_dataset, batched=True)
+    encoded_dataset.set_format("torch")
+    return encoded_dataset
+
+
+with open(args.intent_phrases_path, "r") as fp:
+    all_data = json.load(fp)
+    intent_phrases = OrderedDict(all_data["intent_phrases"])
+    random_phrases = all_data["random_phrases"]
+
+intent_data = {}
+intents = sorted(list(intent_phrases.keys()))
+
+print("Creating  data...")
+print("Intent: number of original phrases")
+id2label, label2id = create_label_map(intents)
+
+classification_model = AutoModelForSequenceClassification.from_pretrained(
+    TRANSFORMERS_MODEL_PATH,
+    num_labels=len(intents),
+    problem_type="multi_label_classification",
+    id2label=id2label,
+    label2id=label2id
+)
+print("Loaded pre-trained model for fine-tuning")
+
+for intent, data in intent_phrases.items():
+    phrases = generate_phrases(data["phrases"], data["punctuation"])
+    intent_data[intent] = {
+        "generated_phrases": phrases,
+        "num_punctuation": len(data["punctuation"]),
+    }
+    print(f"{intent}: {len(phrases)//len(data['punctuation'])}")
+
+dump_dataset(intent_data, random_phrases, intents)
+print("Dumped csv datasets")
+
+encoded_dataset = load(train_path='intent_train.csv', valid_path='intent_valid.csv')
+print("Loaded encoded datasets")
+
+args = TrainingArguments(
+    f"bert-finetuned-sem_eval-english",
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=BATCH_SIZE,
+    per_device_eval_batch_size=BATCH_SIZE,
+    num_train_epochs=5,
+    weight_decay=0.01,
+    load_best_model_at_end=True,
+    metric_for_best_model="f1",
+)
+trainer = Trainer(
+    classification_model,
+    args,
+    train_dataset=encoded_dataset["train"],
+    eval_dataset=encoded_dataset["valid"],
+    tokenizer=tokenizer,
+    compute_metrics=compute_metrics
+)
+print("Initial evaluation")
+trainer.evaluate()
+print("Training model...")
+trainer.train()
+print("Final evaluation")
+trainer.evaluate()
+
+print(f"Saving model to: {args.model_path}")
+trainer.model.save_pretrained(args.model_path)
---
+++ annotators/IntentCatcherTransformers/utils.py
@@ -0,0 +1,88 @@
+from itertools import chain
+
+import numpy as np
+import pandas as pd
+import torch
+from sklearn.metrics import f1_score, roc_auc_score, accuracy_score
+from sklearn.model_selection import StratifiedKFold
+from transformers import EvalPrediction
+from xeger import Xeger
+
+
+TRANSFORMERS_MODEL_PATH = "sentence-transformers/distiluse-base-multilingual-cased-v2"
+BATCH_SIZE = 4
+MAX_LENGTH = 8
+
+
+def generate_phrases(template_re, punctuation, limit=2500):
+    x = Xeger(limit=limit)
+    phrases = []
+    for regex in template_re:
+        try:
+            phrases += list({x.xeger(regex) for _ in range(limit)})
+        except Exception as e:
+            print(e)
+            print(regex)
+            raise e
+    phrases = [phrases] + [[phrase + punct for phrase in phrases] for punct in punctuation]
+    return list(chain.from_iterable(phrases))
+
+
+def create_label_map(labels):
+    id2label = {idx: label for idx, label in enumerate(labels)}
+    label2id = {label: idx for idx, label in enumerate(labels)}
+    return id2label, label2id
+
+
+def dump_dataset(intent_data, random_phrases, labels):
+    texts = []
+    text_labels = []
+    for intent in labels:
+        texts += intent_data[intent]["generated_phrases"]
+        text_labels += [intent] * len(intent_data[intent]["generated_phrases"])
+
+    # random samples (do not belong to any class)
+    texts += random_phrases
+    text_labels += ["none"] * len(random_phrases)
+    df = pd.DataFrame({"text": texts, "intents": text_labels})
+
+    skf = StratifiedKFold(n_splits=2, shuffle=True, random_state=23)
+
+    for train_index, test_index in skf.split(df["text"], df["intents"]):
+        df_train = df.loc[train_index, :]
+        df_train.reset_index(drop=True)
+        df_train.to_csv('intent_train.csv')
+        df_test = df.loc[test_index, :]
+        df_test.reset_index(drop=True)
+        df_test.to_csv('intent_valid.csv')
+        break
+    return
+
+
+# source: https://jesusleal.io/2021/04/21/Longformer-multilabel-classification/
+def multi_label_metrics(predictions, labels, threshold=0.5):
+    # first, apply sigmoid on predictions which are of shape (batch_size, num_labels)
+    sigmoid = torch.nn.Sigmoid()
+    probs = sigmoid(torch.Tensor(predictions))
+    # next, use threshold to turn them into integer predictions
+    y_pred = np.zeros(probs.shape)
+    y_pred[np.where(probs >= threshold)] = 1
+    # finally, compute metrics
+    y_true = labels
+    f1_micro_average = f1_score(y_true=y_true, y_pred=y_pred, average='micro')
+    roc_auc = roc_auc_score(y_true, y_pred, average='micro')
+    accuracy = accuracy_score(y_true, y_pred)
+    # return as dictionary
+    metrics = {'f1': f1_micro_average,
+               'roc_auc': roc_auc,
+               'accuracy': accuracy}
+    return metrics
+
+
+def compute_metrics(p: EvalPrediction):
+    preds = p.predictions[0] if isinstance(p.predictions,
+                                           tuple) else p.predictions
+    result = multi_label_metrics(
+        predictions=preds,
+        labels=p.label_ids)
+    return result
