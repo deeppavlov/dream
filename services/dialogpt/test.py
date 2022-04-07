64ce0ebc48ac9647c309c67fb462799e571fdfa0
dilyararimovna <dilyara.rimovna@gmail.com>
2022-04-07 12:14:52 +0300

    feat: dialogpt en

---
+++ services/dialogpt/test.py
@@ -0,0 +1,18 @@
+import requests
+
+
+def test_respond():
+    url = "http://0.0.0.0:8125/respond"
+
+    contexts = [
+        ["hi", "hi. how are you?"],
+        ["let's chat about movies", "cool. what movies do you like?"]
+    ]
+    gold_result = [["I'm good, how are you?", 0.9], ['I like the new one.', 0.9]]
+    result = requests.post(url, json={"utterances_histories": contexts}).json()
+    assert result == gold_result, f"Got\n{result}\n, but expected:\n{gold_result}"
+    print("Success")
+
+
+if __name__ == "__main__":
+    test_respond()
