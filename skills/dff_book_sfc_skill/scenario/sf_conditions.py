9103449ff223c94aed463a4fc64e3174ad8f99a2
Daniel Kornev <daniel@zetuniverse.com>
2022-04-14 14:55:06 +0300

    fix loop

--- skills/dff_book_sfc_skill/scenario/sf_conditions.py
+++ skills/dff_book_sfc_skill/scenario/sf_conditions.py
@@ -1,6 +1,7 @@
 import logging
-import sentry_sdk
+#import sentry_sdk
 from os import getenv
+import operator
 
 from df_engine.core import Context, Actor
 
@@ -27,6 +28,23 @@ def is_ext_sf(ext_sf_name="React.Respond.Support.Reply.Agree"):
         return ext_sf_name in ctx.misc.get("ext_sf", [[]])[-1]
 
     return is_ext_sf_handler
+
+
+def is_midas(midas_name="opinion"):
+    def is_midas_handler(ctx: Context, actor: Actor, *args, **kwargs):
+        print(logger.info(str(ctx.misc)))
+        try:
+            last_utterance = ctx.misc.get("agent", {}).get("dialog", {}).get("human_utterances", {})[-1]
+            utterance_midas = last_utterance.get("annotations", {}).get("midas_classification", [])
+            #midas = max(utterance_midas.items(), key=operator.itemgetter(1))
+            max_value = max(utterance_midas.values())  # maximum value
+            midas = [k for k, v in max_value.items() if v == max_value]
+        except KeyError:
+            midas = []
+
+        return midas_name in midas
+
+    return is_midas_handler
 
 
 speech_functions = is_sf
