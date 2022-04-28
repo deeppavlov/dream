eea2a11fd53e0eb71ace1c344a3ecf357c6ae798
Kseniya <kpetyxova@mail.ru>
2022-04-28 16:20:01 +0300

    fixed bug in dff-book-recommendation-skill

--- common/get_book_recommendation.py
+++ common/get_book_recommendation.py
@@ -9,11 +9,11 @@ with open('common/book2genre.json', 'r') as f:
     book2genre = json.load(f)
     books = list(book2genre.keys())[:500]
 
-BOOKS_PATTERN = re.compile(r"|".join(books), re.IGNORECASE)
+BOOKS_PATTERN = re.compile(r"\b(" + ("|".join(books) + ")"), re.IGNORECASE)
 
 APPRECIATION_PATTERN = re.compile(r"\b(my favorite|my fav|my favourite|i love|i like)", re.IGNORECASE)
 
-GENRES_PATTERN = re.compile(r"|".join(genres), re.IGNORECASE)
+GENRES_PATTERN = re.compile(r"\b(" + ("|".join(genres) + ")"), re.IGNORECASE)
 
 RECOMMEND_BOOK_PATTERN = re.compile(r"\b(recommend a book|recommend me a book|what book would you suggest to read|what book should i read|what book would you recommend)", re.IGNORECASE)
 
