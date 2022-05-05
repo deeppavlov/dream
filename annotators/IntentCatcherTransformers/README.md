942049410d63b5c4f7fcf3045fbca5ebca74037e
dilyararimovna <dilyara.rimovna@gmail.com>
2022-05-05 17:20:19 +0300

    fix: use dp based model

--- annotators/IntentCatcherTransformers/README.md
+++ annotators/IntentCatcherTransformers/README.md
@@ -6,7 +6,7 @@ English version was trained on `intent_phrases.json` dataset via command:
 python create_data_and_train_model.py --intent_phrases_path intent_phrases.json --model_path ./data/intents_model_v0 --epochs 5
 ```
 
-It consumes 2 Gb GPU RAM during fine-tuning `distilbert-base-uncased`. Classification results after 5 epochs are the following:
+It consumes 3.5Gb GPU RAM during fine-tuning `distilbert-base-uncased`. Classification results after 5 epochs are the following:
 ```json
 {
     'eval_loss': 0.00016819244774524122, 
--- annotators/IntentCatcherTransformers/server.py
+++ annotators/IntentCatcherTransformers/server.py
@@ -4,92 +4,72 @@ from typing import List
 
 import sentry_sdk
 import torch
+from deeppavlov import build_model
+from deeppavlov.core.commands.utils import parse_config, expand_path
 from flask import Flask, jsonify, request
 from sentry_sdk.integrations.flask import FlaskIntegration
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-from utils import create_label_map, get_regexp
+from utils import get_regexp
 
 # logging here because it conflicts with tf
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 sentry_sdk.init(dsn=os.getenv("SENTRY_DSN"), integrations=[FlaskIntegration()])
-
 app = Flask(__name__)
 
-INTENT_PHRASES_PATH = os.environ.get("INTENT_PHRASES_PATH", "intent_phrases.json")
-INTENTS_MODEL_PATH = os.environ.get("INTENTS_MODEL_PATH", "intent_model_v0")
-TRANSFORMERS_MODEL_PATH = os.environ.get("TRANSFORMERS_MODEL_PATH", None)
 
+INTENT_PHRASES_PATH = os.environ.get("INTENT_PHRASES_PATH", "intent_phrases.json")
+CONFIG_NAME = os.environ.get("CONFIG_NAME", None)
+if CONFIG_NAME is None:
+    raise NotImplementedError("No config file name is given.")
+parsed = parse_config(CONFIG_NAME)
+with open(expand_path(parsed["metadata"]["variables"]["MODEL_PATH"]).joinpath("classes.dict"), "r") as f:
+    intents = f.read().strip().splitlines()
+intents = [el.strip() for el in intents]
+logger.info(f"Considered intents: {intents}")
 
 try:
-    tokenizer = AutoTokenizer.from_pretrained(TRANSFORMERS_MODEL_PATH)
-    logger.info("Tokenizer loaded")
-
+    intents_model = build_model(CONFIG_NAME, download=True)
+    logger.info("Model loaded")
     regexp = get_regexp(INTENT_PHRASES_PATH)
-
-    with open(f"/data/{INTENTS_MODEL_PATH}/intents.txt", "r") as f:
-        intents = f.read().splitlines()
-        intents = [intent.strip() for intent in intents if intent]
-    id2label, label2id = create_label_map(intents)
-    logger.info(f"Intents map loaded: {intents}")
-
-    classification_model = AutoModelForSequenceClassification.from_pretrained(
-        f"/data/{INTENTS_MODEL_PATH}",
-        num_labels=len(intents),
-        problem_type="multi_label_classification",
-        id2label=id2label,
-        label2id=label2id,
-    )
-    if torch.cuda.is_available():
-        classification_model.to("cuda")
-        logger.info("Intents model set on cuda")
-    classification_model.eval()
-    logger.info("Intents model loaded")
-
+    logger.info("Regexp model loaded")
 except Exception as e:
     sentry_sdk.capture_exception(e)
     logger.exception(e)
     raise e
 
 
-def predict_intents(text: List[str]):
-    global tokenizer, classification_model, intents, regexp
-    encoding = tokenizer(" ".join(text), return_tensors="pt")
-    resp = {intent: {"detected": 0, "confidence": 0.0} for intent in intents}
+def predict_intents(batch_texts: List[str], intents, regexp):
+    result = []
+    # classify with intent catcher neural model
+    pred_labels, pred_probas = intents_model(batch_texts)
 
-    for intent, regs in regexp.items():
-        for i, utt in enumerate(text):
-            for reg in regs:
-                if reg.fullmatch(utt):
-                    resp[intent]["detected"] = 1
-                    resp[intent]["confidence"] = 1.0
-                    break
-    with torch.no_grad():
-        if torch.cuda.is_available():
-            encoding.to("cuda")
-        outputs = classification_model(**encoding)
-        predictions = torch.sigmoid(outputs.logits)
-        if torch.cuda.is_available():
-            predictions = predictions.cpu()
-        predictions = predictions.numpy()[0]
+    for text_id, text in enumerate(batch_texts):
+        resp = {intent: {"detected": 0, "confidence": 0.0} for intent in intents}
+        for intent, regs in regexp.items():
+            for i, utt in enumerate(text):
+                for reg in regs:
+                    if reg.fullmatch(utt):
+                        resp[intent]["detected"] = 1
+                        resp[intent]["confidence"] = 1.0
+                        break
+        resp = {
+            intent: resp[intent]
+            if resp[intent]["detected"]
+            else {"detected": int(float(proba) > 0.5), "confidence": float(proba)}
+            for intent, proba in zip(intents, pred_probas[text_id])
+        }
+        result += resp
 
-    resp = {
-        intent: resp[intent]
-        if resp[intent]["detected"]
-        else {"detected": int(float(proba) > 0.5), "confidence": float(proba)}
-        for intent, proba in zip(intents, predictions)
-    }
-    return resp
+    return result
 
 
 @app.route("/detect", methods=["POST"])
 def detect():
     utterances = request.json["sentences"]
-    logger.info(f"Number of utterances: {len(utterances)}")
-    results = []
-    for uttr in utterances:
-        results += [predict_intents(uttr)]
+    utterances = [" ".join(uttr) if isinstance(uttr, list) else uttr for uttr in utterances]
+    logger.info(f"Input to classify: `{utterances}`.")
+    results = predict_intents(utterances, intents, regexp)
 
     return jsonify(results)
 
