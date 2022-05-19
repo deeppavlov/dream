521a98bea88be65e33c4aa2ca5fec1ee567d7274
mtalimanchuk <mtalimanchuk@gmail.com>
2022-05-19 11:40:21 +0300

    add actions, fix deployment

--- common/minecraft/bot.py
+++ common/minecraft/bot.py
@@ -1,3 +1,5 @@
+import logging
+
 from javascript import require, On, Once, AsyncTask, once, off
 import requests
 
@@ -6,8 +8,19 @@ from core.serializer import encode_actions, decode_actions
 from core import actions
 
 
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
 USERS = {}
-ACTION_MAP = {"goto": actions.goto}
+ACTION_MAP = {
+    "chat": actions.chat,
+    "goto": actions.goto,
+    "goto_user": actions.goto_user,
+    "stop": actions.stop,
+    "destroy_block": actions.destroy_block,
+    "place_block": actions.place_block,
+}
 
 bot_settings = BotSettings()
 mineflayer = require("mineflayer")
@@ -29,6 +42,7 @@ bot.loadPlugin(pathfinder.pathfinder)
 mc_data = require("minecraft-data")(bot.version)
 movements = pathfinder.Movements(bot, mc_data)
 bot.pathfinder.setMovements(movements)
+logger.info("DREAM bot is bootstrapped and ready to play!")
 
 
 @On(bot, "playerJoin")
@@ -41,25 +55,41 @@ def on_chat(event, user, message, *args):
     if user == bot.username:
         return
 
-    if message.startswith("!name"):
-        try:
-            command, new_user_name = message.split(" ")
-            USERS[user] = new_user_name
-            bot.chat(f"Got it! {user} is now known as {new_user_name}")
-        except ValueError:
-            bot.chat(f"Say '!name NewName' to change your DREAM username")
+    if message.startswith("!"):
+        command_args = message[1:].split()
+        command = command_args.pop(0)
+        action_command = ACTION_MAP.get(command)
+
+        if action_command:
+            for arg_idx in range(len(command_args)):
+                # try converting strings to int to correctly parse bool values like "0"
+                try:
+                    command_args[arg_idx] = int(command_args[arg_idx])
+                except ValueError:
+                    pass
+            logger.debug(f"Calling {action_command} with args {command_args}")
+            action_command(bot, pathfinder, user, *command_args)
+        elif command == "name":
+            try:
+                new_user_name = command_args[0]
+                USERS[user] = new_user_name
+                bot.chat(f"Got it! {user} is now known as {new_user_name}")
+            except IndexError:
+                bot.chat(f"Say '!name NewName' to change your DREAM username")
+        else:
+            bot.chat(f"In-game command {command} is not recognized")
         return
 
     try:
         user_id = USERS.get(user, user)
         response = requests.post(bot_settings.agent_url, json={"user_id": user_id, "payload": message})
         data = response.json()
+        response_message = data["response"]
 
         # mock agent response
-        # actions_list = [{"action": "goto", "args": [10, 10, 2], "kwargs": {"range_goal": 1}}]
+        # actions_list = [{"action": "follow", "args": [], "kwargs": {}}]
         # response_message = f"Thy will be done! #+# {encode_actions(actions_list)}"
         # end mock
-        response_message = data["response"]
     except Exception as e:
         print(e)
         response_text = "Sorry, DREAM agent is unavailable for some reason"
@@ -71,10 +101,6 @@ def on_chat(event, user, message, *args):
             response_actions = decode_actions(response_parts[1])
             for action_data in response_actions:
                 action_f = ACTION_MAP[action_data["action"]]
-                action_f(bot, pathfinder, *action_data["args"], **action_data["kwargs"])
+                action_f(bot, pathfinder, user, *action_data.get("args", []), **action_data.get("kwargs", {}))
 
     bot.chat(response_text)
-
-    # If the message contains stop, remove the event listener and stop logging.
-    # if 'stop' in message:
-    #     off(bot, 'chat', on_chat)
--- common/minecraft/core/actions.py
+++ common/minecraft/core/actions.py
@@ -4,7 +4,208 @@
 Every function must accept ``bot`` and ``pathfinder`` as the first two arguments,
 and other ``args``/``kwargs`` as needed
 """
+import logging
+from math import pi
+
+from javascript import AsyncTask, Once, require
 
 
-def goto(bot, pathfinder, x, y, z, range_goal=1):
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
+)
+logger = logging.getLogger(__name__)
+
+
+def chat(
+    bot,
+    pathfinder,
+    invoker_username,
+    text: str,
+    nod_head: bool = False,
+    shake_head: bool = False,
+    animation_loops: int = 3,
+):
+    """Sends a regular DREAM response to chat with animated actions
+
+    Args:
+        bot: bot instance
+        pathfinder: pathfinder module instance
+        invoker_username: minecraft user who invoked this action
+        text: message
+        nod_head: do nodding animation
+        shake_head: do shaking animation
+        animation_loops: how many times animation is repeated,
+            has no effect if both ``nod_head`` and ``shake_head`` are False
+
+    Returns:
+
+    """
+    user_entity = bot.players[invoker_username].entity
+    bot.lookAt(user_entity.position.offset(0, 1.6, 0))
+
+    if nod_head:
+        # this doesn't look great in game, maybe because of the server-side animations?
+        for _ in range(animation_loops):
+            bot.look(bot.entity.yaw, pi / 2.5)
+            bot.look(bot.entity.yaw, -pi / 2.5)
+        bot.lookAt(user_entity.position.offset(0, 1.6, 0))
+
+    if shake_head:
+        for _ in range(animation_loops):
+            bot.look(bot.entity.yaw + pi / 5, bot.entity.pitch)
+            bot.look(bot.entity.yaw - pi / 5, bot.entity.pitch)
+        bot.lookAt(user_entity.position.offset(0, 1.6, 0))
+
+    bot.chat(text)
+
+
+def goto(
+    bot, pathfinder, invoker_username, x: int, y: int, z: int, range_goal: int = 1
+):
+    """Sends bot to coordinates, stopping when it is close enough
+
+    Args:
+        bot: bot instance
+        pathfinder: pathfinder module instance
+        invoker_username: minecraft user who invoked this action
+        x: target x coordinate
+        y: target y coordinate
+        z: target z coordinate
+        range_goal: assume target is reached when the bot is this many blocks close to it
+
+    Returns:
+
+    """
     bot.pathfinder.setGoal(pathfinder.goals.GoalNear(x, y, z, range_goal))
+
+
+def goto_user(
+    bot, pathfinder, invoker_username, range_goal: int = 3, follow: bool = False
+):
+    """Sends bot to user's coordinates.
+
+
+    Args:
+        bot: bot instance
+        pathfinder: pathfinder module instance
+        invoker_username: minecraft user who invoked this action
+        range_goal: assume target is reached when the bot is this many blocks close to it
+        follow: if True, keep following the user until explicitly told to stop,
+            else stop when reached the goal
+
+    Returns:
+
+    """
+    user_entity = bot.players[invoker_username].entity
+    bot.pathfinder.setGoal(pathfinder.goals.GoalFollow(user_entity, range_goal), follow)
+
+
+def stop(bot, pathfinder, invoker_username, force: bool = True):
+    """Stop current pathfinding goals
+
+    Args:
+        bot: bot instance
+        pathfinder: pathfinder module instance
+        invoker_username: minecraft user who invoked this action
+        force: if True, will stop immediately, else will find a safe place to stop.
+            Using this flag might be unsafe for the bot character
+
+    Returns:
+
+    """
+    if force:
+        bot.pathfinder.setGoal(None)
+    else:
+        bot.pathfinder.stop()
+
+
+def destroy_block(bot, pathfinder, invoker_username, tool=None):
+    """Destroys the block which is targeted by the player
+
+    Args:
+        bot: bot instance
+        pathfinder: pathfinder module instance
+        invoker_username: minecraft user who invoked this action
+        tool: which tool to use
+
+    Returns:
+
+    """
+    user_entity = bot.players[invoker_username].entity
+    target_block = bot.blockAtEntityCursor(user_entity)
+
+    if not target_block:
+        bot.chat(f"{invoker_username} is not looking at any block")
+        return
+
+    logger.debug(f"User: {user_entity}")
+    logger.debug(f"Target block: {target_block}")
+
+    bot.pathfinder.setGoal(
+        pathfinder.goals.GoalLookAtBlock(target_block.position, bot.world)
+    )
+
+    @Once(bot, "goal_reached")
+    def start_digging(event, state_goal):
+        if bot.canDigBlock(target_block):
+            bot.chat(
+                f"Breaking '{target_block.name}' block, "
+                f"will take around {bot.digTime(target_block) // 1000} seconds"
+            )
+            try:
+                @AsyncTask(start=True)
+                def break_block(task):
+                    bot.dig(target_block)
+
+                bot.chat("Started digging!")
+            except Exception as e:
+                bot.chat(f"Couldn't finish digging because {e}")
+                logger.warning(f"Couldn't finish digging because {e}")
+        else:
+            bot.chat(f"Can't break '{target_block.name}' at {target_block.position}!")
+
+
+def place_block(
+    bot,
+    pathfinder,
+    invoker_username,
+    max_range_goal: int = 4,
+    face_vector: tuple = (0, 1, 0),
+):
+    """Places a block adjacent to the block which is targeted by the player
+
+    Args:
+        bot: bot instance
+        pathfinder: pathfinder module instance
+        invoker_username: minecraft user who invoked this action
+        max_range_goal: max number of blocks away from the goal
+        face_vector: (x, y, z) coordinates of the placed block relative to the reference block,
+            e.g. the user targets (10, 10, 10), the face vector is (0, 1, 0),
+            then the new block will be placed at (10, 11, 10)
+
+    Returns:
+
+    """
+    vec3 = require("vec3").Vec3
+    user_entity = bot.players[invoker_username].entity
+    target_block = bot.blockAtEntityCursor(user_entity)
+
+    if not target_block:
+        bot.chat(f"{invoker_username} is not looking at any block")
+        return
+
+    # change to GoalPlaceBlock later
+    bot.pathfinder.setGoal(
+        pathfinder.goals.GoalLookAtBlock(
+            target_block.position, bot.world, {"range": max_range_goal}
+        )
+    )
+
+    @Once(bot, "goal_reached")
+    def try_placing(event, state_goal):
+        try:
+            bot.chat(f"Placing a block near {target_block.position}")
+            bot.placeBlock(target_block, vec3(*face_vector))
+        except Exception as e:
+            bot.chat(f"Couldn't place the block because {e}")
+            logger.warning(f"Couldn't place the block because {e}")
