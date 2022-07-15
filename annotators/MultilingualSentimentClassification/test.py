b6dc44c1187aaa7f75196770f402d44ebf89507c
duny-explorer <duny.explorer@yandex.ru>
2022-07-15 12:21:21 +0300

    more fixes

--- annotators/MultilingualSentimentClassification/test.py
+++ annotators/MultilingualSentimentClassification/test.py
@@ -2,11 +2,11 @@ import requests
 
 
 def test_respond():
-    url = "http://0.0.0.0:3668/respond"
+    url = "http://0.0.0.0:8129/respond"
 
     sentences = ['Hola! Como estas?', 'örnek metin', 'Болван несчастный']
-    gold = [{'Negative': 0.0274, 'Neutral': 0.706, 'Positive': 0.2666}, {'Negative': 0.29077, 'Neutral': 0.33038, 'Positive': 0.37885},
-            {'Negative': 0.94606, 'Neutral': 0.03936, 'Positive': 0.01458}]
+    gold = [{'negative': 0.0274, 'neutral': 0.706, 'positive': 0.2666}, {'negative': 0.29077, 'neutral': 0.33038, 'positive': 0.37885},
+            {'negative': 0.94606, 'neutral': 0.03936, 'positive': 0.01458}]
     request_data = {"sentences": sentences}
     result = requests.post(url, json=request_data).json()
     assert [{i: round(j[i], 5) for i in j} for j in result] == gold, f"Got\n{result}"
--- services/mgpt/test.py
+++ services/mgpt/test.py
@@ -3,12 +3,11 @@ import requests
 
 
 def test_respond():
-    url = "http://0.0.0.0:8125/respond"
+    url = "http://0.0.0.0:8132/respond"
 
     contexts = [["hi", "hi. how are you?"], ["let's chat about movies", "cool. what movies do you like?"]]
     gold_result = [["I'm good, how are you?", 0.9], ["I like the new one.", 0.9]]
     result = requests.post(url, json={"utterances_histories": contexts}).json()
-    print()
 
     assert [
         len(sample[0]) > 0 and all([len(text) > 0 for text in sample[0]]) and all([conf > 0.0 for conf in sample[1]])
