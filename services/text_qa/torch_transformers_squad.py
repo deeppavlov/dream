3b93cf9562dd91d2bc2ea3a62b71685d1d248f32
Дмитрий Евсеев <dmitrij.euseew@yandex.ru>
2022-07-18 16:05:18 +0300

    codestyle

--- services/text_qa/torch_transformers_squad.py
+++ services/text_qa/torch_transformers_squad.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import math
 from logging import getLogger
 from pathlib import Path
 from typing import List, Tuple, Optional, Dict
