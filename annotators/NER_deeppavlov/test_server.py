f3b92b0971aa6846aae3f924596c0b129115b723
dilyararimovna <dilyara.rimovna@gmail.com>
2022-07-20 13:31:36 +0300

    fix: codfestyle

--- annotators/NER_deeppavlov/test_server.py
+++ annotators/NER_deeppavlov/test_server.py
@@ -16,29 +16,29 @@ def main():
     gold_results = [
         [
             [
-                {'confidence': 1, 'end_pos': 14, 'start_pos': 9, 'text': 'ивана', 'type': 'PER'}, 
-                {'confidence': 1, 'end_pos': 23, 'start_pos': 17, 'text': 'москве', 'type': 'LOC'}
+                {"confidence": 1, "end_pos": 14, "start_pos": 9, "text": "ивана", "type": "PER"},
+                {"confidence": 1, "end_pos": 23, "start_pos": 17, "text": "москве", "type": "LOC"},
             ]
-        ], 
+        ],
         [
             [
-                {'confidence': 1, 'end_pos': 14, 'start_pos': 9, 'text': 'Ивана', 'type': 'PER'}, 
-                {'confidence': 1, 'end_pos': 23, 'start_pos': 17, 'text': 'Москве', 'type': 'LOC'}
+                {"confidence": 1, "end_pos": 14, "start_pos": 9, "text": "Ивана", "type": "PER"},
+                {"confidence": 1, "end_pos": 23, "start_pos": 17, "text": "Москве", "type": "LOC"},
             ]
-        ], 
+        ],
         [
             [
-                {'confidence': 1, 'end_pos': 25, 'start_pos': 19, 'text': 'justin', 'type': 'ORG'}, 
-                {'confidence': 1, 'end_pos': 42, 'start_pos': 36, 'text': 'sahara', 'type': 'LOC'}
+                {"confidence": 1, "end_pos": 25, "start_pos": 19, "text": "justin", "type": "ORG"},
+                {"confidence": 1, "end_pos": 42, "start_pos": 36, "text": "sahara", "type": "LOC"},
             ]
-        ], 
+        ],
         [
             [
-                {'confidence': 1, 'end_pos': 25, 'start_pos': 19, 'text': 'Justin', 'type': 'PER'},
-                {'confidence': 1, 'end_pos': 42, 'start_pos': 36, 'text': 'Sahara', 'type': 'ORG'},
-                {'confidence': 1, 'end_pos': 50, 'start_pos': 43, 'text': 'Dessert', 'type': 'ORG'}
+                {"confidence": 1, "end_pos": 25, "start_pos": 19, "text": "Justin", "type": "PER"},
+                {"confidence": 1, "end_pos": 42, "start_pos": 36, "text": "Sahara", "type": "ORG"},
+                {"confidence": 1, "end_pos": 50, "start_pos": 43, "text": "Dessert", "type": "ORG"},
             ]
-        ]
+        ],
     ]
 
     result = requests.post(url, json=request_data).json()
