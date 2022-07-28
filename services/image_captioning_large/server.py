9ec31c202192aded9b87709f2a82d04d3018b3db
Daria Mitciuk <55624698+dariamitciuk@users.noreply.github.com>
2022-07-28 11:35:44 +0300

    comments

--- services/image_captioning_large/server.py
+++ services/image_captioning_large/server.py
@@ -1,7 +1,7 @@
 import logging
 import time
 import os
- 
+
 from transformers import BertTokenizer, BertForMaskedLM
 import torch
 from flask import Flask, request, jsonify
@@ -16,58 +16,50 @@ import skimage.io as io
 import PIL.Image
 from model import ClipCaptionModel, generate_beam, generate2
 from pathlib import Path
- 
+
 sentry_sdk.init(dsn=os.getenv("SENTRY_DSN"), integrations=[FlaskIntegration()])
- 
+
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
- 
-D = torch.device
-CPU = torch.device('cpu')
- 
- 
-def get_device(device_id: int) -> D:
+
+device = 'cpu' or 'cuda'
+
+def get_device(device_id: int):
    if not torch.cuda.is_available():
-       return CPU
+       return torch.device(device)
    device_id = min(torch.cuda.device_count() - 1, device_id)
    return torch.device(f'cuda:{device_id}')
- 
- 
-CUDA = get_device
- 
-PRETRAINED_MODEL_PATH = os.environ.get("PRETRAINED_MODEL_PATH")
 
- 
 try:
-   device = CUDA(0)
- 
+   device = get_device(0)
+
    clip_model, preprocess = clip.load("ViT-B/32", device=device, jit=False)
    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
- 
+
    prefix_length = 10
- 
+
    model = ClipCaptionModel(prefix_length)
- 
-   model.load_state_dict(torch.load('/opt/conda/lib/python3.7/site-packages/data/models/coco_weights.pt', map_location=CPU))
- 
+
+   model.load_state_dict(torch.load('/opt/conda/lib/python3.7/site-packages/data/models/coco_weights.pt', map_location=torch.device(device)))
+
    model = model.eval()
    model = model.to(device)
- 
+
    logger.info("image captioning model is ready")
 except Exception as e:
    sentry_sdk.capture_exception(e)
    logger.exception(e)
    raise e
- 
+
 app = Flask(__name__)
 health = HealthCheck(app, "/healthcheck")
 logging.getLogger("werkzeug").setLevel("WARNING")
- 
- 
+
+
 @app.route("/respond", methods=["POST"])
 def respond():
    st_time = time.time()
- 
+
    image_paths = request.json.get("text", [])
    try:
         images = None
@@ -75,8 +67,8 @@ def respond():
         for image_path in image_paths:
             image = io.imread(image_path)
             pil_image = PIL.Image.fromarray(image)
-            max_size = (256, 256)
-            pil_image.thumbnail(max_size)
+            # max_size = (256, 256)
+            pil_image.thumbnail(os.getenv("MAX_SIZE"))
             image = preprocess(pil_image).unsqueeze(0).to(device)
             if images is None:
                 images = image
@@ -94,7 +86,7 @@ def respond():
         logger.exception(exc)
         sentry_sdk.capture_exception(exc)
         generated_text_prefix = ""
- 
+
    total_time = time.time() - st_time
    logger.info(f"image_captioning_large exec time: {total_time:.3f}s")
    return jsonify({"captions": generated_text_prefixes})
