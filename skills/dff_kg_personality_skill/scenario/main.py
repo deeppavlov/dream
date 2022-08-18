09c8c6e2e5ef182aa194b30041fb98334f8ceb30
Yana Shishkina <yanaalekseevna2000@mail.ru>
2022-08-18 17:29:25 +0300

    try adding entities to graph

--- skills/dff_kg_personality_skill/scenario/main.py
+++ skills/dff_kg_personality_skill/scenario/main.py
@@ -18,94 +18,119 @@ from . import response as loc_rsp
 
 logger = logging.getLogger(__name__)
 
+# flows = {
+#     GLOBAL: {
+#         TRANSITIONS: {
+#             ("greeting", "node1"): cnd.regexp(r"\bhi\b"),
+#         },
+#     },
+#     "sevice": {
+#         "start": {
+#             RESPONSE: "",
+#             # TRANSITIONS: {("greeting", "node1"): cnd.true()},
+#             TRANSITIONS: {"new_node1": cnd.true()},
+#         },
+#         "fallback": {
+#             RESPONSE: "Ooops",
+#             TRANSITIONS: {
+#                 lbl.previous(): cnd.regexp(r"previous", re.IGNORECASE),
+#                 lbl.repeat(0.2): cnd.true(),
+#             },
+#         },
+#         "new_node1": {
+#             RESPONSE: 'What is your name?',
+#             PROCESSING: {
+#                 "set_confidence": int_prs.set_confidence(1.0),
+#                 "set_can_continue": int_prs.set_can_continue()},
+#             TRANSITIONS: {"new_node2": cnd.true()},
+#         },
+#         "new_node2": {
+#             RESPONSE: loc_rsp.find_name,
+#             PROCESSING: {
+#                 "set_confidence": int_prs.set_confidence(1.0),
+#                 "set_can_continue": int_prs.set_can_continue()},
+#             TRANSITIONS: {("greeting", "node1"): loc_cnd.example_lets_talk_about()},
+#         },
+#     },
+#     "greeting": {
+#         LOCAL: {
+#             PROCESSING: {
+#                 "set_confidence": int_prs.set_confidence(1.0),
+#                 "set_can_continue": int_prs.set_can_continue(),
+#                 # "fill_responses_by_slots": int_prs.fill_responses_by_slots(),
+#             },
+#         },
+#         "node1": {
+#             RESPONSE: 'What is your name?',
+#             TRANSITIONS: {"node2": cnd.true()},
+#         },
+#         "node2": {
+#             RESPONSE: loc_rsp.find_name,
+#             TRANSITIONS: {"node3": loc_cnd.example_lets_talk_about()},
+#         },
+#         "node3": {
+#             RESPONSE: "Sorry, I can not talk about that now. Maybe late. Do you like {topic}?",
+#             PROCESSING: {"fill_responses_by_slots": int_prs.fill_responses_by_slots()},
+#             TRANSITIONS: {
+#                 "node4": int_cnd.is_yes_vars,
+#                 "node5": int_cnd.is_no_vars,
+#                 "node6": int_cnd.is_do_not_know_vars,
+#                 "node7": cnd.true(),  # it will be chosen if other conditions are False
+#             },
+#         },
+#         "node4": {
+#             RESPONSE: "I like {topic} too, {user_name}",
+#             PROCESSING: {"fill_responses_by_slots": int_prs.fill_responses_by_slots()},
+#             TRANSITIONS: {("node7", 0.1): cnd.true()},
+#         },
+#         "node5": {
+#             RESPONSE: "I do not like {topic} too, {user_name}",
+#             PROCESSING: {"fill_responses_by_slots": int_prs.fill_responses_by_slots()},
+#             TRANSITIONS: {("node7", 0.1): cnd.true()},
+#         },
+#         "node6": {
+#             RESPONSE: "I have no opinion about {topic} too, {user_name}",
+#             PROCESSING: {"fill_responses_by_slots": int_prs.fill_responses_by_slots()},
+#             TRANSITIONS: {("node7", 0.1): cnd.true()},
+#         },
+#         "node7": {
+#             RESPONSE: int_rsp.multi_response(
+#                 replies=["bye", "goodbye"],
+#                 confidences=[1.0, 0.5],
+#                 hype_attr=[
+#                     {"can_continue": common_constants.MUST_CONTINUE},  # for the first hyp
+#                     {"can_continue": common_constants.CAN_CONTINUE_SCENARIO},  # for the second hyp
+#                 ],
+#             ),
+#             PROCESSING: {"set_confidence": int_prs.set_confidence(0.0)},
+#         },
+#     },
+# }
+
 flows = {
-    GLOBAL: {
-        TRANSITIONS: {
-            ("greeting", "node1"): cnd.regexp(r"\bhi\b"),
-        },
-    },
-    "sevice": {
-        "start": {
+    GLOBAL: {TRANSITIONS: {("story_flow", "fallback_node"): cnd.true()}},
+    "story_flow": {
+        "start_node": {
             RESPONSE: "",
-            # TRANSITIONS: {("greeting", "node1"): cnd.true()},
-            TRANSITIONS: {"new_node1": cnd.true()},
-        },
-        "fallback": {
-            RESPONSE: "Ooops",
             TRANSITIONS: {
-                lbl.previous(): cnd.regexp(r"previous", re.IGNORECASE),
-                lbl.repeat(0.2): cnd.true(),
-            },
+                "choose_story_node": cnd.true()},
         },
-        "new_node1": {
-            RESPONSE: 'What is your name?',
-            PROCESSING: {
-                "set_confidence": int_prs.set_confidence(1.0),
-                "set_can_continue": int_prs.set_can_continue()},
-            TRANSITIONS: {"new_node2": cnd.true()},
-        },
-        "new_node2": {
-            RESPONSE: loc_rsp.find_name,
-            PROCESSING: {
-                "set_confidence": int_prs.set_confidence(1.0),
-                "set_can_continue": int_prs.set_can_continue()},
-            TRANSITIONS: {("greeting", "node1"): loc_cnd.example_lets_talk_about()},
-        },
-    },
-    "greeting": {
-        LOCAL: {
-            PROCESSING: {
-                "set_confidence": int_prs.set_confidence(1.0),
-                "set_can_continue": int_prs.set_can_continue(),
-                # "fill_responses_by_slots": int_prs.fill_responses_by_slots(),
-            },
-        },
-        "node1": {
-            RESPONSE: 'What is your name?',
-            TRANSITIONS: {"node2": cnd.true()},
-        },
-        "node2": {
-            RESPONSE: loc_rsp.find_name,
-            TRANSITIONS: {"node3": loc_cnd.example_lets_talk_about()},
-        },
-        "node3": {
-            RESPONSE: "Sorry, I can not talk about that now. Maybe late. Do you like {topic}?",
-            PROCESSING: {"fill_responses_by_slots": int_prs.fill_responses_by_slots()},
+        "choose_story_node": {
+            RESPONSE: 'You are on the first node!',
             TRANSITIONS: {
-                "node4": int_cnd.is_yes_vars,
-                "node5": int_cnd.is_no_vars,
-                "node6": int_cnd.is_do_not_know_vars,
-                "node7": cnd.true(),  # it will be chosen if other conditions are False
+                "which_story_node": cnd.true(),
             },
         },
-        "node4": {
-            RESPONSE: "I like {topic} too, {user_name}",
-            PROCESSING: {"fill_responses_by_slots": int_prs.fill_responses_by_slots()},
-            TRANSITIONS: {("node7", 0.1): cnd.true()},
+        "which_story_node": {
+            RESPONSE: "You are on the second node!",
         },
-        "node5": {
-            RESPONSE: "I do not like {topic} too, {user_name}",
-            PROCESSING: {"fill_responses_by_slots": int_prs.fill_responses_by_slots()},
-            TRANSITIONS: {("node7", 0.1): cnd.true()},
-        },
-        "node6": {
-            RESPONSE: "I have no opinion about {topic} too, {user_name}",
-            PROCESSING: {"fill_responses_by_slots": int_prs.fill_responses_by_slots()},
-            TRANSITIONS: {("node7", 0.1): cnd.true()},
-        },
-        "node7": {
-            RESPONSE: int_rsp.multi_response(
-                replies=["bye", "goodbye"],
-                confidences=[1.0, 0.5],
-                hype_attr=[
-                    {"can_continue": common_constants.MUST_CONTINUE},  # for the first hyp
-                    {"can_continue": common_constants.CAN_CONTINUE_SCENARIO},  # for the second hyp
-                ],
-            ),
-            PROCESSING: {"set_confidence": int_prs.set_confidence(0.0)},
+        "fallback_node": {
+            RESPONSE: 'This is fallback node!',
+            TRANSITIONS: {"start_node": cnd.true()},
         },
     },
 }
 
 
-actor = Actor(flows, start_label=("sevice", "start"), fallback_label=("sevice", "fallback"))
+# actor = Actor(flows, start_label=("sevice", "start"), fallback_label=("sevice", "fallback"))
+actor = Actor(flows, start_label=("story_flow", "start_node"), fallback_label=("story_flow", "fallback_node"))
