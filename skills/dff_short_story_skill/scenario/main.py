c32a792dd00b63e87d493059df61ee2da7bd047c
Yana Shishkina <yanaalekseevna2000@mail.ru>
2022-08-21 18:08:37 +0300

    check in other skill

--- skills/dff_short_story_skill/scenario/main.py
+++ skills/dff_short_story_skill/scenario/main.py
@@ -28,15 +28,15 @@ flows = {
             },
         },
         "choose_story_node": {
-            RESPONSE: loc_rsp.choose_story,
+            RESPONSE: loc_rsp.find_name,
             TRANSITIONS: {
                 "tell_punchline_node": cnd.any([int_cnd.is_yes_vars, int_cnd.is_do_not_know_vars]),
                 "which_story_node": int_cnd.is_no_vars,
             },
         },
         "which_story_node": {
-            RESPONSE: loc_rsp.which_story,
-            TRANSITIONS: {"choose_story_node": cnd.all([loc_cnd.has_story_type, loc_cnd.has_story_left])},
+            RESPONSE: 'What is your name?',
+            TRANSITIONS: {"choose_story_node": cnd.all([loc_cnd.has_story_left])},
         },
         "tell_punchline_node": {
             RESPONSE: loc_rsp.tell_punchline,
