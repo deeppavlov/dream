1569dfc3e4ce6dcf2c63ff628c2f9e8cf7ffc651
dilyararimovna <dilyara.rimovna@gmail.com>
2022-08-29 18:31:08 +0300

    fix: tests

--- annotators/relative_persona_extractor/test.py
+++ annotators/relative_persona_extractor/test.py
@@ -1,13 +1,22 @@
-import os
 import requests
 
 
-SERVICE_PORT = int(os.getenv("SERVICE_PORT"))
-
-
 def main():
-    url = f"http://0.0.0.0:{SERVICE_PORT}/respond"
-    input_data = {"sentences": ["джейсон стетхэм хочет есть."]}
+    url = f"http://0.0.0.0:8133/respond"
+    input_data = {
+        "contexts": [
+            [
+                "How do you spend your spare time?",
+                "I like to watch movies and eat pizza.",
+                "Cool! What else do you like?"
+            ],
+            [
+                "I like to go to the cinema on fridays"
+                "great. how do you spend your spare time?",
+                "I like to watch movies and eat pizza.",
+            ],
+        ]
+    }
     gold = []
 
     result = requests.post(url, json=input_data).json()
