bcd32bfa8ab7caa4d464761c4d31f4d6e03d8a1c
dilyararimovna <dilyara.rimovna@gmail.com>
2022-08-30 12:50:12 +0300

    fix: tests

--- annotators/relative_persona_extractor/server.py
+++ annotators/relative_persona_extractor/server.py
@@ -43,7 +43,7 @@ def get_result(request):
             most_relevant_sent_ids = np.argsort(scores[curr_ids])[::-1][:N_SENTENCES_OT_RETURN]
             curr_result = {
                 "persona": [PERSONA_SENTENCES[_id] for _id in most_relevant_sent_ids],
-                "max_similarity": scores[curr_ids][most_relevant_sent_ids[0]]
+                "max_similarity": scores[curr_ids][most_relevant_sent_ids[0]],
             }
             result += [curr_result]
     except Exception as exc:
