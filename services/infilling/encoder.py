83c3f69c5f801c7258b4eb19b1d88312c4b0b5bf
Dmitry <dimakarp1996@github.com>
2022-09-17 14:08:40 +0300

    reformat using black 22.8.0 as suggested by Fedor

--- services/infilling/encoder.py
+++ services/infilling/encoder.py
@@ -18,10 +18,10 @@ def bytes_to_unicode():
     bs = list(range(ord("!"), ord("~") + 1)) + list(range(ord("¡"), ord("¬") + 1)) + list(range(ord("®"), ord("ÿ") + 1))
     cs = bs[:]
     n = 0
-    for b in range(2 ** 8):
+    for b in range(2**8):
         if b not in bs:
             bs.append(b)
-            cs.append(2 ** 8 + n)
+            cs.append(2**8 + n)
             n += 1
     cs = [chr(n) for n in cs]
     return dict(zip(bs, cs))
