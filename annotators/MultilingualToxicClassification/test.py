533259a5d3e03618e3ba0368707d68e5289494a8
dilyararimovna <dilyara.rimovna@gmail.com>
2022-09-27 15:15:39 +0300

    fix: test values

--- annotators/MultilingualToxicClassification/test.py
+++ annotators/MultilingualToxicClassification/test.py
@@ -7,33 +7,34 @@ def test_respond():
     sentences = ["Tú morón", "örnek metin", "Я тебя ненавижу"]
     gold = [
         {
-            "identity_attack": 0.0016213383059948683,
-            "insult": 0.4965558648109436,
-            "obscene": 0.03552895411849022,
-            "severe_toxicity": 0.0010808489751070738,
-            "sexual_explicit": 0.0037958528846502304,
-            "threat": 0.003585097612813115,
-            "toxicity": 0.7588052749633789,
+            "identity_attack": 0.00162,
+            "insult": 0.49656,
+            "obscene": 0.03553,
+            "severe_toxicity": 0.00108,
+            "sexual_explicit": 0.0038,
+            "threat": 0.00359,
+            "toxicity": 0.75881,
         },
         {
-            "identity_attack": 0.0031340355053544044,
-            "insult": 0.01786503940820694,
-            "obscene": 0.021325934678316116,
-            "severe_toxicity": 0.002832308877259493,
-            "sexual_explicit": 0.0006158832111395895,
-            "threat": 0.001326677156612277,
-            "toxicity": 0.0005907031591050327,
+            "identity_attack": 0.00313,
+            "insult": 0.01787,
+            "obscene": 0.02133,
+            "severe_toxicity": 0.00283,
+            "sexual_explicit": 0.00062,
+            "threat": 0.00133,
+            "toxicity": 0.00059,
         },
         {
-            "identity_attack": 0.0075791277922689915,
-            "insult": 0.0890873447060585,
-            "obscene": 0.052630290389060974,
-            "severe_toxicity": 0.005724500864744186,
-            "sexual_explicit": 0.00832817517220974,
-            "threat": 0.011315466836094856,
-            "toxicity": 0.9545190930366516,
+            "identity_attack": 0.00758,
+            "insult": 0.08909,
+            "obscene": 0.05263,
+            "severe_toxicity": 0.00572,
+            "sexual_explicit": 0.00833,
+            "threat": 0.01132,
+            "toxicity": 0.95452,
         },
     ]
+
     request_data = {"sentences": sentences}
     result = requests.post(url, json=request_data).json()
     assert [{i: round(j[i], 5) for i in j} for j in result] == gold, f"Got\n{result}"
