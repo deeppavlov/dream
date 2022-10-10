48de78db39b495279f7094d9521cf385a05f673a
dimakarp1996 <dimakarp1996@yandex.ru>
2022-10-10 21:17:16 +0300

    Update test.py

--- annotators/combined_classification/test.py
+++ annotators/combined_classification/test.py
@@ -45,7 +45,7 @@ def main_test():
          "answers_bert": [["Games"]], 
          "multilabel": True},
         {
-            "sentences": ["What is the capital of Great Britain [SEP] I don't know"],
+            "sentences_with_history": ["What is the capital of Great Britain [SEP] I don't know"],
             "task": "cobot_dialogact_intents",
             "answers_bert": [["Topic_SwitchIntent"]],
             "multilabel":True
