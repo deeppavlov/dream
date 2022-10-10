c1ed326121841f211f75302d2ac3a5934ce3c529
dimakarp1996 <dimakarp1996@yandex.ru>
2022-10-10 18:12:53 +0300

    Update server.py

--- annotators/combined_classification/server.py
+++ annotators/combined_classification/server.py
@@ -9,16 +9,6 @@ from sentry_sdk.integrations.flask import FlaskIntegration
 from deeppavlov import build_model
 from common.utils import combined_classes
 
-task_names = [
-    "cobot_topics",
-    "cobot_dialogact_topics",
-    "cobot_dialogact_intents",
-    "emotion_classification",
-    "sentiment_classification",
-    "toxic_classification",
-    "factoid_classification",
-]  # ORDER MATTERS!
-
 logger = logging.getLogger(__name__)
 sentry_sdk.init(dsn=os.getenv("SENTRY_DSN"), integrations=[FlaskIntegration()])
 
@@ -38,31 +28,25 @@ def get_result(sentences, sentences_with_history):
 
     try:
         if sentences and sentences_with_history:
-            res = model(sentences, sentences_with_history)
+            prob_lists = model(sentences, sentences_with_history)
         else:
             raise Exception(
                 f"Empty list of sentences or sentences with history received."
                 f"Sentences: {sentences} "
                 f"Sentences with history: {sentences_with_history}"
             )
-
-        for name, value in zip(task_names, res):
-            for i in range(len(value)):
-                is_toxic = "toxic" in name and value[i][-1] < 0.5
-                if is_toxic:  # sum of probs of all toxic classes >0.5
-                    value[i][-1] = 0
-                    value[i] = [k / sum(value[i]) for k in value[i]]
-                for class_, prob in zip(combined_classes[name], value[i]):
-                    if prob == max(value[i]):
-                        if class_ != "not_toxic" and name == "toxic_classification":
-                            prob = 1
-                        ans[i][name] = {class_: float(prob)}
+        for task_name, prob_list in zip(combined_classes, prob_lists):
+            # we assume toxic has 7 classes
+            for i in range(len(prob_list)):
+                ans[i][task_name] = {
+                    class_: float(prob) for class_, prob in zip(combined_classes[task_name], prob_list[i])
+                }
     except Exception as e:
         sentry_sdk.capture_exception(e)
         logger.exception(e)
 
     total_time = time.time() - st_time
-    logger.info(f"7in1 exec time: {total_time:.3f}s")
+    logger.info(f"Combined classifier exec time: {total_time:.3f}s")
     logger.info(ans)
     return ans
 
@@ -81,24 +65,30 @@ except Exception as e:
 @app.route("/model", methods=["POST"])
 def respond():
     t = time.time()
+    logger.info("Respond")
     sentences = request.json.get("sentences", [" "])
     sentences_with_hist = request.json.get("sentences_with_history", sentences)
+    logger.info(str((sentences, sentences_with_hist)))
     answer = get_result(sentences, sentences_with_hist)
 
-    logger.info(f"7in1 result: {answer}")
+    logger.info(f"9in1 result: {answer}")
     logger.info(f"Combined classifier exec time: {time.time() - t}")
     return jsonify(answer)
 
 
 @app.route("/batch_model", methods=["POST"])
 def batch_respond():
+    logger.info("Batch respond")
     t = time.time()
+    sep = " [SEP] "
     utterances_with_histories = request.json.get("utterances_with_histories", [[" "]])
-    sentences_with_hist = [" [SEP] ".join(s) for s in utterances_with_histories]
-    sentences = [s[-1] for s in utterances_with_histories]
+    logger.info(utterances_with_histories)
+    sentences_with_hist = [sep.join(s) for s in utterances_with_histories]
+    sentences = [s[-1].split(sep)[-1] for s in utterances_with_histories]
+    logger.info(str((sentences, sentences_with_hist)))
     answer = get_result(sentences, sentences_with_hist)
 
-    logger.info(f"7in1 batch result: {answer}")
+    logger.info(f"9in1 batch result: {answer}")
     logger.info(f"Combined classifier exec time: {time.time() - t}")
     return jsonify([{"batch": answer}])
 
