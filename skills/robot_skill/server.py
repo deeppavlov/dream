05ed9fbaf2a99603d49ae232dc42a9fe27eebe4a
Linar Abdrazakov <linar200015@gmail.com>
2022-10-24 12:19:03 -0400

    fix aiohttp in robot_skill

--- skills/robot_skill/server.py
+++ skills/robot_skill/server.py
@@ -4,8 +4,9 @@ from typing import Dict
 import aiohttp
 from fastapi import FastAPI
 
+import json
 
-ROS_FSM_SERVER = "172.17.0.1:5000"
+ROS_FSM_SERVER = "http://172.17.0.1:5000"
 # Please see https://stackoverflow.com/a/62002628
 ROS_FSM_STATUS_ENDPOINT = f"{ROS_FSM_SERVER}/robot_status"
 ROS_FSM_INTENT_ENDPOINT = f"{ROS_FSM_SERVER}/upload_intent"
@@ -17,21 +18,15 @@ app = FastAPI()
 
 @app.post("/respond")
 async def respond(body: Dict):
-    logger.info(f"Received from Dream:\n{body}")
+    logger.error(f"Received from Dream:\n{body}")
 
     async with aiohttp.ClientSession() as sess:
         async with sess.get(ROS_FSM_STATUS_ENDPOINT) as status_response:
-            is_free = await status_response.json()["status"] == "free"
+            is_free = (await status_response.json()).get("status") == "free"
+
+            logger.error(f"Robot status: {is_free}")
 
             if is_free:
                 payload = body  # fix this to actually post what you need
-                async with sess.post(
-                    ROS_FSM_INTENT_ENDPOINT,
-                    data=payload,
-                ) as intent_response:
-                    ros_fsm_data = await intent_response.json()
-                    response = ros_fsm_data
-            else:
-                response = "The robot is busy at the moment. Try again later!"
-
-    return [response]   # wrap response in a list
+                await sess.post(ROS_FSM_INTENT_ENDPOINT, data=json.dumps(payload))
+    return [body]
