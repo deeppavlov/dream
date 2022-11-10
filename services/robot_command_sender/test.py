285d2e7d34aba523f9809201fb180923426e716b
dilyararimovna <dilyara.rimovna@gmail.com>
2022-11-10 16:37:52 +0300

    fix: tests

---
+++ services/robot_command_sender/test.py
@@ -0,0 +1,26 @@
+import requests
+
+
+def main():
+    url = "http://0.0.0.0:8135/send"
+
+    request_data = {
+        "annotated_bot_utterances": [{
+            "text": "Moving forward for 10 meters",
+            "confidence": 1.0,
+            "attributes": {
+                "robot_command": "move_forward_10"
+            },
+        }]
+    }
+
+    result = requests.post(url, json=request_data).json()
+    print(result)
+    gold_result = ["Failed"]
+
+    assert result == gold_result, f"Got\n{result}\n, but expected:\n{gold_result}"
+    print("Success")
+
+
+if __name__ == "__main__":
+    main()
