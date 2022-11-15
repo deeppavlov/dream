d9c1f4f147881261b82c2550492c22964d3e5f75
dimakarp1996 <dimakarp1996@yandex.ru>
2022-11-15 19:01:05 +0300

    Update server.py

--- annotators/combined_classification/server.py
+++ annotators/combined_classification/server.py
@@ -48,7 +48,7 @@ def get_result(sentences, sentences_with_history, postannotations=False):
         for task_name, prob_list in zip(combined_classes, prob_lists):
             for i in range(len(prob_list)):
                 if prob_list[i]:
-                    is_toxic = "toxic" in name and prob_list[i][-1] < 0.5
+                    is_toxic = "toxic" in task_name and prob_list[i][-1] < 0.5
                     if is_toxic:  # sum of probs of all toxic classes >0.5
                         prob_list[i][-1] = 0
                         prob_list[i] = [k / sum(prob_list[i]) for k in prob_list[i]]
