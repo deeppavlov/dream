068c9146b594cf382d0f705b887a71a4c5ca43bb
dilyararimovna <dilyara.rimovna@gmail.com>
2022-11-22 13:41:30 +0800

    fix: change param name

--- skill_selectors/rule_based_selector/connector.py
+++ skill_selectors/rule_based_selector/connector.py
@@ -52,8 +52,8 @@ class RuleBasedSkillSelectorConnector:
                 logger.info(f"rule_based_selector exec time = {total_time:.3f}s")
                 # returning empty list of skills means trigger ALL skills for deeppavlov agent
                 asyncio.create_task(callback(task_id=payload["task_id"], response=[]))
-            elif ALWAYS_TURN_ON_GIVEN_SKILL and user_uttr.get("only_skill", "") != "":
-                skills_for_uttr = [user_uttr.get("only_skill", "")]
+            elif ALWAYS_TURN_ON_GIVEN_SKILL and user_uttr.get("selected_skill", "") != "":
+                skills_for_uttr = [user_uttr.get("selected_skill", "")]
                 logger.info(f"Selected skills: {skills_for_uttr}")
                 total_time = time.time() - st_time
                 logger.info(f"rule_based_selector exec time = {total_time:.3f}s")
