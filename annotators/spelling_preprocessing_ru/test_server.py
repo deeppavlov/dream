e7672cebd2e7ff977b7ca2e2229504606c15a6f7
kpetyxova <kpetyxova@mail.ru>
2022-12-02 12:58:53 +0300

    changed spelling_preprocessing_ru in order to work with english too

--- annotators/spelling_preprocessing_ru/test_server.py
+++ annotators/spelling_preprocessing_ru/test_server.py
@@ -1,12 +1,21 @@
 import requests
+import os
+
+config_name = os.getenv("CONFIG")
 
 
 def main():
+
     url = "http://0.0.0.0:8074/respond"
+    if config_name == "levenshtein_corrector_ru.json":
+        request_data = [{"sentences": ["я ге видел малако"]}]
 
-    request_data = [{"sentences": ["я ге видел малако"]}]
+        gold_results = [["я не видел малакон"]]
+    
+    else:
+        request_data = [{"sentences": ["tge shop is cloed"]}]
 
-    gold_results = [["я не видел малакон"]]
+        gold_results = [["the shop is closed"]]
 
     count = 0
     for data, gold_result in zip(request_data, gold_results):
