bab262942aa2915accaa67814f547db4f09c80fb
kpetyxova <kpetyxova@mail.ru>
2022-12-06 16:33:08 +0300

    fix: deepy_base distribution

--- annotators/emotion_classification_deepy/bert_float_classifier.py
+++ annotators/emotion_classification_deepy/bert_float_classifier.py
@@ -16,14 +16,14 @@ import logging
 from os import getenv
 from typing import List, Union
 
-import sentry_sdk
+# import sentry_sdk
 from bert_dp.preprocessing import InputFeatures
 from overrides import overrides
 
 from deeppavlov.core.common.registry import register
 from deeppavlov.models.bert.bert_classifier import BertClassifierModel
 
-sentry_sdk.init(getenv("SENTRY_DSN"))
+# sentry_sdk.init(getenv("SENTRY_DSN"))
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
