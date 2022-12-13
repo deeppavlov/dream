8d626bbccd6b4fdae1bbd876fe68187ce30791ea
dimakarp1996 <dimakarp1996@yandex.ru>
2022-12-13 12:51:04 +0300

    Update art.py

--- common/art.py
+++ common/art.py
@@ -8,3 +8,12 @@ BIT_LOWER_CONFIDENCE = 0.90
 ZERO_CONFIDENCE = 0.0
 
 ART_PATTERN = re.compile(r"\b(art|artist|drawing|painting|painter|gallery)(\.|\?|\b)", re.IGNORECASE)
+
+def check_about_art(user_uttr):
+    found_topics = get_topics(user_uttr, probs=False, which="all")
+    if any([art_topic in found_topics for art_topic in topic_groups["art"]):
+        return True
+    elif re.findall(ART_PATTERN, user_uttr["text"]):
+        return True
+    else:
+        return False
