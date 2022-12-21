0a033da5bdef421314df3a046b9bb163f32affe1
dilyararimovna <dilyara.rimovna@gmail.com>
2022-12-21 13:43:00 +0800

    fix: test for fake server

--- services/robot_fake_server/test.py
+++ services/robot_fake_server/test.py
@@ -2,23 +2,24 @@ import requests
 
 
 def main():
-    url = "http://0.0.0.0:8135/send"
+    url = "http://0.0.0.0:8137"
 
-    request_data = {
-        "annotated_bot_utterances": [
-            {
-                "text": "Moving forward for 10 meters",
-                "confidence": 1.0,
-                "attributes": {"robot_command": "move_forward_10"},
-            }
-        ]
-    }
-
-    result = requests.post(url, json=request_data).json()
-    print(result)
-    gold_result = [{"human_attributes": {}}]
-
-    assert result == gold_result, f"Got\n{result}\n, but expected:\n{gold_result}"
+    request_datas = [
+        {
+            "command": "move_backward_10",
+            "dialog_ids": "test_dialog_id"
+        },
+        {
+            "command": "move_forward_10",
+            "dialog_ids": "test_dialog_id"
+        }
+    ]
+    for endpoint in ["is_command_valid", "perform_command", "is_command_performed"]:
+        for request_data in request_datas:
+            result = requests.post(f"{url}/{endpoint}", json=request_data).json()
+            print(result)
+            gold_result = [{"human_attributes": {}}]
+            assert result == gold_result, f"Got\n{result}\n, but expected:\n{gold_result}"
     print("Success")
 
 
