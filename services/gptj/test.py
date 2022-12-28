8717d10f03fa06167c4ca2b1ff456b2383054fe9
Nika Smilga <sunny-august@mail.ru>
2022-12-28 14:18:50 +0300

    test for gptj

--- services/gptj/test.py
+++ services/gptj/test.py
@@ -6,20 +6,24 @@ N_HYPOTHESES_TO_GENERATE = int(os.environ.get("N_HYPOTHESES_TO_GENERATE", 1))
 
 
 def test_respond():
-    print('1')
     url = "http://0.0.0.0:8126/respond"
-    print('2')
-    instruction = '''Instruction: You are a travel guide bot. You are informative, creative, and know a lot about landmarks.
-Give an answer to the human's question.'''
-    contexts = [["Hello, who are you?", "I am Marcus, your travel guide. How can I help you today?",
-    "Where can I spend an evening in Beirut?", "You can catch a play at Baalbeck International Festival, or go for a sailing trip on the Mediterranean Sea.", 
-    "Where can I have some fun in Goa?", "Goa has peaceful beaches and fun-filled pubs/clubs.",
-    "Where should I go in Goa if I want to drink some cocktails?"]]
+    contexts = [
+        [
+            """Instruction: You are a travel guide bot. You are informative, creative, and know a lot about landmarks. Give an answer to the human's question.""",
+            "Hello, who are you?",
+            "I am Marcus, your travel guide. How can I help you today?",
+            "Where can I spend an evening in Beirut?",
+            "You can catch a play at Baalbeck International Festival, or go for a sailing trip on the Mediterranean Sea.",
+            "Where can I have some fun in Goa?",
+            "Goa has peaceful beaches and fun-filled pubs/clubs.",
+            "Where should I go in Goa if I want to drink some cocktails?",
+        ]
+    ]
     result = requests.post(url, json={"utterances_histories": contexts}).json()
     assert [
         all(len(sample[0]) > 0 for sample in result)
     ], f"Got\n{result}\n, something is wrong"
-    print('Success')
+    print("Success")
     print(result)
 
 
