bd17394b9c39975ff7aade9694b492283193c3c9
Nika Smilga <sunny-august@mail.ru>
2022-12-30 14:32:00 +0300

    timeout for is_container_running as an arg

--- common/containers.py
+++ common/containers.py
@@ -1,10 +1,12 @@
 import requests
 
 
-def is_container_running(model_url):
+def is_container_running(model_url, timeout=4):
     try:
         requested_data = [{"speaker": "human", "text": "hi"}]
-        response = requests.post(model_url, json={"dialog_contexts": [requested_data]}, timeout=4)
+        response = requests.post(
+            model_url, json={"dialog_contexts": [requested_data]}, timeout=timeout
+        )
         if response.status_code == 200:
             return True
     except Exception as exc:
