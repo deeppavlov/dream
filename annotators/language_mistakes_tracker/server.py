73253ea57489b7384f69a33bae713b5b8bc0ac26
kpetyxova <kpetyxova@mail.ru>
2023-01-07 19:48:41 +0300

    add: scenarios descriptions in user attributes

--- annotators/language_mistakes_tracker/server.py
+++ annotators/language_mistakes_tracker/server.py
@@ -19,15 +19,28 @@ def update_mistakes_state(requested_data):
     results = []
     st_time = time.time()
     dialog = requested_data["dialog"]
+    logger.info(f"dialog: {dialog}")
+    # for filename in os.listdir("../skills/dff_language_practice_skill/data"):
+    #     f = os.path.join("../skills/dff_language_practice_skill/data", filename)
+    #     if os.path.isfile(f):
+    #         scenario = json.load(open(f))
     if dialog["bot_utterances"] != []:
         prev_active_skill = dialog["bot_utterances"][-1]["active_skill"]
+        scenarios_descriptions = dialog["human_utterances"][-1]["user"]["attributes"]["scenarios_descriptions"]
     else:
         prev_active_skill = None
+        scenarios_descriptions = {}
+        for filename in os.listdir("data"):
+            f = os.path.join("data", filename)
+            if os.path.isfile(f):
+                scenario = json.load(open(f))
+                scenarios_descriptions[filename.replace(".json", "")] = scenario["situation_description"]
+    
         
     not2review_skills = ["dff_mistakes_review_skill", "dff_friendship_skill"]
     if prev_active_skill in not2review_skills:
         mistakes_state = None
-        tracker = LanguageMistakes(initial_state=mistakes_state)     
+        tracker = LanguageMistakes(initial_state=mistakes_state)    
     else:
         try:
             mistakes_state = dialog["human"]["attributes"]["language_mistakes"]
@@ -37,7 +50,7 @@ def update_mistakes_state(requested_data):
         tracker.update_language_mistakes_tracker(dialog)
         
     new_state = tracker.dump_state()
-    results.append({"human_attributes": {"language_mistakes": new_state}})
+    results.append({"human_attributes": {"language_mistakes": new_state, "scenarios_descriptions": scenarios_descriptions}})
     total_time = time.time() - st_time
     logger.info(f"language_mistakes exec time: {total_time:.3f}s")
     logger.info(f"language_mistakes state: {new_state}")
