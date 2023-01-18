9898912d050cd1de2c1646f3215b982f0e702042
dilyararimovna <dilyara.rimovna@gmail.com>
2023-01-18 09:53:38 +0800

    fix: formatter

--- state_formatters/dream_persona.py
+++ state_formatters/dream_persona.py
@@ -1,5 +1,5 @@
 import state_formatters.utils as utils
 
 
-def dff_dream_persona_prompt_based_skill(dialog):
+def dff_dream_persona_prompt_based_skill_formatter(dialog):
     return utils.dff_formatter(dialog, "dff_dream_persona_prompt_based_skill")
