6eb35b625efb8608ccb02a5b04b497999438a56c
dilyararimovna <dilyara.rimovna@gmail.com>
2023-01-20 16:28:28 +0800

    fix: add types utterances

--- state_formatters/dream_persona.py
+++ state_formatters/dream_persona.py
@@ -2,4 +2,5 @@ import state_formatters.utils as utils
 
 
 def dff_dream_persona_prompt_based_skill_formatter(dialog):
-    return utils.dff_formatter(dialog, "dff_dream_persona_prompt_based_skill")
+    return utils.dff_formatter(dialog, "dff_dream_persona_prompt_based_skill",
+                               types_utterances=["human_utterances", "bot_utterances", "utterances"])
