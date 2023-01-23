0ac3cc358416b707e01d541d90aaef6d9543aa17
dilyararimovna <dilyara.rimovna@gmail.com>
2023-01-23 16:44:22 +0800

    fix: codestyle

--- annotators/relative_persona_extractor/test.py
+++ annotators/relative_persona_extractor/test.py
@@ -19,21 +19,21 @@ def main():
     }
     gold = [
         {
-            "max_similarity": 0.6948127746582031, 
+            "max_similarity": 0.6948127746582031,
             "persona": [
-                "I like Italian food especially pasta and pizza.", 
-                "I like to watch football and basketball on TV.", 
-                "I like watching travel video blogs."
-            ]
+                "I like Italian food especially pasta and pizza.",
+                "I like to watch football and basketball on TV.",
+                "I like watching travel video blogs.",
+            ],
         },
         {
-            "max_similarity": 0.6451027989387512, 
+            "max_similarity": 0.6451027989387512,
             "persona": [
-                "I like watching travel video blogs.", 
-                "I like to watch football and basketball on TV.", 
-                "I like Italian food especially pasta and pizza."
-            ]
-        }
+                "I like watching travel video blogs.",
+                "I like to watch football and basketball on TV.",
+                "I like Italian food especially pasta and pizza.",
+            ],
+        },
     ]
 
     result = requests.post(url, json=input_data).json()
