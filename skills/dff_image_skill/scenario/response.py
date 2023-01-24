03b8b57dbca2d58b14b2797c0fd30e97a1b563c0
dilyararimovna <dilyara.rimovna@gmail.com>
2023-01-24 15:32:47 +0800

    fix: refactoring and confs

--- skills/dff_image_skill/scenario/response.py
+++ skills/dff_image_skill/scenario/response.py
@@ -7,6 +7,8 @@ import scenario.processing as loc_prs
 logger = logging.getLogger(__name__)
 
 DEFAULT_RESPONSE = "Okay. Why did you send me this picture?"
+DEFAULT_CONFIDENCE = 0.85
+SUPER_CONFIDENCE = 1.0
 
 
 def animals_response(ctx: Context, actor: Actor, excluded_skills=None, *args, **kwargs) -> str:
@@ -19,19 +21,19 @@ def animals_response(ctx: Context, actor: Actor, excluded_skills=None, *args, **
     if caption != "" and caption is not None:
         animal = loc_prs.extract_entity(caption, loc_prs.get_all_possible_entities("animal"))
         if animal != "" and animal is not None:
-            int_ctx.set_confidence(ctx, actor, 1.0)
+            int_ctx.set_confidence(ctx, actor, SUPER_CONFIDENCE)
             response = random.choice(
                 [
-                    "What a muzzle! Would you like to have an animal like that?",
-                    "I would like to have an animal like that. I wonder if it eats a lot.",
+                    f"What a muzzle! Would you like to have {animal} like that?",
+                    f"I would like to have {animal} like that!",
                     f"Is it your {animal}?",
                 ]
             )
         else:
-            int_ctx.set_confidence(ctx, actor, 0.85)
+            int_ctx.set_confidence(ctx, actor, DEFAULT_CONFIDENCE)
             response = DEFAULT_RESPONSE
     else:
-        int_ctx.set_confidence(ctx, actor, 0.85)
+        int_ctx.set_confidence(ctx, actor, DEFAULT_CONFIDENCE)
         response = DEFAULT_RESPONSE
     logger.info(f"dff-image-skill animals response: {response}")
     return response
@@ -47,7 +49,7 @@ def food_response(ctx: Context, actor: Actor, excluded_skills=None, *args, **kwa
     if caption != "" and caption is not None:
         food = loc_prs.extract_entity(caption, loc_prs.get_all_possible_entities("food"))
         if food != "" and food is not None:
-            int_ctx.set_confidence(ctx, actor, 0.85)
+            int_ctx.set_confidence(ctx, actor, DEFAULT_CONFIDENCE)
             response = random.choice(
                 [
                     f"Did you cook this {food} by yourself? It looks delicious!",
@@ -55,10 +57,10 @@ def food_response(ctx: Context, actor: Actor, excluded_skills=None, *args, **kwa
                 ]
             )
         else:
-            int_ctx.set_confidence(ctx, actor, 0.85)
+            int_ctx.set_confidence(ctx, actor, DEFAULT_CONFIDENCE)
             response = DEFAULT_RESPONSE
     else:
-        int_ctx.set_confidence(ctx, actor, 0.85)
+        int_ctx.set_confidence(ctx, actor, DEFAULT_CONFIDENCE)
         response = DEFAULT_RESPONSE
     logger.info(f"dff-image-skill food response: {response}")
     return response
@@ -74,13 +76,24 @@ def people_response(ctx: Context, actor: Actor, excluded_skills=None, *args, **k
     if caption != "" and caption is not None:
         verb = loc_prs.extract_verb_from_sentence(caption)
         if verb != "" and verb is not None:
-            int_ctx.set_confidence(ctx, actor, 1.0)
-            response = f"Do you enjoy {verb} with other people?"
+            int_ctx.set_confidence(ctx, actor, SUPER_CONFIDENCE)
+            response = random.choice(
+                [
+                    f"Do you enjoy {verb} with other people?",
+                    f"Why do these people {verb}?",
+                ]
+            )
         else:
-            int_ctx.set_confidence(ctx, actor, 0.85)
-            response = DEFAULT_RESPONSE
+            int_ctx.set_confidence(ctx, actor, SUPER_CONFIDENCE)
+            response = random.choice(
+                [
+                    "Are they your friends?",
+                    "Do you know these people?",
+
+                ]
+            )
     else:
-        int_ctx.set_confidence(ctx, actor, 0.85)
+        int_ctx.set_confidence(ctx, actor, DEFAULT_CONFIDENCE)
         response = DEFAULT_RESPONSE
     logger.info(f"dff-image-skill people response: {response}")
     return response
@@ -94,7 +107,7 @@ def generic_response(ctx: Context, actor: Actor, excluded_skills=None, *args, **
         .get("caption", "")
     )
     if caption != "" and caption is not None:
-        int_ctx.set_confidence(ctx, actor, 1.0)
+        int_ctx.set_confidence(ctx, actor, SUPER_CONFIDENCE)
         response = random.choice(
             [
                 f"Cool! Why did you send me {caption}?",
@@ -102,7 +115,7 @@ def generic_response(ctx: Context, actor: Actor, excluded_skills=None, *args, **
             ]
         )
     else:
-        int_ctx.set_confidence(ctx, actor, 0.85)
+        int_ctx.set_confidence(ctx, actor, DEFAULT_CONFIDENCE)
         response = DEFAULT_RESPONSE
     logger.info(f"dff-image-skill generic response: {response}")
     return response
