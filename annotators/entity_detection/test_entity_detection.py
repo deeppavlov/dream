7708d5a66dbe0a266d4b21c2a5892f308132c99b
Raushan <raushan.turganbay@alumni.nu.edu.kz>
2023-02-03 08:54:36 +0300

    fix a typo

--- annotators/entity_detection/test_entity_detection.py
+++ annotators/entity_detection/test_entity_detection.py
@@ -2,7 +2,7 @@ import requests
 
 
 def main():
-    url = "http://0.0.0.0:9103/respond"
+    url = "http://0.0.0.0:8103/respond"
 
     request_data = [
         {"sentences": [["what is the capital of russia?"]]},
