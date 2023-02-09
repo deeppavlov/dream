e2a93d61e6d7a129a01b88467a02d0da93111ab8
Dilyara Zharikova (Baymurzina) <dilyara.rimovna@gmail.com>
2023-02-09 14:08:43 +0800

    Fix/refactor transformers lm (#317)
    * fix: refactor transformers lm
    
    * fix: format lm
    
    * fix: starting utterance
    
    * fix: different tests
    
    * fix: codestyle
    
    * fix: correct sending data to transformers lm
    
    * fix: take prompt into account
    
    * fix: empty prompts
    
    * fix: transformers version
    
    * fix: no confidences
    
    * fix: transformers lm works with batches
    
    * fix: transformers lm params and logs
    
    * fix: more hyps
    
    * fix: readme
    
    * fix: dialogpt ru output format compatible
    
    * fix: wait longer
    
    * fix: no extra var in test
    
    * fix: correct is_container_running checks
    
    * fix: black

--- common/containers.py
+++ common/containers.py
@@ -1,10 +1,9 @@
 import requests
 
 
-def is_container_running(model_url, timeout=4):
+def is_container_running(model_url, json_data, timeout=4):
     try:
-        requested_data = [{"speaker": "human", "text": "hi"}]
-        response = requests.post(model_url, json={"dialog_contexts": [requested_data]}, timeout=timeout)
+        response = requests.post(model_url, json=json_data, timeout=timeout)
         if response.status_code == 200:
             return True
     except Exception as exc:
