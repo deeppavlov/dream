b8cffd49361780c091b4eb362f9aa55bd5695055
dilyararimovna <dilyara.rimovna@gmail.com>
2023-02-09 13:16:37 +0800

    fix: format as for transformers lm

--- services/openai_api_lm/server.py
+++ services/openai_api_lm/server.py
@@ -11,28 +11,35 @@ from sentry_sdk.integrations.flask import FlaskIntegration
 
 sentry_sdk.init(dsn=os.getenv("SENTRY_DSN"), integrations=[FlaskIntegration()])
 
+
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
-app = Flask(__name__)
 
 PRETRAINED_MODEL_NAME_OR_PATH = os.environ.get("PRETRAINED_MODEL_NAME_OR_PATH")
 CONFIG_NAME = os.environ.get("CONFIG_NAME")
-logging.info(f"PRETRAINED_MODEL_NAME_OR_PATH = {PRETRAINED_MODEL_NAME_OR_PATH}")
-DEFAULT_CONFIDENCE = 0.9
-ZERO_CONFIDENCE = 0.0
+logger.info(f"PRETRAINED_MODEL_NAME_OR_PATH = {PRETRAINED_MODEL_NAME_OR_PATH}")
+NAMING = ["AI", "Human"]
+
 with open(CONFIG_NAME, "r") as f:
     generation_params = json.load(f)
-logging.info(f"Generation parameters: {generation_params}")
+logger.info(f"Generation parameters: {generation_params}")
 
+app = Flask(__name__)
+logging.getLogger("werkzeug").setLevel("WARNING")
 
-def generate_responses(instruction, context, openai_api_key, openai_org, continue_last_uttr=False):
+def generate_responses(context, openai_api_key, openai_org, prompt, continue_last_uttr=False):
     outputs = []
+    dialog_context = ""
+    if prompt:
+        dialog_context += prompt + "\n"
+    s = len(context) % 2
+    context = [f"{NAMING[(s + uttr_id) % 2]}: {uttr}" for uttr_id, uttr in enumerate(context)]
     if continue_last_uttr:
-        dialog_context = instruction + "\n" + "\n".join(context)
+        dialog_context += "\n".join(context)
     else:
-        dialog_context = instruction + "\n" + "\n".join(context) + "\n" + "AI:"
-    logger.info(f"context inside generate_responses seen as: {[dialog_context]}")
+        dialog_context += "\n".join(context) + f"\n{NAMING[0]}:"
 
+    logger.info(f"context inside generate_responses seen as: {dialog_context}")
     assert openai_api_key, logger.error(f"Error: OpenAI API key is not specified in env")
     openai.api_key = openai_api_key
     openai.organization = openai_org if openai_org else None
@@ -53,31 +60,31 @@ def generate_responses(instruction, context, openai_api_key, openai_org, continu
 def respond():
     st_time = time.time()
     contexts = request.json.get("dialog_contexts", [])
+    prompts = request.json.get("prompts", [])
+    if len(contexts) > 0 and len(prompts) == 0:
+        prompts = [""] * len(contexts)
     openai_api_keys = request.json.get("openai_api_keys", [])
-    openai_organizations = request.json.get("openai_organizations", None)
-    openai_organizations = [None] * len(contexts) if openai_organizations is None else openai_organizations
+    openai_orgs = request.json.get("openai_organizations", None)
+    openai_orgs = [None] * len(contexts) if openai_orgs is None else openai_orgs
 
     try:
         responses = []
-        confidences = []
-        for context, openai_api_key, openai_org in zip(contexts, openai_api_keys, openai_organizations):
-            outputs = generate_responses("", context, openai_api_key, openai_org)
-            logger.info(f"openai-api-lm result: {outputs}")
+        for context, openai_api_key, openai_org, prompt in zip(contexts, openai_api_keys, openai_orgs, prompts):
+            curr_responses = []
+            outputs = generate_responses(context, openai_api_key, openai_org, prompt)
             for response in outputs:
-                if len(response) >= 3:
-                    # drop too short responses
-                    responses += [response]
-                    confidences += [DEFAULT_CONFIDENCE]
+                if len(response) >= 2:
+                    curr_responses += [response]
                 else:
-                    responses += [""]
-                    confidences += [ZERO_CONFIDENCE]
+                    curr_responses += [""]
+            responses += [curr_responses]
 
     except Exception as exc:
         logger.exception(exc)
         sentry_sdk.capture_exception(exc)
         responses = [[""]] * len(contexts)
-        confidences = [[ZERO_CONFIDENCE]] * len(contexts)
 
+    logger.info(f"openai-api-lm result: {responses}")
     total_time = time.time() - st_time
     logger.info(f"openai-api-lm exec time: {total_time:.3f}s")
-    return jsonify(list(zip(responses, confidences)))
+    return jsonify(responses)
--- services/openai_api_lm/test.py
+++ services/openai_api_lm/test.py
@@ -5,13 +5,20 @@ def test_respond():
     url = "http://0.0.0.0:8131/respond"
     contexts = [
         [
-            "Respond like a friendly chatbot",
-            "Human: Hi! I am Marcus. How are you today?",
-        ]
+            "Hi! I am Marcus. How are you today?",
+            "Hi Marcus! I am fine. How are you?",
+            "I am great. What are your plans for today?",
+        ],
+        ["Hi Marcus! I am fine. How are you?", "I am great. What are your plans for today?"],
+    ]
+    prompts = [
+        "Respond like a friendly chatbot.",
+        "Respond like a friendly chatbot.",
     ]
     result = requests.post(url, json={"dialog_contexts": contexts,
                                       "openai_api_keys": ["MYKEY"] * len(contexts),
-                                      "openai_organizations": ["MYORG"] * len(contexts)}).json()
+                                      "openai_organizations": ["MYORG"] * len(contexts),
+                                      "prompts": prompts}).json()
     print(result)
     assert [all(len(sample[0]) > 0 for sample in result)], f"Got\n{result}\n, something is wrong"
     print("Success!")
