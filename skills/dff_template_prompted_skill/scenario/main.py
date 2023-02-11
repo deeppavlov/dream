c90ad8504342035a9cba8d8726faa639e0777fcc
Nika Smilga <sunny-august@mail.ru>
2023-02-11 17:31:10 +0300

    formatting

--- skills/dff_template_prompted_skill/scenario/main.py
+++ skills/dff_template_prompted_skill/scenario/main.py
@@ -7,9 +7,7 @@ import df_engine.labels as lbl
 
 from . import response as loc_rsp
 
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
