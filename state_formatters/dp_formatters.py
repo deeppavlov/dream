69defbfb73dd0b9d831dae608e3daa6a09544f98
Dilyara Zharikova (Baymurzina) <dilyara.rimovna@gmail.com>
2023-03-18 18:36:54 +0300

    Feat/universal prompting (#345)
    * feat: universal folder
    
    * feat: utilize attributes from request
    
    * Feat: universal distribution
    
    * fix: turn on all lm services
    
    * fix: turn on all lm services
    
    * fix: no prompt selector
    
    * fix: turn on universal prompted skill
    
    * fix: no waiting
    
    * fix: paths to configs
    
    * fix: no extra asserts
    
    * fix: choose defaults
    
    * fix: formatter
    
    * fix: attributes logs
    
    * fix: attributes logs
    
    * fix: attributes logs
    
    * fix: add attributes
    
    * fix: secret keys
    
    * fix: var names
    
    * fix: var names
    
    * fix: var namesno extra logs
    
    * fix: proxy for heavy models
    
    * fix: codestyle
    
    * fix: component for universal skill
    
    * fix: component for universal skill
    
    * fix: proxy
    
    * fix: proxy address
    
    * fix: no-restrictions selector for universal prompted dist
    
    * fix: no extra logs
    
    * fix: n hyps
    
    * fix: flake codestyle
    
    * fix: no midas usage

--- state_formatters/dp_formatters.py
+++ state_formatters/dp_formatters.py
@@ -1071,6 +1071,15 @@ def dff_prompted_skill_formatter(dialog, skill_name=None):
     )
 
 
+def dff_universal_prompted_skill_formatter(dialog, skill_name=None):
+    return utils.dff_formatter(
+        dialog,
+        skill_name,
+        types_utterances=["human_utterances", "bot_utterances", "utterances"],
+        wanted_keys=["text", "annotations", "active_skill", "user", "attributes"],
+    )
+
+
 def hypotheses_list_for_dialog_breakdown(dialog: Dict) -> List[Dict]:
     # Used by: dialog_breakdown
     dialog = utils.get_last_n_turns(dialog, bot_last_turns=2)
