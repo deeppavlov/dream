193ca82f9e91e3df7f6b3ac7d01fc21493184c1d
dilyararimovna <dilyara.rimovna@gmail.com>
2023-04-18 11:31:31 +0300

    fix: formatter

--- state_formatters/dp_formatters.py
+++ state_formatters/dp_formatters.py
@@ -1030,6 +1030,7 @@ def dff_dream_persona_ru_prompted_skill_formatter(dialog):
     return utils.dff_formatter(
         dialog,
         "dff_dream_persona_ru_prompted_skill",
+        bot_last_turns=5,
         types_utterances=["human_utterances", "bot_utterances", "utterances"],
     )
 
