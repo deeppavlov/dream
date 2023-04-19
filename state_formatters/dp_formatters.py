bf07f3705050d7196b9cf1cb16e45e45d942d1b4
Dilyara Zharikova (Baymurzina) <dilyara.rimovna@gmail.com>
2023-04-19 17:07:15 +0300

    Feat/ru llama distribution (#383)
    * feat: ru prompted dist based on llama
    
    * feat: ru persona
    
    * fix: versions
    
    * fix duplicate endpoints, missing keys (#371)
    
    * Make MTL refer to DeepPavlov 1.1 instead of my branch (#366)
    
    * Update Dockerfile
    
    * Update combined_classifier.json
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * fix: starting openai services without dev.yml (#373)
    
    * fix: reqs
    
    * fix: dockerfile
    
    * fix: pip update
    
    * fix: reqs
    
    * feat: using peft for gusev's model
    
    * fix: reqs
    
    * fix: working load
    
    * feat: separate folder for peft transformers
    
    * fix: revert to dev
    
    * fix: transformers generation
    
    * fix: use peft model
    
    * fix: component yml
    
    * fix: yml configs
    
    * fix: no half precision
    
    * fix: description
    
    * fix: config is optional for some models
    
    * fix: increase timeout
    
    * fix: formatter
    
    * fix: language
    
    * fixed
    
    * fix: rights
    
    * fix: info ymls
    
    * fix: 5sec timeout
    
    * fix: 10sec timeout
    
    * fix: gpu mem
    
    * feat: ru pipeline and dockers
    
    * feat: badlisted words ru
    
    * feat: use fp16 for faster inference
    
    * feat: rank sentences endpoint
    
    * fix: endpoint func
    
    * fix: ping pong
    
    * fix: rannker url
    
    * fix: prompt selector ru
    
    * fix: env ru
    
    * fix: sentence ranker
    
    * fix: no-scripts selector
    
    * fix: timeout 2.0 for ru toxic
    
    * fix: first try for toxic model before ready
    
    * fix: params for language
    
    * fix: language
    
    * fix: timoeout for dialogrpt
    
    * fix: no use of toxic cls
    
    * fix: revert timeout to 1sec
    
    * feat: ru lang
    
    * fix: ru persona
    
    * feat: new prompt
    
    * fix: prompt
    
    * fix: prompt
    
    * fix: prompt
    
    * Update dream_persona.json
    
    * Update dream_persona.json
    
    * Update dream_persona_ru.json
    
    * fix: rename distr
    
    * fix: formatter
    
    ---------
    
    Co-authored-by: Maxim Talimanchuk <mtalimanchuk@gmail.com>
    Co-authored-by: dimakarp1996 <dimakarp1996@yandex.ru>
    Co-authored-by: Fedor Ignatov <ignatov.fedor@gmail.com>
    Co-authored-by: Lidia Ostyakova <55363402+lostyakova@users.noreply.github.com>

--- state_formatters/dp_formatters.py
+++ state_formatters/dp_formatters.py
@@ -1026,6 +1026,15 @@ def dff_dream_persona_prompted_skill_formatter(dialog):
     )
 
 
+def dff_dream_persona_ru_prompted_skill_formatter(dialog):
+    return utils.dff_formatter(
+        dialog,
+        "dff_dream_persona_ru_prompted_skill",
+        bot_last_turns=5,
+        types_utterances=["human_utterances", "bot_utterances", "utterances"],
+    )
+
+
 def dff_empathetic_marketing_prompted_skill_formatter(dialog):
     return utils.dff_formatter(
         dialog,
