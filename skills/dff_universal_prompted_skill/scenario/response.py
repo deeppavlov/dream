79e8a163642be673d5048a862e32ea0503d8881d
dilyararimovna <dilyara.rimovna@gmail.com>
2023-05-11 12:00:37 +0300

    fix: kwargs items

--- skills/dff_universal_prompted_skill/scenario/response.py
+++ skills/dff_universal_prompted_skill/scenario/response.py
@@ -111,7 +111,7 @@ def generative_response(ctx: Context, actor: Actor, *args, **kwargs) -> Any:
         sending_variables = {}
 
     # adding any other kwargs to request from the last human utterance's attributes
-    for _key, _value in lm_service_kwargs:
+    for _key, _value in lm_service_kwargs.items():
         sending_variables[_key] = deepcopy(_value)
 
     if len(dialog_context) > 0:
