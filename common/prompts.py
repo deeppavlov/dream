1bd186af73a71d4a892b81ddbb5acb9ebbef0f09
dilyararimovna <dilyara.rimovna@gmail.com>
2023-05-29 13:11:19 +0300

    fix: extra file

--- common/prompts.py
+++ common/prompts.py
@@ -10,9 +10,6 @@ logger = logging.getLogger(__name__)
 with open("common/prompts/goals_for_prompts.json", "r") as f:
     META_GOALS_PROMPT = json.load(f)["prompt"]
 
-with open("common/prompts/skill_selector_prompt.json", "r") as f:
-    META_SKILL_SELECTOR_PROMPT = json.load(f)["prompt"]
-
 
 def send_request_to_prompted_generative_service(dialog_context, prompt, url, config, timeout, sending_variables):
     response = requests.post(
