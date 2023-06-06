c05570f176e7ad9d9f074a69613977deb00120da
dilyararimovna <dilyara.rimovna@gmail.com>
2023-06-06 19:35:12 +0300

    fix: do not test proxied components

--- tests/runtests_marketing_gptjt.sh
+++ tests/runtests_marketing_gptjt.sh
@@ -110,9 +110,7 @@ if [[ "$MODE" == "test_skills" || "$MODE" == "all" ]]; then
     echo "Passing test data to each skill selected for testing"
 
 
-    for container in sentseg ranking-based-response-selector combined-classification \
-                     sentence-ranker prompt-selector \
-                     dff-marketing-prompted-skill; do
+    for container in ranking-based-response-selector prompt-selector dff-marketing-prompted-skill; do
 
         echo "Run tests for $container"
         dockercompose_cmd exec -T -u $(id -u) $container ./test.sh
