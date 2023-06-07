39417b6ad68ac8f73a206db414e2c19a5273845d
Nika Smilga <sunny-august@mail.ru>
2023-06-07 17:31:26 +0300

    skill update new format

--- skills/dff_qa_llm_skill/scenario/response.py
+++ skills/dff_qa_llm_skill/scenario/response.py
@@ -1,44 +1,46 @@
 import json
 import logging
 import re
-import requests
 import sentry_sdk
 from os import getenv
+from pathlib import Path
 from typing import Any
 
 from common.build_dataset import build_dataset
 import common.dff.integration.context as int_ctx
 import common.dff.integration.response as int_rsp
 from common.constants import CAN_NOT_CONTINUE
+from common.prompts import send_request_to_prompted_generative_service, get_goals_from_prompt, compose_sending_variables
 from df_engine.core import Context, Actor
 
 
 sentry_sdk.init(getenv("SENTRY_DSN"))
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
-GENERATIVE_TIMEOUT = int(getenv("GENERATIVE_TIMEOUT", 120))
+GENERATIVE_TIMEOUT = int(getenv("GENERATIVE_TIMEOUT", 5))
+GENERATIVE_SERVICE_URL = getenv("GENERATIVE_SERVICE_URL")
+GENERATIVE_SERVICE_CONFIG = getenv("GENERATIVE_SERVICE_CONFIG") # add env!!!
 N_UTTERANCES_CONTEXT = int(getenv("N_UTTERANCES_CONTEXT", 3))
+ENVVARS_TO_SEND = getenv("ENVVARS_TO_SEND", None)
+if GENERATIVE_SERVICE_CONFIG:
+    with open(f"common/generative_configs/{GENERATIVE_SERVICE_CONFIG}", "r") as f:
+        GENERATIVE_SERVICE_CONFIG = json.load(f)
+DEFAULT_PROMPT = "Answer questions based on part of a text."
+ENVVARS_TO_SEND = [] if ENVVARS_TO_SEND is None else ENVVARS_TO_SEND.split(",")
+
+assert GENERATIVE_SERVICE_URL
+
 FIX_PUNCTUATION = re.compile(r"\s(?=[\.,:;])")
 DEFAULT_CONFIDENCE = 0.9
+SUPER_CONFIDENCE = 1.0
 LOW_CONFIDENCE = 0.7
-DEFAULT_PROMPT = "Answer questions based on part of a text."
-CONSIDERED_LM_SERVICES = {
-    "ChatGPT": {
-        "url": "http://openai-api-chatgpt:8145/respond",
-        "config": json.load(open("common/generative_configs/openai-chatgpt.json", "r")),
-        "envvars_to_send": ["OPENAI_API_KEY", "OPENAI_ORGANIZATION"],
-    }
-}
 
 
 def compose_data_for_model(ctx, actor):
     context = int_ctx.get_utterances(ctx, actor)[-N_UTTERANCES_CONTEXT:]
     utterance_texts = [uttr.get("text", "") for uttr in context]
+
     if utterance_texts:
-        with open("test_annotations.json", "w") as f:
-            json.dump(context[-1].get("annotations", {}), f)
         raw_candidates = (
             context[-1]
             .get("annotations", {})
@@ -51,12 +53,7 @@ def compose_data_for_model(ctx, actor):
             .get("doc_retriever", {})
             .get("file_path", "")
         )
-        DATASET_PATH = (
-            context[-1]
-            .get("annotations", {})
-            .get("doc_retriever", {})
-            .get("dataset_path", "")
-        )
+        DATASET_PATH = '/data/temporary_dataset'
         logger.info(
             f"""Building dataset to get candidate texts. raw_candidates: {raw_candidates},
             ORIGINAL_FILE_PATH: {ORIGINAL_FILE_PATH}, DATASET_PATH: {DATASET_PATH}"""
@@ -99,35 +96,30 @@ def generative_response(ctx: Context, actor: Actor, *args, **kwargs) -> Any:
             curr_bot_attrs += [bot_attr]
             curr_attrs += [attr]
 
-    dialog_context = compose_data_for_model(ctx, actor)
+    dialog_context = compose_data_for_model(ctx, actor)    
+    # get variables which names are in `ENVVARS_TO_SEND` (splitted by comma if many)
+    # from user_utterance attributes or from environment
+    human_uttr_attributes = int_ctx.get_last_human_utterance(ctx, actor).get("attributes", {})
+    lm_service_kwargs = human_uttr_attributes.pop("lm_service_kwargs", None)
+    lm_service_kwargs = {} if lm_service_kwargs is None else lm_service_kwargs
+    envvars_to_send = ENVVARS_TO_SEND if len(ENVVARS_TO_SEND) else human_uttr_attributes.get("envvars_to_send", [])
+    sending_variables = compose_sending_variables(
+        lm_service_kwargs,
+        envvars_to_send,
+        **human_uttr_attributes,
+    )
+
     logger.info(f"dialog_context: {dialog_context}")
-    lm_service = "ChatGPT"
-    if "envvars_to_send" in CONSIDERED_LM_SERVICES[lm_service]:
-        sending_variables = {
-            f"{var.lower()}s": [getenv(var, None)]
-            for var in CONSIDERED_LM_SERVICES[lm_service]["envvars_to_send"]
-        }
-        if len(sending_variables.keys()) > 0 and all(
-            [var_value is None for var_value in sending_variables.values()]
-        ):
-            raise NotImplementedError(
-                "ERROR: All environmental variables have None values. At least one of them must have not None value"
-            )
-    else:
-        sending_variables = {}
     if len(dialog_context) > 0:
         try:
-            response = requests.post(
-                CONSIDERED_LM_SERVICES[lm_service]["url"],
-                json={
-                    "dialog_contexts": [dialog_context],
-                    "prompts": [DEFAULT_PROMPT],
-                    "configs": [CONSIDERED_LM_SERVICES[lm_service]["config"]],
-                    **sending_variables,
-                },
-                timeout=GENERATIVE_TIMEOUT,
+            hypotheses = send_request_to_prompted_generative_service(
+                dialog_context,
+                DEFAULT_PROMPT,
+                GENERATIVE_SERVICE_URL,
+                GENERATIVE_SERVICE_CONFIG,
+                GENERATIVE_TIMEOUT,
+                sending_variables,
             )
-            hypotheses = response.json()[0]
         except Exception as e:
             sentry_sdk.capture_exception(e)
             logger.exception(e)
@@ -135,15 +127,15 @@ def generative_response(ctx: Context, actor: Actor, *args, **kwargs) -> Any:
     else:
         hypotheses = []
     logger.info(f"generated hypotheses: {hypotheses}")
+
+
     for hyp in hypotheses:
         confidence = DEFAULT_CONFIDENCE
-        hyp_text = " ".join(hyp.split())
-        if len(hyp_text) and hyp_text[-1] not in [".", "?", "!"]:
-            hyp_text += "."
+        if len(hyp) and hyp[-1] not in [".", "?", "!"]:
+            hyp += "."
             confidence = LOW_CONFIDENCE
-        gathering_responses(
-            hyp_text, confidence, {}, {}, {"can_continue": CAN_NOT_CONTINUE}
-        )
+        _curr_attrs = {"can_continue": CAN_NOT_CONTINUE}
+        gathering_responses(hyp, confidence, {}, {}, _curr_attrs)
 
     if len(curr_responses) == 0:
         return ""
