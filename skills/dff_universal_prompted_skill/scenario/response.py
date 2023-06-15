2797debbcadf830d3f9613e9692f81df3d1ddde3
Dilyara Zharikova (Baymurzina) <dilyara.rimovna@gmail.com>
2023-06-15 16:23:13 +0300

    feat: add chatgpt 16k (#491)
    * feat: add chatgpt 16k
    
    * fix: another port

--- skills/dff_universal_prompted_skill/scenario/response.py
+++ skills/dff_universal_prompted_skill/scenario/response.py
@@ -30,6 +30,7 @@ ENVVARS_TO_SEND = {
     "http://transformers-lm-bloomz7b:8146/respond": [],
     "http://transformers-lm-oasst12b:8158/respond": [],
     "http://openai-api-chatgpt:8145/respond": ["OPENAI_API_KEY", "OPENAI_ORGANIZATION"],
+    "http://openai-api-chatgpt-16k:8167/respond": ["OPENAI_API_KEY", "OPENAI_ORGANIZATION"],
     "http://openai-api-davinci3:8131/respond": ["OPENAI_API_KEY", "OPENAI_ORGANIZATION"],
     "http://openai-api-gpt4:8159/respond": ["OPENAI_API_KEY", "OPENAI_ORGANIZATION"],
     "http://openai-api-gpt4-32k:8160/respond": ["OPENAI_API_KEY", "OPENAI_ORGANIZATION"],
