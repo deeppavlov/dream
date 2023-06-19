04dbfbbb076b149b65f3a982343d846a89bc8c7f
ruthenian8 <ruthenian8@gmail.com>
2023-06-19 09:34:25 +0300

    Upd/dff/template skill (#303)
    * add dff_release folder; add minimal changes to server.py; modify dockerfile to use dff_release; modify tests for use with message class; ensure the passing of tests
    
    * rename actor.py to serializer.py, reimplement serialization
    
    * edit requirements.txt; use MultiMessage from dff
    
    * rename dff_release to dff_api_v1; change dockerfile to import dff_api_v1; remove nulls from skill output; exclude nulls from test data; add run_dff function to serializer file
    
    * add DreamMessage and DreamMultiMessage classes; process in serializer; make message fields optional to discard null values; edit main.py
    
    * remove redundant imports from serializer.py && main.py
    
    * remove DreamMultiMessage
    
    * Move Pre_transition_processing from node_1 to start_node
    
    * apply lint
    
    * Add comments to main.py; remove actor instance
    
    * apply lint
    
    * update requirements: define versions; update import order in main
    
    * do minor fixes
    
    * move slot filling to a separate integration.response module
    
    * change function signature. successful building of dream skill
    
    * fix codestyle
    
    * merge upstream dev:
    
    * Use new dff version in the requirements;
    
    ---------
    
    Co-authored-by: dilyararimovna <dilyara.rimovna@gmail.com>

---
+++ common/dff_api_v1/integration/processing.py
@@ -0,0 +1,92 @@
+import logging
+
+from dff.script import Context
+from dff.pipeline import Pipeline
+import common.constants as common_constants
+from common.wiki_skill import extract_entity
+from .facts_utils import provide_facts_response
+from . import context
+
+
+DREAM_SLOTS_KEY = "slots"
+logger = logging.getLogger(__name__)
+
+
+def save_slots_to_ctx(slots: dict):
+    def save_slots_to_ctx_processing(
+        ctx: Context,
+        _,
+        *args,
+        **kwargs,
+    ) -> Context:
+        ctx.misc[DREAM_SLOTS_KEY] = ctx.misc.get(DREAM_SLOTS_KEY, {}) | slots
+        return ctx
+
+    return save_slots_to_ctx_processing
+
+
+def entities(**kwargs):
+    slot_info = list(kwargs.items())
+
+    def extract_entities(
+        ctx: Context,
+        pipeline: Pipeline,
+        *args,
+        **kwargs,
+    ) -> Context:
+        for slot_name, slot_types in slot_info:
+            if isinstance(slot_types, str):
+                extracted_entity = extract_entity(ctx, slot_types)
+                if extracted_entity:
+                    ctx = save_slots_to_ctx({slot_name: extracted_entity})(ctx, pipeline)
+            elif isinstance(slot_types, list):
+                found = False
+                for slot_type in slot_types:
+                    if not slot_type.startswith("default:"):
+                        extracted_entity = extract_entity(ctx, slot_type)
+                        if extracted_entity:
+                            ctx = save_slots_to_ctx({slot_name: extracted_entity})(ctx, pipeline)
+                            found = True
+                if not found:
+                    default_value = ""
+                    for slot_type in slot_types:
+                        if slot_type.startswith("default:"):
+                            default_value = slot_type.split("default:")[1]
+                    if default_value:
+                        ctx = save_slots_to_ctx({slot_name: default_value})(ctx, pipeline)
+        return ctx
+
+    return extract_entities
+
+
+def fact_provider(page_source, wiki_page):
+    def response(ctx: Context, pipeline: Pipeline, *args, **kwargs):
+        return provide_facts_response(ctx, pipeline, page_source, wiki_page)
+
+    return response
+
+
+def set_confidence(confidence: float = 1.0):
+    def set_confidence_processing(
+        ctx: Context,
+        pipeline: Pipeline,
+        *args,
+        **kwargs,
+    ) -> Context:
+        context.set_confidence(ctx, pipeline, confidence)
+        return ctx
+
+    return set_confidence_processing
+
+
+def set_can_continue(continue_flag: str = common_constants.CAN_CONTINUE_SCENARIO):
+    def set_can_continue_processing(
+        ctx: Context,
+        pipeline: Pipeline,
+        *args,
+        **kwargs,
+    ) -> Context:
+        context.set_can_continue(ctx, pipeline, continue_flag)
+        return ctx
+
+    return set_can_continue_processing
--- skills/dff_template_skill/scenario/condition.py
+++ skills/dff_template_skill/scenario/condition.py
@@ -1,15 +1,16 @@
 import logging
 
-from df_engine.core import Context, Actor
+from dff.script import Context
+from dff.pipeline import Pipeline
 
-from common.dff.integration import condition as int_cnd
+from common.dff_api_v1.integration import condition as int_cnd
 
 logger = logging.getLogger(__name__)
 # ....
 
 
 def example_lets_talk_about():
-    def example_lets_talk_about_handler(ctx: Context, actor: Actor, *args, **kwargs) -> str:
-        return int_cnd.is_lets_chat_about_topic_human_initiative(ctx, actor)
+    def example_lets_talk_about_handler(ctx: Context, pipeline: Pipeline) -> str:
+        return int_cnd.is_lets_chat_about_topic_human_initiative(ctx, pipeline)
 
     return example_lets_talk_about_handler
