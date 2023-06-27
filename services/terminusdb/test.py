8a4d68cd973310c54839325c300d503d3347a3f0
Ramimashkouk <rami.n.mashkouk@gmail.com>
2023-06-27 17:44:10 +0300

    doc: update terminusdb_service readme

--- services/terminusdb/test.py
+++ services/terminusdb/test.py
@@ -13,7 +13,7 @@ def main():
     assert TERMINUSDB_SERVER_PASSWORD, "TerminusDB server password is not specified in env"
     TERMINUSDB_SERVER_DB = os.getenv("TERMINUSDB_SERVER_DB")
     TERMINUSDB_SERVER_TEAM = os.getenv("TERMINUSDB_SERVER_TEAM")
-    INDEX_LOAD_PATH = Path(os.path.expanduser("annotators/user_knowledge_graph"))
+    INDEX_LOAD_PATH = Path(os.path.expanduser("services/terminusdb"))
 
     graph = TerminusdbKnowledgeGraph(
         db_name=TERMINUSDB_SERVER_DB,
