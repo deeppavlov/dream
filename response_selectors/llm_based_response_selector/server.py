24e159acdf749bdefb43de90db0505b60e2616ab
Nika Smilga <42929200+smilni@users.noreply.github.com>
2023-06-27 15:41:07 +0300

    Fix/better llm response selector (#498)
    * moved prompt with task to the end
    
    * openass instead of chatgpt
    
    * longer context and slightly different prompt for transformers
    
    * chatgpt as default selector
    
    * revert proxy file
    
    * revert docker-compose file
    
    * removed configs from response selection
    
    * config fix

--- response_selectors/llm_based_response_selector/server.py
+++ response_selectors/llm_based_response_selector/server.py
@@ -58,7 +58,10 @@ def select_response_by_scores(hypotheses, scores):
 
 def select_response(dialog_context, hypotheses, human_uttr_attributes):
     try:
-        curr_prompt = PROMPT + "\nHypotheses:\n" + "\n".join([f'"{hyp["text"]}"' for hyp in hypotheses])
+        if "transformers" in GENERATIVE_SERVICE_URL:
+            curr_prompt = "Hypotheses:\n" + "\n".join([f'"{hyp["text"]}"' for hyp in hypotheses]) + "\n" + PROMPT
+        else:
+            curr_prompt = PROMPT + "\nHypotheses:\n" + "\n".join([f'"{hyp["text"]}"' for hyp in hypotheses])
         logger.info(f"llm_based_response_selector sends dialog context to llm:\n`{dialog_context}`")
         logger.info(f"llm_based_response_selector sends prompt to llm:\n`{curr_prompt}`")
 
