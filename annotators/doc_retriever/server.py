62448b87bfe95473e030c4edad548a939e787fe8
Nika Smilga <sunny-august@mail.ru>
2023-07-05 20:08:11 +0300

    simplify long train_model func

--- annotators/doc_retriever/server.py
+++ annotators/doc_retriever/server.py
@@ -1,19 +1,12 @@
 import logging
 import os
 import sentry_sdk
-import random
-import string
 import requests
-import time
-import shutil
-import pypdfium2 as pdfium
-from pathlib import PurePath
-from bs4 import BeautifulSoup
 from deeppavlov import build_model
 from flask import Flask, jsonify, request
 from sentry_sdk.integrations.flask import FlaskIntegration
 from deeppavlov.core.common.file import read_json
-from utils import upload_document, build_dataset_and_train_model
+from utils import create_folders_if_not_exist, find_and_download_docs_if_needed, download_files_and_save_links, move_files_and_save_paths, train_upload_return_attributes, remove_files_and_folders
 
 # logging here because it conflicts with tf
 
@@ -34,166 +27,6 @@ MODEL_CONFIG = read_json(CONFIG_PATH)
 MODEL_CONFIG["dataset_reader"]["dataset_format"] = "txt"
 MODEL_CONFIG["chainer"]["pipe"][1]["top_n"] = PARAGRAPHS_NUM
 MODEL_CONFIG["dataset_reader"]["data_path"] = "/data/temporary_dataset/"
-
-
-def train_upload_return_attributes(filepaths_in_container, document_links, docs_and_links, doc_needs_upload=False):
-    logger.info("Started training model.")
-    build_dataset_and_train_model(
-        MODEL_CONFIG, "/data/temporary_dataset/", filepaths_in_container
-    )  # filepaths_in_container are used to create a database to work with
-    logger.info("Started writing model files to server.")
-    model_id, db_link, matrix_link = upload_model_return_id_and_links("/data/odqa/userfile.db", "/data/odqa/userfile_tfidf_matrix.npz", FILE_SERVER_URL)
-    if doc_needs_upload:  # only if doc is not already on fileserver
-        document_links = upload_files_return_links(filepaths_in_container)
-    time.sleep(1)
-    bot_and_human_atts = {
-            "bot_attributes": {
-                "db_link": db_link,  # todo: maybe replace db_link and matrix_link with model_id
-                "matrix_link": matrix_link,
-                "document_links": document_links,
-            },
-            "human_attributes": {
-                "documents_qa_model": {
-                    "model_id": model_id,
-                    "document_ids_and_info": docs_and_links,
-                    "document_links": document_links,
-                }
-            },
-        }
-    return bot_and_human_atts
-
-
-def upload_model_return_id_and_links(db_file, matrix_file, file_server_url):
-    model_id = generate_random_string(10)
-    db_link = upload_document(f"{model_id}.db", db_file, file_server_url)
-    matrix_link = upload_document(f"{model_id}.npz", matrix_file, file_server_url)
-    return model_id, db_link, matrix_link
-
-
-def upload_files_return_links(filepaths_in_container):
-    document_links = []
-    for filepath in filepaths_in_container:
-        new_filename = get_name(filepath)
-        # file already has a random-id name (assigned earlier), so we just get it
-        document_link = upload_document(new_filename, filepath, FILE_SERVER_URL)
-        document_links.append(document_link)
-        # save all the links to relevant files on server
-        # todo: in the future add to folder on server!!!
-    return document_links
-
-
-def create_folders_if_not_exist(folders_list):
-    for folder in folders_list:
-        if not os.path.exists(folder):
-            os.mkdir(folder)
-
-
-def remove_files_and_folders(files_list, folders_list):
-    for file in files_list:
-        os.remove(file)
-    for folder in folders_list:
-        shutil.rmtree(folder, ignore_errors=True)
-    logger.info("Files successfully written to server. Everyting removed from /data.")
-
-
-def download_files_and_save_links(document_links, filepaths_in_container, docs_and_links):
-    for link in document_links:
-        filepath_in_container = download_file_to_data(link)
-        filepaths_in_container.append(filepath_in_container)
-        # we download all incoming files to /data and save paths
-        docs_and_links.append(
-            {
-                "document_id": get_stem(link),
-                "initial_path_or_link": link,
-            }
-        ) #linking ids and initial file information
-
-
-def move_files_and_save_paths(document_paths, filepaths_in_container, docs_and_links):
-    for filepath in document_paths:
-        file_id = generate_random_string(10)
-        filepath_in_container = f"/data/documents/{file_id}.txt"
-        orig_file_text = get_text_from_filepath(filepath)
-        with open(filepath_in_container, "w") as f:
-            f.write(orig_file_text)
-        # move all the files to /data (for uniformness all files are always stored there)
-        docs_and_links.append(
-            {
-                "document_id": get_stem(filepath_in_container),
-                "initial_path_or_link": filepath,
-            }
-        )  # linking ids and initial filenames
-        filepaths_in_container.append(filepath_in_container)  # save paths
-    
-
-def get_extension(filepath):
-    return PurePath(filepath).suffix
-
-
-def get_name(filepath):
-    return PurePath(filepath).name         
-
-
-def get_stem(filepath):
-    return PurePath(filepath).stem
-
-
-def pdf_to_text(file):
-    pdf = pdfium.PdfDocument(file)  # supports file path strings, bytes, and byte buffers
-    n_pages = len(pdf)
-    full_doc_text = ""
-    for page in range(n_pages):
-        page_index = pdf[page]
-        textpage = page_index.get_textpage()
-        text_all = textpage.get_text_range()
-        full_doc_text += text_all
-    return full_doc_text
-
-
-def html_to_text(file):
-    soup = BeautifulSoup(file)
-    full_doc_text = soup.get_text(strip=True)
-    return full_doc_text
-
-
-def get_text_from_filepath(filepath: str) -> str:
-    file_extension = get_extension(filepath)
-    if "pdf" in file_extension:
-        full_doc_text = pdf_to_text(filepath)
-    elif "html" in file_extension:
-        with open(filepath, "r") as f:
-            html_doc = f.read()
-        full_doc_text = html_to_text(html_doc)
-    else:
-        with open(filepath, "r") as f:
-            full_doc_text = f.read()
-    return full_doc_text
-
-
-def get_text_from_fileobject(file_object: str, file_extension: str) -> str:
-    if "pdf" in file_extension:
-        full_doc_text = pdf_to_text(file_object.content)
-    elif "html" in file_extension:
-        full_doc_text = html_to_text(file_object.text)
-    else:
-        full_doc_text = file_object.text
-    return full_doc_text
-
-
-def generate_random_string(length: int) -> str:
-    characters = string.ascii_letters + string.digits
-    return "".join(random.choice(characters) for _ in range(length))
-
-
-def download_file_to_data(filepath: str) -> str:
-    file_id = generate_random_string(10)
-    file_extension = get_extension(filepath)
-    filepath_in_container = f"/data/documents/{file_id}.txt"
-    orig_file = requests.get(filepath, timeout=FILE_SERVER_TIMEOUT)
-    orig_file_text = get_text_from_fileobject(orig_file, file_extension)
-    with open(filepath_in_container, "w") as f:
-        f.write(orig_file_text)
-    return filepath_in_container
 
 
 @app.route("/return_candidates", methods=["POST"])
@@ -232,32 +65,19 @@ def train_and_upload_model():
     create_folders_if_not_exist(["/data/documents", "/data/odqa"])
     for dialog in dialogs:
         filepaths_in_container, document_links, docs_and_links = [], [], []
-        attributes = dialog.get("bot", {}).get("attributes", {})
-        model_needs_train, doc_needs_upload = False, False  # in most cases, we don't re-train the model
+        model_needs_train, doc_needs_upload = False, False
         if not DOC_PATH_OR_LINK:  
-            # if empty, then it's the dreambuilder option - file is already on files:3000; url comes in human_attributes
-            if dialog.get("human_attributes", []):
-                document_links = dialog.get("human_attributes", [{}])[-1].get("documents", [])
-                processed_docs = attributes.get("document_links", [])
-                if document_links != processed_docs:  
-                    # if in dreambuilder the list of docs changed compared to previous step
-                    model_needs_train = True  # if list of files changed, then we need to retrain the model
-                    download_files_and_save_links(document_links, filepaths_in_container, docs_and_links)
-            else:
-                logger.info("No documents specified in human_attributes.")
-        if "document_links" not in attributes:  
-            # if there is no document_link in bot attributes -> the model was never trained
-            model_needs_train, doc_needs_upload = True, True # in all not dreambuilder cases, doc needs to be uploaded to server
-            if "http" in DOC_PATH_OR_LINK[0]:  # if any element is a link
-                # dream option; we get file url, download it, need to upload to files:3000
+            document_links, model_needs_train = find_and_download_docs_if_needed(model_needs_train, filepaths_in_container, docs_and_links)
+        elif "document_links" not in dialog.get("bot", {}).get("attributes", {}):  
+            model_needs_train, doc_needs_upload = True, True
+            if "http" in DOC_PATH_OR_LINK[0]:
                 download_files_and_save_links(DOC_PATH_OR_LINK, filepaths_in_container, docs_and_links)
-            else:  # dream option; we get file path inside our folder, need to upload to files:3000
+            else:
                 move_files_and_save_paths(DOC_PATH_OR_LINK, filepaths_in_container, docs_and_links)
         logger.info(f"filepaths_in_container: {filepaths_in_container}")
         if model_needs_train:
             try:
-                bot_and_human_atts = train_upload_return_attributes(filepaths_in_container, document_links, docs_and_links, doc_needs_upload=doc_needs_upload)
-                # remove all the generated and downloaded files (stateless paradigm)
+                bot_and_human_atts = train_upload_return_attributes(MODEL_CONFIG, filepaths_in_container, document_links, docs_and_links, doc_needs_upload=doc_needs_upload)
                 files_to_remove = filepaths_in_container + ["/data/odqa/userfile.db", "/data/odqa/userfile_tfidf_matrix.npz"]
                 remove_files_and_folders(files_to_remove, ["/data/temporary_dataset/"])
                 attributes_to_add.append(bot_and_human_atts)
