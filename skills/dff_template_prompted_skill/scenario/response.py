6afa4245667b5d4da6f2e84992382a1e9874235d
Anna Korzanova <anna.korzanova12@gmail.com>
2023-07-11 14:36:45 +0300

    refactor: KG code deleted from template-prompted-skill

--- skills/dff_template_prompted_skill/scenario/response.py
+++ skills/dff_template_prompted_skill/scenario/response.py
@@ -19,8 +19,6 @@ logger = logging.getLogger(__name__)
 GENERATIVE_TIMEOUT = int(getenv("GENERATIVE_TIMEOUT", 5))
 GENERATIVE_SERVICE_URL = getenv("GENERATIVE_SERVICE_URL")
 GENERATIVE_SERVICE_CONFIG = getenv("GENERATIVE_SERVICE_CONFIG")
-USE_KG_DATA = getenv("USE_KG_DATA", False)
-USER_KG_SERVICE_URL = getenv("USER_KG_SERVICE_URL")
 if GENERATIVE_SERVICE_CONFIG:
     with open(f"common/generative_configs/{GENERATIVE_SERVICE_CONFIG}", "r") as f:
         GENERATIVE_SERVICE_CONFIG = json.load(f)
@@ -33,7 +31,6 @@ ENVVARS_TO_SEND = [] if ENVVARS_TO_SEND is None else ENVVARS_TO_SEND.split(",")
 
 assert GENERATIVE_SERVICE_URL
 assert PROMPT_FILE
-assert USER_KG_SERVICE_URL
 
 with open(PROMPT_FILE, "r") as f:
     PROMPT_DICT = json.load(f)
@@ -102,18 +99,6 @@ def generative_response(ctx: Context, actor: Actor, *args, **kwargs) -> Any:
     shared_memory = int_ctx.get_shared_memory(ctx, actor)
     prompt = shared_memory.get("prompt", "")
     prompt = prompt if len(prompt) > 0 and ALLOW_PROMPT_RESET else PROMPT
-
-    custom_el = ctx.misc.get("agent", {}).get("dialog", {}).get("human_utterances", [{}])[-1].get("annotations", {}).get("custom_entity_linking")
-    user_kg = ctx.misc.get("agent", {}).get("dialog", {}).get("human_utterances", [{}])[-1].get("annotations", {}).get("user_knowledge_graph")
-
-    if USE_KG_DATA and user_kg and (kg_prompt:=user_kg["kg_prompt"]):
-        logger.info(f"custom_el: {custom_el}")
-        logger.info(f"user_kg: {user_kg}")
-        kg_prompt = re.sub(r'[-\n]', '', kg_prompt[0][0].lower()).split('.')
-        kg_prompt = ",".join(kg_prompt[:-1])
-        prompt = prompt + f"\n\nADDITIONAL INSTRUCTION: You know that {kg_prompt}. Use these facts in your answer."
-
-    logger.info(f"prompt: {prompt}")
     logger.info(f"dialog_context: {dialog_context}")
 
     if len(dialog_context) > 0:
