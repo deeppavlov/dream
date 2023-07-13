38e33e9729a0f77cc4e8116f9f0983584296608b
Nika Smilga <sunny-august@mail.ru>
2023-07-13 20:34:43 +0300

    style

--- skills/external_integration_skill/test.py
+++ skills/external_integration_skill/test.py
@@ -7,10 +7,7 @@ def test_respond():
     url = f"http://0.0.0.0:{SERVICE_PORT}/respond"
     result = requests.post(
         url,
-        json={
-            "sentences": ["hi", ""],
-            "dialog_ids": ["7379921", None]
-        },
+        json={"sentences": ["hi", ""], "dialog_ids": ["7379921", None]},
     ).json()
     assert result == [["Success!", 0.9], ["", 0.0]], print(f"Got result: {result}, something is wrong.")
     print("Success!")
