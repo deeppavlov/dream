71cad71b36f70bc61738171285c04c1c3ff9497c
Nikolay <Kolpakov.na@phystech.edu>
2023-07-24 17:31:03 +0300

    Fixed black and flake8 codestyle

--- services/ruT5_summarizer/server.py
+++ services/ruT5_summarizer/server.py
@@ -10,9 +10,11 @@ from flask import Flask, jsonify, request
 
 sentry_sdk.init(os.getenv("SENTRY_DSN"))
 
-logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 logger = logging.getLogger(__name__)
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = "cuda" if torch.cuda.is_available() else "cpu"
 PRETRAINED_MODEL_NAME = os.environ.get("PRETRAINED_MODEL_NAME")
 
 app = Flask(__name__)
@@ -28,9 +30,14 @@ def respond_batch():
     start_time = time.time()
     sentences = request.json.get("sentences", [])
     logger.debug(f"Sentences: {sentences}")
-    tokenized_text = tokenizer(sentences, add_special_tokens=True, return_tensors="pt",
-                               truncation=True, padding='max_length').to(device)
-    summary = model.generate(tokenized_text['input_ids'])
+    tokenized_text = tokenizer(
+        sentences,
+        add_special_tokens=True,
+        return_tensors="pt",
+        truncation=True,
+        padding="max_length",
+    ).to(device)
+    summary = model.generate(tokenized_text["input_ids"])
     summary = tokenizer.batch_decode(summary, skip_special_tokens=True)
     total_time = time.time() - start_time
     logger.info(f"rut5-summarizer exec time: {round(total_time, 2)} sec")
