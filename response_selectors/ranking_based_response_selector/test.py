5b1ef5624eceef638ca31d0e5313b800ff9ffccf
Dilyara Zharikova (Baymurzina) <dilyara.rimovna@gmail.com>
2023-07-27 17:12:17 +0300

    Feat/ruxglm prompted dist (#528)
    * feat: ignore env secret ru
    
    * feat: add access token
    
    * feat: distribution ruxglm
    
    * fix: ruxglm cards
    
    * fix: use use_auth_token
    
    * fix: eos tokens type
    
    * fix: stats cpu ram
    
    * fix: skills cards
    
    * fix: components cards
    
    * fix: eos tokens
    
    * fix: eos tokens
    
    * fix: eos tokens
    
    * fix: eos tokens
    
    * fix: cards and table
    
    * fix: ADDITIONAL_EOS_TOKENS
    
    * fix: ADDITIONAL_EOS_TOKENS
    
    * fix: codestyle
    
    * fix: universal
    
    * fix: dream persona ru skill name
    
    * fix: prompt selector ru
    
    * fix: replacement
    
    * fix: prompt selecrto
    
    * fix: use params
    
    * fix: timeout and history
    
    * fix: trye very short persona
    
    * fix: increase timeout
    
    * fix: sub replacement tokens correctly
    
    * fix: sub replacement tokens correctly
    
    * fix: use stopping criteria
    
    * fix: typing
    
    * fix: revert long persona
    
    * fix: duplicate spaces
    
    * fix: correct components for russian distribution
    
    * fix: proxy for russian distribution
    
    * feat: universal distr for ru
    
    * feat: universal distr for ru
    
    * fix: remove extra
    
    * fix: working configs
    
    * fix: configs
    
    * feat: ruxglm prompted dists
    
    * fix: component cards
    
    * fix: container name
    
    * fix: remove extra space after new line
    
    * fix: remove extra space after new line
    
    * feat: tests for dream ruxglm
    
    * fix: proxy and ru lang
    
    * fix: change port of universal ru
    
    * fix: rights on file
    
    * fix: tests skills
    
    * fix: test for resp selector
    
    * fix: tests for proxied components
    
    * fix: remove do sample true
    
    * fix: generative params
    
    * feat: used sentence ranker url
    
    * feat: utilized default llm

--- response_selectors/ranking_based_response_selector/test.py
+++ response_selectors/ranking_based_response_selector/test.py
@@ -12,7 +12,7 @@ def main():
     # To skip "Oh, and remember this dialog's id" that raises error due to absence of 'dialog_id' field in test_data.
     data["dialogs"][0]["human_utterances"].append(data["dialogs"][0]["human_utterances"][0])
     result = requests.post(f"http://0.0.0.0:{SERVICE_PORT}/respond", json=data).json()
-    assert result[0][0] == "program_y", print(result)
+    assert result[0][0] in ["program_y", "movie_tfidf_retrieval"], print(result)
 
 
 if __name__ == "__main__":
