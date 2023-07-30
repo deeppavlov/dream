07678c182264136054800bb8c685c50ff5db65bc
Dilyara Zharikova (Baymurzina) <dilyara.rimovna@gmail.com>
2023-07-30 12:57:26 +0300

    feat: replace oasst12b with gptjt (#541)

--- skills/dff_universal_prompted_skill/scenario/response.py
+++ skills/dff_universal_prompted_skill/scenario/response.py
@@ -22,7 +22,7 @@ FIX_PUNCTUATION = re.compile(r"\s(?=[\.,:;])")
 DEFAULT_CONFIDENCE = 0.9
 LOW_CONFIDENCE = 0.7
 DEFAULT_PROMPT = "Respond like a friendly chatbot."
-DEFAULT_LM_SERVICE_URL = getenv("DEFAULT_LM_SERVICE_URL", "http://transformers-lm-oasst12b:8158/respond")
+DEFAULT_LM_SERVICE_URL = getenv("DEFAULT_LM_SERVICE_URL", "http://transformers-lm-gptjt:8161/respond")
 DEFAULT_LM_SERVICE_CONFIG = getenv("DEFAULT_LM_SERVICE_CONFIG", "default_generative_config.json")
 DEFAULT_LM_SERVICE_CONFIG = json.load(open(f"common/generative_configs/{DEFAULT_LM_SERVICE_CONFIG}", "r"))
 ENVVARS_TO_SEND = {
