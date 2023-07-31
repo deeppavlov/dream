acca372bbc6a9a2341bfbf75ca41674f3a044f0f
Nellya Dzhubaeva <dzhubaeva@gpu8.ipavlov.mipt.ru>
2023-07-31 06:44:05 +0300

    dp formatters

--- state_formatters/dp_formatters.py
+++ state_formatters/dp_formatters.py
@@ -347,6 +347,25 @@ def last_human_bot_annotated_utterance(dialog: Dict) -> List[Dict]:
     ]
 
 
+def last_bot_annotated_utterance(dialog: Dict) -> List[Dict]:
+    return [
+        {
+            "bot_utterances": [dialog["bot_utterances"][-1] if len(dialog["bot_utterances"]) else {}],
+            "dialog_ids": [dialog.get("dialog_id", "unknown")],
+        }
+    ]
+
+
+def last_human_bot_annotated_utterance(dialog: Dict) -> List[Dict]:
+    return [
+        {
+            "last_human_utterances": [dialog["human_utterances"][-1]],
+            "bot_utterances": [dialog["bot_utterances"][-1] if len(dialog["bot_utterances"]) else {}],
+            "dialog_ids": [dialog.get("dialog_id", "unknown")],
+        }
+    ]
+
+
 def last_human_utt_nounphrases(dialog: Dict) -> List[Dict]:
     # Used by: comet_conceptnet_annotator
     entities = get_entities(dialog["human_utterances"][-1], only_named=False, with_labels=False)
