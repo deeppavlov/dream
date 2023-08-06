c17424bcb3e81806b327b8f758e0b3a06896baf0
Nellya Dzhubaeva <dzhubaeva@gpu8.ipavlov.mipt.ru>
2023-08-06 16:15:23 +0300

    fixed issues

--- skill_selectors/rule_based_selector/connector.py
+++ skill_selectors/rule_based_selector/connector.py
@@ -97,7 +97,8 @@ class RuleBasedSkillSelectorConnector:
             dialog_len = len(dialog["human_utterances"])
             if user_uttr.get("attributes", {}).get("image") is not None:
                 skills_for_uttr.append("dff_image_skill")
-                skills_for_uttr.append("dff_fromage_image_skill")
+                if dialog_len < 5:
+                    skills_for_uttr.append("dff_fromage_image_skill")
 
             exit_cond = "exit" in intent_catcher_intents and (
                 dialog_len == 1 or (dialog_len == 2 and len(user_uttr_text.split()) > 3)
@@ -181,7 +182,6 @@ class RuleBasedSkillSelectorConnector:
                 skills_for_uttr.append("meta_script_skill")
                 skills_for_uttr.append("dummy_skill")
                 skills_for_uttr.append("dialogpt")  # generative skill
-                skills_for_uttr.append("dff_fromage_image_skill")
                 skills_for_uttr.append("dialogpt_persona_based")  # generative skill persona-based
                 skills_for_uttr.append("seq2seq_persona_based")  # generative skill persona-based
                 skills_for_uttr.append("small_talk_skill")
