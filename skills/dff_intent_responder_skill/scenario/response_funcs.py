6432105174e9f25754045e8fe9e46b503dd54de7
moon-strider <iamthekidyoudoknowwhatimean@gmail.com>
2023-08-09 19:04:43 +0300

    removed dff_embodied_skill and embodied_sender

--- skills/dff_intent_responder_skill/scenario/response_funcs.py
+++ skills/dff_intent_responder_skill/scenario/response_funcs.py
@@ -4,6 +4,7 @@ import json
 import random
 from datetime import datetime
 from os import getenv
+from common.robot import check_if_valid_robot_command
 
 import common.dff.integration.context as int_ctx
 from df_engine.core import Actor, Context
@@ -11,6 +12,7 @@ from df_engine.core import Actor, Context
 
 INTENT_RESPONSE_PHRASES_FNAME = getenv("INTENT_RESPONSE_PHRASES_FNAME", "intent_response_phrases.json")
 LANGUAGE = getenv("LANGUAGE", "EN")
+ROS_FLASK_SERVER = getenv("ROS_FLASK_SERVER")
 logging.basicConfig(format="%(asctime)s - %(pathname)s - %(lineno)d - %(levelname)s - %(message)s", level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 logger.info(f"Intent response phrases are from file: {INTENT_RESPONSE_PHRASES_FNAME}")
@@ -106,6 +108,38 @@ def what_is_current_dialog_id_respond(ctx: Context, actor: Actor, intention: str
     return response
 
 
+def move_forward_respond(ctx: Context, actor: Actor, intention: str):
+    utt = int_ctx.get_last_human_utterance(ctx, actor)
+    dist = re.findall(r"[0-9]+", utt["text"])
+    if len(dist) == 1:
+        command = f"move_forward_{dist[0]}"
+        response = f"Moving forward by {dist[0]} units."
+    else:
+        command = "move_forward"
+        response = "Moving forward."
+
+    if check_if_valid_robot_command(command, ROS_FLASK_SERVER, dialog_id=int_ctx.get_dialog_id(ctx, actor)):
+        return response, 1.0, {}, {}, {"command_to_perform": command}
+    else:
+        return ""
+
+
+def move_backward_respond(ctx: Context, actor: Actor, intention: str):
+    utt = int_ctx.get_last_human_utterance(ctx, actor)
+    dist = re.findall(r"[0-9]+", utt["text"])
+    if len(dist) == 1:
+        command = f"move_backward_{dist[0]}"
+        response = f"Moving backward by {dist[0]} units."
+    else:
+        command = "move_backward"
+        response = "Moving backward."
+
+    if check_if_valid_robot_command(command, ROS_FLASK_SERVER, dialog_id=int_ctx.get_dialog_id(ctx, actor)):
+        return response, 1.0, {}, {}, {"command_to_perform": command}
+    else:
+        return ""
+
+
 def get_respond_funcs():
     return {
         "exit": random_respond,
@@ -121,6 +155,8 @@ def get_respond_funcs():
         "choose_topic": random_respond,
         "cant_do": random_respond,
         "tell_me_a_story": random_respond,
+        "move_forward": move_forward_respond,
+        "move_backward": move_backward_respond,
     }
 
 
