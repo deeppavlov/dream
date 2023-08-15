64e91196f2024006cfea59cea36f83c89e0061d1
Nika Smilga <sunny-august@mail.ru>
2023-08-15 17:36:14 +0300

    revert accidental change in ranking_based_response_selector/test.py

--- response_selectors/ranking_based_response_selector/test.py
+++ response_selectors/ranking_based_response_selector/test.py
@@ -1,4 +1,19 @@
-hyps_and_ies = [[1, 1], [2, 3]]
+import requests
+import json
+from os import getenv
 
-for hyp, ie in hyps_and_ies:
-    print(f"{hyp} {ie} \n\n")
+
+SERVICE_PORT = getenv("SERVICE_PORT")
+
+
+def main():
+    with open("test_data.json", "r") as f:
+        data = json.load(f)
+    # To skip "Oh, and remember this dialog's id" that raises error due to absence of 'dialog_id' field in test_data.
+    data["dialogs"][0]["human_utterances"].append(data["dialogs"][0]["human_utterances"][0])
+    result = requests.post(f"http://0.0.0.0:{SERVICE_PORT}/respond", json=data).json()
+    assert result[0][0] in ["program_y", "movie_tfidf_retrieval"], print(result)
+
+
+if __name__ == "__main__":
+    main()
