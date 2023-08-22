be73c063155ed6567a3a398c85a847249fd95b35
Anastasiia Pokasova <pokasovaai@gmail.com>
2023-08-22 11:39:30 +0300

    move image captioning to dream_formatter

--- state_formatters/dp_formatters.py
+++ state_formatters/dp_formatters.py
@@ -759,7 +759,14 @@ def hypothesis_scorer_formatter(dialog: Dict) -> List[Dict]:
 
 def image_captioning_formatter(dialog: Dict) -> List[Dict]:
     # Used by: image_captioning
-    return [{"image_paths": [dialog["human_utterances"][-1].get("attributes", {}).get("image")]}]
+    return utils.dream_formatter(dialog, ["image_paths"],additional_params={
+            "utterance_type": "human_utterances",
+            "last_n_utts": 1,
+            "attribute": "attributes",
+            "def_result": {},
+            "sub_attribute": "image",
+            "def_subresult": None,
+        },)
 
 
 def external_integration_skill_formatter(dialog: Dict) -> List[Dict]:
