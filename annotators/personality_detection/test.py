80c8c64edc4abab00c8a42270f0d8997e84865d1
moon-strider <iamthekidyoudoknowwhatimean@gmail.com>
2023-08-24 12:53:35 +0300

    dream_ocean distribution added

---
+++ annotators/personality_detection/test.py
@@ -0,0 +1,12 @@
+import requests
+
+
+def test():
+    response = requests.post('http://0.0.0.0:8026/model', json={'personality': ['Hello world']})
+    assert response.status_code == 200
+    assert response.json() == [[0, 1, 1, 0, 1]]
+    print('SUCCESS')
+
+
+if __name__ == '__main__':
+    test()
