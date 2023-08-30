2d0c946bcc4dd69aedf9d2ed458c4385dfce9c9a
moon-strider <iamthekidyoudoknowwhatimean@gmail.com>
2023-08-30 23:15:10 +0300

    conflict resolved, port for emotion_detection changed, codestyle fixed

--- annotators/emotion_detection/server.py
+++ annotators/emotion_detection/server.py
@@ -15,7 +15,15 @@ import cv2
 sentry_sdk.init(dsn=os.getenv("SENTRY_DSN"), integrations=[FlaskIntegration()])
 app = Flask(__name__)
 
-label2id = {'anger': 0, 'disgust': 1, 'fear': 2, 'joy': 3, 'neutral': 4, 'sadness': 5, 'surprise': 6}
+label2id = {
+    "anger": 0,
+    "disgust": 1,
+    "fear": 2,
+    "joy": 3,
+    "neutral": 4,
+    "sadness": 5,
+    "surprise": 6,
+}
 num_labels = 7
 text_model, video_model, audio_model = prepare_models(num_labels, "./")
 
@@ -24,20 +32,24 @@ logger = logging.getLogger(__name__)
 prefix = "Detect emotions:"
 prefix_len = len(prefix)
 
+
 def sample_frame_indices(seg_len, clip_len=16, frame_sample_rate=4, mode="video"):
     converted_len = int(clip_len * frame_sample_rate)
-    converted_len = min(converted_len, seg_len-1)
+    converted_len = min(converted_len, seg_len - 1)
     end_idx = np.random.randint(converted_len, seg_len)
     start_idx = end_idx - converted_len
     if mode == "video":
         indices = np.linspace(start_idx, end_idx, num=clip_len)
     else:
-        indices = np.linspace(start_idx, end_idx, num=clip_len*frame_sample_rate)
+        indices = np.linspace(start_idx, end_idx, num=clip_len * frame_sample_rate)
     indices = np.clip(indices, start_idx, end_idx - 1).astype(np.int64)
     return indices
 
 
-def get_frames(file_path, clip_len=16,):
+def get_frames(
+    file_path,
+    clip_len=16,
+):
     cap = cv2.VideoCapture(file_path)
     v_len = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     indices = sample_frame_indices(v_len)
@@ -47,8 +59,8 @@ def get_frames(file_path, clip_len=16,):
         success, frame = cap.read()
         if success is False:
             continue
-        if (fn in indices):
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  
+        if fn in indices:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             res = cv2.resize(frame, dsize=(224, 224), interpolation=cv2.INTER_CUBIC)
             frames.append(res)
     cap.release()
@@ -60,31 +72,34 @@ def get_frames(file_path, clip_len=16,):
 
     return frames
 
+
 def create_final_model():
     multi_model = MultimodalClassificaionModel(
         text_model,
         video_model,
         audio_model,
         num_labels,
-        input_size=1920, 
-        hidden_size=512
+        input_size=1920,
+        hidden_size=512,
     )
-    checkpoint = torch.load('final_model.pt')
+    checkpoint = torch.load("final_model.pt")
     multi_model.load_state_dict(checkpoint)
 
-    device = 'cuda'
+    device = "cuda"
     return MainModel(multi_model, device=device)
 
 
 def process_text(input_tokens: str):
-    text_model_name = 'bert-large-uncased'
+    text_model_name = "bert-large-uncased"
     tokenizer = AutoTokenizer.from_pretrained(text_model_name)
 
-    return tokenizer(input_tokens,
-                    padding='max_length',
-                    truncation=True,
-                    max_length=128,
-                    return_tensors='pt')
+    return tokenizer(
+        input_tokens,
+        padding="max_length",
+        truncation=True,
+        max_length=128,
+        return_tensors="pt",
+    )
 
 
 def process_video(video_path: str):
@@ -100,21 +115,28 @@ def process_audio(file_path: str):
     smile = opensmile.Smile(
         opensmile.FeatureSet.ComParE_2016,
         opensmile.FeatureLevel.Functionals,
-        sampling_rate=16000,    
-        resample=True,    
+        sampling_rate=16000,
+        resample=True,
         num_workers=5,
         verbose=True,
     )
 
     audio_features = smile.process_files([file_path])
-    return audio_features.values.reshape(audio_features.shape[0], 1, audio_features.shape[1])
+    return audio_features.values.reshape(
+        audio_features.shape[0], 1, audio_features.shape[1]
+    )
 
 
 def inference(text: str, video_path: str):
     text_encoding = process_text(text)
     video_encoding = process_video(video_path)
     audio_features = process_audio(video_path)
-    batch = {'text': text_encoding, 'video': video_encoding, 'audio': audio_features, 'label': None}
+    batch = {
+        "text": text_encoding,
+        "video": video_encoding,
+        "audio": audio_features,
+        "label": None,
+    }
     label = final_model(batch)
     id2label = {v: k for k, v in label2id.items()}
     return id2label[int(label.detach().cpu())]
@@ -144,9 +166,11 @@ def infer():
         except Exception as e:
             raise ValueError(f"The message format is correct, but: {e}")
     else:
-        raise ValueError("Input should be text and a videofile link on two separate lines.")
+        raise ValueError(
+            "Input should be text and a videofile link on two separate lines."
+        )
     return jsonify(emotion)
 
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=8027)
+    app.run(debug=False, host="0.0.0.0", port=8025)
