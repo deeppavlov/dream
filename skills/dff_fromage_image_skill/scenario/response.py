573bcb92b03643262d8e2d740da211070d31dd36
dilyararimovna <dilyara.rimovna@gmail.com>
2023-09-04 13:05:43 +0400

    fix: assign super conf if caption is given

--- skills/dff_fromage_image_skill/scenario/response.py
+++ skills/dff_fromage_image_skill/scenario/response.py
@@ -4,10 +4,11 @@ import common.dff.integration.context as int_ctx
 
 logger = logging.getLogger(__name__)
 
-DEFAULT_CONFIDENCE = 0.95
+SUPER_CONFIDENCE = 1.0
 
 
 def generic_response(ctx: Context, actor: Actor, excluded_skills=None, *args, **kwargs) -> str:
     caption = int_ctx.get_last_human_utterance(ctx, actor).get("annotations", {}).get("fromage", None)
-    int_ctx.set_confidence(ctx, actor, DEFAULT_CONFIDENCE)
+    if caption:
+        int_ctx.set_confidence(ctx, actor, SUPER_CONFIDENCE)
     return caption
