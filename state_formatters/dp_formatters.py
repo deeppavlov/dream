3ccf9dee1e84f77a9649ca07ebd92ee792cd048c
Anna Korzanova <anna.korzanova12@gmail.com>
2023-09-08 14:20:21 +0300

    chore: del template_prompted skill formatter

--- state_formatters/dp_formatters.py
+++ state_formatters/dp_formatters.py
@@ -1074,14 +1074,6 @@ def dff_prompted_skill_formatter(dialog, skill_name=None):
     )
 
 
-def dff_template_prompted_skill_formatter(dialog):
-    return utils.dff_formatter(
-        dialog,
-        "dff_template_prompted_skill",
-        types_utterances=["human_utterances", "bot_utterances", "utterances"],
-    )
-
-
 def dff_universal_prompted_skill_formatter(dialog, skill_name=None):
     return utils.dff_formatter(
         dialog,
