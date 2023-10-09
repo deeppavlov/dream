e8dbf4fb4b16f052e530db46a4fc6503b73de103
lnpetrova <lostaaa15@gmail.com>
2023-10-09 17:41:58 +0300

    New SFP version

--- annotators/speech_function_predictor/test_server.py
+++ annotators/speech_function_predictor/test_server.py
@@ -9,11 +9,11 @@ ANNOTATION_URL = f"{URL}/annotation"
 
 
 def run_test():
-    model_test_data = ["Reply.Acknowledge"]
+    model_test_data = ["React.Respond.Support.Engage"]
     model_hypothesis = requests.post(MODEL_URL, json=model_test_data).json()
 
     print("test name: sfp model_hypothesis")
-    assert model_hypothesis == [{}]
+    assert model_hypothesis == [{'prediction': 'Sustain.Continue.Prolong', 'confidence': 0.6}, {'prediction': 'React.Rejoinder.Support.Track', 'confidence': 0.2}, {'prediction': 'React.Rejoinder.Confront.Challenge', 'confidence': 0.2}]
 
     annotation_test_data = ["Reply.Acknowledge"]
     annotation_hypothesis = requests.post(ANNOTATION_URL, json=annotation_test_data).json()
