208947e369a0418bb76f2a2573824e380ea0325a
dimakarp1996 <dimakarp1996@yandex.ru>
2023-10-21 23:59:12 +0300

    Update utils.py

--- annotators/dnnc_intents/utils.py
+++ annotators/dnnc_intents/utils.py
@@ -1,6 +1,7 @@
 from deeppavlov.core.models.component import Component
 from deeppavlov.core.common.registry import register
 from deeppavlov.models.torch_bert.torch_transformers_classifier import TorchTransformersClassifierModel
+from typing import Dict, Union, List
 import json
 
 
