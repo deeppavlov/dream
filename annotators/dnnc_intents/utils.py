19e41fdddc99991dc18c146d2cc59b04d5ff558e
dimakarp1996 <dimakarp1996@yandex.ru>
2023-10-21 23:57:14 +0300

    Update utils.py

--- annotators/dnnc_intents/utils.py
+++ annotators/dnnc_intents/utils.py
@@ -1,5 +1,6 @@
 from deeppavlov.core.models.component import Component
 from deeppavlov.core.common.registry import register
+from deeppavlov.models.torch_bert import TorchTransformersClassifierModel
 import json
 
 
@@ -11,3 +12,46 @@ class dnnc_preparer(Component):
     def __call__(self, texts):
         datasets = self.data["train"] + self.data["oos_train"]
         return texts, datasets
+
+
+@register('torch_transformers_classifier_batch1')
+class TorchTransformersClassifierModelBatch1(TorchTransformersClassifierModel):
+    def __call__(self, features: Dict[str, torch.tensor]) -> Union[List[int], List[List[float]]]:
+        """Make prediction for given features (texts).
+
+        Args:
+            features: batch of InputFeatures
+
+        Returns:
+            predicted classes or probabilities of each class
+
+        """
+        answer = []
+        for i in range(len(features)):
+            _input = {key: value[i].unsqueeze(0).to(self.device)
+                      for key, value in features.items()}
+    
+            with torch.no_grad():
+                tokenized = {key: value for (key, value) in _input.items()
+                             if key in self.accepted_keys}
+    
+                # Forward pass, calculate logit predictions
+                logits = self.model(**tokenized)
+                logits = logits[0]
+    
+            if self.return_probas:
+                if self.is_binary:
+                    pred = torch.sigmoid(logits).squeeze(1)
+                elif not self.multilabel:
+                    pred = torch.nn.functional.softmax(logits, dim=-1)
+                else:
+                    pred = torch.nn.functional.sigmoid(logits)
+                pred = pred.detach().cpu().numpy()[0]
+            elif self.n_classes > 1:
+                logits = logits.detach().cpu().numpy()
+                pred = np.argmax(logits, axis=1)[0]
+            # regression
+            else:
+                pred = logits.squeeze(-1).detach().cpu().numpy()[0]
+            answer.append(pred)
+        return answer
