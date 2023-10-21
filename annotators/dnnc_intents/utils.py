05ee608037271a7bd1e0a2bed800c9bcc864b79d
dimakarp1996 <dimakarp1996@yandex.ru>
2023-10-21 16:39:50 +0300

    Update utils.py

--- annotators/dnnc_intents/utils.py
+++ annotators/dnnc_intents/utils.py
@@ -1,21 +1,11 @@
-
-from logging import getLogger
-from typing import List
-
-import numpy as np
-from deeppavlov.core.common.registry import register
 from deeppavlov.core.models.component import Component
 from deeppavlov.core.common.registry import register
+import json
 
-@register('dnnc_preparer')
+@register("dnnc_preparer")
 class dnnc_preparer(Component):
     def __init__(self):
-        self.possible_datasets = ... 
-        pass
+        self.data = json.load(open("data_full.json","r"))
     def __call__(self, texts):
-        datasets,final_texts=[],[]
-        for text in texts:
-            for dataset in self.possible_datasets:
-                datasets.append(dataset)
-                final_texts.append(text)
-        return final_texts, datasets
+        datasets = data["train"] + data["oos_train"]
+        return texts, datasets
