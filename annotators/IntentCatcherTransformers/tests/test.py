712e405d465056ad53aefeb5235e8e2d5bb636d6
Anastasiia Pokasova <pokasovaai@gmail.com>
2023-10-24 14:31:04 +0300

    Feat/pytest tests (#548)
    * Add knowledge-grounding pytest tests
    
    * Add pytest tests for annotators and services
    
    * Fixed tests for services
    
    * Add allure test report generation
    
    * massive test update
    
    * move tests to different folder
    
    * return selectors test in main folder
    
    * Fix linting errors
    
    * Add jenkins script modification for test containers, add test containers and test requirements.txt
    
    * Fix linting errors
    
    * Added missing directory
    
    * Fix path errors
    
    * Fix dependency issues in Sentseg
    
    * Fix allure in requirements.txt
    
    * Minor requirements.txt fixes
    
    * Fix kbqa tests and wiki-parser dependencies
    
    * Get rid of pytests tests for russian entity-linking, attempt to fix sentseg test calls
    
    * fix linter errors
    
    * Fix dockerfile name in tests configs
    
    * Fix paths for sentseg
    
    * Fix path for entity-linking tests
    
    * Add fixes for entity-linking tests
    
    * Fixed paths for configs
    
    * Added test skips for text-qa
    
    * Minor fixes for tests
    
    * Fix ner and wiki-parser tests
    
    * Fix sentseg-ru configs
    
    * Fix test script for sentseg-ru
    
    * Fix tests for some russian services
    
    * Fix path for fact-retrieval-ru tests
    
    * Add typing hints for entity-detection-ru tests
    
    * Fix config for entity-detection-ru tests
    
    * Add fixes for fact-retrieval-ru and text-qa-ru
    
    * Manage test cases for text-qa
    
    * Fix linter errors
    
    * Fix gold_results for text-qa
    
    * Add fixes to fact-retrieval-rus tests
    
    * Fixed fact retrieval data transfer

--- annotators/IntentCatcherTransformers/tests/test.py
+++ annotators/IntentCatcherTransformers/tests/test.py
@@ -2,23 +2,9 @@
 
 import requests
 import json
-from os import getenv
-
-INTENT_PHRASES_PATH = getenv("INTENT_PHRASES_PATH")
-SERVICE_PORT = getenv("SERVICE_PORT")
 
 
-def main_test():
-    url = f"http://0.0.0.0:{SERVICE_PORT}/detect"
-    if "RU" in INTENT_PHRASES_PATH and "commands" in INTENT_PHRASES_PATH:
-        tests = json.load(open("tests_commands_RU.json"))
-    elif "RU" in INTENT_PHRASES_PATH:
-        tests = json.load(open("tests_RU.json"))
-    elif "commands" in INTENT_PHRASES_PATH:
-        tests = json.load(open("tests_commands.json"))
-    else:
-        tests = json.load(open("tests.json"))
-
+def test_intent_catcher(url: str, tests: dict):
     for test in tests:
         r = requests.post(url=url, json={"sentences": [[test["sentence"]]]})
         assert r.ok
@@ -30,8 +16,3 @@ def main_test():
             ), print(f"TEST FAILED!\nTest: {test}\nResult:{json.dumps(data, indent=2)}")
         else:
             assert all([intent["detected"] == 0 for intent in data.values()]), f"test: {test}\nprediction: {data}"
-    print("Success")
-
-
-if __name__ == "__main__":
-    main_test()
