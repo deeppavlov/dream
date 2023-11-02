67e07ec0c1e51a8593ceebd9f336714b82a4e411
asleepann <89022735702@mail.ru>
2023-11-03 02:19:10 +0300

    fixed emotion response selector

--- response_selectors/emotion_ranking_based_response_selector/test.py
+++ response_selectors/emotion_ranking_based_response_selector/test.py
@@ -1,19 +1,11 @@
 import requests
 import json
-from os import getenv
 
 
-SERVICE_PORT = getenv("SERVICE_PORT")
-
-
-def main():
+def test_selector(url: str):
     with open("test_data.json", "r") as f:
         data = json.load(f)
     # To skip "Oh, and remember this dialog's id" that raises error due to absence of 'dialog_id' field in test_data.
     data["dialogs"][0]["human_utterances"].append(data["dialogs"][0]["human_utterances"][0])
-    result = requests.post(f"http://0.0.0.0:{SERVICE_PORT}/respond", json=data).json()
-    assert result[0][0] in ["program_y", "movie_tfidf_retrieval"], print(result)
-
-
-if __name__ == "__main__":
-    main()
+    result = requests.post(url, json=data).json()
+    assert result[0][0] == "program_y"
