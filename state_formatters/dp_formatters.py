8d3ae9f5a9bb63d6a516a5849557293931a33fd7
Lida Ostyakova <losta15@gpu7.ipavlov.mipt.ru>
2023-12-12 06:38:03 +0300

    Style fix

--- state_formatters/dp_formatters.py
+++ state_formatters/dp_formatters.py
@@ -1183,7 +1183,6 @@ def speech_function_predictor_formatter(dialog: Dict):
     res = None
     try:
         res = dialog["human_utterances"][-1]["annotations"].get("speech_function_classifier", [""])
-        # res = [item[0] for item in res]
         logger.info(f"formatter log sfp/sfc: {res}")
     except Exception as e:
         logger.error(f"Failed to format sfc output: {e}")
