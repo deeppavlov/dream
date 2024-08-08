0a9f75b8387ca74ac0800904e7819990966fc3de
Anna Korzanova <anna.korzanova12@gmail.com>
2024-08-08 12:01:57 +0300

    add: test for node switch by entity in italy-skill

---
+++ skills/dff_travel_italy_skill/tests/test_response.py
@@ -0,0 +1,21 @@
+import requests
+import time
+import json
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:4242"
+    user_id = "test"
+
+    trials = 0
+    expected_output = 'What are the odds? I also love venice. What impressed you the most there?'
+    result = None
+    while result != expected_output:
+        response = requests.post(url, json={"user_id": user_id, "payload": "my favorite place is venice", "ignore_deadline_timestamp": True}).json()
+        result = response.get('response')
+        # print(result)
+        if result == expected_output:
+            print("Success for node switch based on entity", result)
+        trials += 1
+        if trials > 10:
+            print("Expected output doesn't match the result", result)
