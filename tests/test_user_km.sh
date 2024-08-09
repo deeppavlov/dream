8261e06549e4264d810400f6e405e9c2be7bde78
Anna Korzanova <anna.korzanova12@gmail.com>
2024-08-09 11:17:00 +0300

    add: launch time test for user-km

--- tests/test_user_km.sh
+++ tests/test_user_km.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.yml -f assistant_dists/dream_kg_prompted/docker-compose.override.yml -f assistant_dists/dream_kg_prompted/dev.yml -p bot_test up -d terminusdb-server user-knowledge-memorizer-prompted
-sleep 30
+docker-compose -f docker-compose.yml -f assistant_dists/dream_kg_prompted/docker-compose.override.yml -f assistant_dists/dream_kg_prompted/dev.yml -p bot_test up -d terminusdb-server
+sleep 5
+docker-compose -f docker-compose.yml -f assistant_dists/dream_kg_prompted/docker-compose.override.yml -f assistant_dists/dream_kg_prompted/dev.yml -p bot_test up -d user-knowledge-memorizer-prompted
+python annotators/user_knowledge_memorizer/launch_time_test_user_km.py
 python annotators/user_knowledge_memorizer/test.py
 docker-compose -f docker-compose.yml -f assistant_dists/dream_kg_prompted/docker-compose.override.yml -f assistant_dists/dream_kg_prompted/dev.yml -p bot_test down
