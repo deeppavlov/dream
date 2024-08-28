8f3bd719c45fc8c93560e3eb96991a78b2b94519
Daria Olshevskaya <olshevskaya.d@bk.ru>
2024-08-28 13:34:35 +0300

    add: bash test for personality-detection

---
+++ tests/test_pers_detection.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker-compose -f docker-compose.yml -f assistant_dists/dream_ocean/docker-compose.override.yml -f assistant_dists/dream_ocean/dev.yml -f assistant_dists/dream_ocean/proxy.yml -p bot_test up -d personality-detection
+python annotators/personality_detection/test_launch_time.py
+python annotators/personality_detection/test_time_format.py
+python annotators/personality_detection/test_accuracy.py
+docker-compose -f docker-compose.yml -f assistant_dists/dream_ocean/docker-compose.override.yml -f assistant_dists/dream_ocean/dev.yml -f assistant_dists/dream_ocean/proxy.yml -p bot_test down
