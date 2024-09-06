4130a7129aa268a70b3e512aeec2ab1d56a01f42
Daria Olshevskaya <olshevskaya.d@bk.ru>
2024-09-06 13:03:50 +0300

    fix: tests launch locally

--- tests/test_pers_detection.sh
+++ tests/test_pers_detection.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.yml -f assistant_dists/dream_ocean/docker-compose.override.yml -f assistant_dists/dream_ocean/dev.yml -f assistant_dists/dream_ocean/proxy.yml -p bot_test up -d personality-detection
+docker-compose -f docker-compose.yml -f assistant_dists/dream_ocean/docker-compose.override.yml -f assistant_dists/dream_ocean/dev.yml -p bot_test up -d personality-detection
 python annotators/personality_detection/test_launch_time.py
 python annotators/personality_detection/test_time_format.py
 python annotators/personality_detection/test_accuracy.py
-docker-compose -f docker-compose.yml -f assistant_dists/dream_ocean/docker-compose.override.yml -f assistant_dists/dream_ocean/dev.yml -f assistant_dists/dream_ocean/proxy.yml -p bot_test down
+docker-compose -f docker-compose.yml -f assistant_dists/dream_ocean/docker-compose.override.yml -f assistant_dists/dream_ocean/dev.yml -p bot_test down
