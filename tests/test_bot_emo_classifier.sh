af01d85e1a672c4437c5709393d701abcb120f1b
Daria Olshevskaya <olshevskaya.d@bk.ru>
2024-09-11 13:17:22 +0300

    fix: bash tests

--- tests/test_bot_emo_classifier.sh
+++ tests/test_bot_emo_classifier.sh
@@ -2,5 +2,6 @@
 
 docker-compose -f docker-compose.yml -f assistant_dists/dream_emotion/docker-compose.override.yml -f assistant_dists/dream_emotion/dev.yml -p bot_test up -d bot-emotion-classifier
 python annotators/bot_emotion_classifier/test_launch_time.py
-python annotators/bot_emotion_classifier/test_time_format.py
+python annotators/bot_emotion_classifier/test_time.py
+python annotators/bot_emotion_classifier/test_format.py
 docker-compose -f docker-compose.yml -f assistant_dists/dream_emotion/docker-compose.override.yml -f assistant_dists/dream_emotion/dev.yml -p bot_test down
--- tests/test_pers_detection.sh
+++ tests/test_pers_detection.sh
@@ -2,6 +2,7 @@
 
 docker-compose -f docker-compose.yml -f assistant_dists/dream_ocean/docker-compose.override.yml -f assistant_dists/dream_ocean/dev.yml -p bot_test up -d personality-detection
 python annotators/personality_detection/test_launch_time.py
-python annotators/personality_detection/test_time_format.py
+python annotators/personality_detection/test_time.py
+python annotators/personality_detection/test_format.py
 python annotators/personality_detection/test_accuracy.py
 docker-compose -f docker-compose.yml -f assistant_dists/dream_ocean/docker-compose.override.yml -f assistant_dists/dream_ocean/dev.yml -p bot_test down
